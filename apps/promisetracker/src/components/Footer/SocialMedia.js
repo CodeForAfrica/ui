d0f9153114a2b8ddf70966efdeb30089124c3153
nerdlet <brndnyokabi@gmail.com>
2019-09-19 16:19:51 +0300

    Replace font awesome with react-feather

--- apps/promisetracker/src/components/Footer/SocialMedia.js
+++ apps/promisetracker/src/components/Footer/SocialMedia.js
@@ -1,35 +1,62 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Grid, Typography, makeStyles } from '@material-ui/core';
+import { Facebook, Instagram, Twitter } from 'react-feather';
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  faFacebookF,
-  faInstagram,
-  faTwitter,
-  faTelegram
-} from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import { Grid } from '@material-ui/core';
-import A from '../A';
-import Section from './Section';
 
-library.add(faFacebookF, faTwitter, faInstagram, faTelegram);
+const useStyles = makeStyles({
+  iconGrid: {
+    padding: '2.5rem 0'
+  },
+  fa: {
+    transition: 'all .5s ease-in-out',
+    color: 'grey',
+    ' &:hover': {
+      color: '#257ca3'
+    }
+  },
+  links: { color: '#fff' }
+})
 
 function SocialMedia() {
   return (
-    <Section title="ON SOCIAL MEDIA" spacing={2}>
-      <Grid item>
-        <A
-          href="https://www.facebook.com/AfricanCIR/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon
-            className="Mui-desaturated"
-            icon={['fab', 'facebook-f']}
-            size="2x"
-          />
-        </A>
+    <Grid item xs={12} md={4}>
+      <Typography variant="h3" style={{ color: 'black' }}>ON SOCIAL MEDIA</Typography>
+      <Grid
+        spacing={3}
+        container
+        direction="row"
+        justify="flex-start"
+        alignItems="center"
+        className={classes.iconGrid}>
+        <Grid item>
+          <a
+            href="https://www.facebook.com/AfricanCIR/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Facebook className={classes.fa} />
+          </a>
+        </Grid>
+        <Grid item>
+          <a
+            href="https://www.instagram.com/explore/tags/onsgrond/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Instagram className={classes.fa} />
+          </a>
+        </Grid>
+        <Grid item>
+          <a
+            href="https://twitter.com/africancir"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Twitter className={classes.fa} />
+          </a>
+        </Grid>
       </Grid>
       <Grid item>
         <A
