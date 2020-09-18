690228d86e8190ee5cee50a0fc21afb5077ed2ed
nerdlet <brndnyokabi@gmail.com>
2020-09-18 10:45:09 +0300

    Add navigation components

--- apps/promisetracker/src/config.js
+++ apps/promisetracker/src/config.js
@@ -1,33 +1,18 @@
-import instagram from "@/promisetracker/assets/footer-social-ig.svg";
-import facebook from "@/promisetracker/assets/footer-social-fb.svg";
-import twitter from "@/promisetracker/assets/footer-social-tw.svg";
-import linkedIn from "@/promisetracker/assets/footer-social-ln.svg";
-import github from "@/promisetracker/assets/footer-social-gh.svg";
-import cc from "@/promisetracker/assets/cc.svg";
-
-  settings: {
-    subscribe: {
-      embedCode: `
-        <!-- Begin Mailchimp Signup Form -->
-        <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
-        <style type="text/css">
-          #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
-        </style>
-        <div id="mc_embed_signup">
-        <form action="https://codeforafrica.us6.list-manage.com/subscribe/post?u=65e5825507b3cec760f272e79&amp;id=286e6e3985" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-            <div id="mc_embed_signup_scroll">
-          <label for="mce-EMAIL">Subscribe</label>
-          <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Enter your email address" required>
-            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_65e5825507b3cec760f272e79_286e6e3985" tabindex="-1" value=""></div>
-            <div class="clear"><input type="submit" value="" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-            </div>
-        </form>
-        </div>
-        <!--End mc_embed_signup-->
-      `,
+const config = {
+  analysisMenu: [
+    {
+      name: "Articles",
+      href: "articles",
     },
-  },
+    {
+      name: "Resources",
+      href: "resources",
+    },
+    {
+      name: "Fact-checks",
+      href: "fact-checks",
+    },
+  ],
 };
 
 export default config;
