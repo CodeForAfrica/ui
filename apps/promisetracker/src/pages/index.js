3bb44993abf108ee802fb5a7ea4e2bfa81c9f8a6
nerdlet <brndnyokabi@gmail.com>
2020-09-18 12:02:45 +0300

    Add profile section

--- apps/promisetracker/src/pages/index.js
+++ apps/promisetracker/src/pages/index.js
@@ -35,11 +35,7 @@ function Index(props) {
 
   return (
     <Page>
-      <Hero
-        classes={{
-          section: classes.section,
-        }}
-      />
+      <Hero />
       <LatestPromises
         actionLabel="See All"
         items={Array(6).fill({
