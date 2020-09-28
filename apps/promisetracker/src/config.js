a9856edbb4a5b6ec7a407ab8f0219e65dd62f187
Clemence Kyara <clemence@codeforafrica.org>
2020-09-28 14:59:06 +0300

    Add about page config

--- apps/promisetracker/src/config.js
+++ apps/promisetracker/src/config.js
@@ -181,68 +181,91 @@ const config = {
     title: "Analysis",
     url: "/analysis",
   },
-  colors: {
-    completed: {
-      main: "#005DFD",
-      font: "#FFFFFF",
-    },
-    "in-progress": {
-      main: "#90DAFF",
-      font: "#202020",
-    },
-    stalled: {
-      main: "#FF5255",
-      font: "#FFFFFF",
-    },
-    delayed: {
-      main: "#FFB322",
-      font: "#202020",
-    },
-    unrated: {
-      main: "#EBEBEB",
-      font: "#202020",
-    },
-    unstarted: {
-      main: "#909090",
-      font: "#FFFFFF",
-    },
-  },
-  allStatus: [
+  promiseStatuses: [
     {
-      name: "Completed",
-      slug: "completed",
+      color: "#005DFD",
       description: "The promise is mostly or completely fulfilled.",
+      textColor: "#FFFFFF",
+      title: "Completed",
     },
     {
-      name: "In progress",
-      slug: "in-progress",
+      color: "#90DAFF",
       description: "The promise is in the works or being considered.",
+      title: "In Progress",
     },
     {
-      name: "Stalled",
-      slug: "stalled",
+      color: "#FF5255",
       description:
         "Could occur due to inaction by administration or lack of support from legislative branch.",
+      title: "Stalled",
     },
     {
-      name: "Delayed",
-      slug: "delayed",
+      color: "#FFB322",
       description:
         "No progress, perhaps due to financial limitations, opposition from lawmakers or a change in priorities.",
+      title: "Delayed",
     },
     {
-      name: "Unrated",
-      slug: "unrated",
+      color: "#EBEBEB",
       description:
         "Every promise begins at this level and retains this rating until evidence of progress or proof that it has been shelved.",
+      title: "Unrated",
     },
     {
-      name: "Unstarted",
-      slug: "unstarted",
+      color: "#909090",
       description:
         "The promise is accomplished only in part, but has succeeded at least in part consistently with the goal of the promise.",
+      textColor: "#FFFFFF",
+      title: "Unstarted",
     },
   ],
+  pages: {
+    about: {
+      pages: {
+        project: {
+          content: `
+            <p>Engaging with governments needs data, to this end, this project not only develops a promise tracker but also showcases this information on an easy-to-understand platform, making the PromiseTracker a contributor to public discourse around service delivery by elected officials. This leads to a more robust dialogue and positive engagement with the elected officials or government bodies around this subject.
+            <p>The PromiseTracker initial focus will be on Nairobi tracking progress on Governor Mike Sonko’s seven-point plan to improve the city in the first 100 days of his term and 5 year plan. The tracker will also focus on other county governors whose plans are elaborated in their manifestos, such as Makueni, Mombasa, Kisumu, Nakuru, Kitui and Nandi. While the promises made by the national government have been covered extensively, promises made by the governors have received less coverage, resulting in limited analysis around whether they are actually viable.
+          `,
+          description:
+            "The promise tracker is a platform-based promise tracker where citizens can track various promises and services promised by governors, institutions, political parties in their manifestos during the campaigns leading up to the elections and in the post election period.",
+          title: "The Project",
+        },
+        team: {
+          content: `
+            <p>Engaging with governments needs data, to this end, this project not only develops a promise tracker but also showcases this information on an easy-to-understand platform, making the PromiseTracker a contributor to public discourse around service delivery by elected officials. This leads to a more robust dialogue and positive engagement with the elected officials or government bodies around this subject.
+            <p>The PromiseTracker initial focus will be on Nairobi tracking progress on Governor Mike Sonko’s seven-point plan to improve the city in the first 100 days of his term and 5 year plan. The tracker will also focus on other county governors whose plans are elaborated in their manifestos, such as Makueni, Mombasa, Kisumu, Nakuru, Kitui and Nandi. While the promises made by the national government have been covered extensively, promises made by the governors have received less coverage, resulting in limited analysis around whether they are actually viable.
+          `,
+          description:
+            "The promise tracker is a platform-based promise tracker where citizens can track various promises and services promised by governors, institutions, political parties in their manifestos during the campaigns leading up to the elections and in the post election period.",
+          title: "The Team",
+        },
+        partners: {
+          content: `
+            <p>Engaging with governments needs data, to this end, this project not only develops a promise tracker but also showcases this information on an easy-to-understand platform, making the PromiseTracker a contributor to public discourse around service delivery by elected officials. This leads to a more robust dialogue and positive engagement with the elected officials or government bodies around this subject.
+            <p>The PromiseTracker initial focus will be on Nairobi tracking progress on Governor Mike Sonko’s seven-point plan to improve the city in the first 100 days of his term and 5 year plan. The tracker will also focus on other county governors whose plans are elaborated in their manifestos, such as Makueni, Mombasa, Kisumu, Nakuru, Kitui and Nandi. While the promises made by the national government have been covered extensively, promises made by the governors have received less coverage, resulting in limited analysis around whether they are actually viable.
+          `,
+          description:
+            "The promise tracker is a platform-based promise tracker where citizens can track various promises and services promised by governors, institutions, political parties in their manifestos during the campaigns leading up to the elections and in the post election period.",
+          title: "The Partners",
+        },
+        methodology: {
+          content: `
+            <p>Engaging with governments needs data, to this end, this project not only develops a promise tracker but also showcases this information on an easy-to-understand platform, making the PromiseTracker a contributor to public discourse around service delivery by elected officials. This leads to a more robust dialogue and positive engagement with the elected officials or government bodies around this subject.
+            <p>The PromiseTracker initial focus will be on Nairobi tracking progress on Governor Mike Sonko’s seven-point plan to improve the city in the first 100 days of his term and 5 year plan. The tracker will also focus on other county governors whose plans are elaborated in their manifestos, such as Makueni, Mombasa, Kisumu, Nakuru, Kitui and Nandi. While the promises made by the national government have been covered extensively, promises made by the governors have received less coverage, resulting in limited analysis around whether they are actually viable.
+          `,
+          description:
+            "The promise tracker is a platform-based promise tracker where citizens can track various promises and services promised by governors, institutions, political parties in their manifestos during the campaigns leading up to the elections and in the post election period.",
+          title: "Methodology",
+        },
+      },
+    },
+  },
+};
+
+config.pages.about.pages.project.criteria = {
+  items: config.promiseStatuses,
+  title: "Promise Criteria",
 };
 
 export default config;
