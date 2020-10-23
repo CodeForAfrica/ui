1886b3a8918ff2a9d5fb7ca2a403de56ca7ded3c
Clemence Kyara <clemence@codeforafrica.org>
2020-10-23 17:05:30 +0300

    Switch pages/404 to wp

--- apps/promisetracker/src/pages/404.js
+++ apps/promisetracker/src/pages/404.js
@@ -1,13 +1,20 @@
 import React from "react";
 
 import ErrorPage from "@/promisetracker/components/ErrorPage";
+
+import config from "@/promisetracker/config";
+import wp from "@/promisetracker/lib/wp";
+
 import articleImage from "@/promisetracker/assets/article-thumb-01.png";
 
 function NotFoundErrorPage(props) {
   return <ErrorPage {...props} />;
 }
 
-export async function getStaticProps() {
+export async function getStaticProps({ query = {} }) {
+  const { lang: pageLanguage } = query;
+  const lang = pageLanguage || config.DEFAULT_LANG;
+  const page = await wp().pages({ slug: "not-found", lang }).first;
   const description =
     "Oops!… The page you are looking for cannot be found. Try browsing the menu bar or read one of our recent posts below.";
   const items = Array(3)
@@ -26,8 +33,10 @@ export async function getStaticProps() {
     props: {
       description,
       items,
+      page,
       title,
     },
+    revalidate: 2 * 60, // seconds
   };
 }
 
