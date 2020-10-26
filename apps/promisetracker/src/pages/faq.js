26be5a2c232ec6f97523c722cc41db4c747d1bf6
Clemence Kyara <clemence@codeforafrica.org>
2020-10-26 13:40:36 +0300

    Hook /faq to cms

--- apps/promisetracker/src/pages/faq.js
+++ apps/promisetracker/src/pages/faq.js
@@ -7,6 +7,8 @@ import ActNow from "@/promisetracker/components/ActNow";
 import ContentPage from "@/promisetracker/components/ContentPage";
 import FAQ from "@/promisetracker/components/FAQ";
 
+import wp from "@/promisetracker/lib/wp";
+
 const useStyles = makeStyles(({ breakpoints, typography, widths }) => ({
   section: {
     padding: `0 ${typography.pxToRem(23)}`,
@@ -23,59 +25,53 @@ const useStyles = makeStyles(({ breakpoints, typography, widths }) => ({
   },
 }));
 
-function Index({ errorCode, page, actNow, ...props }) {
-  const {
-    faqs: propsFAQ,
-    title: { rendered: pageTitle },
-  } = page;
-
-  const faqs = propsFAQ.reduce((arr, e) => arr.concat(e.questions_answers), []);
+function FaqPage({ actNow, faqs, footer, navigation, ...props }) {
   const classes = useStyles(props);
 
   return (
-    <Page
-      errorCode={errorCode}
-      page={page}
-      title={pageTitle}
-      classes={{ section: classes.section }}
+    <ContentPage
+      {...props}
+      footer={footer}
+      navigation={navigation}
+      classes={{ section: classes.section, footer: classes.footer }}
+      content={<FAQ items={faqs} />}
+      contentProps={{
+        lg: 8,
+      }}
     >
-      <FAQ faqs={faqs} classes={{ section: classes.section }} />
       <ActNow {...actNow} classes={{ section: classes.section }} />
-    </Page>
+    </ContentPage>
   );
 }
 
-export async function getStaticProps() {
-  const lang = config.DEFAULT_LANG;
-  const page = await getSitePage("faq", lang);
+FaqPage.propTypes = {
+  actNow: PropTypes.shape({}),
+  footer: PropTypes.shape({}),
+  navigation: PropTypes.shape({}),
+  faqs: PropTypes.arrayOf(PropTypes.shape({})),
+};
 
-  const errorCode = null;
+FaqPage.defaultProps = {
+  actNow: undefined,
+  footer: undefined,
+  navigation: undefined,
+  faqs: undefined,
+};
+
+export async function getStaticProps({ query = {} }) {
+  const { lang } = query;
+  const page = await wp().pages({ slug: "faq", lang }).first;
+  const faqs = page.faqs
+    .reduce((arr, e) => arr.concat(e.questions_answers), [])
+    .map((faq) => ({ title: faq.question, summary: faq.answer }));
+
   return {
     props: {
-      errorCode,
-      page: page.page,
-      actNow: page.page.actNow,
+      ...page,
+      faqs,
     },
+    revalidate: 2 * 60, // seconds
   };
 }
 
-Index.propTypes = {
-  page: PropTypes.shape({
-    faqs: PropTypes.shape({}),
-    title: PropTypes.shape({
-      rendered: PropTypes.string,
-    }),
-    actNow: PropTypes.shape({}),
-  }),
-  errorCode: PropTypes.number,
-  actNow: PropTypes.shape({}),
-  subscribe: PropTypes.shape({}),
-};
-Index.defaultProps = {
-  page: undefined,
-  errorCode: undefined,
-  actNow: undefined,
-  subscribe: undefined,
-};
-
-export default Index;
+export default FaqPage;
