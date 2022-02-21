cd049412b0ad427e598f282f77e1eacf5e75d142
Obed Amoasi <obedamoasi@gmail.com>
2022-02-21 13:15:59 +0000

    pass petttitions to act now page

--- apps/promisetracker/src/pages/act-now.js
+++ apps/promisetracker/src/pages/act-now.js
@@ -3,6 +3,7 @@ import React from "react";
 
 import ActNowPage from "@/promisetracker/components/ActNowPage";
 import ActNowLoggedInPage from "@/promisetracker/components/ActNowPage/LoggedIn";
+import actnow from "@/promisetracker/lib/actnow";
 import backendFn from "@/promisetracker/lib/backend";
 import i18n from "@/promisetracker/lib/i18n";
 import wp from "@/promisetracker/lib/wp";
@@ -42,6 +43,7 @@ export async function getStaticProps({ locale }) {
 
   const page = await wp().pages({ slug: "act-now", locale }).first;
   const { actNow = {} } = page;
+  const petitions = await actnow().petitions().list;
 
   const languageAlternates = _.languageAlternates("/act-now");
   actNow.url = process.env.ACTNOW_URL ?? null;
@@ -63,6 +65,7 @@ export async function getStaticProps({ locale }) {
       actNow,
       languageAlternates,
       promises,
+      petitions,
     },
     revalidate: 2 * 60, // seconds
   };
