d5a7d9ea2145fe5ea9f5b1ac1978c6acc57b580c
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-02-22 05:51:36 +0300

    remove hardcoded nav + footer data

--- apps/promisetracker/src/pages/promises/[...slug].js
+++ apps/promisetracker/src/pages/promises/[...slug].js
@@ -182,7 +182,6 @@ export async function getStaticProps({ params: { slug: slugParam }, locale }) {
   const promiseStatuses = await backend.promises({ id }).statuses;
   const site = await backend.sites().current;
 
-  console.log("ACT PAGGGE", actNowPage);
   const notFound = !promisePost;
   if (notFound) {
     return {
