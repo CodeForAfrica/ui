7e8b8d9a9ab821fb3885b2f5768ea16d141adfc2
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2022-03-02 16:17:38 +0300

    Fix layout

--- apps/promisetracker/src/components/ActNowPage/useStyles.js
+++ apps/promisetracker/src/components/ActNowPage/useStyles.js
@@ -65,6 +65,7 @@ const useStyles = makeStyles(
     gridContent: {
       order: 0,
       [breakpoints.up("lg")]: {
+        marginTop: typography.pxToRem(368),
         order: 0,
       },
     },
