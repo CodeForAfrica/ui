51097f55d80bfca3dc47eb0682676fffdd5fb0b6
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2022-03-09 13:57:25 +0300

    Add jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  verbose: true,
+  // Stop running tests after `n` failures
+  bail: 1,
+  setupFilesAfterEnv: ["./jest.setup.js"],
+  // Default timeout of a test in milliseconds.
+  testTimeout: 30000,
+
+  projects: [
+      "./apps/docs/jest.docs.config.js",
+      "./apps/web/jest.web.config.js",
+  ],
+}
