4005fe976d8ba5250ccca2838117c800ab038946
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-03-10 12:30:13 +0300

    add material

---
+++ theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+    },
+  },
+});
+
+export default theme;
