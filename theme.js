e2b943f73977a8e78d82fa8be140fce9f6be4792
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-03-10 14:17:27 +0300

    theme file

--- theme.js
+++ theme.js
@@ -3,14 +3,9 @@ import { createTheme } from '@mui/material/styles';
 // Create a theme instance.
 const theme = createTheme({
   palette: {
-    primary: {
-      main: '#556cd6',
-    },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-    },
+    primary: {},
+    secondary: {},
+    error: {},
   },
 });
 
