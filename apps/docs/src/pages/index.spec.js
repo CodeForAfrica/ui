0d02bf15d15b3d506da14f348a7bc3bca65d5722
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2022-03-16 14:46:18 +0300

    Update apps folders

---
+++ apps/docs/src/pages/index.spec.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect';
+
+import Docs from '../pages/index'
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    render(<Docs />)
+
+    const heading = screen.getByRole('heading', {
+      name: /docs welcome to next\.js!/i,
+    })
+
+    expect(heading).toBeInTheDocument()
+  })
+})
