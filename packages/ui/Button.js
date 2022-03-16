d1ca61a8f7600ebf1abb8892eef0bc7bc4f052d2
_ Kilemensi <clemence@codeforafrica.org>
2022-03-16 09:37:29 +0300

    Switch to MUI button

--- packages/ui/Button.js
+++ packages/ui/Button.js
@@ -1,5 +1,8 @@
+import MuiButton from "@mui/material/Button";
 import * as React from "react";
 
-export const Button = () => {
-  return <button>Boop</button>;
-};
+function Button() {
+  return <MuiButton>Boop</MuiButton>;
+}
+
+export default Button;
