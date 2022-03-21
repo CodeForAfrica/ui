0198221ae383e8c8eed454c5dcfea22c2bc38e78
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2022-03-21 14:14:57 +0300

    Fix index.spec.js

--- packages/ui/index.spec.js
+++ packages/ui/index.spec.js
@@ -1,4 +1,4 @@
-import { render, describe } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
 import Button from "./Button";
