e55e410becb1b1d61ecea450c4c03714d93d1ec0
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2022-03-21 17:03:11 +0300

    Update packages/ui/Button.test.js
    Co-authored-by: _ Kilemensi <kilemensi@users.noreply.github.com>

--- packages/ui/Button.test.js
+++ packages/ui/Button.test.js
@@ -4,7 +4,7 @@ import "@testing-library/jest-dom/extend-expect";
 import Button from "./Button";
 
 describe("Button", () => {
-  it("renders a heading", () => {
+  it("renders a button", () => {
     render(<Button />);
   });
 });
