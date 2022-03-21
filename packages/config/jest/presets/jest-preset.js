ff96bc59dd05c0152629d2f639c7fc6a8a1d3a8c
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2022-03-21 17:06:10 +0300

    Update spaces

--- packages/config/jest/presets/jest-preset.js
+++ packages/config/jest/presets/jest-preset.js
@@ -22,11 +22,6 @@ module.exports = {
   },
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-
-
-
-
-
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
   testEnvironment: 'jsdom',
   transform: {
