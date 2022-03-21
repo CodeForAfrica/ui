4cbcf5f8ccab074d825f1469179a42ad9fe337b2
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2022-03-21 15:00:10 +0300

    Rename test

---
+++ apps/web/src/pages/index.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import Docs from "./index";
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Docs />);
+
+    const heading = screen.getByRole("heading", {
+      name: /web welcome to next\.js!/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
