060055d730d11d6cc0f04479e90f1f3b214d3310
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2022-03-23 13:28:41 +0300

    Update changes

---
+++ .changeset/tame-flowers-buy.md
@@ -0,0 +1,5 @@
+---
+"example-changeset-2": major
+---
+
+This is an example changeset 2
---
+++ packages/example-changeset-2/Button.js
@@ -0,0 +1,8 @@
+import MuiButton from "@mui/material/Button";
+import * as React from "react";
+
+function Button() {
+  return <MuiButton>Boop</MuiButton>;
+}
+
+export default Button;
