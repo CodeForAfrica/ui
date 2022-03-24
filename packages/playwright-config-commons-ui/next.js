1fbe8f4c7cde5b22d2c9dd0b99dd6d1187c2e7c6
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2022-03-24 15:11:59 +0300

    Update playwright config for web && doc

--- packages/playwright-config-commons-ui/next.js
+++ packages/playwright-config-commons-ui/next.js
@@ -1,13 +1,11 @@
 const defaultConfig = require(".");
 
-const { testPathIgnorePatterns } = defaultConfig;
-
 module.exports = {
   ...defaultConfig,
-  testPathIgnorePatterns: [...testPathIgnorePatterns, "<rootDir>/.next/"],
+  testPathIgnorePatterns: ["<rootDir>/.next/"],
   transform: {
     // Use babel-jest to transpile tests with the next/babel preset
     // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
-    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+    "^.+\\.(js|jsx)$": ["babel-jest", { presets: ["next/babel"] }],
   },
 };
