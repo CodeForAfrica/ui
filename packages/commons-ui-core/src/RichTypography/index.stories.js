7f5ae762b32c6cd8040a5941665ba5de2f1674a9
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2022-03-28 14:15:01 +0300

    Update package name

---
+++ packages/commons-ui-core/src/RichTypography/index.stories.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import RichTypography from "./index";
+
+export default {
+  title: "Component/RichTypography",
+  argTypes: {},
+};
+
+function Template() {
+  return <RichTypography> This is an example</RichTypography>;
+}
+
+export const Default = Template.bind({});
+
+Default.args = {};
---
+++ packages/commons-ui-core/src/index.js
@@ -0,0 +1,5 @@
+import RichTypography from "./RichTypography";
+
+export default {
+  RichTypography,
+};
