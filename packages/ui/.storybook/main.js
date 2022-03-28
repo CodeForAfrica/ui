97f98c872d870fcf5e431f92d54f39d5fe6780a5
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2022-03-28 11:54:32 +0300

    Update react dependancies

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -4,7 +4,7 @@ module.exports = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "storybook-addon-material-ui",
+    "storybook-addon-material-ui5",
     {
       name: "storybook-addon-turbo-build",
       options: {
