57d64fe35af71cac45eff77f78fa19509ef37f60
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-03-28 12:00:29 +0300

    add tests

---
+++ packages/next-commons-ui/Link.test.js
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import Link from "./Link";
+
+describe("Link", () => {
+  it("renders a next/router Link", () => {
+    render(<Link />);
+  });
+});
