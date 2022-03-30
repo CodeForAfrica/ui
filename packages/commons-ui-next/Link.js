5377bd3ae97c3c4334326445f1187645d244b504
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-03-30 13:35:01 +0300

    Link implementation

--- packages/commons-ui-next/Link.js
+++ packages/commons-ui-next/Link.js
@@ -1,64 +1,115 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
-import { Link as MuiLink } from "@mui/material";
+import * as React from "react";
+import PropTypes from "prop-types";
 import clsx from "clsx";
 import { useRouter } from "next/router";
-import PropTypes from "prop-types";
-import React, { forwardRef } from "react";
+import NextLink from "next/link";
+import MuiLink from "@mui/material/Link";
+import { styled } from "@mui/material/styles";
 
-import NextComposed from "./NextComposed";
+// Add support for the sx prop for consistency with the other branches.
+const Anchor = styled("a")({});
+
+export const NextLinkComposed = React.forwardRef(function NextLinkComposed(
+  props,
+  ref
+) {
+  const { to, linkAs, replace, scroll, shallow, prefetch, locale, ...other } =
+    props;
+
+  return (
+    <NextLink
+      href={to}
+      prefetch={prefetch}
+      as={linkAs}
+      replace={replace}
+      scroll={scroll}
+      shallow={shallow}
+      passHref
+      locale={locale}
+    >
+      <Anchor ref={ref} {...other} />
+    </NextLink>
+  );
+});
+
+NextLinkComposed.propTypes = {
+  href: PropTypes.any,
+  linkAs: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  locale: PropTypes.string,
+  passHref: PropTypes.bool,
+  prefetch: PropTypes.bool,
+  replace: PropTypes.bool,
+  scroll: PropTypes.bool,
+  shallow: PropTypes.bool,
+  to: PropTypes.oneOfType([PropTypes.object, PropTypes.string]).isRequired,
+};
 
 // A styled version of the Next.js Link component:
-// https://nextjs.org/docs/#with-link
-const Link = forwardRef(function Link(props, ref) {
+// https://nextjs.org/docs/api-reference/next/link
+const Link = React.forwardRef(function Link(props, ref) {
   const {
-    href,
     activeClassName = "active",
+    as,
     className: classNameProps,
-    naked,
+    href,
+    linkAs: linkAsProp,
+    locale,
+    noLinkStyle,
+    prefetch,
+    replace,
+    role, // Link don't have roles.
+    scroll,
+    shallow,
     ...other
   } = props;
 
   const router = useRouter();
-  const pathname = typeof href === "string" ? href : href?.pathname;
+  const pathname = typeof href === "string" ? href : href.pathname;
   const className = clsx(classNameProps, {
     [activeClassName]: router?.pathname === pathname && activeClassName,
   });
 
-  const formattedHref = href;
+  const isExternal =
+    typeof href === "string" &&
+    (href.indexOf("http") === 0 || href.indexOf("mailto:") === 0);
 
-  const isRelative =
-    typeof formattedHref === "string" &&
-    (formattedHref.indexOf("/") === 0 || formattedHref.indexOf("#") === 0) &&
-    formattedHref.indexOf("//") !== 0;
+  if (isExternal) {
+    if (noLinkStyle) {
+      return <Anchor className={className} href={href} ref={ref} {...other} />;
+    }
 
-  if (!isRelative) {
-    const noProtocol = href?.startsWith("www.");
+    return <MuiLink className={className} href={href} ref={ref} {...other} />;
+  }
+
+  const linkAs = linkAsProp || as;
+  const nextjsProps = {
+    to: href,
+    linkAs,
+    replace,
+    scroll,
+    shallow,
+    prefetch,
+    locale,
+  };
+
+  if (noLinkStyle) {
     return (
-      <a
-        href={noProtocol ? `https://${href}` : href}
+      <NextLinkComposed
         className={className}
         ref={ref}
+        {...nextjsProps}
         {...other}
       />
     );
   }
 
-  if (naked) {
-    return (
-      <NextComposed
-        className={className}
-        ref={ref}
-        href={formattedHref}
-        {...other}
-      />
-    );
-  }
   return (
     <MuiLink
-      component={NextComposed}
+      component={NextLinkComposed}
       className={className}
       ref={ref}
-      href={formattedHref}
+      {...nextjsProps}
       {...other}
     />
   );
@@ -66,22 +117,17 @@ const Link = forwardRef(function Link(props, ref) {
 
 Link.propTypes = {
   activeClassName: PropTypes.string,
-  as: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  as: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   className: PropTypes.string,
-  href: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
-  naked: PropTypes.bool,
-  onClick: PropTypes.func,
+  href: PropTypes.any,
+  linkAs: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  locale: PropTypes.string,
+  noLinkStyle: PropTypes.bool,
   prefetch: PropTypes.bool,
-};
-
-Link.defaultProps = {
-  activeClassName: undefined,
-  as: undefined,
-  className: undefined,
-  href: undefined,
-  naked: undefined,
-  onClick: undefined,
-  prefetch: undefined,
+  replace: PropTypes.bool,
+  role: PropTypes.string,
+  scroll: PropTypes.bool,
+  shallow: PropTypes.bool,
 };
 
 export default Link;
