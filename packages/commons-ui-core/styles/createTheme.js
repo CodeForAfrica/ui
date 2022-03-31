0a72e5413c8c942b756b1ba0e20e3ac4cc1a9fb8
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-03-31 16:31:29 +0300

    folder + file restructure

--- packages/commons-ui-core/styles/createTheme.js
+++
@@ -1,7 +0,0 @@
-import { createTheme } from "@mui/material/styles";
-
-const customTheme = (options) => {
-  return createTheme(options);
-};
-
-export default customTheme;
---
+++ packages/commons-ui-core/styles/createTheme/createTheme.js
@@ -0,0 +1,7 @@
+import { createTheme as MuiCreateTheme } from "@mui/material/styles";
+
+const createTheme = (options) => {
+  return MuiCreateTheme(options);
+};
+
+export default createTheme;
