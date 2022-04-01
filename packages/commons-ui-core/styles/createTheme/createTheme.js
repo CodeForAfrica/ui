ea633a12fbc8f7af313bc91e9d94c5b49114d342
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-04-01 14:03:53 +0300

    deepmerge + check

--- packages/commons-ui-core/styles/createTheme/createTheme.js
+++ packages/commons-ui-core/styles/createTheme/createTheme.js
@@ -1,7 +1,31 @@
 import { createTheme as MuiCreateTheme } from "@mui/material/styles";
+import { deepmerge } from "@mui/utils";
 
 const createTheme = (options) => {
-  return MuiCreateTheme(options);
+  const { breakpoints, contentWidths } = options;
+
+  if (breakpoints && contentWidths) {
+    return MuiCreateTheme(options);
+  } else {
+    return MuiCreateTheme(
+      deepmerge(options, {
+        breakpoints: {
+          values: {
+            xs: 0,
+            sm: 360, // mobile
+            md: 720, // tablet
+            lg: 1280, // desktop
+          },
+        },
+        contentWidths: {
+          values: {
+            md: 608, // 0, 80, 0, 80 margin
+            lg: 1160, // 0, 140, 0, 140 margin
+          },
+        },
+      })
+    );
+  }
 };
 
 export default createTheme;
