3eee288dbd3e5154ab8df94c3e155eefaeb11453
_ Kilemensi <clemence@codeforafrica.org>
2022-04-04 09:50:38 +0300

    Add NavBar render unchanged test

---
+++ packages/commons-ui-core/src/NavBar/NavBar.snap.js
@@ -0,0 +1,19 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`NavBar renders unchanged 1`] = `
+<div>
+  <header
+    class="MuiPaper-root MuiPaper-elevation MuiPaper-elevation4 MuiAppBar-root MuiAppBar-colorPrimary MuiAppBar-positionStatic css-1s60hy8-MuiPaper-root-MuiAppBar-root-CuiNavBar-root"
+  >
+    <div
+      class="MuiToolbar-root MuiToolbar-regular css-1cndkqf-MuiToolbar-root"
+    >
+      <div
+        class="MuiTypography-root MuiTypography-h6 css-1shoboz-MuiTypography-root"
+      >
+        LOGO
+      </div>
+    </div>
+  </header>
+</div>
+`;
---
+++ packages/commons-ui-core/src/NavBar/NavBar.test.js
@@ -0,0 +1,19 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+import NavBar from ".";
+
+import RichTypography from "@/commons-ui/core/RichTypography";
+
+describe("NavBar", () => {
+  it("renders unchanged", () => {
+    const { container } = render(
+      <NavBar>
+        <RichTypography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          LOGO
+        </RichTypography>
+      </NavBar>
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
