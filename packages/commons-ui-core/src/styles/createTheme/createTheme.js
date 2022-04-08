6e69060e721a786eecbe459e26b46fd5fcb3d584
_ Kilemensi <clemence@codeforafrica.org>
2022-04-08 15:25:11 +0300

    Use createContentWidths

--- packages/commons-ui-core/src/styles/createTheme/createTheme.js
+++ packages/commons-ui-core/src/styles/createTheme/createTheme.js
@@ -1,25 +1,13 @@
-import { createTheme as MuiCreateTheme } from "@mui/material/styles";
+import { createTheme as muiCreateTheme } from "@mui/material/styles";
 import { deepmerge } from "@mui/utils";
 
-const createTheme = (options = {}) => {
-  const { contentWidths } = options;
+import createContentWidths from "./createContentWidths";
 
-  if (contentWidths) {
-    return MuiCreateTheme(options);
-  }
-  return MuiCreateTheme(
-    deepmerge(options, {
-      contentWidths: {
-        values: {
-          xs: 300,
-          sm: 480,
-          md: 608,
-          lg: 1160,
-          xl: 1440,
-        },
-      },
-    })
-  );
-};
+function createTheme(options = {}) {
+  const { breakpoints, contentWidths: contentWidthsInput } = options;
+  const contentWidths = createContentWidths(contentWidthsInput, breakpoints);
+
+  return muiCreateTheme(deepmerge(options, { contentWidths }));
+}
 
 export default createTheme;
