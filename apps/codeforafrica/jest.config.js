649b9508fd86c775c0ce1f896623256ab6a11bc7
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2022-04-08 11:09:08 +0300

    Update jest config

--- apps/codeforafrica/jest.config.js
+++ apps/codeforafrica/jest.config.js
@@ -1,1 +1,12 @@
-module.exports = require("jest-config-commons-ui/next");
+const defaultConfig = require("jest-config-commons-ui");
+
+const { moduleNameMapper } = defaultConfig;
+
+module.exports = {
+  ...defaultConfig,
+  moduleNameMapper: {
+    ...moduleNameMapper,
+    // Handle module aliases
+    "^@/cdeforafrica/(.*)$": "<rootDir>/src/$1",
+  },
+};
