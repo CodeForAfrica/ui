e11160c8e4dded38bb2ab64fdfa4090c2ff7d837
Brenda Nyokabi Bucha <nyokabi@codeforafrica.org>
2022-04-08 13:23:31 +0300

    Update apps/codeforafrica/src/pages/index.spec.js
    Co-authored-by: _ Kilemensi <kilemensi@users.noreply.github.com>

--- apps/codeforafrica/src/pages/index.spec.js
+++ apps/codeforafrica/src/pages/index.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require("@playwright/test");
 
 test("basic test", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
+  await page.goto("http://localhost:3002/");
   const title = await page.locator(".index_title__gEapU");
   await expect(title).toHaveText("This is the official CFA SITE");
 });
