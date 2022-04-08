4bbb0b970b62ea9805a6dc1cd5416b8b5f53dbf3
_ Kilemensi <clemence@codeforafrica.org>
2022-04-08 15:42:39 +0300

    Fix Storybook main

--- packages/config/storybook/main.js
+++ packages/config/storybook/main.js
@@ -1,10 +1,7 @@
-const path = require("path");
-
 module.exports = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "storybook-addon-material-ui",
   ],
 
   stories: ["../**/*.stories.js"],
