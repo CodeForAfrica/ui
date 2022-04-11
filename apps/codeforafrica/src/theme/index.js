a0c345564eab70908cc248d05934394f9c688d3b
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2022-04-11 10:37:15 +0300

    Update colors

--- apps/codeforafrica/src/theme/index.js
+++ apps/codeforafrica/src/theme/index.js
@@ -3,13 +3,12 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
   palette: {
     mode: "light",
-    primary: { main: "#1020E1" },
-    secondary: { main: "#000000" },
+    primary: { main: "#1020E1", light: "#EFF0FD" },
+    secondary: { main: "#000000", light: "#7F7272" },
     text: {
       primary: { main: "#000000" },
       secondary: { main: "#FFFFFF" },
     },
-    background: { main: "#EFF0FD" },
     highlight: { main: "#ED1C24" },
   },
   breakpoints: {
