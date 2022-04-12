ebace8008cae8010e8bd1af8b6d28b1ead5075d0
_ Kilemensi <clemence@codeforafrica.org>
2022-04-12 11:03:12 +0300

    Export ImageButton

--- packages/commons-ui-core/src/index.js
+++ packages/commons-ui-core/src/index.js
@@ -1,4 +1,5 @@
 export * from "./styles";
 
+export { default as ImageButton } from "./ImageButton";
 export { default as NavBar } from "./NavBar";
 export { default as RichTypography } from "./RichTypography";
