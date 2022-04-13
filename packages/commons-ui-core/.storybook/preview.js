31b4dd9dd208134a89daf58acc61a926e63ed8e1
_ Kilemensi <clemence@codeforafrica.org>
2022-04-13 15:59:21 +0300

    use MUI breakpoints as viewports

--- packages/commons-ui-core/.storybook/preview.js
+++ packages/commons-ui-core/.storybook/preview.js
@@ -1,6 +1,8 @@
 import { ThemeProvider } from "@mui/material/styles";
 import createTheme from "@/commons-ui/core/styles/createTheme";
 
+export * from "config/storybook/preview";
+
 const theme = createTheme();
 
 import "./styles.css";
