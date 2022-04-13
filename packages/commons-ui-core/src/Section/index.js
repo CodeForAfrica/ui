3a4307786981cf8c37c793c9a9d938e0e09c15e1
_ Kilemensi <clemence@codeforafrica.org>
2022-04-13 11:45:19 +0300

    export Section

--- packages/commons-ui-core/src/Section/index.js
+++ packages/commons-ui-core/src/Section/index.js
@@ -1,29 +1,3 @@
-import { styled } from "@mui/material/styles";
-import { Container } from "@mui/material";
-
-const SectionRoot = styled(Container)(({ theme }) => ({
-  display: "flex",
-  [theme.breakpoints.up("xs")]: {
-    justifyContent: "center",
-    maxWidth: theme.contentWidths.values.xs,
-  },
-  [theme.breakpoints.up("sm")]: {
-    maxWidth: theme.contentWidths.values.sm,
-  },
-  [theme.breakpoints.up("md")]: {
-    justifyContent: "flex-start",
-    maxWidth: theme.contentWidths.values.md,
-  },
-  [theme.breakpoints.up("lg")]: {
-    maxWidth: theme.contentWidths.values.lg,
-  },
-  [theme.breakpoints.up("xl")]: {
-    maxWidth: theme.contentWidths.values.xl,
-  },
-}));
-
-function Section({ ...props }) {
-  return <SectionRoot fixed {...props} />;
-}
+import Section from "./Section";
 
 export default Section;
