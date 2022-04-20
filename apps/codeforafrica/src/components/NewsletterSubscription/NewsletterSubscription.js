e56f23f1952e0c7e69139e5a05c48ebe87571819
_ Kilemensi <clemence@codeforafrica.org>
2022-04-20 16:19:57 +0300

    Accept embedCode or children for subscription

--- apps/codeforafrica/src/components/NewsletterSubscription/NewsletterSubscription.js
+++ apps/codeforafrica/src/components/NewsletterSubscription/NewsletterSubscription.js
@@ -1,14 +1,34 @@
 import { RichTypography } from "@commons-ui/core";
 import { styled } from "@mui/material/styles";
+import PropTypes from "prop-types";
 import React from "react";
 
-const NewsletterSubscriptionRoot = styled(RichTypography)(({ theme }) => ({
-  color: theme.palette.text.primary,
-  "& #mc_embed_signup": {
-    backgroundColor: "unset",
-    color: "inherit",
-  },
-}));
+const NewsletterSubscriptionRoot = styled(RichTypography)(
+  ({ theme: { typography } }) => ({
+    "& #mc_embed_signup": {
+      ...typography.body1,
+      backgroundColor: "unset",
+      color: "inherit",
+    },
+    "& #mc_embed_signup input[type=text], & #mc_embed_signup input[type=email]":
+      {
+        border: "1px solid #D0CBCB",
+        display: "flex",
+        height: typography.pxToRem(36),
+        width: "100%",
+        marginBottom: typography.pxToRem(30),
+        padding: `0 ${typography.pxToRem(12)}`,
+      },
+    "& #mc_embed_signup input[type=submit]": {
+      ...typography.subtitle1,
+      background: "none",
+      border: "none",
+      color: "inherit",
+      padding: 0,
+      textDecoration: "underline",
+    },
+  })
+);
 
 /**
  * Section is a special Container that uses theme.contentWidths to set the
@@ -19,7 +39,27 @@ const NewsletterSubscription = React.forwardRef(function NewsletterSubscription(
   props,
   ref
 ) {
-  return <NewsletterSubscriptionRoot {...props} ref={ref} />;
+  const { children: childrenProp, embedCode } = props;
+  const children = childrenProp || embedCode;
+
+  if (!children) {
+    return null;
+  }
+  return (
+    <NewsletterSubscriptionRoot ref={ref}>
+      {children}
+    </NewsletterSubscriptionRoot>
+  );
 });
+
+NewsletterSubscription.propTypes = {
+  children: PropTypes.node,
+  embedCode: PropTypes.node,
+};
+
+NewsletterSubscription.defaultProps = {
+  children: undefined,
+  embedCode: undefined,
+};
 
 export default NewsletterSubscription;
