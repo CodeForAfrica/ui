3c6070cd40acdb83218738fa419884ba6b03f5b2
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-04-20 11:50:46 +0300

    add icons

--- packages/commons-ui-core/src/A/A.js
+++ packages/commons-ui-core/src/A/A.js
@@ -1,4 +1,4 @@
-import Link from "@mui/material";
+import Link from "@mui/material/Link";
 import PropTypes from "prop-types";
 import React from "react";
 
--- packages/commons-ui-core/src/StayInTouch/StayInTouch.stories.js
+++ packages/commons-ui-core/src/StayInTouch/StayInTouch.stories.js
@@ -1,6 +1,39 @@
 import React from "react";
 
 import StayInTouch from "./index";
+import github from "../../public/github.png";
+import instagram from "../../public/instagram.png";
+import facebook from "../../public/facebook.png";
+import twitter from "../../public/twitter.png";
+import slack from "../../public/slack.png";
+import linkedin from "../../public/linkedin.png";
+
+const socialMedia = [
+  {
+    url: "https://www.instagram.com/code4africa__/",
+    image: { alt: "Instagram", url: instagram },
+  },
+  {
+    url: "https://github.com/CodeForAfrica",
+    image: { alt: "Github", url: github },
+  },
+  {
+    url: "https://twitter.com/Code4Africa",
+    image: { alt: "Twitter", url: twitter },
+  },
+  {
+    url: "https://ke.linkedin.com/company/code-for-africa",
+    image: { alt: "LinkedIn", url: linkedin },
+  },
+  {
+    url: "https://ke.linkedin.com/company/code-for-africa",
+    image: { alt: "Slack", url: slack },
+  },
+  {
+    url: "https://www.facebook.com/CodeForAfrica/",
+    image: { alt: "Facebook", url: facebook },
+  },
+];
 
 export default {
   title: "Component/StayInTouch",
@@ -22,4 +55,5 @@ export const Default = Template.bind({});
 
 Default.args = {
   title: "Stay in touch",
+  socialMedia,
 };
