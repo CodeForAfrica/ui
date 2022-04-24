324bee53fae90430c5fa1a02cdb31bb2a23f2f6d
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-04-24 18:54:03 +0300

    stay in touch icons

--- apps/codeforafrica/src/components/Footer/Footer.js
+++ apps/codeforafrica/src/components/Footer/Footer.js
@@ -1,6 +1,5 @@
 import { Section } from "@commons-ui/core";
 import { Link } from "@commons-ui/next";
-// import Box from "@mui/material/Box";
 import { Grid, Typography, List, ListItem } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import Image from "next/image";
@@ -8,8 +7,41 @@ import PropTypes from "prop-types";
 import React from "react";
 
 import cfaIcon from "@/codeforafrica/assets/images/CfAlogoBW.png";
+import facebook from "@/codeforafrica/assets/images/facebook.png";
+import github from "@/codeforafrica/assets/images/github.png";
+import instagram from "@/codeforafrica/assets/images/instagram.png";
+import linkedin from "@/codeforafrica/assets/images/linkedin.png";
+import slack from "@/codeforafrica/assets/images/slack.png";
+import twitter from "@/codeforafrica/assets/images/twitter.png";
 import NewsletterSubscription from "@/codeforafrica/components/NewsletterSubscription";
 import StayInTouch from "@/codeforafrica/components/StayInTouch";
+
+const socialMedia = [
+  {
+    url: "https://twitter.com/Code4Africa",
+    image: { alt: "Twitter", url: twitter.src },
+  },
+  {
+    url: "https://ke.linkedin.com/company/code-for-africa",
+    image: { alt: "Slack", url: slack.src },
+  },
+  {
+    url: "https://ke.linkedin.com/company/code-for-africa",
+    image: { alt: "LinkedIn", url: linkedin.src },
+  },
+  {
+    url: "https://www.facebook.com/CodeForAfrica/",
+    image: { alt: "Facebook", url: facebook.src },
+  },
+  {
+    url: "https://www.instagram.com/code4africa__/",
+    image: { alt: "Instagram", url: instagram.src },
+  },
+  {
+    url: "https://github.com/CodeForAfrica",
+    image: { alt: "Github", url: github.src },
+  },
+];
 
 const FooterRoot = styled("footer")(
   ({ theme: { breakpoints, palette, typography } }) => ({
@@ -53,7 +85,7 @@ const Footer = React.forwardRef(function Footer(props, ref) {
                 </FooterDescription>
               </Grid>
               <Grid item xs={12}>
-                <StayInTouch />
+                <StayInTouch socialMedia={socialMedia} />
               </Grid>
             </Grid>
           </Grid>
--- apps/codeforafrica/src/components/StayInTouch/StayInTouch.js
+++ apps/codeforafrica/src/components/StayInTouch/StayInTouch.js
@@ -1,8 +1,115 @@
-import { A } from "@commons-ui/core";
-import React from "react";
+import { A, RichTypography } from "@commons-ui/core";
+import { Grid } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import PropTypes from "prop-types";
+import * as React from "react";
 
-function StayInTouch() {
-  return <A>Stay in touch</A>;
-}
+const TitleRoot = styled(Grid, {
+  slot: "Root",
+  name: "StayInTouchTitle",
+})(({ theme }) => ({
+  textAlign: "center",
+  [theme.breakpoints.up("md")]: {
+    textAlign: "left",
+  },
+}));
+
+const LinksRoot = styled(Grid, {
+  slot: "Root",
+  name: "StayInTouchLinks",
+})(({ theme }) => ({
+  "& > a": {
+    display: "inline-block",
+    borderRight: "1px solid white",
+  },
+  "& > a:last-of-type": {
+    border: "none",
+  },
+  [theme.breakpoints.up("md")]: {
+    justifyContent: "flex-start",
+    width: "auto",
+  },
+}));
+
+const SocialLinkRoot = styled(A, {
+  slot: "Root",
+  name: "StayInTouchSocialLink",
+})(() => ({
+  display: "inline-block",
+  padding: "0 0.625rem",
+}));
+
+const SupportLinkRoot = styled(A, {
+  slot: "Root",
+  name: "StayInTouchSupportLink",
+})(() => ({
+  display: "inline-block",
+  padding: "0 0.625rem",
+}));
+
+const IconRoot = styled("img", {
+  slot: "Root",
+  name: "StayInTouchIcon",
+})(() => ({
+  width: "1.375rem",
+  height: "1.375rem",
+  objectFit: "contain",
+}));
+
+const StayInTouch = React.forwardRef(function StayInTouch(
+  { children, support, socialMedia, title, ...props },
+  ref
+) {
+  if (!(socialMedia && socialMedia.length)) {
+    return null;
+  }
+
+  return (
+    <Grid container ref={ref} {...props}>
+      {title && (
+        <TitleRoot item xs={12} md="auto">
+          <RichTypography>{title}</RichTypography>
+        </TitleRoot>
+      )}
+      <LinksRoot item xs={12} md="auto" container>
+        {support && (
+          <SupportLinkRoot href={`mailto:${support.email}`}>
+            <IconRoot src={support.image.url} alt={support.image.alt} />
+          </SupportLinkRoot>
+        )}
+        {socialMedia.map((media) => (
+          <SocialLinkRoot key={media.url} href={media.url}>
+            <IconRoot src={media.image.url} alt={media.image.alt} />
+          </SocialLinkRoot>
+        ))}
+      </LinksRoot>
+    </Grid>
+  );
+});
+
+StayInTouch.propTypes = {
+  socialMedia: PropTypes.arrayOf(
+    PropTypes.shape({
+      url: PropTypes.string.isRequired,
+      image: PropTypes.shape({
+        url: PropTypes.string.isRequired,
+        alt: PropTypes.string.isRequired,
+      }).isRequired,
+    }).isRequired
+  ).isRequired,
+  support: PropTypes.shape({
+    email: PropTypes.string.isRequired,
+    image: PropTypes.shape({
+      url: PropTypes.string.isRequired,
+      alt: PropTypes.string.isRequired,
+    }).isRequired,
+  }),
+  title: PropTypes.string,
+};
+
+StayInTouch.defaultProps = {
+  support: undefined,
+  title: "Stay in touch with us @ &nbsp;",
+};
 
 export default StayInTouch;
