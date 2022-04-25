a461efd142584e8a8e39852bf63e418126ef68a0
_ Kilemensi <clemence@codeforafrica.org>
2022-04-25 17:12:50 +0300

    Initial implementation of ArticleCardContent

---
+++ apps/codeforafrica/src/components/ArticleCardContent/ArticleCardContent.js
@@ -0,0 +1,16 @@
+import CardContent from "@mui/material/CardContent";
+import { styled } from "@mui/material/styles";
+import React from "react";
+
+const ArticleCardContentRoot = styled(CardContent)(({ theme }) => ({
+  padding: theme.typography.pxToRem(24),
+}));
+
+const ArticleCardContent = React.forwardRef(function ArticleCardContent(
+  props,
+  ref
+) {
+  return <ArticleCardContentRoot {...props} ref={ref} />;
+});
+
+export default ArticleCardContent;
