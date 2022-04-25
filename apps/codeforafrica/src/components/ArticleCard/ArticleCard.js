6919aef7518abc31695392bf5e6742c8bc0c5801
_ Kilemensi <clemence@codeforafrica.org>
2022-04-25 17:11:24 +0300

    Initial implementation of ArticleCard

---
+++ apps/codeforafrica/src/components/ArticleCard/ArticleCard.js
@@ -0,0 +1,56 @@
+import Card from "@mui/material/Card";
+import { styled } from "@mui/material/styles";
+import React from "react";
+
+const ArticleCardRoot = styled(Card, {
+  overridesResolver: (props, styles) => {
+    const { ownerState } = props;
+
+    return [styles.root, styles[ownerState.variant]];
+  },
+})(({ theme, ownerState }) => ({
+  "&:hover": {
+    ...(ownerState.variant === "outlined" && {
+      border: `1px solid ${theme.palette.highlight.main}`,
+      filter: "none",
+    }),
+  },
+  ...(ownerState.variant === "outlined" && {
+    border: "1px solid #DAD5D5",
+    filter: "drop-shadow(0px 4px 8px rgba(0, 0, 0, 0.1))",
+  }),
+}));
+
+/**
+ * Section is a special Container that uses theme.contentWidths to set the
+ * max-width of the container instead of using the full breakpoints values.
+ * This is only applicable when fixed is true.
+ */
+const ArticleCard = React.forwardRef(function ArticleCard(props, ref) {
+  const {
+    elevation = 0,
+    square = true,
+    variant = "outlined",
+    ...other
+  } = props;
+
+  const ownerState = {
+    ...other,
+    elevation,
+    square,
+    variant,
+  };
+
+  return (
+    <ArticleCardRoot
+      elevation={elevation}
+      square={square}
+      variant={variant}
+      ref={ref}
+      ownerState={ownerState}
+      {...other}
+    />
+  );
+});
+
+export default ArticleCard;
