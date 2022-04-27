4114e961d5c49a9d8ec9e46d24816aed68b4afd3
_ Kilemensi <clemence@codeforafrica.org>
2022-04-27 11:37:03 +0300

    Ensure padding only on mobile

--- apps/codeforafrica/src/components/Footer/Footer.js
+++ apps/codeforafrica/src/components/Footer/Footer.js
@@ -26,7 +26,7 @@ const Footer = React.forwardRef(function Footer(props, ref) {
 
   return (
     <FooterRoot ref={ref}>
-      <Section>
+      <Section sx={{ px: { xs: "20px", sm: 0 } }}>
         <Grid container justifyContent="flex-end">
           <Grid item xs={12} md={3} sx={{ order: { xs: 0, md: 1 } }}>
             <NewsletterSubscription {...subscription} />
