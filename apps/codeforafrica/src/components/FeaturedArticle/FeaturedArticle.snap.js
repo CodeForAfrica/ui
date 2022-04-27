b5db66fcf128ec31e25bdba37139ed6e2712515a
_ Kilemensi <clemence@codeforafrica.org>
2022-04-27 17:02:54 +0300

    Initial implementation of FeaturedArticle

---
+++ apps/codeforafrica/src/components/FeaturedArticle/FeaturedArticle.snap.js
@@ -0,0 +1,85 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`<FeaturedArticle /> renders unchanged 1`] = `
+<div>
+  <div
+    class="MuiPaper-root MuiPaper-outlined MuiCard-root css-1utcoa6-MuiPaper-root-MuiCard-root"
+  >
+    <a
+      class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways MuiButtonBase-root MuiCardActionArea-root css-byxw8u-MuiTypography-root-MuiLink-root-MuiButtonBase-root-MuiCardActionArea-root"
+      href="/stories/article-1"
+      tabindex="0"
+    >
+      <div
+        class="MuiGrid-root MuiGrid-container MuiGrid-direction-xs-[object Object] MuiGrid-wrap-xs-nowrap css-17x9kv1-MuiGrid-root"
+      >
+        <div
+          class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-auto css-gjb3vx-MuiGrid-root"
+        >
+          <img
+            class="MuiCardMedia-root MuiCardMedia-media MuiCardMedia-img css-cyzzxg-MuiCardMedia-root"
+            src="https://res.cloudinary.com/code-for-africa/image/upload/v1650885664/codeforafrica/unsplash_L6hr1BptcNc_of23p3.png"
+          />
+        </div>
+        <div
+          class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-true css-1k3l4qj-MuiGrid-root"
+        >
+          <div
+            class="MuiCardContent-root css-yii6g9-MuiCardContent-root"
+          >
+            <p
+              class="MuiTypography-root MuiTypography-body1 css-112fkr8-MuiTypography-root"
+            >
+              Battle for gender equality in African media continues
+            </p>
+            <p
+              class="MuiTypography-root MuiTypography-body1 css-1sqj5jv-MuiTypography-root"
+            >
+              Lorem ipsum dolor sit amet consectetur adipiscing elit mattis, vestibulum potenti rhoncus eget lacus fermentum taciti quam, quis curae accumsan viverra semper dapibus sed. 
+            </p>
+            <button
+              class="MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium MuiButtonBase-root  css-1fsrm9g-MuiButtonBase-root-MuiButton-root"
+              tabindex="0"
+              type="button"
+            >
+              READ STORY
+            </button>
+            <span
+              class="MuiTypography-root MuiTypography-caption css-uu8fpb-MuiTypography-root"
+            >
+              Jan 6, 2022
+            </span>
+          </div>
+        </div>
+        <div
+          class="MuiGrid-root MuiGrid-container MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-3 css-kk3stl-MuiGrid-root"
+        >
+          <div
+            class="MuiGrid-root MuiGrid-container MuiGrid-item MuiGrid-grid-md-9 css-sio24f-MuiGrid-root"
+          >
+            <div
+              class="MuiCardContent-root css-q7lov9-MuiCardContent-root"
+            >
+              <h1
+                class="MuiTypography-root MuiTypography-h1 css-itx6gn-MuiTypography-root"
+              >
+                Battle for gender equality in African media continues
+              </h1>
+            </div>
+            <button
+              class="MuiButton-root MuiButton-contained-reverse MuiButton-contained-reversePrimary MuiButton-sizeLarge MuiButton-contained-reverseSizeLarge MuiButtonBase-root  css-1bbwrvh-MuiButtonBase-root-MuiButton-root"
+              tabindex="0"
+              type="button"
+            >
+              READ STORY
+            </button>
+          </div>
+        </div>
+      </div>
+      <span
+        class="MuiCardActionArea-focusHighlight css-1v2exvi-MuiCardActionArea-focusHighlight"
+      />
+    </a>
+  </div>
+</div>
+`;
