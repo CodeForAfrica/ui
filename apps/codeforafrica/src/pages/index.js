b2c28fa9a9b5ae3f7dd2e3e9dc2f402d940824cf
_ Kilemensi <clemence@codeforafrica.org>
2022-04-27 17:03:19 +0300

    Add FeaturedArticle to / page

--- apps/codeforafrica/src/pages/index.js
+++ apps/codeforafrica/src/pages/index.js
@@ -2,6 +2,7 @@ import { Section } from "@commons-ui/core";
 import React from "react";
 
 import ArticleCardList from "@/codeforafrica/components/ArticleCardList";
+import FeaturedArticle from "@/codeforafrica/components/FeaturedArticle";
 import Page from "@/codeforafrica/components/Page";
 import { getPageStaticProps } from "@/codeforafrica/lib";
 
@@ -11,6 +12,11 @@ function Index({ sections, ...props }) {
       {sections?.map((section) =>
         section.slug === "news-stories" ? (
           <Section sx={{ px: { xs: "20px", sm: 0 } }} key={section.slug}>
+            <FeaturedArticle
+              sx={{ mb: { xs: "28px", sm: "40px", lg: "55px" } }}
+              {...section.articles[0]}
+              variant="cover"
+            />
             <ArticleCardList items={section.articles.slice(1)} />
           </Section>
         ) : null
