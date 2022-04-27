63874930585b598cdd2ed028b281da52ebc84e18
_ Kilemensi <clemence@codeforafrica.org>
2022-04-27 11:36:30 +0300

    Remove filter (will be tackled on a separate PR)

--- apps/codeforafrica/src/components/ArticleCardMedia/ArticleCardMedia.js
+++ apps/codeforafrica/src/components/ArticleCardMedia/ArticleCardMedia.js
@@ -2,11 +2,7 @@ import CardMedia from "@mui/material/CardMedia";
 import { styled } from "@mui/material/styles";
 import React from "react";
 
-const ArticleCardMediaRoot = styled(CardMedia)({
-  "&:hover": {
-    filter: "none",
-  },
-});
+const ArticleCardMediaRoot = styled(CardMedia)({});
 
 const ArticleCardMedia = React.forwardRef(function ArticleCardMedia(
   props,
