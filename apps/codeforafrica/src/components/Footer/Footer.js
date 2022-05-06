f62d912f32c38f6beed7a4276a2f315406314323
Brenda Nyokabi Bucha <nyokabi@codeforafrica.org>
2022-05-06 17:35:09 +0300

    Update apps/codeforafrica/src/components/Footer/Footer.js
    Co-authored-by: _ Kilemensi <kilemensi@users.noreply.github.com>

--- apps/codeforafrica/src/components/Footer/Footer.js
+++ apps/codeforafrica/src/components/Footer/Footer.js
@@ -29,9 +29,11 @@ const Footer = React.forwardRef(function Footer(props, ref) {
     <FooterRoot ref={ref}>
       <Section sx={{ px: { xs: "20px", sm: 0 } }}>
         <Grid container justifyContent="flex-end">
-          <FooterNavList menu={menu} />
           <Grid item xs={12} md={3} sx={{ order: { xs: 0, md: 2 } }}>
             <NewsletterSubscription {...subscription} />
+          </Grid>
+          <Grid item xs={12} md={3} sx={{ order: { xs: 2, md: 1 } }}>
+            <FooterNavList menu={menu} />
           </Grid>
         </Grid>
       </Section>
