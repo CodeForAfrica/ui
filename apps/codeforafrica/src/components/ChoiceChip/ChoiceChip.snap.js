8cf30c313a75d91354cda3522756ee598b004764
_ Kilemensi <clemence@codeforafrica.org>
2022-05-06 09:46:30 +0300

    Implement ChoiceChip using MUI Chip

---
+++ apps/codeforafrica/src/components/ChoiceChip/ChoiceChip.snap.js
@@ -0,0 +1,15 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`<ChoiceChip /> renders unchanged 1`] = `
+<div>
+  <div
+    class="MuiButtonBase-root MuiChip-root MuiChip-filled MuiChip-sizeMedium MuiChip-colorDefault MuiChip-clickable MuiChip-clickableColorDefault MuiChip-filledDefault css-d0hcy-MuiButtonBase-root-MuiChip-root"
+    role="button"
+    tabindex="0"
+  >
+    <span
+      class="MuiChip-label MuiChip-labelMedium css-1dybbl5-MuiChip-label"
+    />
+  </div>
+</div>
+`;
