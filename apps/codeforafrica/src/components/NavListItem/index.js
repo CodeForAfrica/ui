ae699ac1c53064b7c88fbeeb5ffeef79f28438d3
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2022-05-06 17:29:44 +0300

    Remove keys, fix styles

--- apps/codeforafrica/src/components/NavListItem/index.js
+++ apps/codeforafrica/src/components/NavListItem/index.js
@@ -2,33 +2,20 @@ import { styled } from "@mui/material/styles";
 import PropTypes from "prop-types";
 import * as React from "react";
 
-const NavLinkItemRoot = styled("li")(
-  ({ theme: { breakpoints, typography } }) => ({
-    marginBottom: `${typography.pxToRem(20)}`,
-    listStyle: "none",
-    [breakpoints.up("lg")]: {
-      marginBottom: `${typography.pxToRem(10)}`,
-      listStyle: "none",
-    },
-  })
-);
+const NavLinkItemRoot = styled("li")(() => ({
+  listStyle: "none",
+}));
 
 const NavListItem = React.forwardRef(function NavListItem(props, ref) {
-  const { key, children } = props;
-  return (
-    <NavLinkItemRoot key={key} ref={ref}>
-      {children}
-    </NavLinkItemRoot>
-  );
+  const { children } = props;
+  return <NavLinkItemRoot ref={ref}>{children}</NavLinkItemRoot>;
 });
 
 NavListItem.propTypes = {
-  key: PropTypes.string,
   children: PropTypes.node,
 };
 
 NavListItem.defaultProps = {
-  key: undefined,
   children: undefined,
 };
 
