c7e175df86201cade08fd0a187dd1e9ff256c181
_ Kilemensi <clemence@codeforafrica.org>
2022-05-09 16:51:18 +0300

    Fix stories spec

--- apps/codeforafrica/src/pages/stories/[slug].page.js
+++ apps/codeforafrica/src/pages/stories/[slug].page.js
@@ -11,15 +11,12 @@ function Index({ sections, ...props }) {
       {sections?.map((section) =>
         section.slug === "related-stories" ? (
           <Box
+            key={section.slug}
             sx={{
               bgcolor: { xs: "inherit", sm: "background.main" },
             }}
           >
-            <RelatedStories
-              {...section}
-              sx={{ px: { xs: "20px", sm: 0 } }}
-              key={section.slug}
-            />
+            <RelatedStories {...section} sx={{ px: { xs: "20px", sm: 0 } }} />
           </Box>
         ) : null
       )}
