c07a1d73c0767a9baef972fee1c82d517f9fb59f
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-05-09 13:16:25 +0300

    add filter

--- apps/codeforafrica/src/components/ArticleCardMedia/ArticleCardMedia.js
+++ apps/codeforafrica/src/components/ArticleCardMedia/ArticleCardMedia.js
@@ -4,11 +4,29 @@ import React from "react";
 
 const ArticleCardMediaRoot = styled(CardMedia)({});
 
+const ImageContainer = styled("div")({
+  position: "relative",
+  "::before": {
+    content: '""',
+    position: "absolute",
+    top: 0,
+    left: 0,
+    height: "100%",
+    width: "100%",
+    background: `rgba(16,32,225, 0.3)`,
+    zIndex: 999,
+  },
+});
+
 const ArticleCardMedia = React.forwardRef(function ArticleCardMedia(
   props,
   ref
 ) {
-  return <ArticleCardMediaRoot component="img" {...props} ref={ref} />;
+  return (
+    <ImageContainer>
+      <ArticleCardMediaRoot component="img" {...props} ref={ref} />
+    </ImageContainer>
+  );
 });
 
 export default ArticleCardMedia;
