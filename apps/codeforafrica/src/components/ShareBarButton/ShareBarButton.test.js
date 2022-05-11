6bf9aae4481fc69886fddd1ccb49a7998974a7f8
_ Kilemensi <clemence@codeforafrica.org>
2022-05-11 16:18:26 +0300

    Implement ShareBarButton including know social media

---
+++ apps/codeforafrica/src/components/ShareBarButton/ShareBarButton.test.js
@@ -0,0 +1,18 @@
+import { createRender } from "@commons-ui/testing-library";
+import React from "react";
+
+import ShareBarButton from "./ShareBarButton";
+
+import theme from "@/codeforafrica/theme";
+
+// eslint-disable-next-line testing-library/render-result-naming-convention
+const render = createRender({ theme });
+
+const defaultProps = {};
+
+describe("<ShareBarButton />", () => {
+  it("renders unchanged", () => {
+    const { container } = render(<ShareBarButton {...defaultProps} />);
+    expect(container).toMatchSnapshot();
+  });
+});
