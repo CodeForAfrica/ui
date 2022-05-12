c86ecf3910b18e0c721f5037a97dbc4f3e4775c0
_ Kilemensi <clemence@codeforafrica.org>
2022-05-12 13:06:09 +0300

    Fix NavListItem structure

--- apps/codeforafrica/src/components/NavListItem/NavListItem.test.js
+++ apps/codeforafrica/src/components/NavListItem/NavListItem.test.js
@@ -1,14 +1,14 @@
 import { createRender } from "@commons-ui/testing-library";
 import React from "react";
 
-import NavListItem from ".";
+import NavListItem from "./NavListItem";
 
 import theme from "@/codeforafrica/theme";
 
 // eslint-disable-next-line testing-library/render-result-naming-convention
 const render = createRender({ theme });
 
-describe("<NavList />", () => {
+describe("<NavListItem />", () => {
   it("renders unchanged", () => {
     const { container } = render(<NavListItem />);
     expect(container).toMatchSnapshot();
