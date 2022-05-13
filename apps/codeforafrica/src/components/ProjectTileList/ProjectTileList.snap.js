4057323d2edac7e7f1093868cd9fb1e425b3471c
_ Kilemensi <clemence@codeforafrica.org>
2022-05-13 14:41:20 +0300

    Implement ProjectTileList per designs

---
+++ apps/codeforafrica/src/components/ProjectTileList/ProjectTileList.snap.js
@@ -0,0 +1,52 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`<ProjectTileList /> renders unchanged 1`] = `
+<div>
+  <div
+    class="MuiGrid-root MuiGrid-container css-1vcxjca-MuiGrid-root"
+  >
+    <div
+      class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-6 MuiGrid-grid-md-4 css-bdfmo7-MuiGrid-root"
+    >
+      <div
+        category="Projects"
+        class="MuiPaper-root MuiPaper-outlined MuiCard-root css-9ao0nq-MuiPaper-root-MuiCard-root"
+        slug="african-drone"
+      >
+        <a
+          class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways MuiButtonBase-root MuiCardActionArea-root css-upy2z9-MuiTypography-root-MuiLink-root-MuiButtonBase-root-MuiCardActionArea-root"
+          href="/projects/acfrican-drone"
+          tabindex="0"
+        >
+          <img
+            class="MuiCardMedia-root MuiCardMedia-media MuiCardMedia-img css-1j3tz1p-MuiCardMedia-root"
+            src="https://res.cloudinary.com/code-for-africa/image/upload/v1652431239/codeforafrica/icons/Type_africanDRONE_exwdyu.svg"
+          />
+          <h4
+            class="MuiTypography-root MuiTypography-h4 css-t4mrlz-MuiTypography-root"
+          >
+            africanDRONE
+          </h4>
+          <div
+            class="css-fbzq2r-MuiStack-root"
+          >
+            <span
+              class="MuiTypography-root MuiTypography-body3SemiBold css-1qe0j70-MuiTypography-root"
+            >
+              africanDRONE
+            </span>
+            <p
+              class="MuiTypography-root MuiTypography-body2 css-1u9aawg-MuiTypography-root"
+            >
+              Drones for good
+            </p>
+          </div>
+          <span
+            class="MuiCardActionArea-focusHighlight css-1v2exvi-MuiCardActionArea-focusHighlight"
+          />
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+`;
