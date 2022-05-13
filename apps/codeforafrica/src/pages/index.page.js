ab28f821cd8b07c363f8d9ca6e36df880568cc95
_ Kilemensi <clemence@codeforafrica.org>
2022-05-13 14:41:44 +0300

    Switch to using ProjectTileList

--- apps/codeforafrica/src/pages/index.page.js
+++ apps/codeforafrica/src/pages/index.page.js
@@ -1,10 +1,9 @@
 import { Section } from "@commons-ui/core";
-import Grid from "@mui/material/Grid";
 import React from "react";
 
 import NewsAndStories from "@/codeforafrica/components/NewsAndStories";
 import Page from "@/codeforafrica/components/Page";
-import ProjectTile from "@/codeforafrica/components/ProjectTile";
+import ProjectTileList from "@/codeforafrica/components/ProjectTileList";
 import { getPageStaticProps } from "@/codeforafrica/lib";
 
 function Index({ sections, ...props }) {
@@ -15,13 +14,7 @@ function Index({ sections, ...props }) {
           case "projects": {
             return (
               <Section key={section.slug} sx={{ px: { xs: "20px", sm: 0 } }}>
-                <Grid container spacing="18px">
-                  {section.projects?.map((p) => (
-                    <Grid key={p.href} item xs={12} sm={6} md={4}>
-                      <ProjectTile {...p} variant="detailed" />
-                    </Grid>
-                  ))}
-                </Grid>
+                <ProjectTileList projects={section.projects} />
               </Section>
             );
           }
