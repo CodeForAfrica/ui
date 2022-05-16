2ee446804d043cad4b351a2ec2eb8f7eadbb877c
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2022-05-16 15:15:18 +0300

    Minor fix

--- apps/codeforafrica/next.config.js
+++ apps/codeforafrica/next.config.js
@@ -19,7 +19,7 @@ module.exports = withTM({
         test: /\.svg$/i,
         issuer: /\.[jt]sx?$/,
         resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
-        use: ["@svgr/webpack", "url-loader"],
+        use: ["@svgr/webpack"],
       }
     );
     return config;
