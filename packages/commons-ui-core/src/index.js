55dd2053ad7cea46d954dbec06429c00beb16819
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-05-16 12:05:46 +0300

    remove unused A component

--- packages/commons-ui-core/src/index.js
+++ packages/commons-ui-core/src/index.js
@@ -4,4 +4,3 @@ export { default as ImageButton } from "./ImageButton";
 export { default as NavBar } from "./NavBar";
 export { default as Section } from "./Section";
 export { default as RichTypography } from "./RichTypography";
-export { default as A } from "./A";
