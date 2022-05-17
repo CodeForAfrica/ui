02fb62b496066977e315019a8eff7f5ef659e3f3
_ Kilemensi <clemence@codeforafrica.org>
2022-05-17 12:08:36 +0300

    Unsure page has Section

--- apps/codeforafrica/src/pages/projects/index.page.js
+++ apps/codeforafrica/src/pages/projects/index.page.js
@@ -1,3 +1,4 @@
+import { Section } from "@commons-ui/core";
 import Stack from "@mui/material/Stack";
 import React from "react";
 
@@ -10,11 +11,13 @@ function Index({ sections, ...props }) {
     <Page {...props}>
       {sections?.map((section) =>
         section.slug === "projects" ? (
-          <Stack direction="column" key={section.slug}>
-            {section.projects?.map((project) => (
-              <ProjectCard {...project} key={project.slug} />
-            ))}
-          </Stack>
+          <Section key={section.slug} sx={{ px: { xs: "20px", sm: 0 } }}>
+            <Stack direction="column" spacing={{ xs: 5, md: 7.5 }}>
+              {section.projects?.map((project) => (
+                <ProjectCard {...project} key={project.slug} />
+              ))}
+            </Stack>
+          </Section>
         ) : null
       )}
     </Page>
