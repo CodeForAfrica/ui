be4b8ba1382e371f0feeae05eef8ba0ea0ece54c
_ Kilemensi <clemence@codeforafrica.org>
2022-05-17 09:57:08 +0300

    Add content to ProjectCard

--- apps/codeforafrica/src/components/ProjectCard/ProjectCard.js
+++ apps/codeforafrica/src/components/ProjectCard/ProjectCard.js
@@ -1,4 +1,7 @@
+import { RichTypography } from "@commons-ui/core";
+import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import { styled } from "@mui/material/styles";
 import React from "react";
@@ -8,24 +11,24 @@ import ProjectTile from "@/codeforafrica/components/ProjectTile";
 
 const ProjectCardRoot = styled(Card, {
   slot: "Root",
-})(() => ({
+})(({ theme }) => ({
   display: "flex",
+  flexDirection: "column",
+  gap: theme.spacing(2),
+  padding: theme.spacing(2.5),
+  [theme.breakpoints.up("md")]: {
+    flexDirection: "row",
+    gap: 58,
+    padding: `0 ${theme.spacing(5)}`,
+  },
 }));
 
 const ProjectCard = React.forwardRef(function ProjectCard(props, ref) {
-  const {
-    href,
-    icon,
-    name,
-    square = true,
-    tagLine,
-    thumbnail,
-    ...other
-  } = props;
+  const { href, icon, name, subtitle, tagLine, title, thumbnail, ...other } =
+    props;
 
   const ownerState = {
     ...other,
-    square,
   };
   const tileProps = { href, icon, name, tagLine };
 
@@ -38,9 +41,28 @@ const ProjectCard = React.forwardRef(function ProjectCard(props, ref) {
       {...other}
     >
       <ProjectCardMedia {...thumbnail} component="img" />
-      <CardContent>
+      <CardContent
+        sx={{
+          p: 0,
+          "&:last-child": {
+            p: 0,
+          },
+        }}
+      >
         <ProjectTile {...tileProps} />
+        <RichTypography color="primary" variant="h5" sx={{ mt: 2 }}>
+          {title}
+        </RichTypography>
+        <RichTypography
+          variant="body1"
+          sx={{ mt: 2.5, typography: { md: "body2" } }}
+        >
+          {subtitle}
+        </RichTypography>
       </CardContent>
+      <CardActions sx={{ p: 0 }}>
+        <Button variant="outlined">Learn More</Button>
+      </CardActions>
     </ProjectCardRoot>
   );
 });
