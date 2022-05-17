7cb3b92bab2ae7f347b3ab87c3401fe3a76888a8
_ Kilemensi <clemence@codeforafrica.org>
2022-05-17 17:38:36 +0300

    Use FeaturedProjects on homepage

--- apps/codeforafrica/src/pages/index.page.js
+++ apps/codeforafrica/src/pages/index.page.js
@@ -1,9 +1,8 @@
-import { Section } from "@commons-ui/core";
 import React from "react";
 
+import FeaturedProjects from "@/codeforafrica/components/FeaturedProjects";
 import NewsAndStories from "@/codeforafrica/components/NewsAndStories";
 import Page from "@/codeforafrica/components/Page";
-import ProjectTileList from "@/codeforafrica/components/ProjectTileList";
 import { getPageStaticProps } from "@/codeforafrica/lib";
 
 function Index({ sections, ...props }) {
@@ -12,11 +11,7 @@ function Index({ sections, ...props }) {
       {sections?.map((section) => {
         switch (section.slug) {
           case "projects": {
-            return (
-              <Section key={section.slug} sx={{ px: { xs: "20px", sm: 0 } }}>
-                <ProjectTileList projects={section.projects} />
-              </Section>
-            );
+            return <FeaturedProjects {...section} key={section.slug} />;
           }
           case "news-stories": {
             return <NewsAndStories key={section.slug} {...section} />;
