cbafa285bb84dadbda273994e814d7d92013f032
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-05-18 10:25:21 +0300

    title margin

--- apps/codeforafrica/src/components/StayInTouchTitle/Title.js
+++ apps/codeforafrica/src/components/StayInTouchTitle/Title.js
@@ -2,7 +2,14 @@ import { RichTypography } from "@commons-ui/core";
 import React from "react";
 
 function Title({ children }) {
-  return <RichTypography variant="footerCap">{children}</RichTypography>;
+  return (
+    <RichTypography
+      sx={{ marginBottom: { md: 0, xs: "1.438rem" } }}
+      variant="footerCap"
+    >
+      {children}
+    </RichTypography>
+  );
 }
 
 export default Title;
