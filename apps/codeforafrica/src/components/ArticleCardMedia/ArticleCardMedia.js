0dfa083a6f1aca9af340288853faa8f3241fc51a
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-05-18 23:08:41 +0300

    remove extra div + hover filter

--- apps/codeforafrica/src/components/ArticleCardMedia/ArticleCardMedia.js
+++ apps/codeforafrica/src/components/ArticleCardMedia/ArticleCardMedia.js
@@ -2,19 +2,10 @@ import CardMedia from "@mui/material/CardMedia";
 import { styled } from "@mui/material/styles";
 import React from "react";
 
-const ArticleCardMediaRoot = styled(CardMedia)({});
-
-const ImageContainer = styled("div")({
-  position: "relative",
-  "::before": {
-    content: '""',
-    position: "absolute",
-    top: 0,
-    left: 0,
-    height: "100%",
-    width: "100%",
-    background: `rgba(16,32,225, 0.3)`,
-    zIndex: 999,
+const ArticleCardMediaRoot = styled(CardMedia)({
+  filter: "grayscale(1)",
+  ":hover": {
+    filter: "none",
   },
 });
 
@@ -22,11 +13,7 @@ const ArticleCardMedia = React.forwardRef(function ArticleCardMedia(
   props,
   ref
 ) {
-  return (
-    <ImageContainer>
-      <ArticleCardMediaRoot component="img" {...props} ref={ref} />
-    </ImageContainer>
-  );
+  return <ArticleCardMediaRoot component="img" {...props} ref={ref} />;
 });
 
 export default ArticleCardMedia;
