9c2a5a2c05787a8d6a5cae22a341540c7db6dbe2
_ Kilemensi <clemence@codeforafrica.org>
2022-05-19 10:37:03 +0300

    Add OurPartners to /about page

---
+++ apps/codeforafrica/src/pages/about.page.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import OurPartners from "@/codeforafrica/components/OurPartners";
+import Page from "@/codeforafrica/components/Page";
+import { getPageStaticProps } from "@/codeforafrica/lib";
+
+function Index({ sections, ...props }) {
+  return (
+    <Page {...props}>
+      {sections?.map((section) => {
+        switch (section.slug) {
+          case "our-partners": {
+            return <OurPartners {...section} key={section.slug} />;
+          }
+          default:
+            return null;
+        }
+      })}
+    </Page>
+  );
+}
+
+export async function getStaticProps() {
+  return getPageStaticProps({ slug: "/about" });
+}
+
+export default Index;
---
+++ apps/codeforafrica/src/pages/about.test.js
@@ -0,0 +1,20 @@
+import { createRender } from "@commons-ui/testing-library";
+import React from "react";
+
+import About from "./about.page";
+
+import theme from "@/codeforafrica/theme";
+
+// eslint-disable-next-line testing-library/render-result-naming-convention
+const render = createRender({ theme });
+
+const defaultProps = {
+  sections: [],
+};
+
+describe("/about", () => {
+  it("renders unchanged", () => {
+    const { container } = render(<About {...defaultProps} />);
+    expect(container).toMatchSnapshot();
+  });
+});
