8d8702b2fe747ad96b069458ef47796c7bf2f633
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2022-05-23 11:47:56 +0300

    Add test

---
+++ apps/codeforafrica/src/components/Author/index.snap.js
@@ -0,0 +1,26 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`<ArticleAuthorCopy /> renders unchanged 1`] = `
+<div>
+  <div
+    class="MuiBox-root css-d944bl"
+  >
+    <span
+      class="MuiTypography-root MuiTypography-body2 css-lupkoz-MuiTypography-root"
+    >
+      Article by
+       
+    </span>
+    <div
+      class="MuiBox-root css-139x83s"
+    >
+      <span
+        class="MuiTypography-root MuiTypography-body1SemiBold css-1s0xknt-MuiTypography-root"
+      />
+      <span
+        class="MuiTypography-root MuiTypography-body1 css-4tj74i-MuiTypography-root"
+      />
+    </div>
+  </div>
+</div>
+`;
---
+++ apps/codeforafrica/src/components/NavBarNavList/index.snap.js
@@ -0,0 +1,76 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`<NavBarNavList /> renders unchanged 1`] = `
+<div>
+  <ul
+    class="css-1gpvlh2"
+  >
+    <li
+      class="css-1n7p56k"
+    >
+      <a
+        class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineNone css-13a4wsk-MuiTypography-root-MuiLink-root"
+        href="/our-work"
+      >
+        Our Work
+      </a>
+    </li>
+    <li
+      class="css-1n7p56k"
+    >
+      <a
+        class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineNone css-13a4wsk-MuiTypography-root-MuiLink-root"
+        href="/about"
+      >
+        About
+      </a>
+    </li>
+    <li
+      class="css-1n7p56k"
+    >
+      <a
+        class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineNone css-13a4wsk-MuiTypography-root-MuiLink-root"
+        href="/about"
+      >
+        Stories
+      </a>
+    </li>
+    <li
+      class="css-1n7p56k"
+    >
+      <a
+        class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineNone css-13a4wsk-MuiTypography-root-MuiLink-root"
+        href="/opportunity"
+      >
+        Opportunity
+      </a>
+    </li>
+    <li
+      class="css-1n7p56k"
+    >
+      <a
+        class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineNone css-13a4wsk-MuiTypography-root-MuiLink-root"
+        href="/contact"
+      >
+        Contact
+      </a>
+    </li>
+    <li
+      class="css-1kgp67w"
+    >
+      <a
+        class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways css-1cv723u-MuiTypography-root-MuiLink-root"
+        href="https://twitter.com/?lang=en"
+        target="_blank"
+      >
+        <svg
+          aria-hidden="true"
+          class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-1k4yhcq-MuiSvgIcon-root"
+          focusable="false"
+          viewBox="0 0 24 24"
+        />
+      </a>
+    </li>
+  </ul>
+</div>
+`;
