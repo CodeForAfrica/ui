7ecbb1b103b3f68056c846ed48ca0eec434e92cf
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2022-05-23 11:52:14 +0300

    update test

--- apps/codeforafrica/src/components/Author/index.test.js
+++ apps/codeforafrica/src/components/Author/index.test.js
@@ -1,16 +1,16 @@
 import { createRender } from "@commons-ui/testing-library";
 import React from "react";
 
-import ArticleAuthorCopy from ".";
+import Author from ".";
 
 import theme from "@/codeforafrica/theme";
 
 // eslint-disable-next-line testing-library/render-result-naming-convention
 const render = createRender({ theme });
 
-describe("<ArticleAuthorCopy />", () => {
+describe("<Author />", () => {
   it("renders unchanged", () => {
-    const { container } = render(<ArticleAuthorCopy />);
+    const { container } = render(<Author />);
     expect(container).toMatchSnapshot();
   });
 });
