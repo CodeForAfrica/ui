af71bcee3b6f7364dd9a34f30e45f748b29fb58b
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-05-25 08:01:05 +0300

    mvp layout

--- apps/codeforafrica/src/components/ImpactCard/ImpactCard.js
+++ apps/codeforafrica/src/components/ImpactCard/ImpactCard.js
@@ -1,16 +1,22 @@
-import Card from "@mui/material/Card";
-
+import { Card, CardActionArea, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
+import Image from "next/image";
 import React from "react";
 
 const ImpactCardRoot = styled(Card, {
   slot: "Root",
 })(() => ({}));
 
-const ImpactCard = React.forwardRef(function ImpactCard(ref) {
+const ImpactCard = React.forwardRef(function ImpactCard(ref, props) {
+  const { icon, title, number, description } = props;
   return (
-    <ImpactCardRoot>
-      <div>content here</div>
+    <ImpactCardRoot ref={ref}>
+      <CardActionArea>
+        <Image src={icon} />
+        <Typography>{title}</Typography>
+        <Typography>{number}</Typography>
+        <Typography>{description}</Typography>
+      </CardActionArea>
     </ImpactCardRoot>
   );
 });
