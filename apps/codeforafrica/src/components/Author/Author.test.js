badc643792457c07a9b69575dc73867504113b7a
_ Kilemensi <clemence@codeforafrica.org>
2022-06-01 11:12:50 +0300

    Fix Author folder structure

--- apps/codeforafrica/src/components/Author/Author.test.js
+++ apps/codeforafrica/src/components/Author/Author.test.js
@@ -1,7 +1,7 @@
 import { createRender } from "@commons-ui/testing-library";
 import React from "react";
 
-import Author from ".";
+import Author from "./Author";
 
 import theme from "@/codeforafrica/theme";
 
