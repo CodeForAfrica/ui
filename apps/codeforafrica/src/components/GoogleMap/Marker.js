030b558c117fca440dde1870ce6314902e4b4b2e
_ Kilemensi <clemence@codeforafrica.org>
2022-06-09 17:19:42 +0300

    Initial implementation of Google Map

---
+++ apps/codeforafrica/src/components/GoogleMap/Marker.js
@@ -0,0 +1,28 @@
+/* eslint-env browser */
+import React from "react";
+
+function Marker(options) {
+  const [marker, setMarker] = React.useState();
+
+  React.useEffect(() => {
+    if (!marker) {
+      setMarker(new window.google.maps.Marker());
+    }
+
+    return () => {
+      if (marker) {
+        marker.setMap(null);
+      }
+    };
+  }, [marker]);
+
+  React.useEffect(() => {
+    if (marker) {
+      marker.setOptions(options);
+    }
+  }, [marker, options]);
+
+  return null;
+}
+
+export default Marker;
