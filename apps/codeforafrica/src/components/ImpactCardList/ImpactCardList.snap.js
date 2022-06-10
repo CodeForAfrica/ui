9f439391eadd5867a190ed58a5797d0e57645e27
_ Kilemensi <clemence@codeforafrica.org>
2022-06-10 09:45:50 +0300

    Add and update tests

--- apps/codeforafrica/src/components/ImpactCardList/ImpactCardList.snap.js
+++ apps/codeforafrica/src/components/ImpactCardList/ImpactCardList.snap.js
@@ -1,17 +1,3 @@
 // Jest Snapshot v1, https://goo.gl/fbAQLP
 
-exports[`<ImpactCardList /> renders unchanged 1`] = `
-<div>
-  <div
-    class="MuiBox-root css-tm3fmn"
-  >
-    <div
-      class="MuiContainer-root MuiContainer-maxWidthLg MuiContainer-fixed MuiContainer-disableGutters css-1wooz16-MuiContainer-root"
-    >
-      <div
-        class="MuiGrid-root MuiGrid-container css-11j0bs5-MuiGrid-root"
-      />
-    </div>
-  </div>
-</div>
-`;
+exports[`<ImpactCardList /> renders unchanged 1`] = `<div />`;
---
+++ apps/codeforafrica/src/components/OfficeAddresses/OfficeAddresses.snap.js
@@ -0,0 +1,3 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`<OfficeAddresses /> renders unchanged 1`] = `<div />`;
