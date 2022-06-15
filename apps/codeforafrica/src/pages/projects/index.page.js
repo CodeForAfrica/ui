94d06c75f503cf0f7fe00ebb2e95f1fcc0df991f
Clemence Kyara <kilemensi@users.noreply.github.com>
2022-06-15 14:02:49 +0000

    Switch to using Projects

--- apps/codeforafrica/src/pages/projects/index.page.js
+++ apps/codeforafrica/src/pages/projects/index.page.js
@@ -1,10 +1,8 @@
-import { Section } from "@commons-ui/core";
-import Stack from "@mui/material/Stack";
 import React from "react";
 
 import Page from "@/codeforafrica/components/Page";
 import PageHeader from "@/codeforafrica/components/PageHeader";
-import ProjectCard from "@/codeforafrica/components/ProjectCard";
+import Projects from "@/codeforafrica/components/Projects";
 import { getPageStaticProps } from "@/codeforafrica/lib";
 
 function Index({ sections, ...props }) {
@@ -15,15 +13,7 @@ function Index({ sections, ...props }) {
           case "hero":
             return <PageHeader {...section} key={section.slug} />;
           case "projects":
-            return (
-              <Section key={section.slug} sx={{ px: { xs: "20px", sm: 0 } }}>
-                <Stack direction="column" spacing={{ xs: 5, md: 7.5 }}>
-                  {section.projects?.map((project) => (
-                    <ProjectCard {...project} key={project.slug} />
-                  ))}
-                </Stack>
-              </Section>
-            );
+            return <Projects {...section} key={section.slug} />;
           default:
             return null;
         }
