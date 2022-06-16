51489f56478673b10de7b65fa1ea726949a1f511
_ Kilemensi <clemence@codeforafrica.org>
2022-06-16 17:36:38 +0300

    Extract PaginationButton

---
+++ apps/codeforafrica/src/components/NextPreviousPagination/PaginationButton.js
@@ -0,0 +1,52 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import SvgIcon from "@mui/material/SvgIcon";
+import React from "react";
+
+import ArrowBackIcon from "@/codeforafrica/assets/icons/Type=arrow-left, Size=32, Color=White.svg";
+import ArrowForwardIcon from "@/codeforafrica/assets/icons/Type=arrow-right, Size=32, Color=White.svg";
+
+const PaginationButton = React.forwardRef(function PaginationButton(
+  props,
+  ref
+) {
+  const { component, disabled, onClick, page, type } = props;
+
+  // type === previous
+  let Icon = ArrowBackIcon;
+  let display = page === 0 ? "none" : "initial";
+  let label = type.slice(0, 4);
+  if (type === "next") {
+    // page === 2 means current page === 1
+    Icon = page === 2 ? undefined : ArrowForwardIcon;
+    display = "initial";
+    label = page === 2 ? "See More" : type.slice(0, 4);
+  }
+  const icon = Icon ? (
+    <SvgIcon component={Icon} style={{ fontSize: 16 }} viewBox="0 0 32 32" />
+  ) : undefined;
+  const startIcon = type === "previous" ? icon : undefined;
+  const endIcon = type === "next" ? icon : undefined;
+
+  return (
+    <Box component={component} sx={{ display }} ref={ref}>
+      <Button
+        disabled={disabled}
+        onClick={onClick}
+        startIcon={startIcon}
+        sx={{
+          "&.Mui-disabled": {
+            bgcolor: "primary.main",
+            color: "text.secondary",
+          },
+        }}
+        endIcon={endIcon}
+        variant="contained"
+      >
+        {label}
+      </Button>
+    </Box>
+  );
+});
+
+export default PaginationButton;
---
+++ apps/codeforafrica/src/components/NextPreviousPagination/PaginationButton.snap.js
@@ -0,0 +1,28 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`<PaginationButton /> renders unchanged 1`] = `
+<div>
+  <div
+    class="MuiBox-root css-1msh118"
+  >
+    <button
+      class="MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium MuiButtonBase-root  css-dq9mzi-MuiButtonBase-root-MuiButton-root"
+      tabindex="0"
+      type="button"
+    >
+      <span
+        class="MuiButton-startIcon MuiButton-iconSizeMedium css-gcc2o7-MuiButton-startIcon"
+      >
+        <div
+          aria-hidden="true"
+          class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root"
+          focusable="false"
+          style="font-size: 16px;"
+          viewbox="0 0 32 32"
+        />
+      </span>
+      prev
+    </button>
+  </div>
+</div>
+`;
