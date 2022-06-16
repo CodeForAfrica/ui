674e2e33e31369692b2f8f789e697fd246348cac
_ Kilemensi <clemence@codeforafrica.org>
2022-06-16 09:49:50 +0300

    Add Projects test

---
+++ apps/codeforafrica/src/components/Projects/Projects.snap.js
@@ -0,0 +1,34 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`<Projects /> renders unchanged 1`] = `
+<div>
+  <div
+    class="css-1bf5ofc"
+  >
+    <div
+      class="MuiContainer-root MuiContainer-maxWidthLg MuiContainer-fixed MuiContainer-disableGutters css-usii6f-MuiContainer-root"
+    >
+      <div
+        class="MuiToggleButtonGroup-root css-779xh3-MuiToggleButtonGroup-root"
+        role="group"
+      >
+        <div
+          class="MuiButtonBase-root MuiChip-root MuiChip-filled MuiChip-sizeMedium MuiChip-colorPrimary MuiChip-clickable MuiChip-clickableColorPrimary MuiChip-filledPrimary MuiToggleButtonGroup-grouped MuiToggleButtonGroup-groupedHorizontal css-12j4o5v-MuiButtonBase-root-MuiChip-root"
+          role="button"
+          tabindex="0"
+          value="All"
+        >
+          <span
+            class="MuiChip-label MuiChip-labelMedium css-1dybbl5-MuiChip-label"
+          >
+            All
+          </span>
+        </div>
+      </div>
+      <div
+        class="css-1aeddr5-MuiStack-root"
+      />
+    </div>
+  </div>
+</div>
+`;
