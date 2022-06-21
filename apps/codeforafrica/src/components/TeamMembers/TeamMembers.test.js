b6281c1e246be684dd6327764bfafbd92f67c9af
_ Kilemensi <clemence@codeforafrica.org>
2022-06-21 17:08:55 +0300

    Implement TeamMembers per desings

---
+++ apps/codeforafrica/src/components/TeamMembers/TeamMembers.test.js
@@ -0,0 +1,22 @@
+import { createRender } from "@commons-ui/testing-library";
+import React from "react";
+
+import TeamMembers from "./TeamMembers";
+
+import { team } from "@/codeforafrica/lib";
+import theme from "@/codeforafrica/theme";
+
+// eslint-disable-next-line testing-library/render-result-naming-convention
+const render = createRender({ theme });
+
+const defaultProps = {
+  title: "Team",
+  team: team.slice(0, 3),
+};
+
+describe("<TeamMembers />", () => {
+  it("renders unchanged", () => {
+    const { container } = render(<TeamMembers {...defaultProps} />);
+    expect(container).toMatchSnapshot();
+  });
+});
