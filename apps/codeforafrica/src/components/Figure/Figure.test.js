23cb64590b913512b1a178f439b66f1d63018a72
_ Kilemensi <clemence@codeforafrica.org>
2022-06-22 17:06:10 +0300

    Introduce standalone Figure component

---
+++ apps/codeforafrica/src/components/Figure/Figure.test.js
@@ -0,0 +1,20 @@
+import { createRender } from "@commons-ui/testing-library";
+import React from "react";
+
+import Figure from "./Figure";
+
+import theme from "@/codeforafrica/theme";
+
+// eslint-disable-next-line testing-library/render-result-naming-convention
+const render = createRender({ theme });
+
+const defaultProps = {
+  src: "/",
+};
+
+describe("<Figure />", () => {
+  it("renders unchanged", () => {
+    const { container } = render(<Figure {...defaultProps} />);
+    expect(container).toMatchSnapshot();
+  });
+});
