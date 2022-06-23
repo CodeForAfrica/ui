3f7ef42aea075b620c6ef79ea91e421acbfde470
_ Kilemensi <clemence@codeforafrica.org>
2022-06-23 09:50:14 +0300

    Allow more props to be passed to RichTypography

--- apps/codeforafrica/src/components/CMSContent/CMSContent.js
+++ apps/codeforafrica/src/components/CMSContent/CMSContent.js
@@ -2,20 +2,24 @@ import { RichTypography, Section } from "@commons-ui/core";
 import PropTypes from "prop-types";
 import React from "react";
 
-const CMSContent = React.forwardRef(function CMSContent({ children, sx }, ref) {
+const CMSContent = React.forwardRef(function CMSContent({ sx, ...other }, ref) {
   return (
-    <Section sx={{ px: { xs: 2.5, sm: 0 }, ...sx }} ref={ref}>
-      <RichTypography>{children}</RichTypography>
+    <Section
+      component="section"
+      sx={{ px: { xs: 2.5, sm: 0 }, ...sx }}
+      ref={ref}
+    >
+      <RichTypography {...other} />
     </Section>
   );
 });
 
 CMSContent.propTypes = {
-  children: PropTypes.node,
+  sx: PropTypes.shape({}),
 };
 
 CMSContent.defaultProps = {
-  children: undefined,
+  sx: undefined,
 };
 
 export default CMSContent;
