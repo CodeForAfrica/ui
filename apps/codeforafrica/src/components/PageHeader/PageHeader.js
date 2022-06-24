5a6f041824f484e5fcf094d066f3d2c9acc880f2
_ Kilemensi <clemence@codeforafrica.org>
2022-06-24 14:14:34 +0300

    Fix px spacing + update tests

--- apps/codeforafrica/src/components/PageHeader/PageHeader.js
+++ apps/codeforafrica/src/components/PageHeader/PageHeader.js
@@ -13,21 +13,26 @@ const PageHeader = React.forwardRef(function PageHeader(props, ref) {
     <TwoToneBackground ref={ref} sx={{ backgroundColor: "background.main" }}>
       <Section
         sx={{
+          px: { xs: 2.5, sm: 0 },
           py: { xs: 3.75, sm: 5.5, md: 6.75, lg: 9.6 },
           zIndex: 1,
           textAlign: "center",
         }}
       >
         <RichTypography
+          component="h2"
           sx={{
             color: "primary.main",
             paddingBottom: 2.5,
+            textTransform: "uppercase",
           }}
           variant="h5ExtraBold"
         >
           {title}
         </RichTypography>
-        <RichTypography variant="h2">{subtitle}</RichTypography>
+        <RichTypography component="h2" variant="h2">
+          {subtitle}
+        </RichTypography>
       </Section>
     </TwoToneBackground>
   );
