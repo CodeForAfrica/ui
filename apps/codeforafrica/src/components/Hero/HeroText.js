b99f03ec1e8a9d6c0a1cbf5036e96fe240c1e235
_ Kilemensi <clemence@codeforafrica.org>
2022-06-24 10:45:29 +0300

    Use keyframes to animate messages

--- apps/codeforafrica/src/components/Hero/HeroText.js
+++ apps/codeforafrica/src/components/Hero/HeroText.js
@@ -19,20 +19,167 @@ const Heading = styled(RichTypography)(({ theme }) => ({
   },
 }));
 
-const Message = styled(RichTypography)(({ theme }) => ({
+const MessageHeader = styled(RichTypography)(({ theme }) => ({
   backgroundColor: theme.palette.highlight.main,
   color: theme.palette.text.secondary,
-  padding: "10px",
-  width: "max-content",
+  height: 34 + 20, // h3 line-height + y padding
+  overflow: "hidden",
+  position: "relative",
+  whiteSpace: "nowrap",
+  [theme.breakpoints.up("md")]: {
+    height: 56 + 20, // h1 line-height + padding
+  },
+}));
+
+const MessageAnimation = styled("span")(({ theme }) => ({
+  animation: "openclose 5s ease-in-out infinite",
+  backgroundColor: theme.palette.highlight.main,
+  display: "block",
+  left: 0,
+  overflow: "hidden",
+  position: "absolute",
+  top: 0,
+  [theme.breakpoints.up("md")]: {
+    animation: "opencloseDesktop 5s ease-in-out infinite",
+  },
+
+  "@keyframes openclose": {
+    "0%": {
+      top: 0,
+      width: 0,
+    },
+    "5%": {
+      width: 0,
+    },
+    "15%": {
+      width: "100%",
+    },
+    "30%": {
+      top: 0,
+      width: "100%",
+    },
+    "33%": {
+      top: 0,
+      width: 0,
+    },
+    "35%": {
+      top: 0,
+      width: 0,
+    },
+    "38%": {
+      top: "-54px",
+    },
+    "48%": {
+      top: "-54px",
+      width: "100%",
+    },
+    "62%": {
+      top: "-54px",
+      width: "100%",
+    },
+    "66%": {
+      top: "-54px",
+      width: 0,
+    },
+    "71%": {
+      top: "-108px",
+      width: 0,
+    },
+    "86%": {
+      top: "-108px",
+      width: "100%",
+    },
+    "95%": {
+      top: "-108px",
+      width: "100%",
+    },
+    "98%": {
+      top: "-108px",
+      width: 0,
+    },
+    "100%": {
+      top: 0,
+      width: 0,
+    },
+  },
+
+  "@keyframes opencloseDesktop": {
+    "0%": {
+      top: 0,
+      width: 0,
+    },
+    "5%": {
+      width: 0,
+    },
+    "15%": {
+      width: "100%",
+    },
+    "30%": {
+      top: 0,
+      width: "100%",
+    },
+    "33%": {
+      top: 0,
+      width: 0,
+    },
+    "35%": {
+      top: 0,
+      width: 0,
+    },
+    "38%": {
+      top: "-76px",
+    },
+    "48%": {
+      top: "-76px",
+      width: "100%",
+    },
+    "62%": {
+      top: "-76px",
+      width: "100%",
+    },
+    "66%": {
+      top: "-76px",
+      width: 0,
+    },
+    "71%": {
+      top: "-152px",
+      width: 0,
+    },
+    "86%": {
+      top: "-152px",
+      width: "100%",
+    },
+    "95%": {
+      top: "-152px",
+      width: "100%",
+    },
+    "98%": {
+      top: "-152px",
+      width: 0,
+    },
+    "100%": {
+      top: 0,
+      width: 0,
+    },
+  },
+}));
+
+const Message = styled("span")(({ theme }) => ({
+  display: "block",
+  height: 34 + 20, // h3 line-height + padding
+  padding: 10,
   [theme.breakpoints.up("sm")]: {
     padding: "10px 30px",
+  },
+  [theme.breakpoints.up("md")]: {
+    height: 56 + 20, // h1 line-height + padding
   },
 }));
 
 const HeroFigure = React.forwardRef(function HeroFigure(props, ref) {
-  const { title, subtitle, message } = props;
+  const { title, subtitle, messages } = props;
 
-  if (!(title && message)) {
+  if (!(title && messages?.length === 3)) {
     return null;
   }
   return (
@@ -43,12 +190,16 @@ const HeroFigure = React.forwardRef(function HeroFigure(props, ref) {
       >
         {title}
       </Heading>
-      <Message
+      <MessageHeader
         component="h1"
         sx={{ mt: 2.5, typography: { xs: "h3", md: "h1" } }}
       >
-        {message}
-      </Message>
+        <MessageAnimation className="message">
+          {messages.map((message) => (
+            <Message>{message}</Message>
+          ))}
+        </MessageAnimation>
+      </MessageHeader>
       <RichTypography
         variant="subheading"
         sx={{ mt: 2.5, typography: { xs: "body3", md: "subheading" } }}
