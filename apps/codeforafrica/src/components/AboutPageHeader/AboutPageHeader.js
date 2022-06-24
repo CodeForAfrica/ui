b0714a91ceab71642cc694767c686e0d84a67cf0
_ Kilemensi <clemence@codeforafrica.org>
2022-06-24 14:11:43 +0300

    Implement header per designs

--- apps/codeforafrica/src/components/AboutPageHeader/AboutPageHeader.js
+++ apps/codeforafrica/src/components/AboutPageHeader/AboutPageHeader.js
@@ -1,38 +1,67 @@
-import { Box } from "@mui/material";
-import Image from "next/image";
-import PropTypes from "prop-types";
+import { Section, RichTypography } from "@commons-ui/core";
+import Box from "@mui/material/Box";
 import React from "react";
 
 const AboutPageHeader = React.forwardRef(function AboutPageHeader(props, ref) {
-  const { background } = props;
+  const { image: imageProp, title, subtitle } = props;
 
-  if (!background?.src) {
+  if (!(title || subtitle)) {
     return null;
   }
+  const image = imageProp?.src || imageProp;
   return (
     <Box
       ref={ref}
-      sx={{ width: "100vw", position: "relative", height: "400px" }}
+      sx={{
+        ...(image && {
+          backgroundImage: `url('${image}') `,
+          backgroundSize: "cover",
+          backgroundPositionY: { xs: "30%", md: "45%" },
+        }),
+        position: "relative",
+        width: "100%",
+        "&:before": {
+          content: "''",
+          bgcolor: "primary.main",
+          opacity: 0.5,
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+        },
+      }}
     >
-      <Image
-        alt={background.alt}
-        objectFit="cover"
-        layout="fill"
-        src={background.src}
-      />
+      <Section
+        sx={{
+          color: "text.secondary",
+          position: "relative",
+          px: { xs: 2.5, sm: 0 },
+          py: { xs: "86px", sm: "75px", md: "96px" },
+          textAlign: "center",
+        }}
+      >
+        <RichTypography
+          component="h2"
+          variant="h5ExtraBold"
+          sx={{
+            pb: 5,
+            textTransform: "uppercase",
+          }}
+        >
+          {title}
+        </RichTypography>
+        <RichTypography
+          align="center"
+          component="h2"
+          variant="h2"
+          sx={{ typography: { md: "display2" } }}
+        >
+          {subtitle}
+        </RichTypography>
+      </Section>
     </Box>
   );
 });
-
-AboutPageHeader.propTypes = {
-  background: PropTypes.shape({
-    alt: PropTypes.string,
-    src: PropTypes.string,
-  }),
-};
-
-AboutPageHeader.defaultProps = {
-  background: undefined,
-};
 
 export default AboutPageHeader;
--- apps/codeforafrica/src/components/AboutPageHeader/AboutPageHeader.test.js
+++ apps/codeforafrica/src/components/AboutPageHeader/AboutPageHeader.test.js
@@ -8,9 +8,18 @@ import theme from "@/codeforafrica/theme";
 // eslint-disable-next-line testing-library/render-result-naming-convention
 const render = createRender({ theme });
 
+const defaultProps = {
+  title: "About Us",
+  subtitle:
+    "We are Africa’s largest network of civic technology and data journalism labs",
+  image: {
+    src: "https://res.cloudinary.com/code-for-africa/image/upload/v1656064173/codeforafrica/images/1_IgrT4_1tGZh1WnpYzvZN1A_1_twneqf.jpg",
+  },
+};
+
 describe("<AboutPageHeader />", () => {
   it("renders unchanged", () => {
-    const { container } = render(<AboutPageHeader />);
+    const { container } = render(<AboutPageHeader {...defaultProps} />);
     expect(container).toMatchSnapshot();
   });
 });
