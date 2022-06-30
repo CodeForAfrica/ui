2b70157185710fa2b80d0f5578863d72973edb62
_ Kilemensi <clemence@codeforafrica.org>
2022-06-30 13:41:06 +0300

    Adapt sizing based on fixed prop

--- apps/codeforafrica/src/components/ProjectTileList/ProjectTileList.js
+++ apps/codeforafrica/src/components/ProjectTileList/ProjectTileList.js
@@ -4,15 +4,17 @@ import React from "react";
 import ProjectTile from "@/codeforafrica/components/ProjectTile";
 
 const ProjectTileList = React.forwardRef(function ProjectTileList(props, ref) {
-  const { itemProps, projects, ...other } = props;
+  const { itemProps, fixed, projects, ...other } = props;
 
   if (!projects?.length) {
     return null;
   }
+  const sm = fixed ? 12 : 6;
+  const md = fixed ? 6 : 4;
   return (
     <Grid container spacing="18px" {...other} ref={ref}>
       {projects?.map((project) => (
-        <Grid key={project.href} item xs={12} sm={6} md={4} {...itemProps}>
+        <Grid key={project.href} item xs={12} sm={sm} md={md} {...itemProps}>
           <ProjectTile variant="detailed" {...project} />
         </Grid>
       ))}
