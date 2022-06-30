53a03323bee2b255d238abe9386c463dc7f09ae8
_ Kilemensi <clemence@codeforafrica.org>
2022-06-30 17:27:15 +0300

    Initial implementation of InputSearch

---
+++ apps/codeforafrica/src/components/InputSearch/InputSearch.js
@@ -0,0 +1,33 @@
+import SvgIcon from "@mui/material/SvgIcon";
+import React from "react";
+
+import SearchIcon from "@/codeforafrica/assets/icons/Type=search, Size=16, Color=Black.svg";
+import Input from "@/codeforafrica/components/Input";
+
+const InputSearch = React.forwardRef(function InputSearch(props, ref) {
+  const { InputProps, ...other } = props;
+
+  const endAdornment = (
+    <SvgIcon
+      component={SearchIcon}
+      viewBox="0 0 16 16"
+      sx={{
+        fill: "none",
+        fontSize: "16px",
+      }}
+    />
+  );
+  return (
+    <Input
+      InputProps={{
+        endAdornment,
+        ...InputProps,
+      }}
+      type="search"
+      {...other}
+      ref={ref}
+    />
+  );
+});
+
+export default InputSearch;
