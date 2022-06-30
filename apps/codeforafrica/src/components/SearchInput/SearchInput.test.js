556ed13231e6f7aefb056239b1ec68dc6d46baf9
_ Kilemensi <clemence@codeforafrica.org>
2022-06-30 17:31:07 +0300

    ~Input~SearchInput

--- apps/codeforafrica/src/components/SearchInput/SearchInput.test.js
+++ apps/codeforafrica/src/components/SearchInput/SearchInput.test.js
@@ -1,16 +1,16 @@
 import { createRender } from "@commons-ui/testing-library";
 import React from "react";
 
-import InputSearch from "./InputSearch";
+import SearchInput from "./SearchInput";
 
 import theme from "@/codeforafrica/theme";
 
 // eslint-disable-next-line testing-library/render-result-naming-convention
 const render = createRender({ theme });
 
-describe("<InputSearch />", () => {
+describe("<SearchInput />", () => {
   it("renders unchanged", () => {
-    const { container } = render(<InputSearch />);
+    const { container } = render(<SearchInput />);
     expect(container).toMatchSnapshot();
   });
 });
