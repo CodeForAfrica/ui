e81076675a6206b7b2168520c8621a3b6879cbb8
_ Kilemensi <clemence@codeforafrica.org>
2022-07-01 16:42:42 +0300

    Make OpportunityCard fully clickable

--- apps/codeforafrica/src/components/OpportunityCard/OpportunityCard.js
+++ apps/codeforafrica/src/components/OpportunityCard/OpportunityCard.js
@@ -1,13 +1,12 @@
 import { RichTypography } from "@commons-ui/core";
 import { Link } from "@commons-ui/next";
-import {
-  Card,
-  CardContent,
-  CardActions,
-  CardMedia,
-  Button,
-  Box,
-} from "@mui/material";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 import React from "react";
 
 const OpportunityCard = React.forwardRef(function OpportunityCard(props, ref) {
@@ -18,40 +17,40 @@ const OpportunityCard = React.forwardRef(function OpportunityCard(props, ref) {
   }
   return (
     <Card sx={{ boxShadow: "none", borderRadius: 0, ...other.sx }} ref={ref}>
-      <CardMedia component="img" alt="" src={image?.src} />
-      <CardContent sx={{ padding: 0 }}>
-        <RichTypography sx={{ mt: 5, mb: 2.5 }} variant="h3">
-          {title}
-        </RichTypography>
-        <Box sx={{ display: "flex" }}>
-          <RichTypography sx={{ borderRight: "solid 1px", mr: 1.25, pr: 1.25 }}>
-            {date}
+      <CardActionArea component={href ? Link : undefined} href={href}>
+        <CardMedia component="img" alt="" src={image?.src} />
+        <CardContent sx={{ padding: 0 }}>
+          <RichTypography sx={{ mt: 5, mb: 2.5 }} variant="h3">
+            {title}
           </RichTypography>
-          <RichTypography>{tags?.join(", ")}</RichTypography>
-        </Box>
-        <RichTypography
-          variant="body2"
-          sx={{
-            py: 2.5,
-            display: "-webkit-box",
-            WebkitLineClamp: "3",
-            WebkitBoxOrient: "vertical",
-            overflow: "hidden",
-            maxHeight: 28 * 3,
-          }}
-        >
-          {content}
-        </RichTypography>
-      </CardContent>
-      <CardActions sx={{ p: 0, mt: 2.5 }}>
-        <Button
-          href={href}
-          component={href ? Link : undefined}
-          variant="contained-reverse"
-        >
-          Read More
-        </Button>
-      </CardActions>
+          <Box sx={{ display: "flex" }}>
+            <RichTypography
+              sx={{ borderRight: "solid 1px", mr: 1.25, pr: 1.25 }}
+            >
+              {date}
+            </RichTypography>
+            <RichTypography>{tags?.join(", ")}</RichTypography>
+          </Box>
+          <RichTypography
+            variant="body2"
+            sx={{
+              py: 2.5,
+              display: "-webkit-box",
+              WebkitLineClamp: "3",
+              WebkitBoxOrient: "vertical",
+              overflow: "hidden",
+              maxHeight: 28 * 3,
+            }}
+          >
+            {content}
+          </RichTypography>
+        </CardContent>
+        <CardActions sx={{ p: 0, mt: 2.5 }}>
+          <Button component="div" variant="contained-reverse">
+            Read More
+          </Button>
+        </CardActions>
+      </CardActionArea>
     </Card>
   );
 });
