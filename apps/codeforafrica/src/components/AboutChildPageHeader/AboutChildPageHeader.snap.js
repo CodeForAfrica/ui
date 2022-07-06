00cc3f98e68190868409f065a52b5646cc9ca29b
_ Kilemensi <clemence@codeforafrica.org>
2022-07-06 16:31:55 +0300

    Update test snapshots

---
+++ apps/codeforafrica/src/components/AboutChildPageHeader/AboutChildPageHeader.snap.js
@@ -0,0 +1,39 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`<AboutChildPageHeader /> renders unchanged 1`] = `
+<div>
+  <div
+    class="MuiBox-root css-1dmumqp"
+  >
+    <div
+      class="MuiContainer-root MuiContainer-maxWidthLg MuiContainer-fixed MuiContainer-disableGutters css-h42qkr-MuiContainer-root"
+    >
+      <div
+        class="MuiGrid-root MuiGrid-container css-1fyy4z-MuiGrid-root"
+      >
+        <div
+          class="MuiGrid-root MuiGrid-item css-l2rmlj-MuiGrid-root"
+        >
+          <figure
+            class="css-jevqpy"
+          />
+        </div>
+        <div
+          class="MuiGrid-root MuiGrid-item css-u2r8gx-MuiGrid-root"
+        >
+          <div
+            class="MuiTypography-root MuiTypography-h1 css-19766wr-MuiTypography-root"
+          >
+            Jacobo Ottaviani
+          </div>
+          <div
+            class="MuiTypography-root MuiTypography-h3Light css-1hyoq96-MuiTypography-root"
+          >
+            Chief Data Officer
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+`;
---
+++ apps/codeforafrica/src/pages/about/[unit]/[slug]/index.snap.js
@@ -0,0 +1,218 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`<Pages/About/[Unit]/[Slug] /> renders unchanged 1`] = `
+<div>
+  <header
+    class="MuiPaper-root MuiPaper-elevation MuiPaper-elevation4 MuiAppBar-root MuiAppBar-colorInherit MuiAppBar-positionSticky css-vckksl-MuiPaper-root-MuiAppBar-root-CuiNavBar-root"
+  >
+    <div
+      class="MuiToolbar-root MuiToolbar-regular css-l73zrm-MuiToolbar-root"
+    >
+      <div
+        class="MuiContainer-root MuiContainer-maxWidthLg MuiContainer-fixed MuiContainer-disableGutters css-1y3f71u-MuiContainer-root"
+      >
+        <div
+          class="MuiGrid-root MuiGrid-container css-hs5ptp-MuiGrid-root"
+        >
+          <div
+            class="MuiGrid-root MuiGrid-item css-13i4rnv-MuiGrid-root"
+          >
+            <a
+              class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways MuiButtonBase-root css-2nqmzp-MuiTypography-root-MuiLink-root-MuiButtonBase-root"
+              href="/"
+              tabindex="0"
+            >
+              <span
+                style="box-sizing: border-box; display: inline-block; overflow: hidden; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; position: relative; max-width: 100%;"
+              >
+                <span
+                  style="box-sizing: border-box; display: block; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; max-width: 100%;"
+                >
+                  <img
+                    alt=""
+                    aria-hidden="true"
+                    src="data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%27116%27%20height=%2750%27/%3e"
+                    style="display: block; max-width: 100%; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px;"
+                  />
+                </span>
+                <img
+                  alt="Logo"
+                  data-nimg="intrinsic"
+                  decoding="async"
+                  src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+                  style="position: absolute; top: 0px; left: 0px; bottom: 0px; right: 0px; box-sizing: border-box; padding: 0px; margin: auto; display: block; width: 0px; height: 0px; min-width: 100%; max-width: 100%; min-height: 100%; max-height: 100%;"
+                />
+                <noscript />
+              </span>
+            </a>
+          </div>
+          <div
+            class="MuiGrid-root MuiGrid-item css-13i4rnv-MuiGrid-root"
+          >
+            <button
+              class="MuiButtonBase-root css-r1bof9-MuiButtonBase-root"
+              tabindex="0"
+              type="button"
+            >
+              <span
+                style="box-sizing: border-box; display: inline-block; overflow: hidden; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; position: relative; max-width: 100%;"
+              >
+                <span
+                  style="box-sizing: border-box; display: block; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; max-width: 100%;"
+                >
+                  <img
+                    alt=""
+                    aria-hidden="true"
+                    src="data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%2732%27%20height=%2732%27/%3e"
+                    style="display: block; max-width: 100%; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px;"
+                  />
+                </span>
+                <img
+                  alt="menu icon"
+                  data-nimg="intrinsic"
+                  decoding="async"
+                  src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+                  style="position: absolute; top: 0px; left: 0px; bottom: 0px; right: 0px; box-sizing: border-box; padding: 0px; margin: auto; display: block; width: 0px; height: 0px; min-width: 100%; max-width: 100%; min-height: 100%; max-height: 100%;"
+                />
+                <noscript />
+              </span>
+            </button>
+          </div>
+        </div>
+        <div
+          class="MuiGrid-root MuiGrid-container css-1au5hzx-MuiGrid-root"
+        >
+          <div
+            class="MuiGrid-root MuiGrid-item css-13i4rnv-MuiGrid-root"
+          >
+            <a
+              class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways MuiButtonBase-root css-2nqmzp-MuiTypography-root-MuiLink-root-MuiButtonBase-root"
+              href="/"
+              tabindex="0"
+            >
+              <span
+                style="box-sizing: border-box; display: inline-block; overflow: hidden; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; position: relative; max-width: 100%;"
+              >
+                <span
+                  style="box-sizing: border-box; display: block; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; max-width: 100%;"
+                >
+                  <img
+                    alt=""
+                    aria-hidden="true"
+                    src="data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%27136%27%20height=%2761%27/%3e"
+                    style="display: block; max-width: 100%; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px;"
+                  />
+                </span>
+                <img
+                  alt="Logo"
+                  data-nimg="intrinsic"
+                  decoding="async"
+                  src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+                  style="position: absolute; top: 0px; left: 0px; bottom: 0px; right: 0px; box-sizing: border-box; padding: 0px; margin: auto; display: block; width: 0px; height: 0px; min-width: 100%; max-width: 100%; min-height: 100%; max-height: 100%;"
+                />
+                <noscript />
+              </span>
+            </a>
+          </div>
+          <div
+            class="MuiGrid-root MuiGrid-item css-13i4rnv-MuiGrid-root"
+          >
+            <nav
+              class="MuiBox-root css-1a9getn"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+  <div
+    class="MuiBox-root css-1dmumqp"
+  >
+    <div
+      class="MuiContainer-root MuiContainer-maxWidthLg MuiContainer-fixed MuiContainer-disableGutters css-h42qkr-MuiContainer-root"
+    >
+      <div
+        class="MuiGrid-root MuiGrid-container css-1fyy4z-MuiGrid-root"
+      >
+        <div
+          class="MuiGrid-root MuiGrid-item css-l2rmlj-MuiGrid-root"
+        >
+          <figure
+            class="css-15gmsmg"
+          />
+        </div>
+        <div
+          class="MuiGrid-root MuiGrid-item css-u2r8gx-MuiGrid-root"
+        >
+          <div
+            class="MuiTypography-root MuiTypography-h1 css-19766wr-MuiTypography-root"
+          >
+            Justin Arenstein
+          </div>
+          <div
+            class="MuiTypography-root MuiTypography-h3Light css-1hyoq96-MuiTypography-root"
+          >
+            CEO of Code for Africa
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div
+    class="MuiContainer-root MuiContainer-maxWidthLg MuiContainer-fixed MuiContainer-disableGutters css-1ilq66o-MuiContainer-root"
+  >
+    <div
+      class="css-x446uu-MuiStack-root"
+    >
+      <div
+        class="MuiTypography-root MuiTypography-footerCap css-19iy7wy-MuiTypography-root"
+      >
+        Connect
+      </div>
+      <div
+        class="css-r7or14-MuiStack-root"
+      >
+        <button
+          aria-label="Twitter"
+          class="react-share__ShareButton"
+          data-mui-internal-clone-element="true"
+          style="background-color: transparent; padding: 0px; cursor: pointer;"
+        >
+          <div
+            aria-hidden="true"
+            class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-1dz6c5u-MuiSvgIcon-root"
+            focusable="false"
+            viewbox="0 0 24 24"
+          />
+        </button>
+        <button
+          aria-label="Linkedin"
+          class="react-share__ShareButton"
+          data-mui-internal-clone-element="true"
+          style="background-color: transparent; padding: 0px; cursor: pointer;"
+        >
+          <div
+            aria-hidden="true"
+            class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-1dz6c5u-MuiSvgIcon-root"
+            focusable="false"
+            viewbox="0 0 24 24"
+          />
+        </button>
+        <button
+          aria-label="Facebook"
+          class="react-share__ShareButton"
+          data-mui-internal-clone-element="true"
+          style="background-color: transparent; padding: 0px; cursor: pointer;"
+        >
+          <div
+            aria-hidden="true"
+            class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-1dz6c5u-MuiSvgIcon-root"
+            focusable="false"
+            viewbox="0 0 24 24"
+          />
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+`;
