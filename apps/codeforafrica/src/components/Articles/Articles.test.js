e3040664a95aaff9c014c91ea9d5539a783aa8ed
_ Kilemensi <clemence@codeforafrica.org>
2022-07-17 21:43:48 +0300

    Fix failing test that needed mocking

--- apps/codeforafrica/src/components/Articles/Articles.test.js
+++ apps/codeforafrica/src/components/Articles/Articles.test.js
@@ -7,9 +7,18 @@ import theme from "@/codeforafrica/theme";
 
 // eslint-disable-next-line testing-library/render-result-naming-convention
 const render = createRender({ theme });
+
 const defaultProps = {
   articles: [],
 };
+
+jest.mock("next/router", () => ({
+  useRouter: jest.fn().mockImplementation(() => ({
+    isReady: true,
+    query: {},
+  })),
+}));
+
 describe("<Articles />", () => {
   it("renders unchanged", () => {
     const { container } = render(<Articles {...defaultProps} />);
