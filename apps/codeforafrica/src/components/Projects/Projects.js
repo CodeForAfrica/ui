63bc64add1c10c9ff1b00168548fded1026e038d
_ Kilemensi <clemence@codeforafrica.org>
2022-07-17 21:42:04 +0300

    Use useFilterQuery, FilterBar and updated NextPreviousPagination

--- apps/codeforafrica/src/components/Projects/Projects.js
+++ apps/codeforafrica/src/components/Projects/Projects.js
@@ -1,17 +1,16 @@
-/* eslint-env browser */
 import { Section } from "@commons-ui/core";
 import Stack from "@mui/material/Stack";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 
-import useProjects, { ALL_CATEGORIES } from "./useProjects";
+import useProjects from "./useProjects";
 
-import ChoiceChip from "@/codeforafrica/components/ChoiceChip";
-import ChoiceChipGroup from "@/codeforafrica/components/ChoiceChipGroup";
+import FilterBar from "@/codeforafrica/components/FilterBar";
 import NextPreviousPagination from "@/codeforafrica/components/NextPreviousPagination";
 import ProjectCard from "@/codeforafrica/components/ProjectCard";
-import SearchInput from "@/codeforafrica/components/SearchInput";
-import equalsIgnoreCase from "@/codeforafrica/utils/equalsIgnoreCase";
+import useFilterQuery, {
+  ALL_TAG,
+} from "@/codeforafrica/components/useFilterQuery";
 
 const Projects = React.forwardRef(function Projects(
   {
@@ -24,40 +23,13 @@ const Projects = React.forwardRef(function Projects(
   },
   ref
 ) {
-  const [category, setCategory] = useState(ALL_CATEGORIES);
   const [count, setCount] = useState(countProp);
   const [page, setPage] = useState(pageProp);
   const [projects, setProjects] = useState(resultsProp);
   const [q, setQ] = useState();
-  const [search, setSearch] = useState();
+  const [tag, setTag] = useState(ALL_TAG);
+  const queryParams = useFilterQuery({ page, q, tag });
   const router = useRouter();
-  useEffect(() => {
-    if (router.isReady) {
-      const {
-        category: initialCategory,
-        page: initialPage,
-        q: initialQ,
-      } = router.query;
-      if (initialCategory) {
-        setCategory(initialCategory);
-      }
-      if (initialPage) {
-        setPage(Number.parseInt(initialPage, 10));
-      }
-      if (initialQ) {
-        setQ(initialQ);
-      }
-    }
-    // We're only interested in initial isReady and not any subsequent
-    // router.query changes e.g. due to pagination
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [router.isReady]);
-
-  const handleChangeChoice = (_, value) => {
-    const newValue = value || ALL_CATEGORIES;
-    setCategory(newValue);
-    setPage(1);
-  };
 
   const handleChangePage = (_, value) => {
     setPage(value);
@@ -67,47 +39,13 @@ const Projects = React.forwardRef(function Projects(
     setQ(value || undefined);
   };
 
-  const handleChangeSearch = (e) => {
-    setSearch(e.target.value);
+  const handleChangeTag = (_, value) => {
+    const newValue = value || ALL_TAG;
+    setTag(newValue);
+    setPage(1);
   };
 
-  const handleClickSearch = (e) => {
-    handleChangeQ(e, search);
-  };
-
-  const handleKeyPressSearch = (e) => {
-    if (e.key === "Enter") {
-      handleChangeQ(e, e.target.value);
-    }
-  };
-
-  useEffect(() => {
-    const newQuery = {};
-    if (!equalsIgnoreCase(category, ALL_CATEGORIES)) {
-      newQuery.category = category;
-    }
-    if (page > 1) {
-      newQuery.page = page;
-    }
-    if (q) {
-      newQuery.q = q;
-    }
-    router.push(
-      {
-        query: newQuery,
-      },
-      undefined,
-      {
-        scroll: true,
-        shallow: true,
-      }
-    );
-    // We don't want to listen to router changes here since we're the ones
-    // updating them
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [category, page, q]);
-
-  const { data } = useProjects({ category, page, q });
+  const { data } = useProjects({ category: tag, page, q });
   useEffect(() => {
     if (data) {
       const { results, pagination } = data;
@@ -116,47 +54,33 @@ const Projects = React.forwardRef(function Projects(
     }
   }, [data]);
 
-  const hasProjects = projects?.length > 0;
+  useEffect(() => {
+    router.push(queryParams, undefined, {
+      scroll: true,
+      shallow: true,
+    });
+    // We don't want to listen to router changes here since we're the ones
+    // updating them
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [queryParams]);
+
   return (
     <div ref={ref}>
       <Section
         sx={{ px: { xs: 2.5, sm: 0 }, py: { xs: 2.5, md: 8, lg: 9 }, ...sx }}
       >
-        <Stack
-          direction={{ xs: "column", sm: "row" }}
-          justifyContent="space-between"
-        >
-          <SearchInput
-            disabled={!hasProjects}
-            onChange={handleChangeSearch}
-            onClick={handleClickSearch}
-            onKeyPress={handleKeyPressSearch}
-            placeholder="Search project"
-            size="small"
-            defaultValue={q}
-            sx={{
-              mb: { xs: 2.5, sm: 0 },
-              minWidth: { xs: "auto", sm: "200px" },
-              ml: { xs: 0, sm: 2.5 },
-              order: { xs: 0, sm: 1 },
-              width: { xs: "auto", sm: "200px" },
-            }}
-          />
-          {/* There will always be at least ALL category */}
-          <ChoiceChipGroup
-            color="default"
-            onChange={handleChangeChoice}
-            value={category}
-            sx={{
-              order: { xs: 1, sm: 0 },
-            }}
-          >
-            {categories?.map((c) => (
-              <ChoiceChip label={c} value={c} key={c} />
-            ))}
-          </ChoiceChipGroup>
-        </Stack>
-        {hasProjects ? (
+        <FilterBar
+          allTag={ALL_TAG}
+          onChangeQ={handleChangeQ}
+          onChangeTag={handleChangeTag}
+          q={q}
+          tag={tag}
+          tags={categories}
+          SearchInputProps={{
+            placeholder: "Search projects",
+          }}
+        />
+        {projects?.length > 0 ? (
           <Stack direction="column" spacing={{ xs: 5, md: 7.5 }}>
             {projects.map((project) => (
               <ProjectCard {...project} key={project.slug} />
--- apps/codeforafrica/src/components/Projects/useProjects.js
+++ apps/codeforafrica/src/components/Projects/useProjects.js
@@ -1,25 +1,11 @@
 import useSWR from "swr";
 
-import equalsIgnoreCase from "@/codeforafrica/utils/equalsIgnoreCase";
-
-const ALL_CATEGORIES = "All";
+import useFilterQuery from "@/codeforafrica/components/useFilterQuery";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
-function useProject(query = {}) {
-  const searchParams = new URLSearchParams();
-  const { category, page, q } = query;
-  if (category && !equalsIgnoreCase(query.category, ALL_CATEGORIES)) {
-    searchParams.append("category", category);
-  }
-  if (page > 1) {
-    searchParams.append("page", page);
-  }
-  if (q) {
-    searchParams.append("q", q);
-  }
-  const queryString = searchParams.toString();
-  const queryParams = queryString ? `?${queryString}` : "";
+function useProject(query) {
+  const queryParams = useFilterQuery(query);
   const { data, error } = useSWR(`/api/projects${queryParams}`, fetcher);
 
   return {
@@ -28,7 +14,5 @@ function useProject(query = {}) {
     error,
   };
 }
-
-export { ALL_CATEGORIES };
 
 export default useProject;
