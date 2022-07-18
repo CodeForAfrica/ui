fa482a8fc5bab823321a66e92a294319858fa191
_ Kilemensi <clemence@codeforafrica.org>
2022-07-18 09:54:33 +0300

    Lower case query string

--- apps/codeforafrica/src/components/useFilterQuery/useFilterQuery.js
+++ apps/codeforafrica/src/components/useFilterQuery/useFilterQuery.js
@@ -5,17 +5,17 @@ const ALL_TAG = "All";
 function useFilterQuery(query = {}) {
   const searchParams = new URLSearchParams();
   const { tag, page, q } = query;
-  if (tag && !equalsIgnoreCase(tag, ALL_TAG)) {
-    searchParams.append("tag", tag);
-  }
   if (page > 1) {
     searchParams.append("page", page);
   }
   if (q) {
     searchParams.append("q", q);
   }
+  if (tag && !equalsIgnoreCase(tag, ALL_TAG)) {
+    searchParams.append("tag", tag);
+  }
   const queryString = searchParams.toString();
-  return queryString ? `?${queryString}` : "";
+  return queryString ? `?${queryString.toLocaleLowerCase()}` : "";
 }
 
 export { ALL_TAG };
