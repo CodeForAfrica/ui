af49dc84d066e0f31c788c5b86b2162d46d1eb37
_ Kilemensi <clemence@codeforafrica.org>
2022-07-20 15:34:27 +0300

    Fix tests

--- apps/codeforafrica/src/components/TeamMembers/TeamMembers.snap.js
+++ apps/codeforafrica/src/components/TeamMembers/TeamMembers.snap.js
@@ -11,117 +11,127 @@ exports[`<TeamMembers /> renders unchanged 1`] = `
       Team
     </div>
     <div
-      class="MuiGrid-root MuiGrid-container css-lhayfe-MuiGrid-root"
+      class="MuiBox-root css-n6p80u"
     >
       <div
-        class="MuiGrid-root MuiGrid-item css-13i4rnv-MuiGrid-root"
+        class="MuiGrid-root MuiGrid-container css-zv50xx-MuiGrid-root"
       >
         <div
-          class="MuiPaper-root MuiPaper-elevation MuiPaper-elevation0 MuiCard-root css-y2odjh-MuiPaper-root-MuiCard-root"
-          description="Lorem ipsum dolor sit amet consectetur adipiscing, elit ornare et rhoncus sem, laoreet tortor tristique cursus ultrices. Sed urna nec convallis in tempus lacinia velit viverra neque, molestie malesuada fringilla ut vulputate faucibus ad potenti, nisi enim nam montes nostra curae ridiculus curabitur. Elementum phasellus conubia eget ac dignissim hendrerit ullamcorper magna pretium pulvinar, auctor dui pelle."
-          slug="member-1"
+          class="MuiGrid-root MuiGrid-item css-13i4rnv-MuiGrid-root"
         >
-          <a
-            class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways MuiButtonBase-root MuiCardActionArea-root css-k9g9by-MuiTypography-root-MuiLink-root-MuiButtonBase-root-MuiCardActionArea-root"
-            href="/about/members/member-1"
-            tabindex="0"
+          <div
+            class="MuiPaper-root MuiPaper-elevation MuiPaper-elevation0 MuiCard-root css-m2run-MuiPaper-root-MuiCard-root"
+            country="Georgia"
+            description="Lorem ipsum dolor sit amet consectetur adipiscing, elit ornare et rhoncus sem, laoreet tortor tristique cursus ultrices. Sed urna nec convallis in tempus lacinia velit viverra neque, molestie malesuada fringilla ut vulputate faucibus ad potenti, nisi enim nam montes nostra curae ridiculus curabitur. Elementum phasellus conubia eget ac dignissim hendrerit ullamcorper magna pretium pulvinar, auctor dui pelle."
+            slug="member-1"
+            team="New Initiatives"
           >
-            <img
-              class="MuiCardMedia-root MuiCardMedia-media MuiCardMedia-img css-ys68yq-MuiCardMedia-root"
-              src="https://res.cloudinary.com/code-for-africa/image/upload/v1653298218/codeforafrica/images/team/image_11_jb8a30.jpg"
-            />
-            <div
-              class="MuiCardContent-root css-1bqtlyx-MuiCardContent-root"
+            <a
+              class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways MuiButtonBase-root MuiCardActionArea-root css-k9g9by-MuiTypography-root-MuiLink-root-MuiButtonBase-root-MuiCardActionArea-root"
+              href="/about/members/member-1"
+              tabindex="0"
             >
+              <img
+                class="MuiCardMedia-root MuiCardMedia-media MuiCardMedia-img css-ys68yq-MuiCardMedia-root"
+                src="https://res.cloudinary.com/code-for-africa/image/upload/v1653298218/codeforafrica/images/team/image_11_jb8a30.jpg"
+              />
               <div
-                class="MuiTypography-root MuiTypography-body1SemiBold css-1v4gd4b-MuiTypography-root"
+                class="MuiCardContent-root css-1bqtlyx-MuiCardContent-root"
               >
-                Justin Arenstein
+                <div
+                  class="MuiTypography-root MuiTypography-body1SemiBold css-1v4gd4b-MuiTypography-root"
+                >
+                  Justin Arenstein
+                </div>
+                <div
+                  class="MuiTypography-root MuiTypography-body1 css-fna42f-MuiTypography-root"
+                >
+                  CEO of Code for Africa
+                </div>
               </div>
-              <div
-                class="MuiTypography-root MuiTypography-body1 css-fna42f-MuiTypography-root"
-              >
-                CEO of Code for Africa
-              </div>
-            </div>
-            <span
-              class="MuiCardActionArea-focusHighlight css-1v2exvi-MuiCardActionArea-focusHighlight"
-            />
-          </a>
+              <span
+                class="MuiCardActionArea-focusHighlight css-1v2exvi-MuiCardActionArea-focusHighlight"
+              />
+            </a>
+          </div>
         </div>
-      </div>
-      <div
-        class="MuiGrid-root MuiGrid-item css-13i4rnv-MuiGrid-root"
-      >
         <div
-          class="MuiPaper-root MuiPaper-elevation MuiPaper-elevation0 MuiCard-root css-y2odjh-MuiPaper-root-MuiCard-root"
-          description="Lorem ipsum dolor sit amet consectetur adipiscing, elit ornare et rhoncus sem, laoreet tortor tristique cursus ultrices. Sed urna nec convallis in tempus lacinia velit viverra neque, molestie malesuada fringilla ut vulputate faucibus ad potenti, nisi enim nam montes nostra curae ridiculus curabitur. Elementum phasellus conubia eget ac dignissim hendrerit ullamcorper magna pretium pulvinar, auctor dui pelle."
-          slug="member-2"
+          class="MuiGrid-root MuiGrid-item css-13i4rnv-MuiGrid-root"
         >
-          <a
-            class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways MuiButtonBase-root MuiCardActionArea-root css-k9g9by-MuiTypography-root-MuiLink-root-MuiButtonBase-root-MuiCardActionArea-root"
-            href="/about/members/member-2"
-            tabindex="0"
+          <div
+            class="MuiPaper-root MuiPaper-elevation MuiPaper-elevation0 MuiCard-root css-m2run-MuiPaper-root-MuiCard-root"
+            country="Nigeria"
+            description="Lorem ipsum dolor sit amet consectetur adipiscing, elit ornare et rhoncus sem, laoreet tortor tristique cursus ultrices. Sed urna nec convallis in tempus lacinia velit viverra neque, molestie malesuada fringilla ut vulputate faucibus ad potenti, nisi enim nam montes nostra curae ridiculus curabitur. Elementum phasellus conubia eget ac dignissim hendrerit ullamcorper magna pretium pulvinar, auctor dui pelle."
+            slug="member-2"
+            team="Knowledge"
           >
-            <img
-              class="MuiCardMedia-root MuiCardMedia-media MuiCardMedia-img css-ys68yq-MuiCardMedia-root"
-              src="https://res.cloudinary.com/code-for-africa/image/upload/v1653300741/codeforafrica/images/team/image_11_fdwgvv.jpg"
-            />
-            <div
-              class="MuiCardContent-root css-1bqtlyx-MuiCardContent-root"
+            <a
+              class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways MuiButtonBase-root MuiCardActionArea-root css-k9g9by-MuiTypography-root-MuiLink-root-MuiButtonBase-root-MuiCardActionArea-root"
+              href="/about/members/member-2"
+              tabindex="0"
             >
+              <img
+                class="MuiCardMedia-root MuiCardMedia-media MuiCardMedia-img css-ys68yq-MuiCardMedia-root"
+                src="https://res.cloudinary.com/code-for-africa/image/upload/v1653300741/codeforafrica/images/team/image_11_fdwgvv.jpg"
+              />
               <div
-                class="MuiTypography-root MuiTypography-body1SemiBold css-1v4gd4b-MuiTypography-root"
+                class="MuiCardContent-root css-1bqtlyx-MuiCardContent-root"
               >
-                Tolulope Adeyemo
+                <div
+                  class="MuiTypography-root MuiTypography-body1SemiBold css-1v4gd4b-MuiTypography-root"
+                >
+                  Tolulope Adeyemo
+                </div>
+                <div
+                  class="MuiTypography-root MuiTypography-body1 css-fna42f-MuiTypography-root"
+                >
+                  Senior Programme Manager
+                </div>
               </div>
-              <div
-                class="MuiTypography-root MuiTypography-body1 css-fna42f-MuiTypography-root"
-              >
-                Senior Programme Manager
-              </div>
-            </div>
-            <span
-              class="MuiCardActionArea-focusHighlight css-1v2exvi-MuiCardActionArea-focusHighlight"
-            />
-          </a>
+              <span
+                class="MuiCardActionArea-focusHighlight css-1v2exvi-MuiCardActionArea-focusHighlight"
+              />
+            </a>
+          </div>
         </div>
-      </div>
-      <div
-        class="MuiGrid-root MuiGrid-item css-13i4rnv-MuiGrid-root"
-      >
         <div
-          class="MuiPaper-root MuiPaper-elevation MuiPaper-elevation0 MuiCard-root css-y2odjh-MuiPaper-root-MuiCard-root"
-          description="Lorem ipsum dolor sit amet consectetur adipiscing, elit ornare et rhoncus sem, laoreet tortor tristique cursus ultrices. Sed urna nec convallis in tempus lacinia velit viverra neque, molestie malesuada fringilla ut vulputate faucibus ad potenti, nisi enim nam montes nostra curae ridiculus curabitur. Elementum phasellus conubia eget ac dignissim hendrerit ullamcorper magna pretium pulvinar, auctor dui pelle."
-          slug="member-3"
+          class="MuiGrid-root MuiGrid-item css-13i4rnv-MuiGrid-root"
         >
-          <a
-            class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways MuiButtonBase-root MuiCardActionArea-root css-k9g9by-MuiTypography-root-MuiLink-root-MuiButtonBase-root-MuiCardActionArea-root"
-            href="/about/members/member-3"
-            tabindex="0"
+          <div
+            class="MuiPaper-root MuiPaper-elevation MuiPaper-elevation0 MuiCard-root css-m2run-MuiPaper-root-MuiCard-root"
+            country="South Africa"
+            description="Lorem ipsum dolor sit amet consectetur adipiscing, elit ornare et rhoncus sem, laoreet tortor tristique cursus ultrices. Sed urna nec convallis in tempus lacinia velit viverra neque, molestie malesuada fringilla ut vulputate faucibus ad potenti, nisi enim nam montes nostra curae ridiculus curabitur. Elementum phasellus conubia eget ac dignissim hendrerit ullamcorper magna pretium pulvinar, auctor dui pelle."
+            slug="member-3"
+            team="africanDRONE"
           >
-            <img
-              class="MuiCardMedia-root MuiCardMedia-media MuiCardMedia-img css-ys68yq-MuiCardMedia-root"
-              src="https://res.cloudinary.com/code-for-africa/image/upload/v1653300752/codeforafrica/images/team/image_11_mauuaw.png"
-            />
-            <div
-              class="MuiCardContent-root css-1bqtlyx-MuiCardContent-root"
+            <a
+              class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways MuiButtonBase-root MuiCardActionArea-root css-k9g9by-MuiTypography-root-MuiLink-root-MuiButtonBase-root-MuiCardActionArea-root"
+              href="/about/members/member-3"
+              tabindex="0"
             >
+              <img
+                class="MuiCardMedia-root MuiCardMedia-media MuiCardMedia-img css-ys68yq-MuiCardMedia-root"
+                src="https://res.cloudinary.com/code-for-africa/image/upload/v1653300752/codeforafrica/images/team/image_11_mauuaw.png"
+              />
               <div
-                class="MuiTypography-root MuiTypography-body1SemiBold css-1v4gd4b-MuiTypography-root"
+                class="MuiCardContent-root css-1bqtlyx-MuiCardContent-root"
               >
-                Johnny Miller
+                <div
+                  class="MuiTypography-root MuiTypography-body1SemiBold css-1v4gd4b-MuiTypography-root"
+                >
+                  Johnny Miller
+                </div>
+                <div
+                  class="MuiTypography-root MuiTypography-body1 css-fna42f-MuiTypography-root"
+                >
+                  Co-Founder of africanDRONE
+                </div>
               </div>
-              <div
-                class="MuiTypography-root MuiTypography-body1 css-fna42f-MuiTypography-root"
-              >
-                Co-Founder of africanDRONE
-              </div>
-            </div>
-            <span
-              class="MuiCardActionArea-focusHighlight css-1v2exvi-MuiCardActionArea-focusHighlight"
-            />
-          </a>
+              <span
+                class="MuiCardActionArea-focusHighlight css-1v2exvi-MuiCardActionArea-focusHighlight"
+              />
+            </a>
+          </div>
         </div>
       </div>
     </div>
