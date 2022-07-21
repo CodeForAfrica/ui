de857553aebe8d8680cac3075ec5acd5f21151b6
_ Kilemensi <clemence@codeforafrica.org>
2022-07-21 15:32:05 +0300

    Use client-side data fetching

--- apps/codeforafrica/src/components/Articles/Articles.snap.js
+++ apps/codeforafrica/src/components/Articles/Articles.snap.js
@@ -2,30 +2,62 @@
 
 exports[`<Articles /> renders unchanged 1`] = `
 <div>
-  <div
-    class="MuiContainer-root MuiContainer-maxWidthLg MuiContainer-fixed MuiContainer-disableGutters css-1y3f71u-MuiContainer-root"
-  >
+  <div>
     <div
-      class="MuiGrid-root MuiGrid-container css-teoma9-MuiGrid-root"
+      class="MuiContainer-root MuiContainer-maxWidthLg MuiContainer-fixed MuiContainer-disableGutters css-1y3f71u-MuiContainer-root"
     >
       <div
-        class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 css-17kpph-MuiGrid-root"
+        class="MuiGrid-root MuiGrid-container css-teoma9-MuiGrid-root"
       >
         <div
-          class="MuiToggleButtonGroup-root css-lxlk1k-MuiToggleButtonGroup-root"
-          role="group"
+          class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 css-17kpph-MuiGrid-root"
         >
           <div
-            class="MuiButtonBase-root MuiChip-root MuiChip-filled MuiChip-sizeMedium MuiChip-colorPrimary MuiChip-clickable MuiChip-clickableColorPrimary MuiChip-filledPrimary MuiToggleButtonGroup-grouped MuiToggleButtonGroup-groupedHorizontal css-g678h5-MuiButtonBase-root-MuiChip-root"
-            role="button"
-            tabindex="0"
-            value="All"
+            class="css-1htsxuq-MuiStack-root"
           >
-            <span
-              class="MuiChip-label MuiChip-labelMedium css-1n6oebb-MuiChip-label"
+            <div
+              class="MuiFormControl-root MuiTextField-root css-1p0a819-MuiFormControl-root-MuiTextField-root"
             >
-              All
-            </span>
+              <div
+                class="MuiOutlinedInput-root MuiInputBase-root MuiInputBase-colorPrimary MuiInputBase-formControl MuiInputBase-sizeSmall MuiInputBase-adornedEnd css-1u16ib2-MuiInputBase-root-MuiOutlinedInput-root"
+              >
+                <input
+                  aria-invalid="false"
+                  class="MuiOutlinedInput-input MuiInputBase-input MuiInputBase-inputTypeSearch MuiInputBase-inputSizeSmall MuiInputBase-inputAdornedEnd css-1heb2i2-MuiInputBase-input-MuiOutlinedInput-input"
+                  id="mui-1"
+                  placeholder="Search opportunities"
+                  type="search"
+                  value=""
+                />
+                <button
+                  aria-label="search"
+                  class="MuiButtonBase-root MuiIconButton-root MuiIconButton-edgeEnd MuiIconButton-sizeMedium css-k4oz44-MuiButtonBase-root-MuiIconButton-root"
+                  tabindex="0"
+                  type="button"
+                >
+                  <div
+                    aria-hidden="true"
+                    class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-iusnk6-MuiSvgIcon-root"
+                    focusable="false"
+                    viewbox="0 0 16 16"
+                  />
+                </button>
+                <fieldset
+                  aria-hidden="true"
+                  class="MuiOutlinedInput-notchedOutline css-1d3z3hw-MuiOutlinedInput-notchedOutline"
+                >
+                  <legend
+                    class="css-hdw1oc"
+                  >
+                    <span
+                      class="notranslate"
+                    >
+                      ​
+                    </span>
+                  </legend>
+                </fieldset>
+              </div>
+            </div>
           </div>
         </div>
       </div>
