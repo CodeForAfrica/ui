83bddb7124fa603c51fcb88d87400ff53f1fe944
esir <esirkings@gmail.com>
2022-07-21 09:50:54 +0300

    Remove default fields.

--- apps/codeforafrica/src/lib/api.netlify-cms/utils.js
+++ apps/codeforafrica/src/lib/api.netlify-cms/utils.js
@@ -8,13 +8,13 @@ export function getCollectionSlugs(collectionDir) {
   return fs.readdirSync(collectionDir);
 }
 
-export function getCollectionBySlug(collectionDir, slug, fields = []) {
+export function getCollectionBySlug(collectionDir, slug, fields) {
   const realSlug = slug.replace(/\.md$/, "");
   const fullPath = join(collectionDir, `${realSlug}.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
   const { data, content } = matter(fileContents);
 
-  const items = fields.reduce((acc, curr) => {
+  const items = fields?.reduce((acc, curr) => {
     if (curr === "content") {
       acc.content = marked(content);
     } else if (curr === "slug") {
