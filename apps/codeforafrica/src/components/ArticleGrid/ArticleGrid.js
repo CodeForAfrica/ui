db3568e2c7732c7009f56bae45c50806abfa8c21
_ Kilemensi <clemence@codeforafrica.org>
2022-07-21 15:31:39 +0300

    Switch to FilterBar

--- apps/codeforafrica/src/components/ArticleGrid/ArticleGrid.js
+++ apps/codeforafrica/src/components/ArticleGrid/ArticleGrid.js
@@ -3,32 +3,42 @@ import Grid from "@mui/material/Grid";
 import React from "react";
 
 import ArticleCardList from "@/codeforafrica/components/ArticleCardList";
-import ChoiceChip from "@/codeforafrica/components/ChoiceChip";
-import ChoiceChipGroup from "@/codeforafrica/components/ChoiceChipGroup";
 import FeaturedArticle from "@/codeforafrica/components/FeaturedArticle";
+import FilterBar from "@/codeforafrica/components/FilterBar";
 
 const ArticleGrid = React.forwardRef(function ArticleGrid(props, ref) {
-  const { articles, onChange, selectedTag, tags, title, ...other } = props;
+  const {
+    articles,
+    onChangeTag,
+    onChangeQ,
+    q,
+    selectedTag,
+    sx,
+    tags,
+    title,
+    ...other
+  } = props;
 
   return (
-    <Section sx={{ px: { xs: "20px", sm: 0 } }} {...other} ref={ref}>
+    <Section sx={{ px: { xs: "20px", sm: 0 }, ...sx }} {...other} ref={ref}>
       <Grid container sx={{ py: { xs: "28px", md: 8, lg: "105.29px" } }}>
         <Grid item xs={12} sx={{ order: { xs: 0, md: 1 } }}>
           <RichTypography variant="h4" sx={{ mb: "10px" }}>
             {title}
           </RichTypography>
-          {tags?.length > 0 ? (
-            <ChoiceChipGroup
-              color="default"
-              onChange={onChange}
-              value={selectedTag}
-              sx={{ mb: 5 }}
-            >
-              {tags.map((tag) => (
-                <ChoiceChip label={tag} value={tag} key={tag} />
-              ))}
-            </ChoiceChipGroup>
-          ) : null}
+          <FilterBar
+            onChangeQ={onChangeQ}
+            onChangeTag={onChangeTag}
+            q={q}
+            tag={selectedTag}
+            tags={tags}
+            SearchInputProps={{
+              placeholder: "Search opportunities",
+            }}
+            sx={{
+              mb: { xs: 2.5, md: 5 },
+            }}
+          />
         </Grid>
         {articles?.length > 0 ? (
           <>
