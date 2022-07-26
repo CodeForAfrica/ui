09f06ad1a473aed60c5a3a7fba5e478430091483
esir <esirkings@gmail.com>
2022-07-26 13:34:33 +0300

    Dynamically use NEXT_PUBLIC_VERCEL_URL for github auth

--- apps/codeforafrica/src/pages/api/admin/config.yml.page.js
+++ apps/codeforafrica/src/pages/api/admin/config.yml.page.js
@@ -6,18 +6,14 @@ import site from "@/codeforafrica/utils/site";
 
 export default function handler(req, res) {
   if (req.method === "GET") {
-    // if (process.env.NODE_ENV == "production") {
-    //   // Set production configurations
-    //   config.backend.name = "github";
-    //   config.backend.repo = process.env.GITHUB_BACKEND_REPO;
-    //   config.backend.base_url = site.url.replace(/\/+$/, "");
-    //   config.backend.auth_endpoint = process.env.GITHUB_AUTH_ENDPOINT;
-    //   config.publish_mode = "editorial_workflow";
-    //   // Remove dev configurations
-    //   config.local_backend = undefined;
-    // }
-    console.log("GET2 /api/admin/config", config);
-    console.log("GET /api/admin/config", process.env);
+    if (process.env.NODE_ENV === "production") {
+      // Set production configurations
+      config.backend.name = "github";
+      config.backend.repo = process.env.GITHUB_BACKEND_REPO;
+      config.backend.base_url = site.environmentUrl.replace(/\/+$/, "");
+      // Remove dev configurations
+      config.local_backend = undefined;
+    }
     config.logo_url = site.logoUrl;
     const configFile = yaml.dump(config);
 
