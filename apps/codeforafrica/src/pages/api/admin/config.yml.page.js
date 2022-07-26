adf7997bc0c18387e2527e7150135183c4b80df2
esir <esirkings@gmail.com>
2022-07-26 13:15:25 +0300

    Temp disable environment check

--- apps/codeforafrica/src/pages/api/admin/config.yml.page.js
+++ apps/codeforafrica/src/pages/api/admin/config.yml.page.js
@@ -6,19 +6,18 @@ import site from "@/codeforafrica/utils/site";
 
 export default function handler(req, res) {
   if (req.method === "GET") {
-    console.log("GET /api/admin/config", process.env);
-    console.log("GET1 /api/admin/config", config);
-    if (process.env.NODE_ENV === "production") {
-      // Set production configurations
-      config.backend.name = "github";
-      config.backend.repo = process.env.GITHUB_BACKEND_REPO;
-      config.backend.base_url = site.url.replace(/\/+$/, "");
-      config.backend.auth_endpoint = process.env.GITHUB_AUTH_ENDPOINT;
-      config.publish_mode = "editorial_workflow";
-      // Remove dev configurations
-      config.local_backend = undefined;
-    }
+    // if (process.env.NODE_ENV == "production") {
+    //   // Set production configurations
+    //   config.backend.name = "github";
+    //   config.backend.repo = process.env.GITHUB_BACKEND_REPO;
+    //   config.backend.base_url = site.url.replace(/\/+$/, "");
+    //   config.backend.auth_endpoint = process.env.GITHUB_AUTH_ENDPOINT;
+    //   config.publish_mode = "editorial_workflow";
+    //   // Remove dev configurations
+    //   config.local_backend = undefined;
+    // }
     console.log("GET2 /api/admin/config", config);
+    console.log("GET /api/admin/config", process.env);
     config.logo_url = site.logoUrl;
     const configFile = yaml.dump(config);
 
