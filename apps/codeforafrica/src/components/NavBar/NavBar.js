a3b524e41e5d8cb2e73266a0b409f602089f9ac5
_ Kilemensi <clemence@codeforafrica.org>
2022-08-05 17:03:41 +0300

    Access logo

--- apps/codeforafrica/src/components/NavBar/NavBar.js
+++ apps/codeforafrica/src/components/NavBar/NavBar.js
@@ -5,20 +5,20 @@ import React from "react";
 import DesktopNavBar from "@/codeforafrica/components/DesktopNavBar";
 import MobileNavBar from "@/codeforafrica/components/MobileNavBar";
 
-function NavBar({ menu }) {
+function NavBar({ logo, menu }) {
   return (
     <NavigationBar sx={{ py: { xs: "10px", md: "15.5" } }}>
       <Section sx={{ px: { xs: 2.5, sm: 0 } }}>
         <MobileNavBar
+          logo={logo}
           menu={menu}
-          direction="column"
           sx={{
             display: { xs: "flex", md: "none" },
           }}
         />
         <DesktopNavBar
+          logo={logo}
           menu={menu}
-          direction="row"
           sx={{
             display: { xs: "none", md: "flex" },
           }}
