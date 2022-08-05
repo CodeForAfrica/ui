12e0368aca6f827a3ec12a131505cb2b2a70eec6
_ Kilemensi <clemence@codeforafrica.org>
2022-08-05 16:51:26 +0300

    Support children in FooterDescription

--- apps/codeforafrica/src/components/Footer/FooterDescription.js
+++ apps/codeforafrica/src/components/Footer/FooterDescription.js
@@ -1,36 +1,36 @@
+import { RichTypography } from "@commons-ui/core";
 import { Link } from "@commons-ui/next";
-import { Typography } from "@mui/material";
+import Stack from "@mui/material/Stack";
 import PropTypes from "prop-types";
 import React from "react";
 
 import Figure from "@/codeforafrica/components/Figure";
 
-function FooterDescription({ description, logo }) {
-  if (!description) {
+function FooterDescription({ children, logo }) {
+  if (!(logo || children)) {
     return null;
   }
   return (
-    <>
+    <Stack alignItems={{ xs: "center", md: "flex-start" }}>
       <Link href="/">
         <Figure
-          priority
           {...logo}
           sx={{
             display: {
               sm: "block",
             },
-            height: { sm: "113px", md: "113px", lg: "113px" },
-            width: { sm: "251px", md: "251px", lg: "251px" },
+            height: { xs: "113px", md: "113px", lg: "113px" },
+            width: { xs: "251px", md: "251px", lg: "251px" },
           }}
         />
       </Link>
-      <Typography
-        variant="body1"
-        sx={{ marginTop: "3.125rem", fontSize: { md: 12, xs: 12 } }}
+      <RichTypography
+        variant="footer"
+        sx={{ mt: "3.125rem", textAlign: { xs: "center", md: "left" } }}
       >
-        {description}
-      </Typography>
-    </>
+        {children}
+      </RichTypography>
+    </Stack>
   );
 }
 
