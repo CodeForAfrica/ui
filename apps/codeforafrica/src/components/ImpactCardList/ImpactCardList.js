89b36348c2151cc9d7763ffe07f403f261be2a7d
_ Kilemensi <clemence@codeforafrica.org>
2022-08-12 15:13:06 +0300

    Move title and action out of ImpactCardList

--- apps/codeforafrica/src/components/ImpactCardList/ImpactCardList.js
+++ apps/codeforafrica/src/components/ImpactCardList/ImpactCardList.js
@@ -1,75 +1,42 @@
-import { Section, RichTypography } from "@commons-ui/core";
-import { Link } from "@commons-ui/next";
-import { Grid, Box, Button } from "@mui/material";
+import { Grid } from "@mui/material";
 import PropTypes from "prop-types";
 import React from "react";
 
-import ImpactCard from "../ImpactCard/ImpactCard";
+import ImpactCard from "@/codeforafrica/components/ImpactCard";
 
 const ImpactCardList = React.forwardRef(function ImpactCardList(props, ref) {
-  const { impact: { list, action, title } = {} } = props;
+  const { list, sx } = props;
 
   if (!list?.length) {
     return null;
   }
   return (
-    <Box
-      sx={{
-        backgroundColor: "background.main",
-      }}
+    <Grid
+      container
+      rowSpacing={10}
+      justifyContent="space-between"
+      sx={sx}
       ref={ref}
     >
-      <Section
-        sx={{
-          px: { xs: 2.5, md: 0 },
-          py: { xs: 5, sm: 10, md: 4, lg: 12.5 },
-        }}
-      >
-        {title && (
-          <RichTypography
-            sx={{
-              mb: { xs: 10, md: 5, lg: 7.55 },
-              mt: { xs: 2.25, sm: 0, md: 4, lg: 0 },
-            }}
-            variant="h4"
-          >
-            {title}
-          </RichTypography>
-        )}
-        <Grid container justifyContent="space-between">
-          {list.map((l) => (
-            <Grid item key={l.title}>
-              <ImpactCard {...l} />
-            </Grid>
-          ))}
+      {list.map((impact) => (
+        <Grid item key={impact.title}>
+          <ImpactCard {...impact} />
         </Grid>
-        {action?.href?.length > 0 ? (
-          <Grid container justifyContent="center">
-            <Button
-              variant="contained"
-              component={Link}
-              href={action.href}
-              sx={{ width: { xs: "100%", sm: "unset" }, marginTop: 7.25 }}
-            >
-              {action.title || action.href}
-            </Button>
-          </Grid>
-        ) : null}
-      </Section>
-    </Box>
+      ))}
+    </Grid>
   );
 });
 
 ImpactCardList.propTypes = {
-  initiatives: PropTypes.arrayOf(PropTypes.shape({})),
-  title: PropTypes.string,
-  action: PropTypes.shape({}),
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+    })
+  ),
 };
 
 ImpactCardList.defaultProps = {
-  initiatives: undefined,
-  title: undefined,
-  action: undefined,
+  list: undefined,
 };
 
 export default ImpactCardList;
