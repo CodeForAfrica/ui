2afa1113bfd503d6252eb9490c35ccd2f690d20b
_ Kilemensi <clemence@codeforafrica.org>
2022-08-16 15:10:23 +0300

    Expose getGetInTouch, getOurTeam, and fix others
    + getTeam should check if member has been deactivated
    + getOurGuidingPrinciples should include title

--- apps/codeforafrica/src/lib/api.netlify-cms/getOurGuidingPrinciples.js
+++ apps/codeforafrica/src/lib/api.netlify-cms/getOurGuidingPrinciples.js
@@ -6,8 +6,11 @@ import { getCollectionBySlug } from "./utils";
 const pagesDir = join(process.cwd(), "content/pages");
 
 function getOurGuidingPrinciples() {
-  const list = getCollectionBySlug(pagesDir, "about", ["guiding-principles"])
-    .items["guiding-principles"];
+  const { title, "guiding-principle-list": principleIds } = getCollectionBySlug(
+    pagesDir,
+    "about",
+    ["guiding-principles"]
+  ).items["guiding-principles"];
   const allPrinciples = getGuidingPrinciples([
     "id",
     "title",
@@ -15,7 +18,9 @@ function getOurGuidingPrinciples() {
     "content",
   ]);
 
-  return list?.map((id) => allPrinciples.find((p) => p.id === id)) ?? null;
+  const list =
+    principleIds?.map((id) => allPrinciples.find((p) => p.id === id)) ?? null;
+  return { title, list };
 }
 
 export default getOurGuidingPrinciples;
--- apps/codeforafrica/src/lib/api.netlify-cms/index.js
+++ apps/codeforafrica/src/lib/api.netlify-cms/index.js
@@ -1,7 +1,9 @@
+import getGetInTouch from "./getGetInTouch";
 import getOurGuidingPrinciples from "./getOurGuidingPrinciples";
 import getOurImpact from "./getOurImpact";
 import getOurMission from "./getOurMission";
 import getOurPartners from "./getOurPartners";
+import getOurTeam from "./getOurTeam";
 import getPartners from "./getPartners";
 import getCmsProjects from "./getProjects";
 import getTeam from "./getTeam";
@@ -12,14 +14,16 @@ import getMeetOurTeam from "./sections/getMeetOurTeam";
 
 export {
   getCmsProjects,
+  getFooter,
+  getHeader,
   getHero,
+  getGetInTouch,
   getMeetOurTeam,
   getOurGuidingPrinciples,
   getOurImpact,
   getOurMission,
   getOurPartners,
+  getOurTeam,
   getPartners,
   getTeam,
-  getHeader,
-  getFooter,
 };
