2a3eddba7bb24d777ec2404045bff985eab9cdd2
esir <esirkings@gmail.com>
2022-08-23 18:47:54 +0300

    Use exec from actions/exec

--- bump.js
+++ bump.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const { exec } = require('child_process');
+const { exec } = require("@actions/exec");
 
 function getFiles(dir = ".changeset") {
   return fs.readdirSync(dir).flatMap((item) => {
@@ -10,7 +10,6 @@ function getFiles(dir = ".changeset") {
 }
 
 (async () => {
-  await exec("pnpm changeset", ["version"]);
   const file = getFiles()[0];
   let appContent = fs.readFileSync(file, "utf8");
   appContent = appContent
@@ -28,4 +27,5 @@ function getFiles(dir = ".changeset") {
   const content = fs.readFileSync(dockerFilePath, "utf8");
   const updatedContent = content.replace(/:[^\s]+/g, `:${releaseLine}`);
   fs.writeFileSync(dockerFilePath, updatedContent);
+  await exec("pnpm changeset", ["version"]);
 })();
