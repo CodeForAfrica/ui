30f577bd11826eb5138a7d2a38e47862b3a34c35
esir <esirkings@gmail.com>
2022-08-24 15:17:36 +0300

    Update bump script to run code on the correct level.

--- scripts/bump.js
+++ scripts/bump.js
@@ -2,20 +2,14 @@ const fs = require("fs");
 const path = require("path");
 const { exec } = require("@actions/exec");
 
-process.chdir(path.join(__dirname, ".."));
-
 (async () => {
-  await exec("pnpm changeset", ["status", "--output=./.changeset/status.json"]);
+  await exec("pnpm changeset", ["status", "--output=.changeset/status.json"]);
   await exec("pnpm changeset", ["version"]);
-  const changesetStatus = require("./.changeset/status.json");
+  const changesetStatus = require("../.changeset/status.json");
   changesetStatus?.releases.map((release) => {
     const { name: appName } = release;
-    const newVersion = `${require(`./apps/${appName}/package.json`).version}`;
-    const dockerFilePath = path.join(
-      __dirname,
-      `apps/${appName}`,
-      "contrib/dokku/Dockerfile"
-    );
+    const newVersion = `${require(`../apps/${appName}/package.json`).version}`;
+    const dockerFilePath = `apps/${appName}/contrib/dokku/Dockerfile`;
     const content = fs.readFileSync(dockerFilePath, "utf8");
     const updatedContent = content.replace(/:[^\s]+/g, `:${newVersion}`);
     fs.writeFileSync(dockerFilePath, updatedContent);
