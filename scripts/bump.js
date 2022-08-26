5c05a5e6fa64be3658b75c21edde5d1c4a6ead27
esir <esirkings@gmail.com>
2022-08-26 11:05:15 +0300

    Update all apps Dockerfile assuming the Dockerfile and package.json are placed in the same known location

--- scripts/bump.js
+++ scripts/bump.js
@@ -1,19 +1,18 @@
 const fs = require("fs");
 const path = require("path");
 const { exec } = require("@actions/exec");
-const { argv } = require("process");
-
-const versionFilePath = argv[2];
-const dockerFilePath = argv[3];
 
 (async () => {
   await exec("pnpm changeset", ["version"]);
-  const newVersion = `${require(`../${versionFilePath}`).version}`;
-  const currentImageVersion = fs.readFileSync(dockerFilePath, "utf8");
-  const updatedImageVersion = currentImageVersion.replace(
-    /:[^\s]+/g,
-    `:${newVersion}`
-  );
-  fs.writeFileSync(dockerFilePath, updatedImageVersion);
-  await exec("pnpm format");
+  const apps = fs.readdirSync("apps");
+  apps.map((app) => {
+    const newVersion = `${require(`../apps/${app}/package.json`).version}`;
+    const dockerFilePath = path.join(`apps/${app}/contrib/dokku`, "Dockerfile");
+    const currentImageVersion = fs.readFileSync(dockerFilePath, "utf8");
+    const updatedImageVersion = currentImageVersion.replace(
+      /:[^\s]+/g,
+      `:${newVersion}`
+    );
+    fs.writeFileSync(dockerFilePath, updatedImageVersion);
+  });
 })();
