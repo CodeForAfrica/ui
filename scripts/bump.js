f4c901db323fb592edeb04146ddb996130f19da4
esir <esirkings@gmail.com>
2022-08-29 11:43:24 +0300

    Only perform package version checks for apps

--- scripts/bump.js
+++ scripts/bump.js
@@ -6,14 +6,20 @@ const { exec } = require("@actions/exec");
   await exec("pnpm changeset", ["version"]);
   const apps = fs.readdirSync("apps");
   apps.map((app) => {
-    const newVersion = `${require(`../apps/${app}/package.json`).version}`;
-    const dockerFilePath = path.join(`apps/${app}/contrib/dokku`, "Dockerfile");
-    const currentImageVersion = fs.readFileSync(dockerFilePath, "utf8");
-    const updatedImageVersion = currentImageVersion.replace(
-      /:[^\s]+/g,
-      `:${newVersion}`
-    );
-    fs.writeFileSync(dockerFilePath, updatedImageVersion);
+    const stats = fs.statSync(path.join("apps", app));
+    if (stats.isDirectory()) {
+      const newVersion = `${require(`../apps/${app}/package.json`).version}`;
+      const dockerFilePath = path.join(
+        `apps/${app}/contrib/dokku`,
+        "Dockerfile"
+      );
+      const currentImageVersion = fs.readFileSync(dockerFilePath, "utf8");
+      const updatedImageVersion = currentImageVersion.replace(
+        /:[^\s]+/g,
+        `:${newVersion}`
+      );
+      fs.writeFileSync(dockerFilePath, updatedImageVersion);
+    }
   });
   await exec("pnpm format");
 })();
