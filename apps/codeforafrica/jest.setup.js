982348829924ca9409fadcc27d7d81a8500d40c8
esir <esirkings@gmail.com>
2022-09-06 10:36:19 +0300

    polyfill TextDecoder and TextDecoder since they're not available in jsdom

--- apps/codeforafrica/jest.setup.js
+++ apps/codeforafrica/jest.setup.js
@@ -1,1 +1,7 @@
+const util = require("util");
+
 module.exports = require("@commons-ui/testing-library/jest.setup");
+
+// TextDecoder and TextDecoder are not available in jsdom, so we need to polyfill them
+global.TextEncoder = util.TextEncoder;
+global.TextDecoder = util.TextDecoder;
