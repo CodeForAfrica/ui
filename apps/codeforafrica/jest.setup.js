6f3dfc61f0115fb08cd4d6afd14f444a241dd6b8
esir <esirkings@gmail.com>
2022-09-06 10:48:56 +0300

    Only create global TextEncoder and TextDecoder when they don't exist

--- apps/codeforafrica/jest.setup.js
+++ apps/codeforafrica/jest.setup.js
@@ -1,7 +1,9 @@
-const util = require("util");
-
 module.exports = require("@commons-ui/testing-library/jest.setup");
 
-// TextDecoder and TextDecoder are not available in jsdom, so we need to polyfill them
-global.TextEncoder = util.TextEncoder;
-global.TextDecoder = util.TextDecoder;
+if (!global.TextEncoder || !global.TextDecoder) {
+  // TextDecoder and TextDecoder are not available in jsdom, so we need to polyfill them
+  // eslint-disable-next-line global-require
+  const { TextEncoder, TextDecoder } = require("util");
+  global.TextEncoder = TextEncoder;
+  global.TextDecoder = TextDecoder;
+}
