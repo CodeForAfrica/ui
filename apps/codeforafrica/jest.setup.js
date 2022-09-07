b866e87e74c5d964e6e01d15ef0ef595adf8ac91
esir <esirkings@gmail.com>
2022-09-07 09:52:04 +0300

    Dynamically load jsdom depending on if we are at the browser or server side

--- apps/codeforafrica/jest.setup.js
+++ apps/codeforafrica/jest.setup.js
@@ -1,9 +1,1 @@
 module.exports = require("@commons-ui/testing-library/jest.setup");
-
-if (!global.TextEncoder || !global.TextDecoder) {
-  // TextDecoder and TextDecoder are not available in jsdom, so we need to polyfill them
-  // eslint-disable-next-line global-require
-  const { TextEncoder, TextDecoder } = require("util");
-  global.TextEncoder = TextEncoder;
-  global.TextDecoder = TextDecoder;
-}
--- apps/codeforafrica/src/lib/marked.js
+++ apps/codeforafrica/src/lib/marked.js
@@ -1,15 +1,23 @@
+/* eslint-env browser */
 import DOMPurify from "dompurify";
-import { JSDOM } from "jsdom";
 import { marked as realMarked } from "marked";
 
-const { window } = new JSDOM("");
-const purify = DOMPurify(window);
+let global;
+if (typeof window !== "undefined") {
+  global = window;
+} else {
+  // lib/marked will only be used server-side hence require is fine
+  // eslint-disable-next-line global-require
+  const { JSDOM } = require("jsdom");
+  global = new JSDOM("").window;
+}
 
+const purify = DOMPurify(global);
 function marked(src, opt, callback) {
   return purify.sanitize(realMarked(src, opt, callback));
 }
 
-marked.parseInline = function (src, opt) {
+marked.parseInline = function parseInline(src, opt) {
   return purify.sanitize(realMarked.parseInline(src, opt));
 };
 
