504f28c405c70ab66a2951e2504db51d072065c4
_ Kilemensi <clemence@codeforafrica.org>
2022-09-08 11:02:54 +0300

    Handle slug for file-based collections
    Update get-in-touch as test case

--- apps/codeforafrica/src/lib/api.netlify-cms/getGetInTouch.js
+++ apps/codeforafrica/src/lib/api.netlify-cms/getGetInTouch.js
@@ -1,9 +1,6 @@
 import { getCollectionBySlug } from "./utils";
 
 export default function getGetInTouch() {
-  const getInTouch = getCollectionBySlug("content/pages", "about", [
-    "get-in-touch",
-  ]).items["get-in-touch"];
-
-  return { ...getInTouch, slug: "get-in-touch" };
+  return getCollectionBySlug("content/pages", "about", ["get-in-touch", "slug"])
+    .items["get-in-touch"];
 }
