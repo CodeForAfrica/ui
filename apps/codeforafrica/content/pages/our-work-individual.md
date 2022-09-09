1bdb570c52d7a795ddfbcf957222a613f48d72cc
_ Kilemensi <clemence@codeforafrica.org>
2022-09-09 11:24:06 +0300

    Add related project config to our-work-individual

--- apps/codeforafrica/content/pages/our-work-individual.md
+++ apps/codeforafrica/content/pages/our-work-individual.md
@@ -1,4 +1,6 @@
 ---
+related-projects:
+  title: Explore other projects
 seo:
   title-template: "%s | Our Work | Code for Africa"
 ---
--- apps/codeforafrica/src/pages/api/admin/config.js
+++ apps/codeforafrica/src/pages/api/admin/config.js
@@ -695,6 +695,18 @@ module.exports = {
           file: `${APP_DIRECTORY}content/pages/our-work-individual.md`,
           fields: [
             {
+              label: "Related Projects",
+              name: "related-projects",
+              widget: "object",
+              fields: [
+                {
+                  label: "Title",
+                  name: "title",
+                  widget: "string",
+                },
+              ],
+            },
+            {
               ...seoFields,
             },
           ],
