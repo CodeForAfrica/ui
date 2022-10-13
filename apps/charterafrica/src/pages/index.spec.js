06ca40f62b0dd76e0bc8a6fac23fd6ea3efa7241
_ Kilemensi <clemence@codeforafrica.org>
2022-10-13 11:38:56 +0300

    Fix expected title

--- apps/charterafrica/src/pages/index.spec.js
+++ apps/charterafrica/src/pages/index.spec.js
@@ -3,5 +3,5 @@ const { test, expect } = require("@playwright/test");
 test("/", async ({ page }) => {
   await page.goto("/");
   const title = await page.title();
-  expect(title).toBe("Charter.AFRICA");
+  expect(title).toBe("charter.AFRICA");
 });
