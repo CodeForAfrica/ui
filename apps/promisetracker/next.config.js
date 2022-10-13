ec3ee6afd2e0dcb93e0fdd2385bbadbfd2ce2b0b
_ Kilemensi <clemence@codeforafrica.org>
2022-10-13 11:01:03 +0300

    images.unoptimized is no longer experimental

--- apps/promisetracker/next.config.js
+++ apps/promisetracker/next.config.js
@@ -16,11 +16,6 @@ const outputFileTracingRoot = PROJECT_ROOT
 const moduleExports = withTM({
   experimental: {
     outputFileTracingRoot,
-    images: {
-      unoptimized:
-        process.env.NEXT_PUBLIC_IMAGE_UNOPTIMIZED?.trim()?.toLowerCase() ===
-        "true",
-    },
   },
   i18n: {
     locales: process.env.NEXT_PUBLIC_I18N_LOCALES?.split(",")
@@ -32,6 +27,9 @@ const moduleExports = withTM({
     domains: process.env.NEXT_PUBLIC_IMAGE_DOMAINS?.split(",")
       ?.map((d) => d.trim())
       ?.filter((d) => d),
+    unoptimized:
+      process.env.NEXT_PUBLIC_IMAGE_UNOPTIMIZED?.trim()?.toLowerCase() ===
+      "true",
   },
   output: "standalone",
   pageExtensions: ["page.js"],
