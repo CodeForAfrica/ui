067b38a05707ff086fd70d8522aaf76b4b258063
_ Kilemensi <clemence@codeforafrica.org>
2022-10-20 17:24:13 +0300

    Jest 29 seem to require plugin-proporal-private-methods

--- packages/jest-config-commons-ui/next.js
+++ packages/jest-config-commons-ui/next.js
@@ -8,6 +8,12 @@ module.exports = {
   transform: {
     // Use babel-jest to transpile tests with the next/babel preset
     // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
-    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+    "^.+\\.(js|jsx|ts|tsx)$": [
+      "babel-jest",
+      {
+        presets: ["next/babel"],
+        plugins: ["@babel/plugin-proposal-private-methods"],
+      },
+    ],
   },
 };
