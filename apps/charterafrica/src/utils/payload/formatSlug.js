c8a4aadd2535563b846bb2b3fa410dd33906945b
_ Kilemensi <clemence@codeforafrica.org>
2022-10-20 16:23:29 +0300

    Add initial payload config

---
+++ apps/charterafrica/src/utils/payload/formatSlug.js
@@ -0,0 +1,23 @@
+const format = (val) =>
+  val
+    .replace(/ /g, "-")
+    .replace(/[^\w-/]+/g, "")
+    .toLowerCase();
+
+const formatSlug =
+  (fallback) =>
+  ({ value, originalDoc, data }) => {
+    if (typeof value === "string") {
+      return format(value);
+    }
+    const fallbackData =
+      (data && data[fallback]) || (originalDoc && originalDoc[fallback]);
+
+    if (fallbackData && typeof fallbackData === "string") {
+      return format(fallbackData);
+    }
+
+    return value;
+  };
+
+export default formatSlug;
