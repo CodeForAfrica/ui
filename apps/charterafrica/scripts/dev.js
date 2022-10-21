27c6353c8ef981fc006f1334b8af5164c97bab48
_ Kilemensi <clemence@codeforafrica.org>
2022-10-21 13:40:28 +0300

    Tell eslint @babel/register is only needed in dev

--- apps/charterafrica/scripts/dev.js
+++ apps/charterafrica/scripts/dev.js
@@ -1,3 +1,5 @@
+// @babel/register is only needed in dev
+// eslint-disable-next-line import/no-extraneous-dependencies
 require("@babel/register")({
   presets: ["next/babel"],
   extensions: [".js", ".jsx"],
