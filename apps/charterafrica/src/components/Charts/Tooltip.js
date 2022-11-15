47a8137749de29c004e7a40c5cf09efa63c19f74
_ Kilemensi <clemence@codeforafrica.org>
2022-11-15 17:53:34 +0300

    Implement custom tooltip that matches design

---
+++ apps/charterafrica/src/components/Charts/Tooltip.js
@@ -0,0 +1,33 @@
+import Box from "@mui/material/Box";
+import React from "react";
+
+import LineClampedRichTypography from "@/charterafrica/components/LineClampedRichTypography";
+
+function Tooltip({ label, value }) {
+  return (
+    <Box
+      bgcolor="common.white"
+      borderRadius="10px"
+      gap="5px"
+      px={2.5}
+      py="29.77px"
+      width={221}
+    >
+      <LineClampedRichTypography
+        color="neutral.dark"
+        lineClamp={1}
+        variant="numberSmall"
+      >
+        {value}
+      </LineClampedRichTypography>
+      <LineClampedRichTypography
+        color="neutral.dark"
+        lineClamp={1}
+        variant="captionCap"
+      >
+        {label}
+      </LineClampedRichTypography>
+    </Box>
+  );
+}
+export default Tooltip;
