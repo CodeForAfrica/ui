40b1059279096c2d4156012dc34cfca4876a6b0c
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2022-11-18 18:05:24 +0300

    Regenerated snapshots

--- apps/charterafrica/src/components/DesktopNavBar/DesktopNavBar.snap.js
+++ apps/charterafrica/src/components/DesktopNavBar/DesktopNavBar.snap.js
@@ -30,53 +30,6 @@ exports[`<DesktopNavBar /> renders unchanged 1`] = `
       class="MuiGrid-root MuiGrid-container css-1o93lmx-MuiGrid-root"
     >
       <div
-        class="MuiFormControl-root MuiTextField-root css-swoirf-MuiFormControl-root-MuiTextField-root"
-      >
-        <div
-          class="MuiInputBase-root MuiOutlinedInput-root MuiInputBase-colorPrimary MuiInputBase-formControl MuiInputBase-sizeSmall MuiInputBase-adornedEnd css-l9o6hu-MuiInputBase-root-MuiOutlinedInput-root"
-        >
-          <input
-            aria-invalid="false"
-            class="MuiInputBase-input MuiOutlinedInput-input MuiInputBase-inputTypeSearch MuiInputBase-inputSizeSmall MuiInputBase-inputAdornedEnd css-vvf66v-MuiInputBase-input-MuiOutlinedInput-input"
-            id=":r0:"
-            placeholder="Search"
-            type="search"
-            value=""
-          />
-          <button
-            aria-label="search"
-            class="MuiButtonBase-root MuiIconButton-root MuiIconButton-edgeEnd MuiIconButton-sizeMedium css-1yq5fb3-MuiButtonBase-root-MuiIconButton-root"
-            tabindex="0"
-            type="button"
-          >
-            <div
-              aria-hidden="true"
-              class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-iusnk6-MuiSvgIcon-root"
-              focusable="false"
-              viewbox="0 0 16 16"
-            />
-          </button>
-          <fieldset
-            aria-hidden="true"
-            class="MuiOutlinedInput-notchedOutline css-1d3z3hw-MuiOutlinedInput-notchedOutline"
-          >
-            <legend
-              class="css-ihdtdm"
-            >
-              <span
-                class="notranslate"
-              >
-                ​
-              </span>
-            </legend>
-          </fieldset>
-        </div>
-      </div>
-    </div>
-    <div
-      class="MuiGrid-root MuiGrid-item css-13i4rnv-MuiGrid-root"
-    >
-      <div
         class="MuiGrid-root MuiGrid-item css-13i4rnv-MuiGrid-root"
       >
         <ul
@@ -149,6 +102,53 @@ exports[`<DesktopNavBar /> renders unchanged 1`] = `
             About
           </a>
         </ul>
+      </div>
+      <div
+        class="MuiGrid-root MuiGrid-item css-13i4rnv-MuiGrid-root"
+      >
+        <div
+          class="MuiFormControl-root MuiTextField-root css-swoirf-MuiFormControl-root-MuiTextField-root"
+        >
+          <div
+            class="MuiInputBase-root MuiOutlinedInput-root MuiInputBase-colorPrimary MuiInputBase-formControl MuiInputBase-sizeSmall MuiInputBase-adornedEnd css-l9o6hu-MuiInputBase-root-MuiOutlinedInput-root"
+          >
+            <input
+              aria-invalid="false"
+              class="MuiInputBase-input MuiOutlinedInput-input MuiInputBase-inputTypeSearch MuiInputBase-inputSizeSmall MuiInputBase-inputAdornedEnd css-vvf66v-MuiInputBase-input-MuiOutlinedInput-input"
+              id=":r0:"
+              placeholder="Search"
+              type="search"
+              value=""
+            />
+            <button
+              aria-label="search"
+              class="MuiButtonBase-root MuiIconButton-root MuiIconButton-edgeEnd MuiIconButton-sizeMedium css-1yq5fb3-MuiButtonBase-root-MuiIconButton-root"
+              tabindex="0"
+              type="button"
+            >
+              <div
+                aria-hidden="true"
+                class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-iusnk6-MuiSvgIcon-root"
+                focusable="false"
+                viewbox="0 0 16 16"
+              />
+            </button>
+            <fieldset
+              aria-hidden="true"
+              class="MuiOutlinedInput-notchedOutline css-1d3z3hw-MuiOutlinedInput-notchedOutline"
+            >
+              <legend
+                class="css-ihdtdm"
+              >
+                <span
+                  class="notranslate"
+                >
+                  ​
+                </span>
+              </legend>
+            </fieldset>
+          </div>
+        </div>
       </div>
       <div
         class="MuiGrid-root MuiGrid-item css-13i4rnv-MuiGrid-root"
