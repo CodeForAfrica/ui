e1b79bdddd612e1702e4e687e5de2dee0618c280
kilemensi <kilemensi@gmail.com>
2022-12-05 14:52:50 +0300

    Include translations and enable submenus

--- apps/charterafrica/src/payload/globals/Navigation.js
+++ apps/charterafrica/src/payload/globals/Navigation.js
@@ -1,23 +1,44 @@
-/* eslint-disable import/prefer-default-export */
 import link from "../fields/link";
 
 const linkField = link();
 linkField.fields.push({
-  name: "children",
-  type: "array",
-  fields: [linkField],
+  type: "row",
+  fields: [
+    {
+      name: "children",
+      type: "array",
+      labels: {
+        singular: {
+          en: "Submenu",
+          fr: "Sous-menu",
+        },
+        plural: {
+          en: "Submenus",
+          fr: "Sous-menus",
+        },
+      },
+      fields: [link()],
+    },
+  ],
 });
 
-export const Navigation = {
+const Navigation = {
   slug: "navigation",
+  label: {
+    en: "Navigation",
+    fr: "La navigation",
+    pt: "Navegação",
+  },
   access: {
     read: () => true,
   },
   fields: [
     {
-      name: "navItems",
+      name: "menus",
       type: "array",
-      fields: [link()],
+      fields: [linkField],
     },
   ],
 };
+
+export default Navigation;
