b807f081333ce92094e50113ccad1f5326b73407
_ Kilemensi <clemence@codeforafrica.org>
2022-12-09 08:12:04 +0300

    Show LanguageButton on DesktopNavBar

--- apps/charterafrica/src/components/DesktopNavBar/DesktopNavBar.js
+++ apps/charterafrica/src/components/DesktopNavBar/DesktopNavBar.js
@@ -2,6 +2,7 @@ import { Grid, Button } from "@mui/material";
 import React from "react";
 
 import { neutral, secondary } from "@/charterafrica/colors";
+import LanguageButton from "@/charterafrica/components/LanguageButton";
 import Logo from "@/charterafrica/components/Logo";
 import NavBarNavList from "@/charterafrica/components/NavBarNavList";
 
@@ -11,25 +12,44 @@ const DesktopNavBar = React.forwardRef(function DesktopNavBar(props, ref) {
   return (
     <Grid justifyContent="space-between" alignItems="center" ref={ref} sx={sx}>
       <Grid item>
-        <Logo {...logo} width={230} height={58} />
-      </Grid>
-      <Grid container justifyContent="flex-end" columnSpacing={3.75}>
-        <Grid item>
-          <NavBarNavList direction="row" menus={menus} sx={{ gap: 2.5 }} />
+        <Grid
+          container
+          alignItems="center"
+          justifyContent="flex-start"
+          wrap="nowrap"
+        >
+          <Grid item>
+            <Logo {...logo} width={230} height={58} />
+          </Grid>
+          <Grid item>
+            <LanguageButton />
+          </Grid>
         </Grid>
-        <Grid item>
-          <Button
-            variant="contained"
-            sx={{
-              backgroundColor: secondary[500],
-              color: neutral[900],
-              "&:hover": {
-                backgroundColor: secondary[600],
-              },
-            }}
-          >
-            Join
-          </Button>
+      </Grid>
+      <Grid item>
+        <Grid
+          container
+          justifyContent="flex-end"
+          columnSpacing={3.75}
+          wrap="nowrap"
+        >
+          <Grid item>
+            <NavBarNavList direction="row" menus={menus} sx={{ gap: 0 }} />
+          </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              sx={{
+                backgroundColor: secondary[500],
+                color: neutral[900],
+                "&:hover": {
+                  backgroundColor: secondary[600],
+                },
+              }}
+            >
+              Join
+            </Button>
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
