724947ec41bdbd92a9693f010ddee7c3c3755bb0
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2022-12-09 11:33:17 +0300

    Updated Failing snapshots

--- apps/charterafrica/src/components/Layout/Layout.snap.js
+++ apps/charterafrica/src/components/Layout/Layout.snap.js
@@ -151,47 +151,41 @@ exports[`<Layout /> renders unchanged 1`] = `
               class="MuiGrid-root MuiGrid-item css-13i4rnv-MuiGrid-root"
             >
               <div
-                class="MuiFormControl-root MuiTextField-root css-86cj57-MuiFormControl-root-MuiTextField-root"
+                class="MuiInputBase-root MuiOutlinedInput-root MuiInputBase-colorPrimary MuiInputBase-sizeSmall MuiInputBase-adornedEnd css-56jcky-MuiInputBase-root-MuiOutlinedInput-root"
               >
-                <div
-                  class="MuiInputBase-root MuiOutlinedInput-root MuiInputBase-colorPrimary MuiInputBase-formControl MuiInputBase-sizeSmall MuiInputBase-adornedEnd css-l9o6hu-MuiInputBase-root-MuiOutlinedInput-root"
+                <input
+                  class="MuiInputBase-input MuiOutlinedInput-input MuiInputBase-inputTypeSearch MuiInputBase-inputSizeSmall MuiInputBase-inputAdornedEnd css-1grzdsv-MuiInputBase-input-MuiOutlinedInput-input"
+                  placeholder="Search"
+                  type="search"
+                  value=""
+                />
+                <button
+                  aria-label="search"
+                  class="MuiButtonBase-root MuiIconButton-root MuiIconButton-edgeEnd MuiIconButton-sizeMedium css-1yq5fb3-MuiButtonBase-root-MuiIconButton-root"
+                  tabindex="0"
+                  type="button"
                 >
-                  <input
-                    aria-invalid="false"
-                    class="MuiInputBase-input MuiOutlinedInput-input MuiInputBase-inputTypeSearch MuiInputBase-inputSizeSmall MuiInputBase-inputAdornedEnd css-vvf66v-MuiInputBase-input-MuiOutlinedInput-input"
-                    id=":r0:"
-                    placeholder="Search"
-                    type="search"
-                    value=""
-                  />
-                  <button
-                    aria-label="search"
-                    class="MuiButtonBase-root MuiIconButton-root MuiIconButton-edgeEnd MuiIconButton-sizeMedium css-1yq5fb3-MuiButtonBase-root-MuiIconButton-root"
-                    tabindex="0"
-                    type="button"
-                  >
-                    <div
-                      aria-hidden="true"
-                      class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-iusnk6-MuiSvgIcon-root"
-                      focusable="false"
-                      viewbox="0 0 16 16"
-                    />
-                  </button>
-                  <fieldset
+                  <div
                     aria-hidden="true"
-                    class="MuiOutlinedInput-notchedOutline css-1d3z3hw-MuiOutlinedInput-notchedOutline"
+                    class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-iusnk6-MuiSvgIcon-root"
+                    focusable="false"
+                    viewbox="0 0 16 16"
+                  />
+                </button>
+                <fieldset
+                  aria-hidden="true"
+                  class="MuiOutlinedInput-notchedOutline css-1d3z3hw-MuiOutlinedInput-notchedOutline"
+                >
+                  <legend
+                    class="css-ihdtdm"
                   >
-                    <legend
-                      class="css-ihdtdm"
+                    <span
+                      class="notranslate"
                     >
-                      <span
-                        class="notranslate"
-                      >
-                        ​
-                      </span>
-                    </legend>
-                  </fieldset>
-                </div>
+                      ​
+                    </span>
+                  </legend>
+                </fieldset>
               </div>
             </div>
             <div
