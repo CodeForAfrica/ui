a54860d7a286f6f62a51ddaadadf23d9d65f3249
_ Kilemensi <clemence@codeforafrica.org>
2022-12-13 17:08:47 +0300

    Add languages to globals

--- apps/charterafrica/src/payload/globals/Site.js
+++ apps/charterafrica/src/payload/globals/Site.js
@@ -1,3 +1,7 @@
+import { array } from "payload/dist/fields/validations";
+
+import { locales } from "../utils/locales";
+
 const Site = {
   slug: "site",
   access: {
@@ -21,20 +25,54 @@ const Site = {
       ],
     },
     {
-      name: "language",
-      type: "group",
+      name: "languages",
+      label: {
+        en: "Languages",
+        ft: "Langues",
+        pt: "Idiomas",
+      },
+      type: "array",
       fields: [
         {
-          name: "title",
+          name: "locale",
+          label: {
+            en: "Locale",
+            pt: "Localidade",
+          },
+          type: "select",
+          options: locales,
+          unique: true,
+          required: true,
+          admin: {
+            isClearable: false,
+            isSortable: true,
+          },
+          validate: (val, options) => {
+            const { data, t } = options || {};
+            if (data?.language?.filter((l) => l.locale === val)?.length > 1) {
+              return t("charterafrica.site:uniqueLocales");
+            }
+            return array(val, options);
+          },
+        },
+        {
+          name: "label",
+          label: {
+            en: "Label",
+            pt: "Rótulo",
+          },
           type: "text",
           required: true,
         },
-        {
-          name: "description",
-          type: "textarea",
-          required: true,
-        },
       ],
+      admin: {
+        initCollapsed: true,
+        components: {
+          RowLabel: ({ data }) => {
+            return data?.label || data?.locale || data?.id;
+          },
+        },
+      },
     },
   ],
 };
