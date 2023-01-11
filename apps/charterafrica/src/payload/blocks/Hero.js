a84bc1979391de34ac8b013486be2823aa22fe95
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-01-11 13:38:25 +0300

    Complete Hero CMS Data

--- apps/charterafrica/src/payload/blocks/Hero.js
+++ apps/charterafrica/src/payload/blocks/Hero.js
@@ -11,28 +11,96 @@ const Hero = {
   fields: [
     {
       name: "title",
-      type: "text",
+      type: "group",
       required: true,
-    },
-    {
-      name: "subtitle",
-      type: "text",
-      required: true,
-    },
-    {
-      name: "buttons",
-      type: "array",
-      minRows: 1,
       fields: [
         {
-          name: "label",
+          name: "color",
           type: "text",
           required: true,
         },
         {
-          name: "link",
+          name: "content",
           type: "text",
           required: true,
+        },
+      ],
+    },
+    {
+      name: "subheading",
+      type: "group",
+      required: true,
+      fields: [
+        {
+          name: "color",
+          type: "text",
+          required: true,
+        },
+        {
+          name: "content",
+          type: "text",
+          required: true,
+        },
+      ],
+    },
+    {
+      name: "background",
+      type: "group",
+      required: true,
+      fields: [
+        {
+          name: "src",
+          label: "Background Image",
+          type: "relationship",
+          relationTo: "media",
+          hasMany: false,
+          required: true,
+        },
+        {
+          name: "blendMode",
+          type: "text",
+          required: true,
+        },
+        {
+          name: "color",
+          type: "text",
+          required: true,
+        },
+      ],
+    },
+    {
+      name: "links",
+      type: "array",
+      minRows: 1,
+      fields: [
+        {
+          name: "button",
+          type: "group",
+          required: true,
+          fields: [
+            {
+              name: "color",
+              type: "text",
+              required: true,
+            },
+            {
+              name: "content",
+              type: "text",
+              required: true,
+            },
+            {
+              name: "icon",
+              type: "relationship",
+              relationTo: "media",
+              hasMany: false,
+              required: true,
+            },
+            {
+              name: "link",
+              type: "text", // TODO: change to relationship to link
+              required: true,
+            },
+          ],
         },
       ],
     },
