945630bb2f8aad0b7f91c99f8f0904a90efbafaa
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-01-11 14:50:17 +0300

    Working Spotlight CMS Data

---
+++ apps/charterafrica/src/payload/blocks/Spotlight.js
@@ -0,0 +1,58 @@
+const Spotlight = {
+  slug: "spotlight",
+  access: {
+    read: () => true,
+  },
+  admin: {
+    group: "Blocks",
+    useAsTitle: "category",
+  },
+  fields: [
+    {
+      name: "category",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "item",
+      type: "group",
+      required: true,
+      fields: [
+        {
+          name: "title",
+          type: "text",
+          required: true,
+        },
+        {
+          name: "src",
+          label: "Image",
+          type: "relationship",
+          relationTo: "media",
+          required: true,
+        },
+        {
+          name: "topic",
+          type: "text",
+          required: true,
+        },
+        {
+          name: "excerpt",
+          type: "text",
+          required: true,
+        },
+        {
+          name: "date",
+          type: "date",
+          required: true,
+        },
+        {
+          name: "link",
+          type: "text", // TODO: change to relationship to links
+          required: true,
+        },
+      ],
+    },
+  ],
+};
+
+export default Spotlight;
