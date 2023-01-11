401f725f39ca9acc5ab2d51d1ec0e3aa3da7a940
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-01-11 15:52:40 +0300

    Added Translations

--- apps/charterafrica/src/payload/blocks/Spotlight.js
+++ apps/charterafrica/src/payload/blocks/Spotlight.js
@@ -11,21 +11,40 @@ const Spotlight = {
     {
       name: "category",
       type: "text",
+      label: {
+        en: "Category",
+        fr: "Catégorie",
+        pt: "Categoria",
+      },
       required: true,
     },
     {
       name: "item",
       type: "group",
+      label: {
+        en: "Item",
+        fr: "Objet",
+        pt: "Item",
+      },
       required: true,
       fields: [
         {
           name: "title",
           type: "text",
+          label: {
+            en: "Title",
+            fr: "Titre",
+            pt: "Título",
+          },
           required: true,
         },
         {
           name: "image",
-          label: "Image",
+          label: {
+            en: "Image",
+            fr: "Image",
+            pt: "Imagem",
+          },
           type: "relationship",
           relationTo: "media",
           required: true,
@@ -33,21 +52,41 @@ const Spotlight = {
         {
           name: "topic",
           type: "text",
+          label: {
+            en: "Topic",
+            fr: "Sujet",
+            pt: "Tópico",
+          },
           required: true,
         },
         {
           name: "excerpt",
           type: "text",
+          label: {
+            en: "Excerpt",
+            fr: "Extrait",
+            pt: "Excerto",
+          },
           required: true,
         },
         {
           name: "date",
           type: "date",
+          label: {
+            en: "Date",
+            fr: "Date",
+            pt: "Encontro",
+          },
           required: true,
         },
         {
           name: "link",
           type: "text", // TODO: change to relationship to links
+          label: {
+            en: "Link",
+            fr: "Lien",
+            pt: "Ligação",
+          },
           required: true,
         },
       ],
