f70a10703588990411d0bbbc5a1050643bf0b9fa
_ Kilemensi <clemence@codeforafrica.org>
2023-01-16 14:30:20 +0300

    Add NavList for mobile screens

---
+++ apps/charterafrica/src/components/NavBarNavList/NavBarNavList.js
@@ -0,0 +1,105 @@
+import { Link } from "@commons-ui/next";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListSubheader from "@mui/material/ListSubheader";
+import { useRouter } from "next/router";
+import React from "react";
+
+import { neutral } from "@/charterafrica/colors";
+
+const NavBarNavMenu = React.forwardRef(function NavBarNavMenu(props, ref) {
+  const { menus, sx } = props;
+  const router = useRouter();
+  const { pathname } = router;
+
+  if (!menus?.length) {
+    return null;
+  }
+  return (
+    <Box
+      color="text.secondary"
+      display="flex"
+      flex={1}
+      flexDirection="column"
+      sx={{
+        rowGap: "20px",
+        ...sx,
+      }}
+      ref={ref}
+    >
+      {menus.map((menu) => (
+        <List
+          component="nav"
+          subheader={
+            <ListSubheader
+              color="inherit"
+              component={Link}
+              disableSticky
+              href={menu.href || "#"}
+              underline="none"
+              variant="p3SemiBold"
+              sx={{
+                background: "inherit",
+                borderRadius: "5px",
+                display: "flex",
+                flex: 1,
+                p: 1.25,
+                typography: "p3SemiBold",
+              }}
+            >
+              {menu.label}
+            </ListSubheader>
+          }
+          sx={{
+            background: neutral[800],
+            borderRadius: "5px",
+            display: "flex",
+            flex: 1,
+            flexDirection: "column",
+            p: 0,
+          }}
+          key={menu.label}
+        >
+          {menu.children?.map((item) => (
+            <ListItemButton
+              color="inherit"
+              component={Link}
+              href={item.href || "#"}
+              underline="none"
+              variant="caption"
+              selected={item.href === pathname}
+              disableGutters
+              disablePadding
+              sx={{
+                borderBottom: 1,
+                borderColor: neutral[800],
+                background: neutral[50],
+                color: neutral[900],
+                p: 1.25,
+                typography: "caption",
+                "&.Mui-selected": {
+                  background: neutral[100],
+                },
+                "&:hover": {
+                  backgroundColor: neutral[200],
+                },
+                "&.Mui-selected:hover": {
+                  backgroundColor: neutral[200],
+                },
+                "&:last-of-type": {
+                  borderBottom: "none",
+                },
+              }}
+              key={item.label}
+            >
+              {item.label}
+            </ListItemButton>
+          )) || null}
+        </List>
+      ))}
+    </Box>
+  );
+});
+
+export default NavBarNavMenu;
---
+++ apps/charterafrica/src/components/NavBarNavList/NavBarNavList.snap.js
@@ -0,0 +1,127 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`<NavBarNavList /> renders unchanged 1`] = `
+<div>
+  <div
+    class="MuiBox-root css-12tykev"
+  >
+    <nav
+      class="MuiList-root MuiList-padding MuiList-subheader css-15o1hun-MuiList-root"
+    >
+      <a
+        class="MuiTypography-root MuiTypography-p3SemiBold MuiLink-root MuiLink-underlineNone MuiListSubheader-root MuiListSubheader-colorInherit MuiListSubheader-gutters css-8y40qm-MuiTypography-root-MuiLink-root-MuiListSubheader-root"
+        href="/resources"
+      >
+        Resources
+      </a>
+      <a
+        class="MuiTypography-root MuiTypography-caption MuiLink-root MuiLink-underlineNone MuiButtonBase-root MuiListItemButton-root MuiListItemButton-root css-yayf3y-MuiTypography-root-MuiLink-root-MuiButtonBase-root-MuiListItemButton-root"
+        href="/resources/tools"
+        tabindex="0"
+      >
+        Tools
+      </a>
+      <a
+        class="MuiTypography-root MuiTypography-caption MuiLink-root MuiLink-underlineNone MuiButtonBase-root MuiListItemButton-root MuiListItemButton-root css-yayf3y-MuiTypography-root-MuiLink-root-MuiButtonBase-root-MuiListItemButton-root"
+        href="/resources/data"
+        tabindex="0"
+      >
+        Data
+      </a>
+      <a
+        class="MuiTypography-root MuiTypography-caption MuiLink-root MuiLink-underlineNone MuiButtonBase-root MuiListItemButton-root MuiListItemButton-root css-yayf3y-MuiTypography-root-MuiLink-root-MuiButtonBase-root-MuiListItemButton-root"
+        href="/resources/people"
+        tabindex="0"
+      >
+        People
+      </a>
+    </nav>
+    <nav
+      class="MuiList-root MuiList-padding MuiList-subheader css-15o1hun-MuiList-root"
+    >
+      <a
+        class="MuiTypography-root MuiTypography-p3SemiBold MuiLink-root MuiLink-underlineNone MuiListSubheader-root MuiListSubheader-colorInherit MuiListSubheader-gutters css-8y40qm-MuiTypography-root-MuiLink-root-MuiListSubheader-root"
+        href="/knowledge"
+      >
+        Knowledge
+      </a>
+      <a
+        class="MuiTypography-root MuiTypography-caption MuiLink-root MuiLink-underlineNone MuiButtonBase-root MuiListItemButton-root MuiListItemButton-root css-yayf3y-MuiTypography-root-MuiLink-root-MuiButtonBase-root-MuiListItemButton-root"
+        href="/knowledge/explainers"
+        tabindex="0"
+      >
+        Explainers
+      </a>
+      <a
+        class="MuiTypography-root MuiTypography-caption MuiLink-root MuiLink-underlineNone MuiButtonBase-root MuiListItemButton-root MuiListItemButton-root css-yayf3y-MuiTypography-root-MuiLink-root-MuiButtonBase-root-MuiListItemButton-root"
+        href="/knowledge/news"
+        tabindex="0"
+      >
+        News
+      </a>
+      <a
+        class="MuiTypography-root MuiTypography-caption MuiLink-root MuiLink-underlineNone MuiButtonBase-root MuiListItemButton-root MuiListItemButton-root css-yayf3y-MuiTypography-root-MuiLink-root-MuiButtonBase-root-MuiListItemButton-root"
+        href="/knowledge/research"
+        tabindex="0"
+      >
+        Research
+      </a>
+      <a
+        class="MuiTypography-root MuiTypography-caption MuiLink-root MuiLink-underlineNone MuiButtonBase-root MuiListItemButton-root MuiListItemButton-root css-yayf3y-MuiTypography-root-MuiLink-root-MuiButtonBase-root-MuiListItemButton-root"
+        href="/knowledge/academy"
+        tabindex="0"
+      >
+        Academy
+      </a>
+    </nav>
+    <nav
+      class="MuiList-root MuiList-padding MuiList-subheader css-15o1hun-MuiList-root"
+    >
+      <a
+        class="MuiTypography-root MuiTypography-p3SemiBold MuiLink-root MuiLink-underlineNone MuiListSubheader-root MuiListSubheader-colorInherit MuiListSubheader-gutters css-8y40qm-MuiTypography-root-MuiLink-root-MuiListSubheader-root"
+        href="/opportunities"
+      >
+        Oppportunities
+      </a>
+      <a
+        class="MuiTypography-root MuiTypography-caption MuiLink-root MuiLink-underlineNone MuiButtonBase-root MuiListItemButton-root MuiListItemButton-root css-yayf3y-MuiTypography-root-MuiLink-root-MuiButtonBase-root-MuiListItemButton-root"
+        href="/opportunities/grants-and-fellowships"
+        tabindex="0"
+      >
+        Grants and Fellowships
+      </a>
+      <a
+        class="MuiTypography-root MuiTypography-caption MuiLink-root MuiLink-underlineNone MuiButtonBase-root MuiListItemButton-root MuiListItemButton-root css-yayf3y-MuiTypography-root-MuiLink-root-MuiButtonBase-root-MuiListItemButton-root"
+        href="/opportunities/events"
+        tabindex="0"
+      >
+        Events
+      </a>
+      <a
+        class="MuiTypography-root MuiTypography-caption MuiLink-root MuiLink-underlineNone MuiButtonBase-root MuiListItemButton-root MuiListItemButton-root css-yayf3y-MuiTypography-root-MuiLink-root-MuiButtonBase-root-MuiListItemButton-root"
+        href="/opportunities/community"
+        tabindex="0"
+      >
+        Community
+      </a>
+      <a
+        class="MuiTypography-root MuiTypography-caption MuiLink-root MuiLink-underlineNone MuiButtonBase-root MuiListItemButton-root MuiListItemButton-root css-yayf3y-MuiTypography-root-MuiLink-root-MuiButtonBase-root-MuiListItemButton-root"
+        href="/opportunities/democracy-helpdesk"
+        tabindex="0"
+      >
+        Democracy Helpdesk
+      </a>
+    </nav>
+    <nav
+      class="MuiList-root MuiList-padding MuiList-subheader css-15o1hun-MuiList-root"
+    >
+      <a
+        class="MuiTypography-root MuiTypography-p3SemiBold MuiLink-root MuiLink-underlineNone MuiListSubheader-root MuiListSubheader-colorInherit MuiListSubheader-gutters css-8y40qm-MuiTypography-root-MuiLink-root-MuiListSubheader-root"
+        href="/about"
+      >
+        About
+      </a>
+    </nav>
+  </div>
+</div>
+`;
