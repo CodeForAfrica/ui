ebdb81bc467c48367dd400a0668ec84ed8fc4788
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-01-18 11:44:13 +0300

    Hard coded MOOC Content

--- apps/charterafrica/src/pages/index.page.js
+++ apps/charterafrica/src/pages/index.page.js
@@ -308,6 +308,21 @@ export async function getStaticProps({ defaultLocale, locale, locales }) {
           ],
         },
         {
+          slug: "mooc",
+          title: {
+            content:
+              "Learn how to use digital tools to <i>strengthen</i> your democracy",
+            color: "common.white",
+          },
+          link: {
+            content: "Learn more",
+            color: "primary",
+          },
+          image: {
+            src: "/images/mooc.png",
+          },
+        },
+        {
           slug: "helpdesk",
           description: `
           <p>
