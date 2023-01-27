f544d86a12f16408e054fdff014bea190b8197e9
Kevin Koech <koechkevin92@gmail.com>
2023-01-27 10:44:06 +0300

    Update explainers snapshot

--- apps/charterafrica/src/components/Layout/Layout.snap.js
+++ apps/charterafrica/src/components/Layout/Layout.snap.js
@@ -29,6 +29,7 @@ exports[`<Layout /> renders unchanged 1`] = `
                 height="38"
                 loading="lazy"
                 src="/images/charter-logo.svg"
+                srcset="/images/charter-logo.svg 1x, /images/charter-logo.svg 2x"
                 style="color: transparent;"
                 width="147.29"
               />
@@ -76,6 +77,7 @@ exports[`<Layout /> renders unchanged 1`] = `
                     height="58"
                     loading="lazy"
                     src="/images/charter-logo.svg"
+                    srcset="/images/charter-logo.svg 1x, /images/charter-logo.svg 2x"
                     style="color: transparent;"
                     width="230"
                   />
