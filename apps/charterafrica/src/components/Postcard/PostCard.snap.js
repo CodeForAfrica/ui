3f7719047ea5843c3cc2f676cefa8c201b31101e
Kevin Koech <koechkevin92@gmail.com>
2023-02-01 16:59:54 +0300

    @/charterafrica/components/PostCard

---
+++ apps/charterafrica/src/components/Postcard/PostCard.snap.js
@@ -0,0 +1,32 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`<Popper /> renders unchanged 1`] = `
+<div>
+  <div
+    class="MuiPaper-root MuiPaper-elevation MuiPaper-rounded MuiPaper-elevation1 MuiCard-root css-uuhpmr-MuiPaper-root-MuiCard-root"
+  >
+    <div
+      class="MuiCardMedia-root css-muxve-MuiCardMedia-root"
+    />
+    <div
+      class="MuiCardContent-root css-46bh2p-MuiCardContent-root"
+    >
+      <div
+        class="MuiTypography-root MuiTypography-h5SemiBold css-rrvc0f-MuiTypography-root"
+      >
+        Some title
+      </div>
+      <div
+        class="MuiTypography-root MuiTypography-p1 css-m0rejx-MuiTypography-root"
+      >
+        Sakwa
+      </div>
+      <div
+        class="MuiTypography-root MuiTypography-p1 css-1x6jy03-MuiTypography-root"
+      >
+        2023-09-01
+      </div>
+    </div>
+  </div>
+</div>
+`;
---
+++ apps/charterafrica/src/components/Postcard/PostCard.test.js
@@ -0,0 +1,22 @@
+import { createRender } from "@commons-ui/testing-library";
+import React from "react";
+
+import PostCard from "./PostCard";
+
+import theme from "@/charterafrica/theme";
+
+// eslint-disable-next-line testing-library/render-result-naming-convention
+const render = createRender({ theme });
+
+const defaultProps = {
+  title: "Some title",
+  author: "Sakwa",
+  date: "2023-09-01",
+};
+
+describe("<PostCard />", () => {
+  it("renders unchanged", () => {
+    const { container } = render(<PostCard {...defaultProps} />);
+    expect(container).toMatchSnapshot();
+  });
+});
