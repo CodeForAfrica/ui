588e0f79751146a6ebe0ef51ae2e94589077814e
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-02-02 16:16:13 +0300

    Fixed layout

--- apps/charterafrica/src/components/FeaturedPostCard/FeaturedPostCard.js
+++ apps/charterafrica/src/components/FeaturedPostCard/FeaturedPostCard.js
@@ -1,7 +1,8 @@
-import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { Section } from "@commons-ui/core";
+import { Card, CardContent, CardMedia, Typography, Box } from "@mui/material";
 import React from "react";
 
-import { neutral } from "@/charterafrica/colors";
+import { neutral, secondary } from "@/charterafrica/colors";
 import LineClampedRichTypography from "@/charterafrica/components/LineClampedRichTypography";
 import RichText from "@/charterafrica/components/RichText";
 
@@ -9,71 +10,73 @@ const FeaturedPostCard = React.forwardRef(function FeaturedPostCard(
   props,
   ref
 ) {
-  const { title, date, excerpt, image } = props;
+  const { title, date, excerpt, image, sx } = props;
 
   return (
-    <Card
-      sx={{
-        display: "flex",
-        boxShadow: "none",
-      }}
-      ref={ref}
-    >
-      <CardMedia
-        component="img"
-        image={image.url}
-        alt={image.alt}
-        sx={{ width: "820px", height: "487px" }}
-      />
-      <CardContent
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-between",
-        }}
-      >
-        <LineClampedRichTypography
-          color={neutral[900]}
-          html={false}
-          mb={{ xs: 2.5, md: 5 }}
-          textAlign="left"
-          typography="h2SemiBold"
-        >
-          {title}
-        </LineClampedRichTypography>
-        <LineClampedRichTypography
-          color={neutral[500]}
-          html={false}
-          mb={{ xs: 2.5, md: 5 }}
-          textAlign="left"
-          typography="p1"
-        >
-          {new Date(date).toLocaleDateString("en-US", {
-            year: "numeric",
-            month: "short",
-            day: "numeric",
-          })}
-        </LineClampedRichTypography>
-
-        <RichText
-          component="h1"
-          color={neutral[900]}
-          typography="subheading"
-          textAlign="left"
-          elements={excerpt}
-        />
-        <Typography
-          color={neutral[900]}
-          typography="p3"
+    <Box sx={{ backgroundColor: secondary[50], ...sx }} ref={ref}>
+      <Section sx={{ px: { xs: 5, sm: 0 }, py: { xs: 5, md: "74.5px" } }}>
+        <Card
           sx={{
-            cursor: "pointer",
-            textDecoration: "underline",
+            display: { xs: "none", md: "flex" },
+            boxShadow: "none",
+            backgroundColor: secondary[50],
           }}
         >
-          Read More
-        </Typography>
-      </CardContent>
-    </Card>
+          <CardMedia
+            component="img"
+            image={image.url}
+            alt={image.alt}
+            sx={{ width: "820px", height: "487px" }}
+          />
+          <CardContent
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+            }}
+          >
+            <LineClampedRichTypography
+              color={neutral[900]}
+              html={false}
+              textAlign="left"
+              typography="h2SemiBold"
+            >
+              {title}
+            </LineClampedRichTypography>
+            <LineClampedRichTypography
+              color={neutral[500]}
+              html={false}
+              textAlign="left"
+              typography="p1"
+            >
+              {new Date(date).toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "short",
+                day: "numeric",
+              })}
+            </LineClampedRichTypography>
+
+            <RichText
+              component="h1"
+              color={neutral[900]}
+              typography="subheading"
+              textAlign="left"
+              elements={excerpt}
+            />
+            <Typography
+              color={neutral[900]}
+              typography="p3"
+              sx={{
+                cursor: "pointer",
+                textDecoration: "underline",
+              }}
+            >
+              Read More
+            </Typography>
+          </CardContent>
+        </Card>
+      </Section>
+    </Box>
   );
 });
 
