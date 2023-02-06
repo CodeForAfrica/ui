6f3c3f4aa0afa1e706381e88b72175a31472b3a6
_ Kilemensi <clemence@codeforafrica.org>
2023-02-06 12:28:44 +0300

    Fix spacing in md

--- apps/charterafrica/src/components/Articles/Articles.js
+++ apps/charterafrica/src/components/Articles/Articles.js
@@ -1,17 +1,25 @@
 import { Section } from "@commons-ui/core";
 import { Box, Grid } from "@mui/material";
 import PropTypes from "prop-types";
-import { forwardRef } from "react";
-
-import PostCard from "../Postcard";
+import React from "react";
 
 import { secondary } from "@/charterafrica/colors";
+import PostCard from "@/charterafrica/components/PostCard";
 
-const Articles = forwardRef((props, ref) => {
+const Articles = React.forwardRef((props, ref) => {
   const { sx, articles } = props;
   return (
     <Box bgcolor={secondary[50]} sx={sx} ref={ref}>
-      <Section sx={{ px: { xs: "57.5px", sm: 0 } }}>
+      <Section
+        sx={{
+          px: {
+            xs: "57.5px",
+            // (theme.contentWidths.values.sm - 590) / 2
+            sm: "69px",
+            md: 0,
+          },
+        }}
+      >
         <Grid container rowSpacing={5} columnSpacing={{ xs: 5, md: 2.5 }}>
           {articles?.map((item) => (
             <Grid key={item.id} item xs={12} sm={6} md={4}>
@@ -35,6 +43,7 @@ Articles.propTypes = {
     })
   ),
 };
+
 Articles.defaultProps = {
   articles: undefined,
 };
--- apps/charterafrica/src/components/Articles/Articles.snap.js
+++ apps/charterafrica/src/components/Articles/Articles.snap.js
@@ -6,7 +6,7 @@ exports[`<Articles /> renders unchanged 1`] = `
     class="MuiBox-root css-156ejva"
   >
     <div
-      class="MuiContainer-root MuiContainer-maxWidthLg MuiContainer-fixed MuiContainer-disableGutters css-xfsymw-MuiContainer-root"
+      class="MuiContainer-root MuiContainer-maxWidthLg MuiContainer-fixed MuiContainer-disableGutters css-sethvq-MuiContainer-root"
     >
       <div
         class="MuiGrid-root MuiGrid-container css-1girwym-MuiGrid-root"
