47ed78f72205c7f3a0747fc2309d37feff7d7214
Kevin Koech <koechkevin92@gmail.com>
2023-02-07 15:34:45 +0300

    @/charterafrica/pages/ErrorPage #319

---
+++ apps/charterafrica/src/components/ErrorPage/ErrorPage.snap.js
@@ -0,0 +1,45 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`<ErrorPage /> renders unchanged 1`] = `
+<div>
+  <div
+    class="MuiBox-root css-18fh7zv"
+  >
+    <div
+      class="MuiContainer-root MuiContainer-maxWidthLg MuiContainer-fixed MuiContainer-disableGutters css-cifu55-MuiContainer-root"
+    >
+      <div
+        class="MuiTypography-root MuiTypography-h1 css-1o8nli7-MuiTypography-root"
+      >
+        404
+      </div>
+      <div
+        class="MuiTypography-root MuiTypography-h3 css-375gn-MuiTypography-root"
+      >
+        Explainers
+      </div>
+      <p
+        class="MuiTypography-root MuiTypography-p3 css-pnto23-MuiTypography-root"
+      >
+        Description Text
+      </p>
+      <div
+        class="MuiTypography-root MuiTypography-p1 css-n2hmnu-MuiTypography-root"
+      >
+        <a
+          class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineNone active css-1xt2njg-MuiTypography-root-MuiLink-root"
+          href=""
+        >
+          <button
+            class="MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedSecondary MuiButton-sizeMedium MuiButton-containedSizeMedium MuiButton-root MuiButton-contained MuiButton-containedSecondary MuiButton-sizeMedium MuiButton-containedSizeMedium css-u67a7t-MuiButtonBase-root-MuiButton-root"
+            tabindex="0"
+            type="button"
+          >
+            NOT FOUND
+          </button>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+`;
