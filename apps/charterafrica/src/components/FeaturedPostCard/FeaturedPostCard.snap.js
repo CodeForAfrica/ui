8637c6defcfa3ecad23ee9fbc8b2a7b94b3535f3
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-02-07 10:16:54 +0300

    Fixed failing tests

--- apps/charterafrica/src/components/FeaturedPostCard/FeaturedPostCard.snap.js
+++ apps/charterafrica/src/components/FeaturedPostCard/FeaturedPostCard.snap.js
@@ -13,32 +13,36 @@ exports[`FeaturedPostCard should render 1`] = `
       >
         <img
           alt="Featured Post"
-          class="MuiCardMedia-root MuiCardMedia-media MuiCardMedia-img css-1baaqnk-MuiCardMedia-root"
+          class="MuiCardMedia-root MuiCardMedia-media MuiCardMedia-img css-1mfohn3-MuiCardMedia-root"
           src="/images/featured_post.png"
         />
         <div
-          class="MuiCardContent-root css-wsecsz-MuiCardContent-root"
+          class="MuiCardContent-root css-1xqh41x-MuiCardContent-root"
         >
-          <p
-            class="MuiTypography-root MuiTypography-p1 css-g39x1j-MuiTypography-root"
+          <h2
+            class="MuiTypography-root MuiTypography-h2SemiBold css-1geltqj-MuiTypography-root"
           >
             News Story title goes here and spans over second line
-          </p>
+          </h2>
           <p
             class="MuiTypography-root MuiTypography-p1 css-1euz2oy-MuiTypography-root"
           >
             Oct 10, 2020
           </p>
-          <h1
-            class="MuiTypography-root MuiTypography-p1 css-1eglp57-MuiTypography-root"
+          <p
+            class="MuiTypography-root MuiTypography-subheading css-1d4ifcx-MuiTypography-root"
           >
             Lorem ipsum dolor sit amet consectetur adipiscing elit tempus nibh cursus, urna porta sagittis non eget taciti nunc sed felis dui, praesent ullamcorper facilisi euismod ut in platea laoreet integer. Lorem ipsum dolor sit amet consectetur
-          </h1>
-          <p
-            class="MuiTypography-root MuiTypography-p1 css-15efwg4-MuiTypography-root"
-          >
-            Read More
           </p>
+          <div
+            class="MuiCardActions-root MuiCardActions-spacing css-1t6e9jv-MuiCardActions-root"
+          >
+            <p
+              class="MuiTypography-root MuiTypography-p1 css-15efwg4-MuiTypography-root"
+            >
+              Read More
+            </p>
+          </div>
         </div>
       </div>
     </div>
