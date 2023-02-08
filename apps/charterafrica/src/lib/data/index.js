8b6095d41570d037e97792c102edb5f82cebd283
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-02-08 14:00:33 +0300

    Pull Reseach data from CMS

--- apps/charterafrica/src/lib/data/index.js
+++ apps/charterafrica/src/lib/data/index.js
@@ -74,32 +74,25 @@ async function processPageNews({ blocks }) {
 }
 
 async function processPageResearch({ blocks }) {
-  // TODO(kilemensi): Pull data from CMS
-  blocks.push({
-    slug: "research",
-    title: "Research",
-    articles: Array.from({ length: 30 }, (_, i) => ({
-      id: i,
-      title: "Research title goes here and spans over second line. "
-        .repeat((i % 2) + 1)
-        .trim(),
-      author: "Sakwa G",
-      date: "2023-02-11",
-      image: {
-        id: "63d2622aafe25f6469605eae",
-        alt: `Research ${i}`,
-        prefix: "media",
-        filename: "Rectangle 113.jpg",
-        mimeType: "image/jpg",
-        filesize: 257010,
-        width: 1236,
-        height: 696,
-        createdAt: "2023-01-26T11:21:14.868Z",
-        updatedAt: "2023-01-26T11:21:14.868Z",
-        url: "http://localhost:3000/images/Rectangle 113.jpg",
-      },
-    })),
-  });
+  const { docs: articles } = await payload.getCollection("research");
+
+  const featuredArticle = articles.find(
+    (article) => article.featured === "True"
+  );
+  const featuredArticleIndex = articles.indexOf(featuredArticle);
+
+  blocks.push(
+    {
+      slug: "featured-post",
+      title: "Featured Post",
+      ...featuredArticle,
+    },
+    {
+      slug: "research",
+      title: "Research",
+      articles: articles.splice(featuredArticleIndex, 1),
+    }
+  );
 }
 
 async function processPageSpecificBlocks(page) {
