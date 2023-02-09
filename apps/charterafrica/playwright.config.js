bf4b6e72e6c2a35cae014ceefb26ae47b0e5ab5b
kilemensi <kilemensi@gmail.com>
2023-02-09 14:22:21 +0300

    Re-commit mistakenly deleted file

---
+++ apps/charterafrica/playwright.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const defaultConfig = require("playwright-config-commons-ui");
+
+const { use, webServer } = defaultConfig;
+const PORT = 3003;
+const config = {
+  ...defaultConfig,
+  use: {
+    ...use,
+    baseURL: `http://localhost:${PORT}/`,
+  },
+  webServer: {
+    ...webServer,
+    command: `npm run start -- --port ${PORT}`,
+    port: PORT,
+  },
+};
+
+module.exports = config;
