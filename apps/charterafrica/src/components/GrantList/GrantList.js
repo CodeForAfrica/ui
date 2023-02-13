75bab4d0be4394d8cb61dd581094aa5ee04ca13e
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-02-13 09:44:25 +0300

    Basic grants page layout

--- apps/charterafrica/src/components/GrantList/GrantList.js
+++ apps/charterafrica/src/components/GrantList/GrantList.js
@@ -1,22 +1,52 @@
+import Link from "@/commons-ui/next/Link";
+import { Typography } from "@mui/material";
 import React from "react";
 
 import OpportunityCard from "../OpportunityCard";
 
+import { neutral } from "@/charterafrica/colors";
+
 const GrantsList = React.forwardRef(function GrantsList(props) {
-  const { grants } = props;
+  const { title, grants } = props;
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        flexWrap: "nowrap",
-        justifyContent: "space-between",
-      }}
-    >
-      {grants.slice(0, 4).map((grant) => {
-        return <OpportunityCard opportunity={grant} key={grant.id} />;
-      })}
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <Typography variant="h5SemiBold" color={neutral[900]}>
+          {title}
+        </Typography>
+        <Link
+          href="/grants"
+          underline="always"
+          sx={{
+            color: neutral[900],
+            variant: "p3SemiBold",
+          }}
+        >
+          View All
+        </Link>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "nowrap",
+          justifyContent: "space-between",
+        }}
+      >
+        {grants.slice(0, 4).map((grant) => {
+          return (
+            <div>
+              <OpportunityCard opportunity={grant} key={grant.id} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 });
