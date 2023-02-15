06b3b4d558af9736bb60c189800e4e10b9ccc028
kilemensi <kilemensi@gmail.com>
2023-02-15 08:36:16 +0300

    Delegate all payload rest data processing to common

--- apps/charterafrica/src/lib/data/index.js
+++ apps/charterafrica/src/lib/data/index.js
@@ -1,2 +1,7 @@
+// eslint-disable-next-line import/prefer-default-export
 export { getPageServerSideProps } from "./local";
-export { getPageStaticProps } from "./rest";
+
+// TODO(kilemensi): export getPageStaticProps from "./rest"
+//                  Ideally we would also export the above method here but
+//                  since we use it only when local payload is not available,
+//                  the two method can not co-exist in the same file yet.
--- apps/charterafrica/src/lib/data/rest/index.js
+++ apps/charterafrica/src/lib/data/rest/index.js
@@ -1,117 +1,77 @@
-import fetchApi from "./fetchApi";
+import fetchJson from "./fetchJson";
+
+import { getPageProps } from "@/charterafrica/lib/data/common";
 
 const BASE_URL = process.env.PAYLOAD_PUBLIC_APP_URL;
-export const fetchGlobal = async (collection, { defaultLocale, locale }) => {
-  const params = { locale, "fallback-locale": defaultLocale };
-  const res = await fetchApi.get(`${BASE_URL}/api/globals/${collection}`, {
+
+const SUPPORTED_OPTIONS = {
+  locale: "locale",
+  fallbackLocale: "fallback-locale",
+  where: "where",
+};
+
+function optionsToParams(options) {
+  const params = Object.keys(SUPPORTED_OPTIONS).reduce((acc, curr) => {
+    const optionValue = options?.[curr];
+    if (optionValue) {
+      acc[SUPPORTED_OPTIONS[curr]] = optionValue;
+    }
+    return acc;
+  }, {});
+  return params;
+}
+
+const findGlobal = async (slug, options) => {
+  const params = optionsToParams(options);
+  return fetchJson.get(`${BASE_URL}/api/globals/${slug}`, {
     params,
   });
-  return res;
 };
 
-export const fetchCollection = async (
-  collection,
-  { defaultLocale, locale, ...rest }
-) => {
-  const params = { locale, "fallback-locale": defaultLocale, ...rest };
-  const res = await fetchApi.get(
-    `${process.env.PAYLOAD_PUBLIC_APP_URL}/api/${collection}`,
-    { params }
-  );
-  return res;
+const findCollection = async (slug, options) => {
+  const params = optionsToParams(options);
+  return fetchJson.get(`${BASE_URL}/api/${slug}`, {
+    params,
+  });
 };
 
-export const fetchPage = async (slug, args) => {
-  const params = {
-    ...args,
+const findPage = async (slug, options) => {
+  const pageOptions = {
+    ...options,
     where: {
+      ...options?.where,
       slug: {
         equals: slug,
       },
     },
   };
-  const res = await fetchCollection("pages", params);
-  return res;
+  return findCollection("pages", pageOptions);
 };
 
-export async function fetchGlobalProps({ locale, defaultLocale }) {
-  const settings = await fetchGlobal("settings", {
-    locale,
+export const api = {
+  findCollection,
+  findGlobal,
+  findPage,
+};
+
+export async function getPageStaticProps({
+  defaultLocale,
+  slug,
+  resolvedUrl,
+  locale,
+  locales,
+}) {
+  const props = await getPageProps(slug, api, {
     defaultLocale,
-  });
-  const { languages } = settings;
-  const { actions, menus } = await fetchGlobal("navigation", {
     locale,
-    defaultLocale,
-  });
-  const navbar = {
-    actions,
-    languages: languages ?? null,
-    logo: {
-      alt: "Charter Africa",
-      src: "/images/charter-logo.svg",
-      href: "/",
-      priority: true,
-    },
-    menus: menus ?? null,
-  };
-  const footer = await fetchGlobal("footer", {
-    locale,
-    defaultLocale,
+    locales,
+    pathname: resolvedUrl,
   });
 
-  return { footer, navbar, settings };
+  if (!props) {
+    return { notFound: true };
+  }
+  return {
+    props,
+  };
 }
-
-export const process404Page = (docs) => {
-  if (!docs.length) {
-    throw new Error("Not found");
-  }
-  const [{ blocks, slug }] = docs;
-  if (!blocks.length) {
-    throw new Error("Not found");
-  }
-  const data = blocks.find(({ statusCode }) => statusCode === 404);
-  return {
-    slug,
-    link: {},
-    ...data,
-  };
-};
-// TODO Cannot process in common because of payload import
-export const processErrorPage = (docs, code) => {
-  if (code === 404) {
-    return process404Page(docs);
-  }
-  if (!docs.length) {
-    return { notFound: true };
-  }
-  const [{ blocks, slug }] = docs;
-  if (!blocks.length) {
-    return { notFound: true };
-  }
-  const data = blocks.find(({ statusCode }) => statusCode === code);
-  if (!data) {
-    return { notFound: true };
-  }
-  return {
-    slug,
-    link: {},
-    ...data,
-  };
-};
-
-export const getPageStaticProps = async ({ query, ...args }) => {
-  const { slug, statusCode } = query;
-  const { docs } = await fetchPage(slug, args);
-  const blocks = [];
-  if (slug === "error") {
-    const { notFound, ...block } = processErrorPage(docs, statusCode);
-    if (notFound) {
-      return { notFound };
-    }
-    blocks.push(block);
-  }
-  const { footer, navbar } = await fetchGlobalProps(args);
-  return { props: { footer, navbar, blocks } };
-};
