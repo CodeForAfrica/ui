cd083dd8d4db856685383782f54ff71bdff4c426
kilemensi <kilemensi@gmail.com>
2023-02-16 09:14:48 +0300

    Implement a Global selection block
    This block will help select where exactly in a page one of the globals should appear rather than the location being hard-coded.

---
+++ apps/charterafrica/src/payload/blocks/Global.js
@@ -0,0 +1,30 @@
+const Hero = {
+  slug: "global",
+  fields: [
+    {
+      name: "block",
+      label: {
+        en: "Block",
+        fr: "Bloc",
+        pt: "Bloco",
+      },
+      type: "select",
+      options: [
+        {
+          value: "helpdesk",
+          label: "Democracy Helpdesk",
+        },
+        {
+          value: "focal-countries",
+          label: "Focal Countries",
+        },
+      ],
+      required: true,
+      admin: {
+        isClearable: true,
+      },
+    },
+  ],
+};
+
+export default Hero;
