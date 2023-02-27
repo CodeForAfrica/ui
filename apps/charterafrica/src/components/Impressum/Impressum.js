a50c6464e99ece3a44968f419d5a0f18b11ae0b8
kilemensi <kilemensi@gmail.com>
2023-02-27 14:10:12 +0300

    Finish Impressum component implementation

--- apps/charterafrica/src/components/Impressum/Impressum.js
+++ apps/charterafrica/src/components/Impressum/Impressum.js
@@ -1,21 +1,61 @@
 import { Section } from "@commons-ui/core";
+import { Figure } from "@commons-ui/next";
 import { Box, Grid } from "@mui/material";
 import React from "react";
 
+import { secondary } from "@/charterafrica/colors";
 import RichText from "@/charterafrica/components/RichText";
 
 const Impressum = React.forwardRef(function Impressum(props, ref) {
-  const { content, sx } = props;
+  const { content, image, sx } = props;
 
   if (!content) {
     return null;
   }
   return (
-    <Box bgcolor="secondary.light" sx={sx} ref={ref}>
-      <Section sx={{ px: { xs: 5, sm: 0 }, py: { xs: 5, md: "74.5px" } }}>
-        <Grid container>
-          <Grid item xs={12} order={{ xs: 0, md: 1 }} />
-          <Grid item xs={12} order={{ xs: 1, md: 0 }}>
+    <Box bgcolor={secondary[50]} sx={sx} ref={ref}>
+      <Section
+        sx={{
+          px: { xs: 5, sm: 0 },
+          py: { xs: 5, md: "74.5px" },
+        }}
+      >
+        <Grid container justifyContent="space-between" spacing={5}>
+          {image?.url?.length > 0 ? (
+            <Grid item xs={12} md="auto" order={{ xs: 0, md: 1 }}>
+              <Figure
+                sx={{
+                  alignItems: "center",
+                  backgroundColor: "common.white",
+                  boxShadow: "px 6px 12px rgba(0, 0, 0, 0.1)",
+                  borderRadius: 5,
+                  display: "flex",
+                  justifyContent: "center",
+                  maxWidth: { xs: "100%", md: "270px" },
+                  minHeight: { xs: "auto", sm: "390px", md: "187px" },
+                  overflow: "hidden",
+                  objectFit: {
+                    xs: "fill",
+                    md: "contain",
+                  },
+                }}
+                ImageProps={{
+                  alt: image.alt,
+                  fill: false,
+                  height: 0,
+                  priority: true,
+                  src: image.url,
+                  style: {
+                    height: "auto",
+                    objectFit: "inherit",
+                    width: "100%",
+                  },
+                  width: 0,
+                }}
+              />
+            </Grid>
+          ) : null}
+          <Grid item xs={12} md order={{ xs: 1, md: 0 }}>
             <Box
               color="neutral.dark"
               sx={(theme) => ({
--- apps/charterafrica/src/components/Impressum/Impressum.test.js
+++ apps/charterafrica/src/components/Impressum/Impressum.test.js
@@ -9,41 +9,63 @@ import theme from "@/charterafrica/theme";
 const render = createRender({ theme });
 
 const defaultProps = {
-  variant: "image",
-  title: [
+  content: [
     {
       children: [
         {
-          text: "The largest digital database for ",
+          text: "European Partnership for Democracy (EPD)\nRue Froissart 123-133\n1040 Brussels, Belgium",
           children: null,
         },
+      ],
+    },
+    {
+      children: [
         {
-          text: "African",
-          bold: true,
+          text: "",
           children: null,
         },
+      ],
+    },
+    {
+      children: [
         {
-          text: " communites",
+          text: "EPD is registered in Belgium (0648.708.779).",
+          children: null,
+        },
+      ],
+    },
+    {
+      children: [
+        {
+          text: "",
+          children: null,
+        },
+      ],
+    },
+    {
+      children: [
+        {
+          text: "You can contact EPD via email (info@epd.eu) or by phone (+32 2 733 2282).",
           children: null,
         },
       ],
     },
   ],
-  link: {
-    linkType: "internal",
-    href: null,
+  image: {
+    id: "63f9da37de53f4d08657fdee",
+    alt: "S",
+    prefix: "media",
+    filename: "3a134a6ee61bdaf42fd06b59f46cf9fe-1.png",
+    mimeType: "image/png",
+    filesize: 42224,
+    width: 731,
+    height: 287,
+    createdAt: "2023-02-25T09:51:51.611Z",
+    updatedAt: "2023-02-25T09:51:51.611Z",
+    url: "http://localhost:3000/media/3a134a6ee61bdaf42fd06b59f46cf9fe-1.png",
   },
-  media: {
-    image: {
-      alt: "About",
-      width: 1140,
-      height: 760,
-      url: "/media/s.jpg",
-    },
-    align: "0",
-  },
-  id: "63eb6164c33ade83d9875ee3",
-  slug: "page-header",
+  id: "63f85813073246f4d1629a78",
+  slug: "impressum",
 };
 
 describe("<Impressum />", () => {
