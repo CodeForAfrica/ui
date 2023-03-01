8e4d38c991ed0d230bf36e519d58a665dba8fc39
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-03-01 12:46:47 +0300

    Handle undefined CSRF and CORS
    Signed-off-by: Kipruto <43873157+kelvinkipruto@users.noreply.github.com>

--- apps/charterafrica/payload.config.ts
+++ apps/charterafrica/payload.config.ts
@@ -38,11 +38,11 @@ const appURL = process.env.PAYLOAD_PUBLIC_APP_URL;
 
 const cors = process?.env?.PAYLOAD_CORS?.split(",")
   ?.map((d) => d.trim())
-  ?.filter(Boolean);
+  ?.filter(Boolean) ?? [];
 
 const csrf = process?.env?.PAYLOAD_CSRF?.split(",")
   ?.map((d) => d.trim())
-  ?.filter(Boolean);
+  ?.filter(Boolean) ?? [];
 
 const adapter = s3Adapter({
   config: {
