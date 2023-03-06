7db094ecb871632837e82130fc60a09d2cd1be4b
kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-03-06 16:03:28 +0300

    Removed unused store values

--- apps/charterafrica/src/store/articlesStore.js
+++ apps/charterafrica/src/store/articlesStore.js
@@ -3,23 +3,11 @@ import { create } from "zustand";
 const useArticlesStore = create((set) => ({
   articles: [],
   tags: [],
-  filterTag: "",
-  filteredArticles: [],
 
   setArticles: (articles) => set({ articles, filteredArticles: articles }),
   setTags: (tags) => set({ tags }),
-  setFilterTag: (filterTag) =>
-    set((state) => ({
-      filterTag,
-      filteredArticles: state.articles.filter((a) =>
-        a.tags.map((t) => t.name).includes(filterTag)
-      ),
-    })),
-
   getAll: () => useArticlesStore.getState(),
-
-  reset: () =>
-    set({ articles: [], tags: [], filterTag: "", filteredArticles: [] }),
+  reset: () => set({ articles: [], tags: [] }),
 }));
 
 export default useArticlesStore;
