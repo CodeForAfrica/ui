32f9fc35e5309812f439d32dca0a9c8009da3d2e
kilemensi <kilemensi@gmail.com>
2023-03-07 20:28:39 +0300

    Add Opportunities block to CMS

---
+++ apps/charterafrica/src/payload/blocks/Opportunities.js
@@ -0,0 +1,88 @@
+const Opportunities = {
+  slug: "opportunities",
+  labels: {
+    singular: {
+      en: "Opportunities",
+      fr: "Opportunités",
+      pt: "Oportunidades",
+    },
+    plural: {
+      en: "Opportunities",
+      fr: "Opportunités",
+      pt: "Oportunidades",
+    },
+  },
+  fields: [
+    {
+      name: "title",
+      type: "text",
+      label: {
+        en: "Title",
+        fr: "Titre",
+        pt: "Título",
+      },
+      localized: true,
+    },
+    {
+      name: "featured",
+      label: {
+        en: "Featured",
+        pt: "Destaque",
+        fr: "Qualité",
+      },
+      type: "relationship",
+      relationTo: ["events", "fellowships", "grants"],
+    },
+    {
+      name: "items",
+      type: "array",
+      fields: [
+        {
+          name: "collection",
+          label: {
+            en: "Item",
+          },
+          type: "select",
+          options: [
+            {
+              label: "Events",
+              value: "events",
+            },
+            {
+              label: "Fellowships",
+              value: "fellowships",
+            },
+            {
+              label: "Grants",
+              value: "grants",
+            },
+          ],
+          required: true,
+          admin: {
+            isClearable: true,
+          },
+        },
+        {
+          name: "label",
+          type: "text",
+          label: {
+            en: "Label",
+            pt: "Rótulo",
+          },
+          required: true,
+          localized: true,
+        },
+      ],
+      admin: {
+        initCollapsed: true,
+        components: {
+          RowLabel: ({ data }) => {
+            return data?.label || data?.id;
+          },
+        },
+      },
+    },
+  ],
+};
+
+export default Opportunities;
