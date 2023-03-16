f43ccb2b9f7a0f98f3c4b7adc3861e58698b4aaa
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-03-16 14:12:51 +0300

    regenerate snapshots
    Signed-off-by: Kipruto <43873157+kelvinkipruto@users.noreply.github.com>

--- apps/charterafrica/src/components/EmbeddedDocumentViewer/EmbeddedDocumentViewer.snap.js
+++ apps/charterafrica/src/components/EmbeddedDocumentViewer/EmbeddedDocumentViewer.snap.js
@@ -1,3 +1,3 @@
 // Jest Snapshot v1, https://goo.gl/fbAQLP
 
-exports[`<SingleDocumentation /> renders unchanged 1`] = `<div />`;
+exports[`<EmbeddedDocumentViewer /> renders unchanged 1`] = `<div />`;
