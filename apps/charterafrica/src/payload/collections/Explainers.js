b71433b02780d4e407d22def0f2334c9bf315eac
kilemensi <kilemensi@gmail.com>
2023-03-17 10:55:24 +0300

    Add title toggle per explainer

--- apps/charterafrica/src/payload/collections/Explainers.js
+++ apps/charterafrica/src/payload/collections/Explainers.js
@@ -11,14 +11,30 @@ const Explainers = {
   },
   fields: [
     {
-      name: "title",
-      label: {
-        en: "Title",
-        fr: "Titre",
-        pt: "Título",
-      },
-      type: "text",
-      localized: true,
+      type: "collapsible",
+      label: "Title",
+      fields: [
+        {
+          name: "title",
+          label: {
+            en: "Title",
+            fr: "Titre",
+            pt: "Título",
+          },
+          type: "text",
+          required: true,
+          localized: true,
+        },
+        {
+          name: "showTitle",
+          label: {
+            en: "Show on the website",
+          },
+          type: "checkbox",
+          defaultValue: false,
+          required: true,
+        },
+      ],
     },
     richText({
       name: "description",
