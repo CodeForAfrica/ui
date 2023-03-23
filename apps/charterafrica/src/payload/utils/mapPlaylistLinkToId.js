546759334fbd6f56492f8a346e6aa78c4eb727f8
Kevin Koech <koechkevin92@gmail.com>
2023-03-23 17:36:54 +0300

    Update URL

--- apps/charterafrica/src/payload/utils/mapPlaylistLinkToId.js
+++ apps/charterafrica/src/payload/utils/mapPlaylistLinkToId.js
@@ -1,16 +1,14 @@
 export const mapPlaylistLinkToId = ({ siblingData }) => {
   const { playlistLink } = siblingData;
-  const searchParams = playlistLink?.split("?")?.[1];
-  const playlistId = new URLSearchParams(searchParams).get("list");
+  const url = new URL(playlistLink);
+  const playlistId = url.searchParams.get("list");
   return playlistId;
 };
 
 export const mapVideoUrlToId = ({ siblingData }) => {
   const { videoLink } = siblingData;
-
   const sharedId = videoLink.split("https://youtu.be/")?.[1];
-  const searchParams = videoLink?.split("?")?.[1];
-  const videoId = new URLSearchParams(searchParams).get("v");
-
+  const url = new URL(videoLink);
+  const videoId = url.searchParams.get("v");
   return sharedId || videoId || "";
 };
