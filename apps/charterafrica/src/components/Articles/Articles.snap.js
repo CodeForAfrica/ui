1230a332fba25e6cced60a0518e310df9343afb9
kilemensi <kilemensi@gmail.com>
2023-03-24 14:55:33 +0300

    Update test snapshots

--- apps/charterafrica/src/components/Articles/Articles.snap.js
+++ apps/charterafrica/src/components/Articles/Articles.snap.js
@@ -3,7 +3,7 @@
 exports[`<Articles /> renders unchanged 1`] = `
 <div>
   <div
-    class="MuiBox-root css-1105iuu"
+    class="MuiBox-root css-1oqbd6t"
     id="articles"
   >
     <div
@@ -13,7 +13,7 @@ exports[`<Articles /> renders unchanged 1`] = `
         class="MuiContainer-root MuiContainer-maxWidthLg MuiContainer-fixed MuiContainer-disableGutters css-dygcit-MuiContainer-root"
       >
         <div
-          class="MuiGrid-root MuiGrid-container MuiGrid-wrap-xs-nowrap css-ztj35b-MuiGrid-root"
+          class="MuiGrid-root MuiGrid-container MuiGrid-wrap-xs-nowrap css-1bnx1lo-MuiGrid-root"
         >
           <div
             class="MuiGrid-root MuiGrid-item MuiGrid-grid-md-auto css-11ydr13-MuiGrid-root"
@@ -32,7 +32,7 @@ exports[`<Articles /> renders unchanged 1`] = `
                 class="MuiInputBase-input MuiOutlinedInput-input MuiInputBase-inputTypeSearch MuiInputBase-inputSizeSmall MuiInputBase-inputAdornedEnd css-1u6lhwd-MuiInputBase-input-MuiOutlinedInput-input"
                 placeholder="Search"
                 type="search"
-                value=""
+                value=" "
               />
               <button
                 aria-label="search"
