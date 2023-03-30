af9de8d105242a8900a2bf6e4a85a42afab23d4d
kilemensi <kilemensi@gmail.com>
2023-03-30 12:05:18 +0300

    Update to closely match designs

--- apps/charterafrica/src/components/Consultations/ConsultationList.js
+++ apps/charterafrica/src/components/Consultations/ConsultationList.js
@@ -11,41 +11,50 @@ import formatDateTime from "@/charterafrica/utils/formatDate";
 
 const ConsultationList = forwardRef(function ConsultationList(props, ref) {
   const { items, config, isFeatured, sx, title } = props;
+
   if (!items?.length) {
     return null;
   }
-
   return (
-    <Box
-      bgcolor={isFeatured ? neutral[50] : "common.white"}
-      sx={{ p: 6.25, ...sx }}
-      ref={ref}
-    >
-      <LineClampedRichTypography
-        color="neutral.dark"
-        lineClamp={1}
-        textAlign="center"
-        variant="h2"
-        sx={{ py: 6.25 }}
+    <Box bgcolor={isFeatured ? neutral[50] : "common.white"} sx={sx} ref={ref}>
+      <Section
+        sx={{
+          maxWidth: { md: "640px", lg: "1080px", xl: "1080px" },
+          px: { xs: 5, sm: 0 },
+          py: { xs: 6.25, md: 12.5 },
+        }}
       >
-        {title}
-      </LineClampedRichTypography>
-      <Section>
+        <LineClampedRichTypography
+          color="neutral.dark"
+          textAlign="center"
+          variant="h2"
+          sx={{ mb: 6.25 }}
+        >
+          {title}
+        </LineClampedRichTypography>
         {items.map((consultation) => {
           const { description, id } = consultation;
           return (
-            <Box sx={{ pb: 6.25 }} key={id}>
+            <Box
+              sx={{
+                pb: 6.25,
+                "&:last-of-type": {
+                  pb: 0,
+                },
+              }}
+              key={id}
+            >
               <LineClampedRichTypography
                 color="neutral.dark"
                 fontWeight={{ xs: 400, md: 400 }}
-                lineClamp={1}
-                variant="h3"
+                variant="h3SemiBold"
+                sx={{ mb: 1.25 }}
               >
                 {consultation.title}
               </LineClampedRichTypography>
               <LineClampedRichTypography
                 lineClamp={1}
-                sx={{ pb: 1.25 }}
+                sx={{ mb: 4 }}
                 variant="p3"
               >
                 {config.airedOnText}{" "}
--- apps/charterafrica/src/components/Consultations/Consultations.snap.js
+++ apps/charterafrica/src/components/Consultations/Consultations.snap.js
@@ -4,6 +4,16 @@ exports[`<Consultations /> renders unchanged 1`] = `
 <div>
   <div
     class="MuiBox-root css-1k9ek97"
-  />
+  >
+    <div
+      class="MuiContainer-root MuiContainer-maxWidthLg MuiContainer-fixed MuiContainer-disableGutters css-27tpcc-MuiContainer-root"
+    >
+      <div
+        class="MuiTypography-root MuiTypography-h2 css-j49p8a-MuiTypography-root"
+      >
+        Sample tiltle
+      </div>
+    </div>
+  </div>
 </div>
 `;
