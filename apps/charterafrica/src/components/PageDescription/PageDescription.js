a95d8cd011a0c29e206bfaf727896dfbc9406a6c
kilemensi <kilemensi@gmail.com>
2023-04-12 12:27:14 +0300

    Use LongFormRichText so styling is consistent across site

--- apps/charterafrica/src/components/PageDescription/PageDescription.js
+++ apps/charterafrica/src/components/PageDescription/PageDescription.js
@@ -2,7 +2,7 @@ import { Section } from "@commons-ui/core";
 import { Box } from "@mui/material";
 import React from "react";
 
-import RichText from "@/charterafrica/components/RichText";
+import LongFormRichText from "@/charterafrica/components/LongFormRichText";
 
 const PageDescription = React.forwardRef(function PageDescription(props, ref) {
   const { description, sx } = props;
@@ -13,43 +13,10 @@ const PageDescription = React.forwardRef(function PageDescription(props, ref) {
   return (
     <Box bgcolor="common.white" sx={sx} ref={ref}>
       <Section sx={{ px: { xs: 5, sm: 0 }, py: { xs: 5, md: 10 } }}>
-        <Box
+        <LongFormRichText
           color="neutral.dark"
-          sx={(theme) => ({
-            "& h1": {
-              ...theme.typography.h1Small,
-              mb: 3.75,
-              [theme.breakpoints.up("md")]: {
-                ...theme.typography.h1,
-              },
-            },
-            "& h2": {
-              mb: 2.5,
-              ...theme.typography.h2Small,
-              [theme.breakpoints.up("md")]: {
-                ...theme.typography.h2,
-              },
-            },
-            "& p": {
-              ...theme.typography.p1,
-              mb: 2,
-              [theme.breakpoints.up("md")]: {
-                ...theme.typography.subheading,
-              },
-            },
-            "& li": {
-              ...theme.typography.p1,
-              [theme.breakpoints.up("md")]: {
-                ...theme.typography.subheading,
-              },
-            },
-            "& p:last-of-type": {
-              mb: 0,
-            },
-          })}
-        >
-          <RichText color="neutral.dark" elements={description} />
-        </Box>
+          richTextBlockFields={{ content: description }}
+        />
       </Section>
     </Box>
   );
