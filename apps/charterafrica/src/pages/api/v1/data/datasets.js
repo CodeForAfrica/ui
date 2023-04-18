2b2e92c9f71e49c2ac2f81a8b0c8a6a4ba02bf20
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-04-18 13:15:06 +0300

    Fetch datasets Next API

---
+++ apps/charterafrica/src/pages/api/v1/data/datasets.js
@@ -0,0 +1,29 @@
+import { fetchDatasets } from "@/charterafrica/lib/openAfrica";
+
+export default async function handler(req, res) {
+  const {
+    query: { organization, tags = [], q = "" },
+  } = req;
+
+  if (organization) {
+    try {
+      const organizationQuery = organization
+        ? `organization:${organization}`
+        : "";
+      const tagsQuery = tags.map((tag) => `tags:${tag}`).join(" OR ");
+
+      const filterQuery = [organizationQuery, tagsQuery]
+        .filter(Boolean)
+        .join(" AND ");
+
+      const datasets = await fetchDatasets(q, {
+        fq: filterQuery,
+      });
+      res.status(200).json(datasets);
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  }
+
+  return res.json(404).json({ message: "NO ORGANIZATION PROVIDED" });
+}
