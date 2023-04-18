2087dd1cfe8ba77a4cdf03c27b0ca800763e7ccd
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-04-18 10:58:02 +0300

    Set Payload CMS
    Signed-off-by: Kipruto <43873157+kelvinkipruto@users.noreply.github.com>

---
+++ apps/charterafrica/src/payload/blocks/Datasets.js
@@ -0,0 +1,18 @@
+const Datasets = {
+  slug: "datasets",
+  fields: [
+    {
+      name: "organizationId",
+      label: {
+        en: "Organization ID",
+        fr: "ID de l'organisation",
+        pt: "ID da organização",
+      },
+      localized: true,
+      type: "text",
+      required: true,
+    },
+  ],
+};
+
+export default Datasets;
