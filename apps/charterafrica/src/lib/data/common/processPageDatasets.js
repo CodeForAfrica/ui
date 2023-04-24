1efed401d572ca52fb34fe925943765f08900d97
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-04-24 15:28:04 +0300

    Fixed fetch datasets props"

--- apps/charterafrica/src/lib/data/common/processPageDatasets.js
+++ apps/charterafrica/src/lib/data/common/processPageDatasets.js
@@ -1,4 +1,4 @@
-import { fetchDatasets } from "@/charterafrica/lib/openAfrica";
+import fetchDatasets from "@/charterafrica/lib/openAfrica";
 
 async function processPageDatasets(page) {
   const { blocks } = page;
@@ -6,9 +6,7 @@ async function processPageDatasets(page) {
   const datasetsIndex = blocks.findIndex(({ slug }) => slug === "datasets");
 
   if (datasetsIndex > -1) {
-    const { organizationId } = blocks[datasetsIndex];
     const datasets = await fetchDatasets({
-      fq: `organization:${organizationId}`,
       rows: 10,
       start: 0,
     });
@@ -16,7 +14,6 @@ async function processPageDatasets(page) {
     blocks[datasetsIndex] = {
       slug: "datasets",
       datasets,
-      fq: `organization:${organizationId}`,
     };
   }
 
