4531888806f85395f1a13986b5560636cb171e25
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-04-25 11:03:00 +0300

    Modularize datasets api

--- apps/charterafrica/src/pages/api/v1/data/[path].js
+++ apps/charterafrica/src/pages/api/v1/data/[path].js
@@ -1,6 +1,6 @@
 import fetchDatasets from "@/charterafrica/lib/openAfrica";
 
-export default async function handler(req, res) {
+const datasets = async (req, res) => {
   const {
     query: {
       page = 0,
@@ -12,17 +12,32 @@ export default async function handler(req, res) {
   } = req;
 
   try {
-    const datasets = await fetchDatasets({
+    const data = await fetchDatasets({
       q,
       rows: perPage,
       start: page,
       sort,
       tags,
     });
-    res.status(200).json(datasets);
+    res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error });
   }
 
   return res.status(404).json({ message: "ERROR FETCHING DATASETS" });
+};
+
+const sourceMap = {
+  datasets,
+};
+
+export default async function handler(req, res) {
+  const {
+    query: { path },
+  } = req;
+  const response = sourceMap[path];
+  if (response) {
+    return response(req, res);
+  }
+  return res.status(404).json({ message: "UNKNOWN_SOURCE", path });
 }
