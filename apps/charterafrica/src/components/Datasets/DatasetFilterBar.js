6ed6c1e8094d75b27489a4e6c61678bd76c46753
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-04-26 13:52:49 +0300

    Datasets filter bar

--- apps/charterafrica/src/components/Datasets/DatasetFilterBar.js
+++ apps/charterafrica/src/components/Datasets/DatasetFilterBar.js
@@ -1,15 +1,19 @@
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, Select, MenuItem, Chip, Typography } from "@mui/material";
 import React, { useState } from "react";
 
+import { neutral } from "@/charterafrica/colors";
 import SearchInput from "@/charterafrica/components/SearchInput";
 
 const DatasetFilterBar = React.forwardRef(function DatasetFilterBar(
   props,
   ref
 ) {
-  const { onChange, search: searchProp } = props;
+  const { onChange, search: searchProp, countries, tags } = props;
 
   const [value, setValue] = useState(searchProp || "");
+  const [sort, setSort] = useState("metadata_modified desc");
+  const [country, setCountry] = useState("All Countries");
+  const [tag, setTag] = useState("All Tags");
 
   const handleChangeQ = (e) => {
     setValue(e.target.value);
@@ -29,10 +33,41 @@ const DatasetFilterBar = React.forwardRef(function DatasetFilterBar(
     }
   };
 
+  const handleChangeSort = (e) => {
+    setSort(e.target.value);
+  };
+
+  const handleChangeCountry = (e) => {
+    setCountry(e.target.value);
+  };
+
+  const handleChangeTag = (e) => {
+    setTag(e.target.value);
+  };
+
+  const sortOrder = [
+    {
+      label: "Most Recent",
+      value: "metadata_modified desc",
+    },
+    {
+      label: "Least Recent",
+      value: "metadata_modified asc",
+    },
+    {
+      label: "Name (A-Z)",
+      value: "name asc",
+    },
+    {
+      label: "Name (Z-A)",
+      value: "name desc",
+    },
+  ];
+
   return (
     <Box py={5} ref={ref}>
       <Grid container spacing={2}>
-        <Grid item xs={12} sm={6} md={4}>
+        <Grid item xs={12} md={2.4}>
           <SearchInput
             placeholder="Search Database"
             value={value}
@@ -45,6 +80,117 @@ const DatasetFilterBar = React.forwardRef(function DatasetFilterBar(
               typography: "p1",
               width: "200px",
             }}
+          />
+        </Grid>
+        <Grid item xs={12} md={2.4}>
+          <Select
+            inputProps={{
+              "aria-label": "Without label",
+            }}
+            onChange={handleChangeSort}
+            MenuProps={{
+              sx: {
+                color: "neutral.dark",
+                typography: "p1",
+              },
+            }}
+            sx={{
+              backgroundColor: neutral[50],
+              height: "36px",
+              minWidth: "200px",
+              typography: "p1",
+            }}
+            value={sort}
+          >
+            {sortOrder.map((order) => (
+              <MenuItem value={order.value} key={order.value}>
+                {order.label}
+              </MenuItem>
+            ))}
+          </Select>
+        </Grid>
+        <Grid item xs={12} md={2.4}>
+          <Select
+            inputProps={{
+              "aria-label": "Without label",
+            }}
+            onChange={handleChangeCountry}
+            MenuProps={{
+              sx: {
+                color: "neutral.dark",
+                typography: "p1",
+              },
+            }}
+            sx={{
+              backgroundColor: neutral[50],
+              height: "36px",
+              minWidth: "200px",
+              typography: "p1",
+            }}
+            value={country}
+          >
+            {countries.map((singleCountry) => (
+              <MenuItem value={singleCountry} key={singleCountry}>
+                {singleCountry}
+              </MenuItem>
+            ))}
+          </Select>
+        </Grid>
+        <Grid item xs={12} md={2.4}>
+          <Select
+            inputProps={{
+              "aria-label": "Without label",
+            }}
+            onChange={handleChangeTag}
+            MenuProps={{
+              sx: {
+                color: "neutral.dark",
+                typography: "p1",
+              },
+            }}
+            sx={{
+              backgroundColor: neutral[50],
+              height: "36px",
+              minWidth: "200px",
+              typography: "p1",
+            }}
+            value={tag}
+          >
+            {tags.map((singleTag) => (
+              <MenuItem value={singleTag} key={singleTag}>
+                {singleTag}
+              </MenuItem>
+            ))}
+          </Select>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={2.4}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography variant="p1" color="neutral.main" pr={2}>
+            Show:
+          </Typography>
+          <Chip
+            label="Documents"
+            sx={(theme) => ({
+              backgroundColor: theme.palette.success.main,
+              ...theme.typography.caption,
+              borderRadius: "10px",
+              mr: 1.75,
+            })}
+          />
+          <Chip
+            label="Datasets"
+            sx={(theme) => ({
+              backgroundColor: theme.palette.error.main,
+              ...theme.typography.caption,
+              borderRadius: "10px",
+              mr: 1.75,
+            })}
           />
         </Grid>
       </Grid>
