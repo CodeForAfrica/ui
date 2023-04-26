56b95fc17d0bee8dc2b7231d6e560a870b6dcebe
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-04-26 13:58:44 +0300

    Added pagination

--- apps/charterafrica/src/components/Datasets/Datasets.js
+++ apps/charterafrica/src/components/Datasets/Datasets.js
@@ -1,12 +1,20 @@
 import { Section } from "@commons-ui/core";
 import { Box } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 
 import DatasetCard from "./DatasetCard";
 import DatasetFilterBar from "./DatasetFilterBar";
 
+import NextPrevPagination from "@/charterafrica/components/NextPrevPagination";
+
 const Datasets = React.forwardRef(function Datasets(props, ref) {
-  const { sx, data, tags, countries } = props;
+  const { sx, data, tags, countries, count } = props;
+  const [page, setPage] = useState(1);
+
+  const handleChangePage = (_, value) => {
+    setPage(value);
+  };
+
   return (
     <Box bgcolor="common.white" sx={sx} ref={ref}>
       <Section sx={{ px: { xs: 2.5, sm: 0 }, py: { xs: 5, md: "50px" } }}>
@@ -14,6 +22,14 @@ const Datasets = React.forwardRef(function Datasets(props, ref) {
         {data.map((dataset) => (
           <DatasetCard {...dataset} key={dataset.name} />
         ))}
+        <NextPrevPagination
+          count={count}
+          onChange={handleChangePage}
+          page={page}
+          sx={{
+            bgcolor: "common.white",
+          }}
+        />
       </Section>
     </Box>
   );
