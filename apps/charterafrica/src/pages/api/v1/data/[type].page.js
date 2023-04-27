41785722e2a5eccf8e6a2201aca0400378507c6b
kilemensi <kilemensi@gmail.com>
2023-04-27 10:44:20 +0300

    Ensure organizationId is read and passed to fetchDatasets

--- apps/charterafrica/src/pages/api/v1/data/[type].page.js
+++ apps/charterafrica/src/pages/api/v1/data/[type].page.js
@@ -1,10 +1,11 @@
 import fetchDatasets from "@/charterafrica/lib/openAfrica";
+import payload from "@/charterafrica/lib/payload";
 
-const datasets = async (req, res) => {
+async function datasets(req, res) {
   const {
     query: {
-      page = 0,
-      perPage = 10,
+      page = 1,
+      pageSize = 10,
       sort = "metadata_created desc",
       tags = [],
       q = "",
@@ -12,32 +13,31 @@ const datasets = async (req, res) => {
   } = req;
 
   try {
-    const data = await fetchDatasets({
+    const { organizationId } = await payload.findGlobal("datasets");
+    const data = await fetchDatasets(organizationId, {
       q,
-      rows: perPage,
-      start: page,
+      rows: pageSize,
+      start: (page - 1) * pageSize,
       sort,
       tags,
     });
-    res.status(200).json(data);
+    return res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error });
+    return res.status(500).json({ error });
   }
+}
 
-  return res.status(404).json({ message: "ERROR FETCHING DATASETS" });
-};
-
-const sourceMap = {
+const dataHandlerMap = {
   datasets,
 };
 
 export default async function handler(req, res) {
   const {
-    query: { path },
+    query: { type },
   } = req;
-  const response = sourceMap[path];
-  if (response) {
-    return response(req, res);
+  const typeHandler = dataHandlerMap[type];
+  if (typeHandler) {
+    return typeHandler(req, res);
   }
-  return res.status(404).json({ message: "UNKNOWN_SOURCE", path });
+  return res.status(404).json({ message: "UNKNOWN_DATA_TYPE", type });
 }
