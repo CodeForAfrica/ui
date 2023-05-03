34d50f93aa1429563653eb248665782baeba7f2c
Kevin Koech <koechkevin92@gmail.com>
2023-05-03 14:44:29 +0300

    Get configuration Options

--- apps/charterafrica/src/payload/fields/democracyToolsSelect.js
+++ apps/charterafrica/src/payload/fields/democracyToolsSelect.js
@@ -2,18 +2,21 @@ import { Select } from "payload/components/forms";
 import { select } from "payload/dist/fields/validations";
 import { createElement } from "react";
 
+import gitData from "../../lib/data/_mock/gitData";
+
+const options = Object.keys(gitData?.[0] || {});
 export const validateSelect = async (value, { hasMany, required, t }) => {
   try {
-    return select(value, { hasMany, options: [], required, t });
+    return select(value, { hasMany, options, required, t });
   } catch (error) {
-    return select(value, { hasMany, options: [], required, t });
+    return select(value, { hasMany, options, required, t });
   }
 };
 
 export function SheetSelect(props) {
-  return createElement(Select, { ...props, options: [] });
+  return createElement(Select, { ...props, options });
 }
 
 export function ColumnSelect(props) {
-  return createElement(Select, { ...props, options: [] });
+  return createElement(Select, { ...props, options });
 }
