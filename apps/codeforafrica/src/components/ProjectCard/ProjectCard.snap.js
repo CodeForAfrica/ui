e54a63cba879c3b5e944468f726333edb55bedce
kilemensi <kilemensi@gmail.com>
2023-05-05 17:34:38 +0300

    Update test snapshots

--- apps/codeforafrica/src/components/ProjectCard/ProjectCard.snap.js
+++ apps/codeforafrica/src/components/ProjectCard/ProjectCard.snap.js
@@ -6,7 +6,7 @@ exports[`<ProjectCard /> renders unchanged 1`] = `
     class="MuiPaper-root MuiPaper-elevation MuiPaper-elevation0 MuiCard-root css-1lxhh98-MuiPaper-root-MuiCard-root"
   >
     <button
-      class="MuiButtonBase-root MuiCardActionArea-root css-gthups-MuiButtonBase-root-MuiCardActionArea-root"
+      class="MuiButtonBase-root MuiCardActionArea-root css-1c12nz6-MuiButtonBase-root-MuiCardActionArea-root"
       tabindex="0"
       type="button"
     >
