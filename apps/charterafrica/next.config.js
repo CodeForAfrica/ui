eb0bd27d7c690904d1ffeef3f4c2195db6497618
Kevin Koech <koechkevin92@gmail.com>
2023-05-15 12:32:20 +0300

    ADD SENTRY

--- apps/charterafrica/next.config.js
+++ apps/charterafrica/next.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 
+const { withSentryConfig } = require("@sentry/nextjs");
+
 const PROJECT_ROOT = process.env.PROJECT_ROOT?.trim();
 const outputFileTracingRoot = PROJECT_ROOT
   ? path.resolve(__dirname, PROJECT_ROOT)
@@ -17,7 +19,7 @@ const defaultLocale =
     process.env.PAYLOAD_PUBLIC_DEFAULT_LOCALE
   )?.trim() || locales?.[0];
 
-module.exports = {
+const moduleExports = {
   experimental: {
     outputFileTracingRoot,
   },
@@ -83,4 +85,30 @@ module.exports = {
 
     return config;
   },
+  sentry: {
+    // See the 'Configure Source Maps' and 'Configure Legacy Browser Support'
+    // sections below for information on the following options:
+    //   - disableServerWebpackPlugin
+    //   - disableClientWebpackPlugin
+    //   - hideSourceMaps
+    //   - widenClientFileUpload
+    //   - transpileClientSDK
+    hideSourceMaps: false,
+  },
 };
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
