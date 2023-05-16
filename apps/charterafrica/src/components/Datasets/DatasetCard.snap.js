177776ff1e7b8ad7105c8f7ebca7e4af040ca57c
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-05-16 16:52:31 +0300

    Updated dataset card

--- apps/charterafrica/src/components/Datasets/DatasetCard.snap.js
+++ apps/charterafrica/src/components/Datasets/DatasetCard.snap.js
@@ -3,72 +3,85 @@
 exports[`<DatasetCard /> renders unchanged 1`] = `
 <div>
   <div
-    class="MuiPaper-root MuiPaper-elevation MuiPaper-rounded MuiPaper-elevation1 MuiCard-root css-6s8z3q-MuiPaper-root-MuiCard-root"
+    class="MuiPaper-root MuiPaper-elevation MuiPaper-rounded MuiPaper-elevation1 MuiCard-root css-19232nk-MuiPaper-root-MuiCard-root"
   >
-    <div
-      class="MuiGrid-root MuiGrid-container MuiGrid-spacing-xs-2 MuiGrid-direction-xs-[object Object] css-bkp0sa-MuiGrid-root"
+    <a
+      class="MuiButtonBase-root MuiCardActionArea-root css-1aib4ax-MuiButtonBase-root-MuiCardActionArea-root"
+      href="#"
+      tabindex="0"
     >
       <div
-        class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-4 css-aoe11o-MuiGrid-root"
+        class="MuiCardContent-root css-46bh2p-MuiCardContent-root"
       >
         <div
-          class="MuiTypography-root MuiTypography-h5SmallSemiBold css-mecr4x-MuiTypography-root"
+          class="MuiGrid-root MuiGrid-container MuiGrid-spacing-xs-2 MuiGrid-direction-xs-[object Object] css-bkp0sa-MuiGrid-root"
         >
-          Document Title
-        </div>
-        <p
-          class="MuiTypography-root MuiTypography-p1 css-1pop01s-MuiTypography-root"
-        >
-           
-          January 1, 2021
-           |
-           
-           
-          January 1, 2021
-        </p>
-        <p
-          class="MuiTypography-root MuiTypography-p1SemiBold css-1egbhxv-MuiTypography-root"
-        >
-          Author Name
-        </p>
-      </div>
-      <div
-        class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-4 css-ls0fcs-MuiGrid-root"
-      >
-        <div
-          class="MuiTypography-root MuiTypography-p1 css-1xsgaa8-MuiTypography-root"
-        >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit.
-        </div>
-        <button
-          class="MuiButtonBase-root MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeMedium MuiButton-textSizeMedium MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeMedium MuiButton-textSizeMedium css-iwoc5g-MuiButtonBase-root-MuiButton-root"
-          tabindex="0"
-          type="button"
-        />
-      </div>
-      <div
-        class="MuiGrid-root MuiGrid-container MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-4 css-zuysgr-MuiGrid-root"
-      >
-        <div
-          class="MuiChip-root MuiChip-filled MuiChip-sizeMedium MuiChip-colorDefault MuiChip-filledDefault css-6pueiv-MuiChip-root"
-        >
-          <span
-            class="MuiChip-label MuiChip-labelMedium css-6od3lo-MuiChip-label"
+          <div
+            class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-4 css-aoe11o-MuiGrid-root"
           >
-            CSV
-          </span>
-        </div>
-        <div
-          class="MuiChip-root MuiChip-filled MuiChip-sizeMedium MuiChip-colorDefault MuiChip-filledDefault css-12sagfs-MuiChip-root"
-        >
-          <span
-            class="MuiChip-label MuiChip-labelMedium css-6od3lo-MuiChip-label"
+            <div
+              class="MuiTypography-root MuiTypography-h5SmallSemiBold css-mecr4x-MuiTypography-root"
+            >
+              Document Title
+            </div>
+            <p
+              class="MuiTypography-root MuiTypography-p1 css-1pop01s-MuiTypography-root"
+            >
+               
+              January 1, 2021
+               
+              | 
+               
+              January 1, 2021
+            </p>
+            <p
+              class="MuiTypography-root MuiTypography-p1SemiBold css-1egbhxv-MuiTypography-root"
+            >
+              Author Name
+            </p>
+          </div>
+          <div
+            class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-4 css-ls0fcs-MuiGrid-root"
           >
-            PDF
-          </span>
+            <div
+              class="MuiTypography-root MuiTypography-p1 css-1xsgaa8-MuiTypography-root"
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit.
+            </div>
+            <button
+              class="MuiButtonBase-root MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeMedium MuiButton-textSizeMedium MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeMedium MuiButton-textSizeMedium css-iwoc5g-MuiButtonBase-root-MuiButton-root"
+              tabindex="0"
+              type="button"
+            />
+          </div>
+          <div
+            class="MuiGrid-root MuiGrid-container MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-4 css-zuysgr-MuiGrid-root"
+          >
+            <div
+              class="MuiChip-root MuiChip-filled MuiChip-sizeMedium MuiChip-colorDefault MuiChip-filledDefault css-6pueiv-MuiChip-root"
+            >
+              <span
+                class="MuiChip-label MuiChip-labelMedium css-6od3lo-MuiChip-label"
+              >
+                CSV
+              </span>
+            </div>
+            <div
+              class="MuiChip-root MuiChip-filled MuiChip-sizeMedium MuiChip-colorDefault MuiChip-filledDefault css-12sagfs-MuiChip-root"
+            >
+              <span
+                class="MuiChip-label MuiChip-labelMedium css-6od3lo-MuiChip-label"
+              >
+                PDF
+              </span>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      <span
+        class="MuiCardActionArea-focusHighlight css-1v2exvi-MuiCardActionArea-focusHighlight"
+      />
+    </a>
   </div>
 </div>
 `;
