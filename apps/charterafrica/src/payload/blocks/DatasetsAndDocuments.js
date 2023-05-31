95cc662773c1b89aa0f369d4afa0f5bbd185f261
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-05-31 18:37:20 +0300

    Use tabs for datasets and documents

--- apps/charterafrica/src/payload/blocks/DatasetsAndDocuments.js
+++ apps/charterafrica/src/payload/blocks/DatasetsAndDocuments.js
@@ -17,138 +17,281 @@ const DatasetsAndDocuments = {
   slug: "datasetsAndDocuments",
   fields: [
     {
-      label: {
-        en: "Search & Filter Values",
-        fr: "Valeurs de recherche et de filtrage",
-        pt: "Valores de pesquisa e filtro",
-      },
-      type: "collapsible",
-      fields: [
+      type: "tabs",
+      tabs: [
         {
-          name: "sortOptions",
-          type: "array",
-          minRows: 1,
           label: {
-            en: "Sort Options",
-            fr: "Options de tri",
-            pt: "Opções de classificação",
+            en: "Datasets",
+            fr: "Ensembles de données",
+            pt: "Conjuntos de dados",
+          },
+          description: {
+            en: "Configure the datasets block",
+            fr: "Configurez le bloc de jeux de données",
+            pt: "Configure o bloco de conjuntos de dados",
           },
           fields: [
             {
-              name: "value",
-              type: "select",
-              options: sortOptions.map((value) => {
-                return {
-                  value,
-                  label: value,
-                };
-              }),
-              unique: true,
-              required: true,
-              validate: (val, options) => {
-                const { data, t } = options || {};
-                if (data?.options?.filter((l) => l.value === val)?.length > 1) {
-                  return t("charterafrica.site:uniqueSortOptions");
-                }
-                return array(val, options);
-              },
-            },
-            {
-              name: "label",
-              type: "text",
               label: {
-                en: "Label",
-                fr: "Étiquette",
-                pt: "Rótulo",
+                en: "Search & Filter Values",
+                fr: "Valeurs de recherche et de filtrage",
+                pt: "Valores de pesquisa e filtro",
               },
-              required: true,
-            },
-          ],
-          admin: {
-            initCollapsed: true,
-            components: {
-              RowLabel: ({ data }) => {
-                return data?.label || data?.value || data?.id;
-              },
-            },
-          },
-        },
-      ],
-    },
-    {
-      label: {
-        en: "Search & Filter Labels",
-        fr: "Étiquettes de recherche et de filtrage",
-        pt: "Rótulos de pesquisa e filtro",
-      },
-      type: "collapsible",
-      fields: [
-        {
-          name: "labels",
-          type: "group",
-          label: {
-            en: "Labels",
-            fr: "Étiquettes",
-            pt: "Rótulos",
-          },
-          fields: [
-            {
-              type: "row",
+              type: "collapsible",
               fields: [
                 {
-                  name: "search",
-                  type: "text",
+                  name: "sortOptions",
+                  type: "array",
+                  minRows: 1,
                   label: {
-                    en: "Search Label",
-                    fr: "Étiquette de recherche",
-                    pt: "Rótulo de pesquisa",
+                    en: "Sort Options",
+                    fr: "Options de tri",
+                    pt: "Opções de classificação",
                   },
-                  required: true,
-                  localized: true,
+                  fields: [
+                    {
+                      name: "value",
+                      type: "select",
+                      options: sortOptions.map((value) => {
+                        return {
+                          value,
+                          label: value,
+                        };
+                      }),
+                      unique: true,
+                      required: true,
+                      validate: (val, options) => {
+                        const { data, t } = options || {};
+                        if (
+                          data?.options?.filter((l) => l.value === val)
+                            ?.length > 1
+                        ) {
+                          return t("charterafrica.site:uniqueSortOptions");
+                        }
+                        return array(val, options);
+                      },
+                    },
+                    {
+                      name: "label",
+                      type: "text",
+                      label: {
+                        en: "Label",
+                        fr: "Étiquette",
+                        pt: "Rótulo",
+                      },
+                      required: true,
+                    },
+                  ],
                   admin: {
-                    width: "50%",
+                    initCollapsed: true,
+                    components: {
+                      RowLabel: ({ data }) => {
+                        return data?.label || data?.value || data?.id;
+                      },
+                    },
                   },
                 },
+              ],
+            },
+            {
+              label: {
+                en: "Search & Filter Labels",
+                fr: "Étiquettes de recherche et de filtrage",
+                pt: "Rótulos de pesquisa e filtro",
+              },
+              type: "collapsible",
+              fields: [
                 {
-                  name: "sort",
-                  type: "text",
+                  name: "labels",
+                  type: "group",
                   label: {
-                    en: "Sort Label",
-                    fr: "Étiquette de tri",
-                    pt: "Rótulo de classificação",
+                    en: "Labels",
+                    fr: "Étiquettes",
+                    pt: "Rótulos",
                   },
-                  required: true,
-                  localized: true,
-                  admin: {
-                    width: "50%",
+                  fields: [
+                    {
+                      type: "row",
+                      fields: [
+                        {
+                          name: "search",
+                          type: "text",
+                          label: {
+                            en: "Search Label",
+                            fr: "Étiquette de recherche",
+                            pt: "Rótulo de pesquisa",
+                          },
+                          required: true,
+                          localized: true,
+                          admin: {
+                            width: "50%",
+                          },
+                        },
+                        {
+                          name: "sort",
+                          type: "text",
+                          label: {
+                            en: "Sort Label",
+                            fr: "Étiquette de tri",
+                            pt: "Rótulo de classificação",
+                          },
+                          required: true,
+                          localized: true,
+                          admin: {
+                            width: "50%",
+                          },
+                        },
+                        {
+                          name: "countries",
+                          type: "text",
+                          label: {
+                            en: "Countries Label",
+                            fr: "Étiquette des pays",
+                            pt: "Rótulo dos países",
+                          },
+                          required: true,
+                          localized: true,
+                          admin: {
+                            width: "50%",
+                          },
+                        },
+                        {
+                          name: "tags",
+                          type: "text",
+                          label: {
+                            en: "Tags Label",
+                            fr: "Étiquette des tags",
+                            pt: "Rótulo das tags",
+                          },
+                          required: true,
+                          localized: true,
+                          admin: {
+                            width: "50%",
+                          },
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          label: {
+            en: "Documents",
+            fr: "Documents",
+            pt: "Documentos",
+          },
+          description: {
+            en: "Configure the documents block",
+            fr: "Configurez le bloc de documents",
+            pt: "Configure o bloco de documentos",
+          },
+          fields: [
+            {
+              label: {
+                en: "Documents",
+                fr: "Documents",
+                pt: "Documentos",
+              },
+              type: "collapsible",
+              fields: [
+                {
+                  name: "showDocuments",
+                  type: "checkbox",
+                  label: {
+                    en: "Show Documents",
+                    fr: "Afficher les documents",
+                    pt: "Mostrar documentos",
                   },
+                  defaultValue: true,
                 },
                 {
-                  name: "countries",
-                  type: "text",
+                  name: "documents",
+                  type: "group",
                   label: {
-                    en: "Countries Label",
-                    fr: "Étiquette des pays",
-                    pt: "Rótulo dos países",
+                    en: "Documents",
+                    fr: "Documents",
+                    pt: "Documentos",
                   },
-                  required: true,
-                  localized: true,
+                  fields: [
+                    {
+                      name: "groupID",
+                      type: "text",
+                      label: {
+                        en: "Group ID",
+                        fr: "ID de groupe",
+                        pt: "ID do grupo",
+                      },
+                      required: true,
+                    },
+                    {
+                      name: "options",
+                      label: {
+                        en: "Options",
+                        fr: "Options",
+                        pt: "Opções",
+                      },
+                      type: "group",
+                      fields: [
+                        {
+                          type: "row",
+                          fields: [
+                            {
+                              name: "showNotes",
+                              label: {
+                                en: "Show Notes",
+                                fr: "Afficher les notes",
+                                pt: "Mostrar notas",
+                              },
+                              type: "checkbox",
+                              defaultValue: true,
+                              required: true,
+                            },
+                            {
+                              name: "showSearch",
+                              label: {
+                                en: "Show Search",
+                                fr: "Afficher la recherche",
+                                pt: "Mostrar pesquisa",
+                              },
+                              type: "checkbox",
+                              defaultValue: false,
+                              required: true,
+                            },
+                            {
+                              name: "showText",
+                              label: {
+                                en: "Show Text",
+                                fr: "Afficher le texte",
+                                pt: "Mostrar texto",
+                              },
+                              type: "checkbox",
+                              defaultValue: false,
+                              required: true,
+                            },
+                            {
+                              name: "showZoom",
+                              label: {
+                                en: "Show Zoom",
+                                fr: "Afficher le zoom",
+                                pt: "Mostrar zoom",
+                              },
+                              type: "checkbox",
+                              defaultValue: false,
+                              required: true,
+                            },
+                          ],
+                        },
+                      ],
+                      admin: {
+                        hideGutter: true,
+                      },
+                    },
+                  ],
                   admin: {
-                    width: "50%",
-                  },
-                },
-                {
-                  name: "tags",
-                  type: "text",
-                  label: {
-                    en: "Tags Label",
-                    fr: "Étiquette des tags",
-                    pt: "Rótulo das tags",
-                  },
-                  required: true,
-                  localized: true,
-                  admin: {
-                    width: "50%",
+                    condition: (_, siblingData) => siblingData?.showDocuments,
                   },
                 },
               ],
@@ -292,113 +435,6 @@ const DatasetsAndDocuments = {
               ],
             },
           ],
-        },
-      ],
-    },
-    {
-      label: {
-        en: "Documents",
-        fr: "Documents",
-        pt: "Documentos",
-      },
-      type: "collapsible",
-      fields: [
-        {
-          name: "showDocuments",
-          type: "checkbox",
-          label: {
-            en: "Show Documents",
-            fr: "Afficher les documents",
-            pt: "Mostrar documentos",
-          },
-          defaultValue: true,
-        },
-        {
-          name: "documents",
-          type: "group",
-          label: {
-            en: "Documents",
-            fr: "Documents",
-            pt: "Documentos",
-          },
-          fields: [
-            {
-              name: "groupID",
-              type: "text",
-              label: {
-                en: "Group ID",
-                fr: "ID de groupe",
-                pt: "ID do grupo",
-              },
-              required: true,
-            },
-            {
-              name: "options",
-              label: {
-                en: "Options",
-                fr: "Options",
-                pt: "Opções",
-              },
-              type: "group",
-              fields: [
-                {
-                  type: "row",
-                  fields: [
-                    {
-                      name: "showNotes",
-                      label: {
-                        en: "Show Notes",
-                        fr: "Afficher les notes",
-                        pt: "Mostrar notas",
-                      },
-                      type: "checkbox",
-                      defaultValue: true,
-                      required: true,
-                    },
-                    {
-                      name: "showSearch",
-                      label: {
-                        en: "Show Search",
-                        fr: "Afficher la recherche",
-                        pt: "Mostrar pesquisa",
-                      },
-                      type: "checkbox",
-                      defaultValue: false,
-                      required: true,
-                    },
-                    {
-                      name: "showText",
-                      label: {
-                        en: "Show Text",
-                        fr: "Afficher le texte",
-                        pt: "Mostrar texto",
-                      },
-                      type: "checkbox",
-                      defaultValue: false,
-                      required: true,
-                    },
-                    {
-                      name: "showZoom",
-                      label: {
-                        en: "Show Zoom",
-                        fr: "Afficher le zoom",
-                        pt: "Mostrar zoom",
-                      },
-                      type: "checkbox",
-                      defaultValue: false,
-                      required: true,
-                    },
-                  ],
-                },
-              ],
-              admin: {
-                hideGutter: true,
-              },
-            },
-          ],
-          admin: {
-            condition: (_, siblingData) => siblingData?.showDocuments,
-          },
         },
       ],
     },
