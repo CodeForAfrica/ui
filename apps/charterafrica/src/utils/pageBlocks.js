764f96b268ae21d9766ea56836a5d51714d13c12
kilemensi <kilemensi@gmail.com>
2023-06-05 16:28:46 +0300

    Avoid block slug duplication

--- apps/charterafrica/src/utils/pageBlocks.js
+++ apps/charterafrica/src/utils/pageBlocks.js
@@ -1,3 +1,5 @@
 export const LONGFORM = "longform";
+export const NEWS = "our-news";
 export const OUR_EXPLAINERS = "our-explainers";
 export const OUR_GRANTEES = "our-grantees";
+export const POST = "post";
