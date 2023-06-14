2b11241ee705b420142cba35eb935f1a98619d1f
Kevin Koech <koechkevin92@gmail.com>
2023-06-14 12:45:22 +0300

    Cache invalidate

--- apps/charterafrica/src/lib/ecosystem/airtable.js
+++ apps/charterafrica/src/lib/ecosystem/airtable.js
@@ -3,18 +3,32 @@ import fetchJson from "@/charterafrica/utils/fetchJson";
 export const processToolFromAirtable = async () => {};
 
 const cache = {};
+
+function getFromCache(key) {
+  const rawData = cache[key];
+  if (rawData) {
+    const { lastUpdated, value } = rawData;
+    const fiveMinutesAgo = new Date();
+    fiveMinutesAgo.setMinutes(fiveMinutesAgo.getMinutes() - 5);
+    if (fiveMinutesAgo < lastUpdated) {
+      return value;
+    }
+  }
+  return null;
+}
+
 export async function schema(req) {
   const { url } = req.query;
   const headers = {
     "Content-Type": "application/json",
     Authorization: `Bearer ${process.env.AIRTABLE_API_TOKEN}`,
   };
-  if (cache[url]) {
-    return cache[url];
+  if (getFromCache(url)) {
+    return getFromCache(url);
   }
-  const response = fetchJson.get(`https://api.airtable.com/v0${url}`, {
+  const value = fetchJson.get(`https://api.airtable.com/v0${url}`, {
     headers,
   });
-  cache[url] = response;
-  return response;
+  cache[url] = { value, lastUpdated: new Date() };
+  return value;
 }
