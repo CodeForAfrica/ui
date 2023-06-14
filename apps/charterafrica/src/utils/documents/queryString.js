c922a95b12ac0a856094bde13ae9386042cbe76f
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-06-14 13:44:27 +0300

    Documents filter

--- apps/charterafrica/src/utils/documents/queryString.js
+++ apps/charterafrica/src/utils/documents/queryString.js
@@ -7,12 +7,26 @@
  */
 function queryString(query = {}) {
   const searchParams = new URLSearchParams();
-  const { contributor = true, per_page: pageSize = 8, q, ...rest } = query;
+  const {
+    contributor = true,
+    per_page: pageSize = 8,
+    q,
+    sort,
+    search,
+    ...rest
+  } = query;
   if (q) {
-    searchParams.append("q", q);
+    if (search) {
+      searchParams.append("q", `${q} ${search}`);
+    } else {
+      searchParams.append("q", q);
+    }
   }
   if (pageSize) {
     searchParams.append("per_page", pageSize);
+  }
+  if (sort) {
+    searchParams.append("order", sort);
   }
   if (contributor !== undefined) {
     searchParams.append("contributor", contributor);
