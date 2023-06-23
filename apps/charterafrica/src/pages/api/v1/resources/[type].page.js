1304b73880112c4b751404ca33de987fe29c2cd9
kilemensi <kilemensi@gmail.com>
2023-06-23 12:59:44 +0300

    Handle both datasets and documents at /resources/[type]
    + Remove documents from /opportunites/consultation/[media]
    + Send errors to Sentry

--- apps/charterafrica/src/pages/api/v1/resources/[type].page.js
+++ apps/charterafrica/src/pages/api/v1/resources/[type].page.js
@@ -1,39 +1,54 @@
+import * as Sentry from "@sentry/nextjs";
+
 import fetchDatasets from "@/charterafrica/lib/openAfrica";
+import { fetchDocuments } from "@/charterafrica/lib/sourceAfrica";
 
 async function datasets(req, res) {
   const {
-    query: {
-      sort = "metadata_created desc",
-      tags,
-      countries,
-      q = "",
-      page = 1,
-      pathname,
-      locale,
-      organizationId,
-    },
-  } = req;
+    sort = "metadata_created desc",
+    tags,
+    countries,
+    q = "",
+    page = 1,
+    pathname,
+    locale,
+    organizationId,
+  } = req.query;
 
-  try {
-    const data = await fetchDatasets(organizationId, pathname, {
-      q,
-      page,
-      sort,
-      tags: tags?.split(","),
-      countries: countries?.split(","),
-      locale,
-    });
-
-    return res.status(200).json(data);
-  } catch (error) {
-    return res.status(500).json({ error });
-  }
+  const data = await fetchDatasets(organizationId, pathname, {
+    q,
+    page,
+    sort,
+    tags: tags?.split(","),
+    countries: countries?.split(","),
+    locale,
+  });
+  return res.status(200).json(data);
 }
 
+async function documents(req, res) {
+  const { q, pathname, ...rest } = req.query;
+
+  const data = await fetchDocuments(q, pathname, rest);
+  return res.status(200).json(data);
+}
+
+const fetchResourcesByType = {
+  datasets,
+  documents,
+};
+
 export default async function handler(req, res) {
-  try {
-    return await datasets(req, res);
-  } catch (error) {
-    return res.status(500).json({ error });
+  const { type } = req.query;
+
+  const fetchResources = fetchResourcesByType[type];
+  if (fetchResources) {
+    try {
+      return fetchResources(req, res);
+    } catch (err) {
+      Sentry.captureException(err);
+      return res.status(500).json(err);
+    }
   }
+  return res.status(404).json({ message: "UNKNOWN_TYPE", type });
 }
