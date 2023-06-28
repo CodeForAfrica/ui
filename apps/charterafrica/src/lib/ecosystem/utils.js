8606ae9ad2eb168b16ce1d5634d041fdd4bc7c0b
Kevin Koech <koechkevin92@gmail.com>
2023-06-28 11:10:57 +0300

    Implement feedback

--- apps/charterafrica/src/lib/ecosystem/utils.js
+++ apps/charterafrica/src/lib/ecosystem/utils.js
@@ -1,18 +1,27 @@
 export function localizeData(data = {}) {
+  const getValue = (value, locale) => {
+    if (value?.[locale] !== undefined) {
+      return value?.[locale];
+    }
+    if (value?.en !== undefined) {
+      return value?.en;
+    }
+    return value;
+  };
   return Object.keys(data).reduce(
     (result, key) => ({
       ...result,
       en: {
         ...result.en,
-        [key]: data[key].en || data[key],
+        [key]: getValue(data[key], "en"),
       },
       pt: {
         ...result.pt,
-        [key]: data[key].pt || data[key],
+        [key]: getValue(data[key], "pt"),
       },
       fr: {
         ...result.fr,
-        [key]: data[key].fr || data[key],
+        [key]: getValue(data[key], "fr"),
       },
     }),
     { en: {}, pt: {}, fr: {} }
