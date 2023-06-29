372a4c600bcf0ab67a69cfbf5578eaaa401fda26
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-06-29 13:17:04 +0300

    regenerate snapshots

--- apps/charterafrica/src/components/FlourishCharts/FlourishCharts.snap.js
+++ apps/charterafrica/src/components/FlourishCharts/FlourishCharts.snap.js
@@ -6,7 +6,7 @@ exports[`<FlourishCharts /> renders unchanged 1`] = `
     class="MuiBox-root css-1k9ek97"
   >
     <div
-      class="MuiContainer-root MuiContainer-maxWidthLg MuiContainer-fixed MuiContainer-disableGutters css-1bgpcsq-MuiContainer-root"
+      class="MuiContainer-root MuiContainer-maxWidthLg MuiContainer-fixed MuiContainer-disableGutters css-jingrl-MuiContainer-root"
     >
       <div>
         <div>
