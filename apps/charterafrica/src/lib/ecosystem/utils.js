8110c326d8818b5b5c148f8d29cb4c6a38d064db
Kevin Koech <koechkevin92@gmail.com>
2023-06-30 13:19:25 +0300

    Update utils

--- apps/charterafrica/src/lib/ecosystem/utils.js
+++ apps/charterafrica/src/lib/ecosystem/utils.js
@@ -1,22 +1,22 @@
-function localize(data, locales = ["en"]) {
+function localize(data, locales = ["en"], localized = false) {
   const getValue = (value, locale) => {
     if (value?.[locale] !== undefined) {
       return value?.[locale];
     }
-    if (value?.en !== undefined) {
+    if (value?.en !== undefined && !localized) {
       return value?.en;
     }
     return value;
   };
   return locales.reduce((loc, locale) => {
-    const localized = loc;
-    localized[locale] = {};
+    const localizedData = loc;
+    localizedData[locale] = {};
     Object.keys(data).reduce((locData, key) => {
       const localeData = locData;
       localeData[key] = getValue(data[key], locale);
       return localeData;
-    }, localized[locale]);
-    return localized;
+    }, localizedData[locale]);
+    return localizedData;
   }, {});
 }
 
