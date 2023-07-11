04c2a5277e610acd19c3389de4de76863974a105
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-07-11 10:50:28 +0300

    Add GA

--- apps/codeforafrica/src/pages/_app.page.js
+++ apps/codeforafrica/src/pages/_app.page.js
@@ -1,6 +1,7 @@
 import { CacheProvider } from "@emotion/react";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import Head from "next/head";
+import Script from "next/script";
 import { DefaultSeo } from "next-seo";
 import PropTypes from "prop-types";
 import React from "react";
@@ -17,6 +18,21 @@ function MyApp(props) {
 
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+      />
+      <Script id="gtag-init" strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
+          page_path: window.location.pathname,
+        });
+      `}
+      </Script>
+
       <DefaultSeo {...SEO} />
       <CacheProvider value={emotionCache}>
         <Head>
