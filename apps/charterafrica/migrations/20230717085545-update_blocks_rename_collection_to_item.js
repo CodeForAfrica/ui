eced265fe75f9141d3383bf44082d8ab502cffc8
Kevin Koech <koechkevin92@gmail.com>
2023-07-18 14:42:57 +0300

    use agregation pipeline

--- apps/charterafrica/migrations/20230717085545-update_blocks_rename_collection_to_item.js
+++ apps/charterafrica/migrations/20230717085545-update_blocks_rename_collection_to_item.js
@@ -1,71 +1,61 @@
 module.exports = {
   async up(db) {
-    const bulkOps = [];
-
-    await db
-      .collection("pages")
-      .find()
-      .forEach((doc) => {
-        if (doc.blocks && doc.blocks.length > 0) {
-          doc.blocks.forEach((block) => {
-            if (block.items && block.items.length > 0) {
-              block.items.forEach((item) => {
-                if (item.collection) {
-                  // eslint-disable-next-line no-param-reassign
-                  item.item = item.collection;
-                  // eslint-disable-next-line no-param-reassign
-                  delete item.collection;
-                  const { _id } = doc;
-                  bulkOps.push({
-                    updateOne: {
-                      filter: { _id },
-                      update: { $set: { blocks: doc.blocks } },
+    await db.collection("pages").updateMany({}, [
+      {
+        $set: {
+          blocks: {
+            $map: {
+              input: "$blocks",
+              in: {
+                $mergeObjects: [
+                  "$$this",
+                  {
+                    items: {
+                      $map: {
+                        input: "$$this.items",
+                        in: {
+                          item: "$$this.collection",
+                          label: "$$this.label",
+                        },
+                      },
                     },
-                  });
-                }
-              });
-            }
-          });
-        }
-      });
-
-    if (bulkOps.length > 0) {
-      db.collection("pages").bulkWrite(bulkOps);
-    }
+                  },
+                ],
+              },
+            },
+          },
+        },
+      },
+    ]);
   },
 
   async down(db) {
-    const bulkOps = [];
-
-    await db
-      .collection("pages")
-      .find()
-      .forEach((doc) => {
-        if (doc.blocks && doc.blocks.length > 0) {
-          doc.blocks.forEach((block) => {
-            if (block.items && block.items.length > 0) {
-              block.items.forEach((item) => {
-                if (item.item) {
-                  // eslint-disable-next-line no-param-reassign
-                  item.collection = item.item;
-                  // eslint-disable-next-line no-param-reassign
-                  delete item.item;
-                  const { _id } = doc;
-                  bulkOps.push({
-                    updateOne: {
-                      filter: { _id },
-                      update: { $set: { blocks: doc.blocks } },
+    await db.collection("pages").updateMany({}, [
+      {
+        $set: {
+          blocks: {
+            $map: {
+              input: "$blocks",
+              in: {
+                $mergeObjects: [
+                  "$$this",
+                  {
+                    items: {
+                      $map: {
+                        input: "$$this.items",
+                        in: {
+                          collection: "$$this.item",
+                          label: "$$this.label",
+                        },
+                      },
                     },
-                  });
-                }
-              });
-            }
-          });
-        }
-      });
-
-    if (bulkOps.length > 0) {
-      db.collection("pages").bulkWrite(bulkOps);
-    }
+                  },
+                ],
+              },
+            },
+          },
+        },
+      },
+    ]);
   },
 };
