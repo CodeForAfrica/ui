dac5afa7a60a77a3eba1a06d0086b5ab8523d14f
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-07-24 14:29:55 +0300

    Add payload

--- apps/codeforafrica/src/lib/payload/data/index.js
+++ apps/codeforafrica/src/lib/payload/data/index.js
@@ -1,17 +1,15 @@
 import { getPageStaticProps } from "@/codeforafrica/lib";
-// import api from "@/codeforafrica/lib/payload";
-// import getPageProps from "@/codeforafrica/lib/payload/data/local";
+import api from "@/codeforafrica/lib/payload";
+import getPageProps from "@/codeforafrica/lib/payload/data/local";
 // import { api } from "@/codeforafrica/lib/payload/data/rest";
 
-// eslint-disable-next-line no-unused-vars
 export default async function getPageServerSideProps(context, slug) {
-  // return getPageStaticProps({ slug });
-  // const props = await getPageProps(api, context);
-  // if (!props) {
-  //   return { notFound: true };
-  // }
+  const props = await getPageProps(api, context);
+  if (!props) {
+    return { notFound: true };
+  }
   const { props: staticProps } = await getPageStaticProps({ slug });
   return {
-    props: { ...staticProps },
+    props: { ...staticProps, ...props },
   };
 }
