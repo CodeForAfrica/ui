3a1108eab3aae306dad8f76d2b1068eeb0f5563e
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-07-24 12:14:33 +0300

    Test Fix

--- apps/codeforafrica/next.config.js
+++ apps/codeforafrica/next.config.js
@@ -26,6 +26,11 @@ module.exports = {
         "node_modules/.pnpm/@swc+core-linux-x64-musl@1.3.56",
         "node_modules/.pnpm/@esbuild+linux-x64@0.17.18",
       ],
+      "/**/*": [
+        "node_modules/.pnpm/@swc+core-linux-x64-gnu@1.3.56",
+        "node_modules/.pnpm/@swc+core-linux-x64-musl@1.3.56",
+        "node_modules/.pnpm/@esbuild+linux-x64@0.17.18",
+      ],
     },
   },
   modularizeImports: {
