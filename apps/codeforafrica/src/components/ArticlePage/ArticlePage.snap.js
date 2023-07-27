6cc5fdfab8f7dd898604ee5893c3e862f7e32dd3
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-07-27 11:10:03 +0300

    Regenerate snapshpts

--- apps/codeforafrica/src/components/ArticlePage/ArticlePage.snap.js
+++ apps/codeforafrica/src/components/ArticlePage/ArticlePage.snap.js
@@ -29,7 +29,7 @@ exports[`<ArticlePage /> renders unchanged 1`] = `
         Article
       </div>
       <div
-        class="MuiStack-root css-1rvuxdl-MuiStack-root"
+        class="MuiStack-root css-1het2hz-MuiStack-root"
       >
         <div
           class="MuiTypography-root MuiTypography-footerCap css-scsc4a-MuiTypography-root"
@@ -37,7 +37,7 @@ exports[`<ArticlePage /> renders unchanged 1`] = `
           Share This Article
         </div>
         <div
-          class="MuiStack-root css-163j9ep-MuiStack-root"
+          class="MuiStack-root css-1jqw290-MuiStack-root"
         >
           <button
             aria-label="Twitter"
@@ -95,7 +95,7 @@ exports[`<ArticlePage /> renders unchanged 1`] = `
       class="MuiContainer-root MuiContainer-maxWidthLg MuiContainer-fixed MuiContainer-disableGutters css-1fwha7w-MuiContainer-root"
     >
       <div
-        class="MuiStack-root css-1rua2vy-MuiStack-root"
+        class="MuiStack-root css-1yu3riq-MuiStack-root"
       >
         <div
           class="MuiTypography-root MuiTypography-body2 css-tua52g-MuiTypography-root"
@@ -103,7 +103,7 @@ exports[`<ArticlePage /> renders unchanged 1`] = `
           Article by
         </div>
         <div
-          class="MuiStack-root css-l87b14-MuiStack-root"
+          class="MuiStack-root css-190nj74-MuiStack-root"
         />
       </div>
     </address>
