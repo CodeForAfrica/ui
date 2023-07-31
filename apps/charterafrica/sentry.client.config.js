4072260a3789fc63f3e55dfd11a722b3b04d8a3f
kilemensi <kilemensi@gmail.com>
2023-07-31 14:44:04 +0300

    Update senty configs
    Add edge config to cover middleware

--- apps/charterafrica/sentry.client.config.js
+++ apps/charterafrica/sentry.client.config.js
@@ -4,9 +4,7 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-const SENTRY_DSN =
-  process.env.CHARTERAFRICA_SENTRY_DSN ||
-  process.env.PUBLIC_CHARTERAFRICA_SENTRY_DSN;
+const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
   dsn: SENTRY_DSN,
