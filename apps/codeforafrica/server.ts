8b5e66b65b9af16b6ed69cabc88d115a9099bb0c
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-08-01 10:18:26 +0300

    Fix codeforafrica building

--- apps/codeforafrica/server.ts
+++ apps/codeforafrica/server.ts
@@ -1,28 +1,35 @@
 import path from "path";
-
+import { spawn } from "child_process";
 import express from "express";
 import next from "next";
 import nextBuild from "next/dist/build";
 import payload from "payload";
+import { Payload } from "payload/dist/payload";
 import { loadEnvConfig } from "@next/env";
 
 const projectDir = process.cwd();
 loadEnvConfig(projectDir);
 
-const app = express();
-
 const hostname = process.env.NEXT_HOSTNAME || "localhost";
 const PORT = parseInt(process.env.PAYLOAD_PUBLIC_PORT || "3000", 10);
 const dev = process.env.NODE_ENV !== "production";
 
-const start = async () => {
+if (!process.env.NEXT_MANUAL_SIG_HANDLE) {
+  process.on("SIGTERM", () => process.exit(0));
+  process.on("SIGINT", () => process.exit(0));
+}
+
+const app = express();
+
+const start = async (): Promise<void> => {
+  let localPayload: Payload;
   try {
-    await payload.init({
+    localPayload = await payload.init({
       secret: process.env.PAYLOAD_SECRET,
       mongoURL: process.env.MONGODB_URL,
       express: app,
-      onInit: () => {
-        payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
+      onInit: (initPayload) => {
+        initPayload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
       },
     });
   } catch (e: any) {
@@ -30,27 +37,66 @@ const start = async () => {
     process.exit();
   }
 
-  if (!process.env.NEXT_BUILD) {
-    const nextApp = next({ dev, hostname, port: PORT });
-
-    const nextHandler = nextApp.getRequestHandler();
-    await nextApp.prepare();
-
-    app.get("*", (req: any, res: any) => nextHandler(req, res));
-
+  if (process.env.NEXT_BUILD) {
     app.listen(PORT, async () => {
-      console.info(`Server listening on ${PORT}...`);
-    });
-  } else {
-    app.listen(PORT, async () => {
-      console.info("NextJS is now building...");
-      try {
-        await nextBuild(path.resolve(projectDir));
-      } finally {
-        process.exit();
-      }
+      localPayload.logger.info("NextJS is now building...");
+      const nextBuild = spawn(
+        "pnpm",
+        ["next", "build", path.resolve(projectDir)],
+        {
+          shell: true,
+          stdio: "inherit",
+        },
+      );
+      nextBuild.on("close", (code) => {
+        process.exit(code);
+      });
+
+      nextBuild.on("error", (err) => {
+        localPayload.logger.error(err);
+        process.exit(1);
+      });
+      return;
     });
   }
+
+  const nextApp = next({ dev, hostname, port: PORT });
+  const nextHandler = nextApp.getRequestHandler();
+  nextApp.prepare().then(() => {
+    localPayload.logger.info("NextJS started");
+
+    app.get("*", (req: any, res: any) => nextHandler(req, res));
+    app.post("*", (req: any, res: any) => nextHandler(req, res));
+    app.put("*", (req: any, res: any) => nextHandler(req, res));
+
+    app.listen(PORT, async () => {
+      localPayload.logger.info(
+        `Next.js App URL: ${process.env.NEXT_PUBLIC_APP_URL}`,
+      );
+    });
+  });
+
+  // if (!process.env.NEXT_BUILD) {
+  //   const nextApp = next({ dev, hostname, port: PORT });
+
+  //   const nextHandler = nextApp.getRequestHandler();
+  //   await nextApp.prepare();
+
+  //   app.get("*", (req: any, res: any) => nextHandler(req, res));
+
+  //   app.listen(PORT, async () => {
+  //     console.info(`Server listening on ${PORT}...`);
+  //   });
+  // } else {
+  //   app.listen(PORT, async () => {
+  //     console.info("NextJS is now building...");
+  //     try {
+  //       await nextBuild(path.resolve(projectDir));
+  //     } finally {
+  //       process.exit();
+  //     }
+  //   });
+  // }
 };
 
 start();
