6fda560b3aea28c92e8c08b223a41bba74947485
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-08-01 16:01:48 +0300

    Add Social Media links in nav

--- apps/codeforafrica/src/payload/globals/Navigation.js
+++ apps/codeforafrica/src/payload/globals/Navigation.js
@@ -1,5 +1,16 @@
+import { array } from "payload/dist/fields/validations";
+
 import link from "../fields/links/link";
 import linkArray from "../fields/links/linkArray";
+
+const socialMediaOptions = [
+  "facebook",
+  "twitter",
+  "instagram",
+  "linkedin",
+  "github",
+  "slack",
+];
 
 const linkField = link({
   disableOpenInNewTab: true,
@@ -49,6 +60,48 @@ const Navigation = {
         fields: [linkField],
       },
     }),
+    {
+      name: "socialLinks",
+      label: "Social Media Links",
+      type: "array",
+      minRows: 1,
+      admin: {
+        components: {
+          RowLabel: ({ data }) => {
+            return (
+              data.platform.charAt(0).toUpperCase() + data.platform.slice(1)
+            );
+          },
+        },
+      },
+      fields: [
+        {
+          name: "platform",
+          label: "Platform",
+          type: "select",
+          options: socialMediaOptions.map((platform) => ({
+            label: platform.charAt(0).toUpperCase() + platform.slice(1),
+            value: platform,
+          })),
+          required: true,
+          validate: (val, options) => {
+            const { data } = options || {};
+            if (
+              data?.socialLinks?.filter((l) => l.platform === val)?.length > 1
+            ) {
+              return "Please select a unique platform";
+            }
+            return array(val, options);
+          },
+        },
+        {
+          name: "url",
+          label: "URL",
+          type: "text",
+          required: true,
+        },
+      ],
+    },
   ],
 };
 
