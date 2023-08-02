842d280ee4c545363a604831163bad6ec43408a8
Kevin Koech <koechkevin92@gmail.com>
2023-08-02 09:48:34 +0300

    Use fetch

--- apps/charterafrica/scripts/ecosystem.js
+++ apps/charterafrica/scripts/ecosystem.js
@@ -1,18 +1,20 @@
-const { exec } = require("child_process");
-
 require("dotenv").config({ path: "./.env" });
 require("dotenv").config({ path: "./.env.local" });
 
 (async () => {
   const { RESOURCES_SECRET_TOKEN } = process.env;
   const { NEXT_PUBLIC_APP_URL } = process.env;
-  const METHOD = process.argv[2];
-  const command = `curl -X ${METHOD} -H "x-api-key: ${RESOURCES_SECRET_TOKEN}" ${NEXT_PUBLIC_APP_URL}/api/v1/resources/ecosystem/entities`;
-  exec(command, (err, stdout) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(stdout);
-  });
+  const method = process.argv[2];
+  const headers = {
+    "x-api-key": RESOURCES_SECRET_TOKEN,
+  };
+  const res = await fetch(
+    `${NEXT_PUBLIC_APP_URL}/api/v1/resources/ecosystem/entities`,
+    {
+      method,
+      headers,
+    },
+  );
+  const response = await res.json();
+  console.log(response);
 })();
