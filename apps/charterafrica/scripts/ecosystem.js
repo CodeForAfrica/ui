003ba4a45a4c6c6392b03b02bf2e1d28efdfeb7f
Kevin Koech <koechkevin92@gmail.com>
2023-08-02 08:40:58 +0300

    Update ecosystem cron

--- apps/charterafrica/scripts/ecosystem.js
+++ apps/charterafrica/scripts/ecosystem.js
@@ -1,4 +1,4 @@
-const { exec } = require("@actions/exec");
+const { exec } = require("child_process");
 
 require("dotenv").config({ path: "./.env" });
 require("dotenv").config({ path: "./.env.local" });
@@ -8,5 +8,11 @@ require("dotenv").config({ path: "./.env.local" });
   const { NEXT_PUBLIC_APP_URL } = process.env;
   const METHOD = process.argv[2];
   const command = `curl -X ${METHOD} -H "x-api-key: ${RESOURCES_SECRET_TOKEN}" ${NEXT_PUBLIC_APP_URL}/api/v1/resources/ecosystem/entities`;
-  await exec(command);
+  exec(command, (err, stdout) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(stdout);
+  });
 })();
