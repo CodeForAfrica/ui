ce1745cc7ac3830c461de9656ad02cfa8fd40b2e
Kevin Koech <koechkevin92@gmail.com>
2023-08-23 13:12:00 +0300

    Update tool processing

--- apps/charterafrica/src/lib/ecosystem/airtable/processData.js
+++ apps/charterafrica/src/lib/ecosystem/airtable/processData.js
@@ -244,7 +244,7 @@ export function processOrganisation(
     avatarUrl:
       getValue(data, organisationTableColumns.avatarUrl)?.[0]?.url ?? null,
     externalId,
-    type: getValue(data, organisationTableColumns.type)?.replace(/&/g, "and"),
+    type: getValue(data, organisationTableColumns.type),
     repoLink: getValue(data, organisationTableColumns.source.url),
     supporters,
     partners,
