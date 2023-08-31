856fc51bcfbc6a8bf3d3c8d9edc46f85d8c7d870
Kevin Koech <koechkevin92@gmail.com>
2023-08-31 11:41:31 +0300

    Implement feedback

--- apps/codeforafrica/src/lib/data/common/index.js
+++ apps/codeforafrica/src/lib/data/common/index.js
@@ -1,12 +1,7 @@
-export async function getGlobalProps(api) {
-  const footer = await api.findGlobal("footer");
-  return { footer };
-}
-
 export async function getPageProps(api) {
-  const globals = await getGlobalProps(api);
+  const footer = await api.findGlobal("footer");
   return {
-    ...globals,
+    footer,
   };
 }
 
