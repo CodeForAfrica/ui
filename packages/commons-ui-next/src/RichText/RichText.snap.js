715af4186148efa6233601e331cb69e653bdbeb0
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-09-07 11:51:23 +0300

    Update snapshots

--- packages/commons-ui-next/src/RichText/RichText.snap.js
+++ packages/commons-ui-next/src/RichText/RichText.snap.js
@@ -6,21 +6,21 @@ exports[`<RichText /> renders unchanged 1`] = `
     class="MuiBox-root css-0"
   >
     <p
-      class="MuiTypography-root MuiTypography-p1 css-fabev4-MuiTypography-root"
+      class="MuiTypography-root MuiTypography-body1 css-1g2fhdp-MuiTypography-root"
     >
       The Charter Project is a pan-African initiative by a coalition of watchdog organisations that use civic technologies to strengthen democracy.
     </p>
     <p
-      class="MuiTypography-root MuiTypography-p1 css-fabev4-MuiTypography-root"
+      class="MuiTypography-root MuiTypography-body1 css-1g2fhdp-MuiTypography-root"
     >
       We do this by helping digital activists and democracy changemakers leverage the African Union’s Charter on Democracy, Elections and Governance (ACDEG).
     </p>
     <p
-      class="MuiTypography-root MuiTypography-p1 css-fabev4-MuiTypography-root"
+      class="MuiTypography-root MuiTypography-body1 css-1g2fhdp-MuiTypography-root"
     >
       The project currently supports initiatives in 11 countries. Find out more 
       <a
-        class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways active css-1sa2x3w-MuiTypography-root-MuiLink-root"
+        class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways active css-v13ftq-MuiTypography-root-MuiLink-root"
         href="/"
       >
         here
