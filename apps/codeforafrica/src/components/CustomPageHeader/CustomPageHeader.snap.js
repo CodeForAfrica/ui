efaf16d2a857b95aab589171d27b95741cd22400
kilemensi <kilemensi@gmail.com>
2023-09-09 20:53:56 +0300

    Update test snapshots

--- apps/codeforafrica/src/components/CustomPageHeader/CustomPageHeader.snap.js
+++ apps/codeforafrica/src/components/CustomPageHeader/CustomPageHeader.snap.js
@@ -1,6 +1,6 @@
 // Jest Snapshot v1, https://goo.gl/fbAQLP
 
-exports[`<AboutPageHeader /> renders unchanged 1`] = `
+exports[`<CustomPageHeader /> renders unchanged 1`] = `
 <div>
   <div
     class="MuiBox-root css-g6ohb3"
