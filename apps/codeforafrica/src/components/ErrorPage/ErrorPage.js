255daf3b9eda6c80ebc1ca1fa90a45344c218ef1
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-09-12 13:37:55 +0300

    Remove unnecessary tags

--- apps/codeforafrica/src/components/ErrorPage/ErrorPage.js
+++ apps/codeforafrica/src/components/ErrorPage/ErrorPage.js
@@ -4,40 +4,32 @@ import ErrorHero from "@/codeforafrica/components/ErrorHero";
 import RelatedStories from "@/codeforafrica/components/RelatedStories";
 
 function ErrorPage({ sections, ...props }) {
-  return (
-    // eslint-disable-next-line react/jsx-no-useless-fragment
-    <>
-      {sections?.map((section) => {
-        switch (section.slug) {
-          case "hero":
-            return (
-              <ErrorHero
-                title={section.title}
-                subtitle={section.subtitle}
-                key={section.slug}
-                {...props}
-              />
-            );
-          case "news-stories":
-            return (
-              <Box
-                key={section.slug}
-                sx={{
-                  bgcolor: { xs: "inherit", sm: "background.default" },
-                }}
-              >
-                <RelatedStories
-                  {...section}
-                  sx={{ px: { xs: "20px", sm: 0 } }}
-                />
-              </Box>
-            );
-          default:
-            return null;
-        }
-      })}
-    </>
-  );
+  return sections?.map((section) => {
+    switch (section.slug) {
+      case "hero":
+        return (
+          <ErrorHero
+            title={section.title}
+            subtitle={section.subtitle}
+            key={section.slug}
+            {...props}
+          />
+        );
+      case "news-stories":
+        return (
+          <Box
+            key={section.slug}
+            sx={{
+              bgcolor: { xs: "inherit", sm: "background.default" },
+            }}
+          >
+            <RelatedStories {...section} sx={{ px: { xs: "20px", sm: 0 } }} />
+          </Box>
+        );
+      default:
+        return null;
+    }
+  });
 }
 
 export default ErrorPage;
