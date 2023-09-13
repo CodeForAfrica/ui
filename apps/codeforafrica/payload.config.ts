fce534cb7ae92790c19897e269e20301035739d5
Kevin Koech <koechkevin92@gmail.com>
2023-09-13 15:41:05 +0300

    Revert changes

--- apps/codeforafrica/payload.config.ts
+++ apps/codeforafrica/payload.config.ts
@@ -33,7 +33,7 @@ export default buildConfig({
   collections: [Pages, Media, Partners] as CollectionConfig[],
   globals: [Settings] as GlobalConfig[],
   admin: {
-    // css: path.resolve(__dirname, "./src/payload/admin/scss/custom.scss"),
+    css: path.resolve(__dirname, "./src/payload/admin/scss/custom.scss"),
     webpack: (config) => ({
       ...config,
       resolve: {
