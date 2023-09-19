51d56d93d2597d330f30a95826308f1c57d4e6e4
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-09-19 15:02:30 +0300

    Default value for error pages

--- apps/codeforafrica/src/lib/data/rest/index.js
+++ apps/codeforafrica/src/lib/data/rest/index.js
@@ -36,13 +36,6 @@ export const api = {
 
 export async function getPageStaticProps(context) {
   const props = await getPageProps(api, context);
-  if (!props) {
-    return {
-      props: {
-        title: "Page not found",
-      },
-    };
-  }
   return {
     props,
   };
