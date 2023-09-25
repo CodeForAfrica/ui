d914d0f4965adb1a373ef9a44e2b3cab7a806ff2
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-09-25 13:32:09 +0300

    Regenerate snapshots

--- apps/codeforafrica/src/components/Articles/Articles.test.js
+++ apps/codeforafrica/src/components/Articles/Articles.test.js
@@ -9,8 +9,48 @@ import theme from "@/codeforafrica/theme";
 const render = createRender({ theme });
 
 const defaultProps = {
-  articles: [],
-  featured: [],
+  articles: [
+    {
+      title: "Battle for gender equality in African media continues",
+      excerpt: [
+        {
+          children: [
+            {
+              text: "Lorem ipsum dolor sit amet consectetur adipiscing elit mattis, vestibulum potenti rhoncus eget lacus fermentum taciti quam, quis curae accumsan viverra semper dapibus sed. ",
+              children: null,
+            },
+          ],
+        },
+      ],
+      publishedOn: "Jan 6, 2022",
+      image: {
+        src: "https://res.cloudinary.com/code-for-africa/image/upload/v1650885664/codeforafrica/unsplash_L6hr1BptcNc_of23p3.png",
+        alt: "Featured Article Image",
+      },
+      readMoreLabel: "Read Story",
+      href: "/stories/article-1",
+    },
+  ],
+  featured: {
+    title: "Battle for gender equality in African media continues",
+    excerpt: [
+      {
+        children: [
+          {
+            text: "Lorem ipsum dolor sit amet consectetur adipiscing elit mattis, vestibulum potenti rhoncus eget lacus fermentum taciti quam, quis curae accumsan viverra semper dapibus sed. ",
+            children: null,
+          },
+        ],
+      },
+    ],
+    publishedOn: "Jan 6, 2022",
+    image: {
+      src: "https://res.cloudinary.com/code-for-africa/image/upload/v1650885664/codeforafrica/unsplash_L6hr1BptcNc_of23p3.png",
+      alt: "Featured Article Image",
+    },
+    readMoreLabel: "Read Story",
+    href: "/stories/article-1",
+  },
   pagination: {
     count: 10,
     page: 1,
