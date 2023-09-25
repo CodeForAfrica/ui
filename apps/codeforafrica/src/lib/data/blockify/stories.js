59deac586556b80d2840c82e7d301ae37603be7a
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-09-25 16:32:32 +0300

    get all tags

--- apps/codeforafrica/src/lib/data/blockify/stories.js
+++ apps/codeforafrica/src/lib/data/blockify/stories.js
@@ -3,9 +3,22 @@ import {
   formatStory,
 } from "@/codeforafrica/lib/data/utils/stories";
 
+function formatTags(tags) {
+  const tagCounts = tags.reduce((counts, tag) => {
+    // eslint-disable-next-line no-param-reassign
+    counts[tag] = (counts[tag] || 0) + 1;
+    return counts;
+  }, {});
+  const sortedTags = Object.keys(tagCounts).sort((a, b) => {
+    return tagCounts[b] - tagCounts[a];
+  });
+  return sortedTags;
+}
+
 async function stories(block, api, context) {
   const { query } = context;
   const { featured, title, labels } = block;
+
   const featuredStory = featured && (formatStory(featured) || null);
   const featuredStorySlug = featuredStory
     ? featuredStory.href.split("/").pop()
@@ -24,10 +37,21 @@ async function stories(block, api, context) {
 
   const { stories: articles, pagination } = await getStories(api, options);
 
+  const { docs: allStories } = await api.getCollection("article", {
+    limit: 0,
+  });
+
+  const allTags = allStories.reduce((acc, story) => {
+    const { tags = [] } = story;
+    return [...acc, ...tags.map((tag) => tag.name)];
+  }, []);
+
+  const tags = formatTags(allTags);
+
   return {
     title,
     labels,
-    tags: [],
+    tags,
     featured: featuredStory || null,
     articles,
     pagination,
