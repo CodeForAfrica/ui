66c854b79007743849e7ceb52aadbea2ce8c7960
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-10-05 14:15:05 +0300

    Fix error pages blank page

--- apps/codeforafrica/src/lib/data/common/index.js
+++ apps/codeforafrica/src/lib/data/common/index.js
@@ -50,94 +50,104 @@ function getPageSlug({ params }) {
 function getDefaultErrorPageProps(slug = "404") {
   if (slug === "500") {
     return {
-      title: "Server Error. ",
-      subtitle: [
+      blocks: [
         {
-          children: [
+          title: "Server Error. ",
+          subtitle: [
             {
-              text: "Don't worry!, you can head back to our ",
-              children: null,
-            },
-            {
-              type: "link",
-              newTab: false,
-              url: "/",
               children: [
                 {
-                  text: "homepage",
+                  text: "Don't worry!, you can head back to our ",
+                  children: null,
+                },
+                {
+                  type: "link",
+                  newTab: false,
+                  url: "/",
+                  children: [
+                    {
+                      text: "homepage",
+                      children: null,
+                    },
+                  ],
+                  href: "/",
+                },
+                {
+                  text: "check out our most recent ",
+                  children: null,
+                },
+                {
+                  type: "link",
+                  newTab: false,
+                  url: "/projects",
+                  children: [
+                    {
+                      text: "projects",
+                      children: null,
+                    },
+                  ],
+                  href: "/projects",
+                },
+                {
+                  text: ", or read below some of the contents produced by our amazing team while the technical team is working on fixing the issue.",
                   children: null,
                 },
               ],
-              href: "/",
-            },
-            {
-              text: "check out our most recent ",
-              children: null,
-            },
-            {
-              type: "link",
-              newTab: false,
-              url: "/projects",
-              children: [
-                {
-                  text: "projects",
-                  children: null,
-                },
-              ],
-              href: "/projects",
-            },
-            {
-              text: ", or read below some of the contents produced by our amazing team while the technical team is working on fixing the issue.",
-              children: null,
             },
           ],
+          slug: "error",
         },
       ],
     };
   }
 
   return {
-    title: "Whoops! This page got lost in conversation! ",
-    subtitle: [
+    blocks: [
       {
-        children: [
+        title: "Whoops! This page got lost in conversation! ",
+        subtitle: [
           {
-            text: "Don't worry!, you can head back to our ",
-            children: null,
-          },
-          {
-            type: "link",
-            newTab: false,
-            url: "/",
             children: [
               {
-                text: "homepage",
+                text: "Don't worry!, you can head back to our ",
+                children: null,
+              },
+              {
+                type: "link",
+                newTab: false,
+                url: "/",
+                children: [
+                  {
+                    text: "homepage",
+                    children: null,
+                  },
+                ],
+                href: "/",
+              },
+              {
+                text: "check out our most recent ",
+                children: null,
+              },
+              {
+                type: "link",
+                newTab: false,
+                url: "/projects",
+                children: [
+                  {
+                    text: "projects",
+                    children: null,
+                  },
+                ],
+                href: "/projects",
+              },
+              {
+                text: ", or read below some of the contents produced by our amazing team.",
                 children: null,
               },
             ],
-            href: "/",
-          },
-          {
-            text: "check out our most recent ",
-            children: null,
-          },
-          {
-            type: "link",
-            newTab: false,
-            url: "/projects",
-            children: [
-              {
-                text: "projects",
-                children: null,
-              },
-            ],
-            href: "/projects",
-          },
-          {
-            text: ", or read below some of the contents produced by our amazing team.",
-            children: null,
           },
         ],
+        slug: "error",
       },
     ],
   };
