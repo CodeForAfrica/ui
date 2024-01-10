4bbe19fe0c1ac67908273a406490d020c56430e3
Kevin Koech <koechkevin92@gmail.com>
2024-01-10 14:00:34 +0300

    Fix linting issues

--- apps/charterafrica/payload.config.ts
+++ apps/charterafrica/payload.config.ts
@@ -111,12 +111,12 @@ export default buildConfig({
   ] as GlobalConfig[],
   ...(locales?.length
     ? {
-      localization: {
-        locales,
-        defaultLocale,
-        fallback: true,
-      },
-    }
+        localization: {
+          locales,
+          defaultLocale,
+          fallback: true,
+        },
+      }
     : undefined),
 
   admin: {
