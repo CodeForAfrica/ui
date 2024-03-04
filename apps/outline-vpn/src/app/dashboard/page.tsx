dce97e45e08d16ab92ab25174a25105cfe43ce60
Kevin Koech <koechkevin92@gmail.com>
2024-03-04 12:52:34 +0300

    Add Setup, authentication of Outline VPN

--- apps/outline-vpn/src/app/dashboard/page.tsx
+++ apps/outline-vpn/src/app/dashboard/page.tsx
@@ -1,13 +1,12 @@
 import Dashboard from "@/outline-vpn/app/components/Dashboard";
 import { getUsers, getHeaderProps } from "@/outline-vpn/app/data";
-import { logoutUser } from "../utils";
+import { redirect } from "next/navigation";
 
 export async function Page(): Promise<JSX.Element | null> {
   const users = await getUsers();
   const header = await getHeaderProps();
-
-  if (!header) {
-    return null;
+  if (!header || !users) {
+    return redirect("/");
   }
   return <Dashboard users={users} header={header} />;
 }
--- apps/outline-vpn/src/middleware.ts
+++ apps/outline-vpn/src/middleware.ts
@@ -1,14 +1,21 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { cookies } from "next/headers";
+import { jwtDecode } from "jwt-decode";
+import { GoogleUser } from "@/outline-vpn/app/types";
 
 export async function middleware(request: NextRequest) {
+  const allowedEmails =
+    process.env.NEXT_PUBLIC_ALLOWED_EMAILS?.split(",") ?? [];
   const url = request.nextUrl.clone();
   try {
     const cookie = cookies().get("token") ?? {};
     const { value: token } = cookie as any;
-    if (token) {
-      return NextResponse.next();
+    const { email } = jwtDecode(token) as GoogleUser;
+    if (email) {
+      if (allowedEmails.includes(email)) {
+        return NextResponse.next();
+      }
     }
     url.pathname = "/";
     return NextResponse.redirect(url);
