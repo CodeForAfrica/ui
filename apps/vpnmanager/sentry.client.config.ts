cc1c5fd6eb163321afafdc091dc5e420e064f1f2
Kevin Koech <koechkevin92@gmail.com>
2024-03-14 15:11:27 +0300

    Rename sentry env variable

--- apps/vpnmanager/sentry.client.config.ts
+++ apps/vpnmanager/sentry.client.config.ts
@@ -1,7 +1,7 @@
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: process.env.NEXT_APP_SENTRY_DSN,
+  dsn: process.env.SENTRY_DSN,
   tracesSampleRate: 1,
   environment: process.env.SENTRY_ENV,
   debug: false,
