f64763bb9ba33c506861a8b365845ff1af3331c0
Vincent Otieno <vincent.otieno@codeforafrica.org>
2024-05-06 12:31:15 +0300

    Apply suggestions from code review
    Co-authored-by: Clemence Kyara <kilemensi@users.noreply.github.com>

--- apps/vpnmanager/sentry.server.config.ts
+++ apps/vpnmanager/sentry.server.config.ts
@@ -1,11 +1,5 @@
 import * as Sentry from "@sentry/nextjs";
 
 export function initSentry() {
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    environment: process.env.SENTRY_ENV ?? "local",
-    tracesSampleRate: 1,
-  });
+  Sentry.init();
 }
-
-initSentry();
