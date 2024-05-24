fdeabc7578302255b73f7d0410c8667bc13d5845
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2024-05-24 17:49:41 +0300

    FIx linting and slim app

--- apps/hurumap/src/functions/replaceMultisitePrefix.js
+++
@@ -1,7 +0,0 @@
-export default function replaceMultisitePrefix(url) {
-  const multsitePrefix = process.env.WORDPRESS_MULTISITE_PREFIX;
-  if (multsitePrefix?.length && url?.startsWith(multsitePrefix)) {
-    return url.replace(multsitePrefix, "");
-  }
-  return url;
-}
