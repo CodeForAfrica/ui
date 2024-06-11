a9bce0e3ac064285301ee9a7c18d393d533a6332
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2024-06-11 16:39:59 +0300

    Improve Delays component layout and spacing
    Signed-off-by: Kipruto <43873157+kelvinkipruto@users.noreply.github.com>

--- apps/robots-generator/src/components/Delays/Delays.tsx
+++ apps/robots-generator/src/components/Delays/Delays.tsx
@@ -61,47 +61,58 @@ export default function Delays({
           py: 2,
         }}
       >
-        <Stack spacing={2} alignItems="center" sx={{ width: "100%", mb: 2 }}>
-          <InputLabel
-            sx={{
-              color: "text.primary",
-              fontSize: "16px",
-              width: "100%",
-            }}
-          >
-            Crawl delay
-            <Tooltip title="The crawl delay directive specifies the minimum time between requests to your server from a bot.">
-              <IconButton color="info">
-                <InfoIcon />
-              </IconButton>
-            </Tooltip>
-          </InputLabel>
-          <Input
-            onChange={handleCrawlDelayChange}
-            initialValue={crawlDelay ? crawlDelay.toString() : ""}
-            sx={{ width: "100%" }}
-          />
-        </Stack>
-        <Stack spacing={2} alignItems="center" sx={{ width: "100%", mb: 2 }}>
-          <InputLabel
-            sx={{
-              color: "text.primary",
-              fontSize: "16px",
-              width: "100%",
-            }}
-          >
-            Cache delay
-            <Tooltip title="The cache delay directive specifies the time that a cached copy of a page should be considered fresh.">
-              <IconButton color="info">
-                <InfoIcon />
-              </IconButton>
-            </Tooltip>
-          </InputLabel>
-          <Input
-            onChange={handleCacheDelayChange}
-            initialValue={cachedDelay ? cachedDelay.toString() : ""}
-            sx={{ width: "100%" }}
-          />
+        <Stack
+          spacing={2}
+          alignItems="center"
+          justifyContent="space-between"
+          direction={{
+            xs: "column",
+            md: "row",
+          }}
+          sx={{ width: "100%", mb: 2 }}
+        >
+          <Stack spacing={2} alignItems="center" sx={{ width: "100%", mb: 2 }}>
+            <InputLabel
+              sx={{
+                color: "text.primary",
+                fontSize: "16px",
+                width: "100%",
+              }}
+            >
+              Crawl delay
+              <Tooltip title="The crawl delay directive specifies the minimum time between requests to your server from a bot.">
+                <IconButton color="info">
+                  <InfoIcon />
+                </IconButton>
+              </Tooltip>
+            </InputLabel>
+            <Input
+              onChange={handleCrawlDelayChange}
+              initialValue={crawlDelay ? crawlDelay.toString() : ""}
+              sx={{ width: "100%" }}
+            />
+          </Stack>
+          <Stack spacing={2} alignItems="center" sx={{ width: "100%", mb: 2 }}>
+            <InputLabel
+              sx={{
+                color: "text.primary",
+                fontSize: "16px",
+                width: "100%",
+              }}
+            >
+              Cache delay
+              <Tooltip title="The cache delay directive specifies the time that a cached copy of a page should be considered fresh.">
+                <IconButton color="info">
+                  <InfoIcon />
+                </IconButton>
+              </Tooltip>
+            </InputLabel>
+            <Input
+              onChange={handleCacheDelayChange}
+              initialValue={cachedDelay ? cachedDelay.toString() : ""}
+              sx={{ width: "100%" }}
+            />
+          </Stack>
         </Stack>
         <Stack spacing={2} alignItems="center" sx={{ width: "100%", mb: 2 }}>
           <InputLabel
