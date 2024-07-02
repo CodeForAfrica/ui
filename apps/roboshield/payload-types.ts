48f6f6bd2e88677f68d6bef0ab30b53b25331444
Michael Hudson Nkotagu <mnkotagu@gmail.com>
2024-07-02 16:38:09 +0300

    Auto generate updated Payload Types

--- apps/roboshield/payload-types.ts
+++ apps/roboshield/payload-types.ts
@@ -11,11 +11,11 @@ export interface Config {
     media: Media;
     pages: Page;
     users: User;
-    "payload-preferences": PayloadPreference;
-    "payload-migrations": PayloadMigration;
+    'payload-preferences': PayloadPreference;
+    'payload-migrations': PayloadMigration;
   };
   globals: {
-    "settings-site": SettingsSite;
+    'settings-site': SettingsSite;
   };
 }
 /**
@@ -47,13 +47,31 @@ export interface Page {
   fullTitle?: string | null;
   slug?: string | null;
   blocks?:
-    | {
-        title: string;
-        subtitle: string;
-        id?: string | null;
-        blockName?: string | null;
-        blockType: "page-header";
-      }[]
+    | (
+        | {
+            title: string;
+            subtitle: string;
+            id?: string | null;
+            blockName?: string | null;
+            blockType: 'page-header';
+          }
+        | {
+            heroHeaders?:
+              | {
+                  headingType?: ('largeHeading' | 'subHeading' | 'rotatingText') | null;
+                  title?: string | null;
+                  id?: string | null;
+                }[]
+              | null;
+            heroDescriptiveText: {
+              [k: string]: unknown;
+            }[];
+            heroButtonText?: string | null;
+            id?: string | null;
+            blockName?: string | null;
+            blockType: 'page-hero';
+          }
+      )[]
     | null;
   meta?: {
     title?: string | null;
@@ -71,7 +89,7 @@ export interface Page {
     | null;
   updatedAt: string;
   createdAt: string;
-  _status?: ("draft" | "published") | null;
+  _status?: ('draft' | 'published') | null;
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
@@ -81,7 +99,7 @@ export interface User {
   id: string;
   firstName: string;
   lastName: string;
-  roles: ("admin" | "editor")[];
+  roles: ('admin' | 'editor')[];
   updatedAt: string;
   createdAt: string;
   email: string;
@@ -102,7 +120,7 @@ export interface User {
 export interface PayloadPreference {
   id: string;
   user: {
-    relationTo: "users";
+    relationTo: 'users';
     value: string | User;
   };
   key?: string | null;
@@ -145,9 +163,9 @@ export interface SettingsSite {
     menus?:
       | {
           label: string;
-          linkType?: ("custom" | "internal") | null;
+          linkType?: ('custom' | 'internal') | null;
           doc?: {
-            relationTo: "pages";
+            relationTo: 'pages';
             value: string | Page;
           } | null;
           url?: string | null;
@@ -155,17 +173,15 @@ export interface SettingsSite {
           id?: string | null;
         }[]
       | null;
-    connect?:
-      | ("Facebook" | "Twitter" | "Instagram" | "Linkedin" | "Github" | "Slack")
-      | null;
+    connect?: ('Facebook' | 'Twitter' | 'Instagram' | 'Linkedin' | 'Github' | 'Slack') | null;
   };
   secondaryNavigation?: {
     menus?:
       | {
           label: string;
-          linkType?: ("custom" | "internal") | null;
+          linkType?: ('custom' | 'internal') | null;
           doc?: {
-            relationTo: "pages";
+            relationTo: 'pages';
             value: string | Page;
           } | null;
           url?: string | null;
@@ -178,13 +194,7 @@ export interface SettingsSite {
     title: string;
     links?:
       | {
-          platform:
-            | "Facebook"
-            | "Twitter"
-            | "Instagram"
-            | "Linkedin"
-            | "Github"
-            | "Slack";
+          platform: 'Facebook' | 'Twitter' | 'Instagram' | 'Linkedin' | 'Github' | 'Slack';
           url: string;
           id?: string | null;
         }[]
@@ -204,9 +214,9 @@ export interface SettingsSite {
           name: string;
           logo: string | Media;
           label: string;
-          linkType?: ("custom" | "internal") | null;
+          linkType?: ('custom' | 'internal') | null;
           doc?: {
-            relationTo: "pages";
+            relationTo: 'pages';
             value: string | Page;
           } | null;
           url?: string | null;
@@ -219,6 +229,7 @@ export interface SettingsSite {
   createdAt?: string | null;
 }
 
-declare module "payload" {
+
+declare module 'payload' {
   export interface GeneratedTypes extends Config {}
 }
