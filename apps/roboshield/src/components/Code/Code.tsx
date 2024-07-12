0bf24085c36be0feb2b92d32fddaf761069fedf8
Michael Hudson Nkotagu <mnkotagu@gmail.com>
2024-07-12 08:07:51 +0300

    fix: Add icons to action buttons

--- apps/roboshield/src/components/Code/Code.tsx
+++ apps/roboshield/src/components/Code/Code.tsx
@@ -1,4 +1,9 @@
-import { Box, Button, Grid, Stack } from "@mui/material";
+import { Box, Button, Grid, Stack, SvgIcon } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import CopyIcon from "@/roboshield/assets/icons/Type=copy, Size=24, Color=CurrentColor.svg";
+import SaveIcon from "@/roboshield/assets/icons/Type=save, Size=24, Color=CurrentColor.svg";
+import BackIcon from "@/roboshield/assets/icons/Type=back, Size=24, Color=CurrentColor.svg";
+import ResetIcon from "@/roboshield/assets/icons/Type=reset, Size=24, Color=CurrentColor.svg";
 
 import CodeEditor from "./CodeEditor";
 
@@ -28,6 +33,8 @@ export default function Code(props: CodeProps) {
   const handleCodeChange = (newCode: string) => {
     onCodeChange(newCode);
   };
+  const theme = useTheme();
+
   return (
     <Box
       sx={{
@@ -47,7 +54,20 @@ export default function Code(props: CodeProps) {
             <Button
               variant="contained"
               color="primary"
-              sx={{ mt: 2 }}
+              sx={{
+                mt: 2,
+                "&:hover .MuiSvgIcon-root path": {
+                  stroke: theme.palette.primary.main,
+                },
+              }}
+              startIcon={
+                <SvgIcon
+                  component={CopyIcon}
+                  sx={{
+                    fill: "transparent",
+                  }}
+                />
+              }
               onClick={onCopy}
               disabled={!showButtons}
             >
@@ -55,6 +75,14 @@ export default function Code(props: CodeProps) {
             </Button>
             <Button
               variant="contained"
+              startIcon={
+                <SvgIcon
+                  component={SaveIcon}
+                  sx={{
+                    fill: "transparent",
+                  }}
+                />
+              }
               sx={{
                 mt: 2,
                 background: "#000000",
@@ -62,6 +90,9 @@ export default function Code(props: CodeProps) {
                 "&:hover": {
                   background: "none",
                   color: "#000000",
+                },
+                "&:hover .MuiSvgIcon-root path": {
+                  stroke: theme.palette.text.primary,
                 },
               }}
               onClick={onDownload}
@@ -71,6 +102,17 @@ export default function Code(props: CodeProps) {
             </Button>
             <Button
               variant="outlined"
+              startIcon={
+                <SvgIcon
+                  component={BackIcon}
+                  sx={{
+                    stroke: "red",
+                    fill: "white",
+                    color: "red",
+                    transition: "stroke 0.3s, fill 0.3s",
+                  }}
+                />
+              }
               color="primary"
               sx={{
                 mt: 2,
@@ -94,13 +136,24 @@ export default function Code(props: CodeProps) {
           >
             <Button
               variant="contained"
+              startIcon={
+                <SvgIcon
+                  component={ResetIcon}
+                  sx={{
+                    fill: "transparent",
+                  }}
+                />
+              }
               sx={{
-                display: "inline-block",
-                background: "#FE2500",
-                border: "1px solid #FE2500",
+                mt: 2,
+                background: theme.palette.error.main,
+                border: `1px solid ${theme.palette.error.main}`,
                 "&:hover": {
                   background: "none",
-                  color: "#FE2500",
+                  color: theme.palette.error.main,
+                },
+                "&:hover .MuiSvgIcon-root path": {
+                  stroke: theme.palette.error.main,
                 },
               }}
               onClick={onReset}
