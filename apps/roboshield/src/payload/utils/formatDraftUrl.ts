56e07718fe9d634f6a2e84e90b8d2059a014626c
Kevin Koech <koechkevin92@gmail.com>
2024-07-12 11:33:30 +0300

    Fix URLS error

--- apps/roboshield/src/payload/utils/formatDraftUrl.ts
+++ apps/roboshield/src/payload/utils/formatDraftUrl.ts
@@ -1,15 +1,13 @@
 import formatPagePath from "./formatPagePath";
 import { Document } from "payload/types";
 
-const payloadUrl = process.env.PAYLOAD_PUBLIC_APP_URL || process.env.NEXT_PUBLIC_APP_URL;
+const payloadUrl =
+  process.env.PAYLOAD_PUBLIC_APP_URL || process.env.NEXT_PUBLIC_APP_URL;
 function formatDraftUrl(collection: string, doc: Document) {
   const pagePath = formatPagePath(collection, doc);
   if (pagePath) {
     const slug = pagePath;
-    const url = new URL(
-      `/api/draft?slug=${slug}`,
-      payloadUrl,
-    );
+    const url = new URL(`/api/draft?slug=${slug}`, payloadUrl);
     return url.href;
   }
   return null;
