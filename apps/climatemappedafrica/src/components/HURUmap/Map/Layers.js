087156aa39819c730b5fe24e1980943313a664ef
Clemence Kyara <clemence@codeforafrica.org>
2024-07-15 08:48:52 +0300

    Use @hurumap/core/LocationTag in climatemappedafrica

--- apps/climatemappedafrica/src/components/HURUmap/Map/Layers.js
+++ apps/climatemappedafrica/src/components/HURUmap/Map/Layers.js
@@ -1,6 +1,5 @@
 import { LocationTag } from "@hurumap/core";
 import { ThemeProvider, StyledEngineProvider } from "@mui/material/styles";
-import makeStyles from "@mui/styles/makeStyles";
 import L from "leaflet";
 import PropTypes from "prop-types";
 import React, { useCallback, useEffect, useRef } from "react";
@@ -12,14 +11,6 @@ import theme, {
   CHART_PRIMARY_COLOR_SCHEME,
   CHART_SECONDARY_COLOR_SCHEME,
 } from "@/climatemappedafrica/theme";
-
-const useStyles = makeStyles(() => ({
-  locationtag: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-  },
-}));
 
 const primaryGeoStyles = {
   inactive: {
@@ -100,12 +91,10 @@ function Layers({
   parentsGeometries,
   secondaryGeography,
   selectedBoundary,
-  ...props
 }) {
   const map = useMap();
   const groupRef = useRef();
   const siblingRef = useRef();
-  const classes = useStyles(props);
 
   const pinIcon = L.divIcon({
     html: ReactDOMServer.renderToStaticMarkup(
@@ -114,13 +103,22 @@ function Layers({
           <LocationTag
             IconButtonProps={{
               children: <CancelIcon />,
+              sx: {
+                left: 0,
+                position: "absolute",
+                top: 0,
+                "&:hover": {
+                  color: "#666666",
+                  "& .Component108-4_svg__b": {
+                    stroke: theme.palette.text?.secondary,
+                  },
+                },
+              },
             }}
             level={geography?.level}
             name={geography?.name?.toLowerCase()}
             code={geography?.code}
-            classes={{ root: classes.locationtag }}
             color="primary"
-            variant="marker"
           />
         </ThemeProvider>
       </StyledEngineProvider>,
@@ -147,10 +145,14 @@ function Layers({
             <StyledEngineProvider injectFirst>
               <ThemeProvider theme={theme}>
                 <LocationTag
+                  color={isPinOrCompare ? "secondary" : "primary"}
                   level={level}
                   name={name.toLowerCase()}
-                  classes={{ root: classes.locationtag }}
-                  color={isPinOrCompare ? "secondary" : "primary"}
+                  sx={{
+                    left: 0,
+                    position: "absolute",
+                    top: 0,
+                  }}
                 />
               </ThemeProvider>
             </StyledEngineProvider>,
@@ -212,14 +214,7 @@ function Layers({
         }
       }
     },
-    [
-      classes.locationtag,
-      geography,
-      isPinOrCompare,
-      secondaryGeography,
-      locationCodes,
-      onClick,
-    ],
+    [geography, isPinOrCompare, secondaryGeography, locationCodes, onClick],
   );
 
   useEffect(() => {
