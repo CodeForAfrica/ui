bfe5d36c0ec04ebb01fa759ad81d1ee1bef217fb
Kevin Koech <koechkevin92@gmail.com>
2024-07-18 15:11:51 +0300

    update tooltip

--- packages/hurumap-core/src/ChartTooltip/ChartTooltip.snap.js
+++ packages/hurumap-core/src/ChartTooltip/ChartTooltip.snap.js
@@ -3,16 +3,20 @@
 exports[`ChartTooltip renders unchanged 1`] = `
 <div>
   <div
-    class="MuiGrid-root MuiGrid-container css-1j8tqrs-MuiGrid-root"
+    class="MuiGrid-root MuiGrid-container css-11lq3yg-MuiGrid-root"
+    style="box-shadow: 0px 3px 6px #00000029; border-radius: 0.25rem; opacity: 0.8; color: rgba(0, 0, 0, 0.6); padding: 0.78125rem 0px 0.78125rem 0.78125rem; display: inline-block;"
   >
     <div
-      class="MuiGrid-root MuiGrid-item css-ev762u-MuiGrid-root"
+      class="MuiGrid-root MuiGrid-item css-13i4rnv-MuiGrid-root"
+      style="display: flex; align-items: center;"
     >
       <div
-        class="MuiTypography-root MuiTypography-body2 css-qbjx02-MuiTypography-root"
+        class="MuiTypography-root MuiTypography-body2 css-e784if-MuiTypography-root"
+        style="margin-right: 0.78125rem; max-width: 9.25rem;"
       />
       <div
-        class="MuiTypography-root MuiTypography-body2 css-qbjx02-MuiTypography-root"
+        class="MuiTypography-root MuiTypography-body2 css-e784if-MuiTypography-root"
+        style="margin-right: 0.78125rem; max-width: 9.25rem;"
       />
     </div>
   </div>
