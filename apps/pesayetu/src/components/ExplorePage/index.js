31e5aea881183b3fe2b5bc94711cf1a8149b00b3
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2024-07-18 11:18:33 +0300

    Revert Back to main
    Signed-off-by: Kipruto <43873157+kelvinkipruto@users.noreply.github.com>

--- apps/pesayetu/src/components/ExplorePage/index.js
+++ apps/pesayetu/src/components/ExplorePage/index.js
@@ -1,6 +1,4 @@
-import { LocationTag, LocationHighlight } from "@hurumap/core";
 import { Box } from "@mui/material";
-import { alpha } from "@mui/material/styles";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 import PropTypes from "prop-types";
@@ -10,6 +8,7 @@ import useExplore from "./useExplore";
 import useProfileGeography from "./useProfileGeography";
 import useStyles from "./useStyles";
 
+import Location from "@/pesayetu/components/HURUmap/Location";
 import Panel from "@/pesayetu/components/HURUmap/Panel";
 
 const Map = dynamic(() => import("@/pesayetu/components/HURUmap/Map"), {
@@ -135,73 +134,12 @@ function ExplorePage({ panelProps, profile: profileProp, ...props }) {
             {...props}
             className={classes.map}
           />
-          <Box
-            sx={(theme) => ({
-              display: "none",
-              [theme.breakpoints.up("md")]: {
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              },
-              left: 0,
-              margin: "0 auto",
-              position: "absolute",
-              right: 0,
-              top: theme.typography.pxToRem(52),
-              zIndex: theme.zIndex.appBar,
-              background: alpha("#FFFFFF", 0.9), // #FFFFFFE6
-              borderRadius: theme.typography.pxToRem(5),
-              bottom: "auto",
-              boxShadow: `0px 3px 6px ${alpha("#000000", 0.16)}`, // #00000029
-              padding: `${theme.typography.pxToRem(4.12)} ${theme.typography.pxToRem(
-                19,
-              )} ${theme.typography.pxToRem(12)} ${theme.typography.pxToRem(21)}`,
-              width: theme.typography.pxToRem(600),
-            })}
-          >
-            <Box display="flex" flexWrap="nowrap" justifyContent="center">
-              {tags.map((tag, index) => (
-                <LocationTag
-                  key={`${tag.level}-${tag.name}`}
-                  isLoading={isLoading}
-                  {...tag}
-                  active={index === tags.length - 1}
-                  variant="highlight"
-                  sx={(theme) => ({
-                    "&:not(:first-of-type)": {
-                      marginLeft: theme.typography.pxToRem(10),
-                    },
-                  })}
-                />
-              ))}
-            </Box>
-            {highlights?.length > 0 ? (
-              <Box
-                display="flex"
-                flexWrap="nowrap"
-                justifyContent="center"
-                sx={(theme) => ({
-                  borderTop: `1px solid ${theme.palette.grey.main}`,
-                  marginTop: "4.5px",
-                  width: "100%",
-                })}
-              >
-                {highlights.map((highlight) => (
-                  <LocationHighlight
-                    key={highlight.title}
-                    isLoading={isLoading}
-                    {...highlight}
-                    sx={(theme) => ({
-                      paddingTop: "4.5px",
-                      "&:not(:first-of-type)": {
-                        borderLeft: `1px solid ${theme.palette.grey.main}`,
-                      },
-                    })}
-                  />
-                ))}
-              </Box>
-            ) : null}
-          </Box>
+          <Location
+            highlights={highlights}
+            isLoading={isLoading}
+            tags={tags}
+            className={classes.location}
+          />
         </div>
       </Box>
       <Panel
