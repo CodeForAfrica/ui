02c6c88672172f21f7f985b4e39702edad75316c
Kevin Koech <koechkevin92@gmail.com>
2024-07-18 08:38:53 +0300

    Fix failing tests

---
+++ packages/hurumap-core/src/ChartTooltip/ChartTooltip.snap.js
@@ -0,0 +1,20 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`ChartTooltip renders unchanged 1`] = `
+<div>
+  <div
+    class="MuiGrid-root MuiGrid-container css-1j8tqrs-MuiGrid-root"
+  >
+    <div
+      class="MuiGrid-root MuiGrid-item css-ev762u-MuiGrid-root"
+    >
+      <div
+        class="MuiTypography-root MuiTypography-body2 css-qbjx02-MuiTypography-root"
+      />
+      <div
+        class="MuiTypography-root MuiTypography-body2 css-qbjx02-MuiTypography-root"
+      />
+    </div>
+  </div>
+</div>
+`;
