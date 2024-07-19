1340ebfe1b45375ebc3ee6c86f1ba45807cdd3e8
Muma David Bwalya <muma.bwalya@codeforafrica.org>
2024-07-19 12:26:11 +0200

    @engineering-blog: Completed initial setup

---
+++ apps/engineering-blog/src/utils/AppConfig.ts
@@ -0,0 +1,8 @@
+export const AppConfig = {
+	site_name: 'Code For Africa Engineering',
+	title: 'CFA Engineering',
+	description: 'A blog dedicated to engineering at Code For Africa',
+	author: 'CFA Tech Team',
+	locale: 'en',
+	locale_region: 'en-gb',
+};
