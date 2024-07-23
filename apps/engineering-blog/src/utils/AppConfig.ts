b09f3aad31af4b68281bf910d3217850a3380993
Muma David Bwalya <muma.bwalya@codeforafrica.org>
2024-07-23 10:32:08 +0200

    #791: Deleted Astro project

--- apps/engineering-blog/src/utils/AppConfig.ts
+++
@@ -1,8 +0,0 @@
-export const AppConfig = {
-	site_name: 'Code For Africa Engineering',
-	title: 'CFA Engineering',
-	description: 'A blog dedicated to engineering at Code For Africa',
-	author: 'CFA Tech Team',
-	locale: 'en',
-	locale_region: 'en-gb',
-};
