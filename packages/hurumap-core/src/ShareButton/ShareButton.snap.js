59157ae393ab2acb8c83f74a9d312f7429ae474c
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2024-07-26 16:22:41 +0300

    Fix failing tests
    Signed-off-by: Kipruto <43873157+kelvinkipruto@users.noreply.github.com>

--- packages/hurumap-core/src/ShareButton/ShareButton.snap.js
+++ packages/hurumap-core/src/ShareButton/ShareButton.snap.js
@@ -11,15 +11,10 @@ exports[`ShareButton renders unchanged 1`] = `
     >
       <svg
         aria-hidden="true"
-        class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root"
-        data-testid="FacebookIcon"
+        class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-wsb35j-MuiSvgIcon-root"
         focusable="false"
         viewBox="0 0 24 24"
-      >
-        <path
-          d="M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2m13 2h-2.5A3.5 3.5 0 0 0 12 8.5V11h-2v3h2v7h3v-7h3v-3h-3V9a1 1 0 0 1 1-1h2V5z"
-        />
-      </svg>
+      />
     </button>
   </div>
 </div>
