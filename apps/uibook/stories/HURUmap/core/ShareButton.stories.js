d0a2c64a2b7f46b7dc4025460adea4916605dd92
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2024-07-29 11:16:53 +0300

    Fix missing storybook icons
    Signed-off-by: Kipruto <43873157+kelvinkipruto@users.noreply.github.com>

--- apps/uibook/stories/HURUmap/core/ShareButton.stories.js
+++ apps/uibook/stories/HURUmap/core/ShareButton.stories.js
@@ -1,4 +1,11 @@
 import { ShareButton } from "@hurumap/core";
+import EmailIcon from "@mui/icons-material/Email";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import XIcon from "@mui/icons-material/X";
 import React from "react";
 
 export default {
@@ -21,8 +28,19 @@ export default {
   },
 };
 
-function Template({ ...args }) {
-  return <ShareButton {...args} />;
+const iconMapping = {
+  Facebook: FacebookIcon,
+  Twitter: XIcon,
+  LinkedIn: LinkedInIcon,
+  WhatsApp: WhatsAppIcon,
+  Email: EmailIcon,
+  Telegram: TelegramIcon,
+  Pinterest: PinterestIcon,
+};
+
+function Template({ name, ...args }) {
+  const IconComponent = iconMapping[name];
+  return <ShareButton {...args} name={name} icon={IconComponent} />;
 }
 
 export const Default = Template.bind({});
