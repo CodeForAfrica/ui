9719f5528bf091aed7acafc105cd7c6c4424536e
Michael Hudson Nkotagu <mnkotagu@gmail.com>
2024-08-01 13:42:22 +0300

    chore: Move LineChartScope to Hurumap-core

--- packages/hurumap-core/src/Scope/index.js
+++ packages/hurumap-core/src/Scope/index.js
@@ -1,7 +1,9 @@
 import BarChartScope from "./BarChartScope";
+import LineChartScope from "./LineChartScope";
 import Scope from "./Scope";
 
 export default {
   Scope,
   BarChartScope,
+  LineChartScope,
 };
