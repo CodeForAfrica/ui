ca803d6f3b1966e405ee7051c8226c28345b4797
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2024-08-01 14:18:38 +0300

    Move button component
    Signed-off-by: Kipruto <43873157+kelvinkipruto@users.noreply.github.com>

--- packages/hurumap-core/src/index.js
+++ packages/hurumap-core/src/index.js
@@ -5,7 +5,6 @@ export { default as LocationHighlight } from "./LocationHighlight";
 export { default as Location } from "./Location";
 export { default as IndicatorTitle } from "./IndicatorTitle";
 export { default as Scope } from "./Scope";
-export { default as ShareButton } from "./ShareButton";
 export { default as Share } from "./Share";
 export { default as Action } from "./Action";
 export { default as Download } from "./Download";
