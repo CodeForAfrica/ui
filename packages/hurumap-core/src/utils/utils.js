2901835a4e211d09b217236080aa4b50e8a49f46
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2024-08-05 10:54:53 +0300

    Remove unused file
    Signed-off-by: Kipruto <43873157+kelvinkipruto@users.noreply.github.com>

--- packages/hurumap-core/src/utils/utils.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export function idify(string) {
-  return string
-    .replace(/^\s+|\s+$/g, "")
-    .replace(/[^a-z0-9]/g, "")
-    .replace(/\s+/g, "_")
-    .replace(/_+/g, "_");
-}
