4746b1e305a512a680d984502873821b794cf5be
Muma Bwalya <muma.bwalya@codeforafrica.org>
2024-08-20 11:09:15 +0200

    #blog: added .mdx files to lint rules

--- apps/engineeringblog/.lintstagedrc.js
+++ apps/engineeringblog/.lintstagedrc.js
@@ -8,7 +8,7 @@ const buildEslintCommand = (filenames) =>
 module.exports = {
   // Since we don't have eslint json/md plugins installed in this app, we can't
   // use the eslint to lint json,md here
-  "*.{json,md}": ["prettier --write"],
+  "*.{json,md,mdx}": ["prettier --write"],
   "*.{yaml,yml}": "prettier --write",
   "*.{js,mjs,cjs,jsx,ts,mts,cts,tsx,mdx}": [buildEslintCommand],
 };
