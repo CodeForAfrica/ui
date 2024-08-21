7e5121c9b9f5e632519148ba76cc661bc2ad209e
Code Is Art <18518167+DavidTheProgrammer@users.noreply.github.com>
2024-08-21 08:35:03 +0200

    Update apps/engineeringblog/next.config.mjs
    Remove js extensions
    
    Co-authored-by: Clemence Kyara <kilemensi@users.noreply.github.com>

--- apps/engineeringblog/next.config.mjs
+++ apps/engineeringblog/next.config.mjs
@@ -2,7 +2,7 @@ import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  pageExtensions: ["mdx", "tsx"],
 };
 
 const withMDX = createMDX({
