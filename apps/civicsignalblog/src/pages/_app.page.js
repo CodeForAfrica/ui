667c1a11887c26678f4f80facbd473e376e29756
Clemence Kyara <clemence@codeforafrica.org>
2024-08-22 11:27:57 +0300

    Switch to using next.js's ga analytics package

--- apps/civicsignalblog/src/pages/_app.page.js
+++ apps/civicsignalblog/src/pages/_app.page.js
@@ -1,16 +1,15 @@
 import { CacheProvider } from "@emotion/react";
-import { ThemeProvider, CssBaseline } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import Head from "next/head";
-import { useRouter } from "next/router";
-import Script from "next/script";
 import { DefaultSeo } from "next-seo";
 import PropTypes from "prop-types";
-import React, { useEffect } from "react";
+import React from "react";
 
 import Page from "@/civicsignalblog/components/Page";
 import SEO from "@/civicsignalblog/next-seo.config";
-import "@/civicsignalblog/theme/fonts.css";
 import theme from "@/civicsignalblog/theme";
+import "@/civicsignalblog/theme/fonts.css";
 import createEmotionCache from "@/civicsignalblog/utils/createEmotionCache";
 
 const clientSideEmotionCache = createEmotionCache();
@@ -19,26 +18,11 @@ function getDefaultLayout(page, pageProps) {
   return <Page {...pageProps}>{page}</Page>;
 }
 
-function MyApp(props) {
-  const router = useRouter();
+function App(props) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   const getLayout = Component.getLayout || getDefaultLayout;
-  useEffect(() => {
-    if (process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID) {
-      const handleRouteChange = (url) => {
-        /* eslint-env browser */
-        window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID, {
-          page_path: url,
-        });
-      };
-      router.events.on("routeChangeComplete", handleRouteChange);
-
-      return () => {
-        router.events.off("routeChangeComplete", handleRouteChange);
-      };
-    }
-    return undefined;
-  }, [router.events]);
+  const { analytics } = pageProps;
+  const { analyticsId: gaId } = analytics || {};
 
   return (
     <>
@@ -46,40 +30,21 @@ function MyApp(props) {
       <CacheProvider value={emotionCache}>
         <Head>
           <meta name="viewport" content="initial-scale=1, width=device-width" />
-          <script
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{
-              __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments)}
-          gtag('js', new Date());
-
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
-          `,
-            }}
-          />
         </Head>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           {getLayout(<Component {...pageProps} />, pageProps)}
         </ThemeProvider>
       </CacheProvider>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-      />
+      <GoogleAnalytics gaId={gaId} />
     </>
   );
 }
 
-MyApp.propTypes = {
+App.propTypes = {
   Component: PropTypes.elementType.isRequired,
   emotionCache: PropTypes.shape({}),
   pageProps: PropTypes.shape({}).isRequired,
 };
 
-MyApp.defaultProps = {
-  emotionCache: undefined,
-};
-
-export default MyApp;
+export default App;
