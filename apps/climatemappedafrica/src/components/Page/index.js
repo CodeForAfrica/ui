94f0a1e08081ea6ac99bc4136424d09de7894d73
Michael Hudson Nkotagu <mnkotagu@gmail.com>
2024-08-28 15:18:20 +0300

    chore: Remove default props in Page/index.js

--- apps/climatemappedafrica/src/components/Page/index.js
+++ apps/climatemappedafrica/src/components/Page/index.js
@@ -7,16 +7,12 @@ import Base from "./Base";
  * Page component that adds error handling.
  * TODO(kilemensi): Add error handling once we have error page designs
  */
-function Page({ errorCode, ...props }) {
+function Page({ ...props }) {
   return <Base {...props} />;
 }
 
 Page.propTypes = {
   errorCode: PropTypes.number,
-};
-
-Page.defaultProps = {
-  errorCode: undefined,
 };
 
 export default Page;
