93965a000687eed88e4caab7e609b18b55c39807
Clemence Kyara <clemence@codeforafrica.org>
2024-08-28 16:34:28 +0300

    Improve our next/jest config

--- packages/jest-config-commons-ui/next.js
+++ packages/jest-config-commons-ui/next.js
@@ -1,9 +1,19 @@
 const defaultConfig = require(".");
 
-const { testPathIgnorePatterns } = defaultConfig;
+const { moduleNameMapper, testPathIgnorePatterns } = defaultConfig;
 
 module.exports = {
   ...defaultConfig,
+  moduleNameMapper: {
+    ...moduleNameMapper,
+
+    // Handle @next/font
+    "@next/font/(.*)": `<rootDir>/__mocks__/nextFontMock.js`,
+    // Handle next/font
+    "next/font/(.*)": `<rootDir>/__mocks__/nextFontMock.js`,
+    // Disable server-only
+    "server-only": `<rootDir>/__mocks__/empty.js`,
+  },
   testPathIgnorePatterns: [...testPathIgnorePatterns, "<rootDir>/.next/"],
   transform: {
     // Use babel-jest to transpile tests with the next/babel preset
