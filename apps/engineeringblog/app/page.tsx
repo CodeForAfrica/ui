24fd71e3ef3d15250fe971721b2addcb2c887914
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2024-09-02 12:07:25 +0300

    Render blog
    Signed-off-by: Kipruto <43873157+kelvinkipruto@users.noreply.github.com>

--- apps/engineeringblog/app/page.tsx
+++ apps/engineeringblog/app/page.tsx
@@ -1,9 +1,42 @@
 import { Box, Container } from "@mui/material";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
 
-export default function index() {
+export function getAllPosts() {
+  const postsDirectory = path.join(process.cwd(), "content");
+  const fileNames = fs
+    .readdirSync(postsDirectory)
+    .filter((fileName) => fileName.endsWith(".mdx"));
+
+  return fileNames.map((fileName) => {
+    const filePath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(filePath, "utf8");
+    const { data } = matter(fileContents);
+
+    return {
+      slug: fileName.replace(/\.mdx$/, ""),
+      title: data.title,
+      description: data.description,
+      date: data.date,
+      featuredImage: data?.featuredImage,
+    };
+  });
+}
+
+export default async function index() {
+  const posts = await getAllPosts();
   return (
     <Container>
-      <Box minHeight="1000px">Homepage</Box>
+      <Box>
+        {posts.map((post) => (
+          <Box key={post.slug}>
+            <h2>{post.title}</h2>
+            <p>{post.description}</p>
+            <p>{post.date}</p>
+          </Box>
+        ))}
+      </Box>
     </Container>
   );
 }
