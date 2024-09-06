db77fcb55600bf7ca8392e84b48918d9248e1726
Clemence Kyara <clemence@codeforafrica.org>
2024-09-06 14:11:22 +0300

    Export StyledLink and PagesRouterLink from package

--- packages/commons-ui-next/src/index.js
+++ packages/commons-ui-next/src/index.js
@@ -1,5 +1,6 @@
-/* eslint-disable import/prefer-default-export */
-
 export { default as Figure } from "./Figure";
+
 export { default as Link } from "./Link";
+export * from "./Link";
+
 export { default as RichTypography } from "./RichTypography";
