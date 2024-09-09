c8c6cadeec628bca6c31587df0663c5962609a1f
Clemence Kyara <clemence@codeforafrica.org>
2024-09-09 11:52:53 +0300

    Standardise public interfaces and ensure get methods are async

--- apps/engineeringblog/utils/index.ts
+++ apps/engineeringblog/utils/index.ts
@@ -1,59 +1,69 @@
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
 import { format } from "date-fns";
-export type Article = {
-  slug: string;
+import { promises as fs } from "fs";
+import matter from "gray-matter";
+import path from "path";
+
+type MdFileContentProps = {
   title: string;
-  description: string;
-  publishDate: string;
+  excerpt: string;
+  publishedDate: string;
   featuredImage: string;
   content: string;
 };
 
-export type ArticleWithoutContent = Omit<Article, "content">;
-
-export function getAllContents(): ArticleWithoutContent[] {
-  const postsDirectory = path.join(process.cwd(), "content");
-  const fileNames = fs
-    .readdirSync(postsDirectory)
-    .filter((fileName) => fileName.endsWith(".mdx"));
-
-  const posts = fileNames.map((fileName) => {
-    const filePath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(filePath, "utf8");
-    const { data } = matter(fileContents);
-
-    return {
-      slug: fileName.replace(/\.mdx$/, ""),
-      title: data.title,
-      description: data.description,
-      publishDate: data.publishDate,
-      formattedDate: format(new Date(data.publishDate), "MMM dd, yyyy"),
-      featuredImage: data?.featuredImage,
-    };
-  });
-
-  posts.sort(
-    (a, b) =>
-      new Date(b.publishDate).getTime() - new Date(a.publishDate).getTime(),
-  );
-
-  return posts;
+export interface ArticleProps extends MdFileContentProps {
+  slug: string;
 }
 
-export async function getContent(slug: string): Promise<Article> {
-  const postsDirectory = path.join(process.cwd(), "content");
-  const filePath = path.join(postsDirectory, `${slug}.mdx`);
-  const fileContents = fs.readFileSync(filePath, "utf8");
-  const { data, content } = matter(fileContents);
+export type ArticleWithoutContentProps = Omit<ArticleProps, "content">;
+
+async function readMdFile(filePath: string): Promise<MdFileContentProps> {
+  const fileContent = await fs.readFile(filePath, "utf8");
+  const { data, content } = matter(fileContent);
 
   return {
-    slug,
     title: data.title,
-    description: data.description,
-    publishDate: format(new Date(data.publishDate), "MMM dd, yyyy"),
+    excerpt: data.excerpt,
+    publishedDate: format(new Date(data.publishedDate), "MMM dd, yyyy"),
     featuredImage: data.featuredImage,
     content,
+  };
+}
+
+export async function getAllContents(): Promise<ArticleWithoutContentProps[]> {
+  const contentDir = path.join(process.cwd(), "content");
+  const files = await fs.readdir(contentDir);
+  const contentsPromises = files
+    .filter((fileName) => fileName.endsWith(".mdx"))
+    .map(async (fileName) => {
+      const filePath = path.join(contentDir, fileName);
+      const { content, ...fileContent } = await readMdFile(filePath);
+
+      return {
+        ...fileContent,
+        slug: fileName.replace(/\.mdx$/, ""),
+      };
+    });
+  const contents = (
+    await Promise.allSettled<ArticleWithoutContentProps>(contentsPromises)
+  )
+    // TODO: log/send to Sentry those that fail
+    .filter((p) => p.status === "fulfilled")
+    .map((p) => p.value)
+    .sort(
+      (a, b) =>
+        new Date(b.publishedDate).getTime() -
+        new Date(a.publishedDate).getTime(),
+    );
+  return contents;
+}
+
+export async function getContent(slug: string): Promise<ArticleProps> {
+  const filePath = path.join(process.cwd(), "content", `${slug}.mdx`);
+  const fileContent = await readMdFile(filePath);
+
+  return {
+    ...fileContent,
+    slug,
   };
 }
