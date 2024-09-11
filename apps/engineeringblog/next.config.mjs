402c5a6d28abf4284e47c82986049feebc7b1525
Clemence Kyara <clemence@codeforafrica.org>
2024-09-11 11:26:59 +0300

    Fix SVG loader

--- apps/engineeringblog/next.config.mjs
+++ apps/engineeringblog/next.config.mjs
@@ -6,19 +6,24 @@ const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["@commons-ui/core", "@commons-ui/next"],
   webpack: (config) => {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg"),
+    );
     config.module.rules.push(
       {
+        ...fileLoaderRule,
         test: /\.svg$/i,
-        type: "asset",
         resourceQuery: /url/, // *.svg?url
       },
       {
         test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
         use: ["@svgr/webpack"],
       },
     );
+    // Since *.svg files are now handled ☝️, we can safely ignore file loader rule.
+    fileLoaderRule.exclude = /\.svg$/i;
     config.experiments = { ...config.experiments, topLevelAwait: true }; // eslint-disable-line no-param-reassign
     return config;
   },
