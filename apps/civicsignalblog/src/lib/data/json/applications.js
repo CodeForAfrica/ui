c19c427d1c118b0761c30d259b871fd369cae82f
Michael Hudson Nkotagu <mnkotagu@gmail.com>
2024-09-23 16:02:06 +0300

    chore: Refractor application list

--- apps/civicsignalblog/src/lib/data/json/applications.js
+++ apps/civicsignalblog/src/lib/data/json/applications.js
@@ -1,10 +1,18 @@
-const applications = [
-  "CivicSignal",
-  "Explorer",
-  "Topic Mapper",
-  "Source Manager",
-  "Research Blog",
-].map((label) => ({
+export const MAIN = "main";
+export const EXPLORER = "explorer";
+export const TOPIC_MAPPER = "topic-mapper";
+export const SOURCE_MAPPER = "source-manager";
+export const RESEARCH = "research";
+
+const applicationLabels = {
+  MAIN: "CivicSignal",
+  EXPLORER: "Explorer",
+  TOPIC_MAPPER: "Topic Mapper",
+  SOURCE_MAPPER: "Source Manager",
+  RESEARCH: "Research",
+};
+
+const applications = Object.entries(applicationLabels).map(([, label]) => ({
   label,
   value: label.toLowerCase().replace(/\s+/g, "_"),
 }));
