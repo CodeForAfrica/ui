42af03214d4a52bf7f497e3affb25f0eef3daeb1
Michael Hudson Nkotagu <m453h@users.noreply.github.com>
2024-09-25 12:32:05 +0300

    Update apps/civicsignalblog/src/payload/access/canAccessApplication.js
    Co-authored-by: Clemence Kyara <kilemensi@users.noreply.github.com>

--- apps/civicsignalblog/src/payload/access/canAccessApplication.js
+++ apps/civicsignalblog/src/payload/access/canAccessApplication.js
@@ -1,10 +1,7 @@
 export default function canAccessApplication(user, searchString) {
   if (user) {
     const app = user.currentApp || user.defaultApp;
-    if (app) {
-      return app === searchString.toLowerCase();
-    }
-    return false;
+    return app === searchString.toLowerCase();
   }
   return false;
 }
