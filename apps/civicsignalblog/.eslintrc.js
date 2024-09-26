a24d724a35562859af42ab26b1707ccb2a4e6713
Michael Hudson Nkotagu <mnkotagu@gmail.com>
2024-09-26 17:47:51 +0300

    chore: Allow jsx in both Typescript and JavaScript files

--- apps/civicsignalblog/.eslintrc.js
+++ apps/civicsignalblog/.eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
   root: true,
   extends: ["eslint-config-commons-ui/next"],
+  rules: {
+    "react/jsx-filename-extension": [
+      "warn",
+      { extensions: [".jsx", ".tsx", "ts", ".tsx"] },
+    ],
+  },
   settings: {
     "import/resolver": {
       webpack: {
