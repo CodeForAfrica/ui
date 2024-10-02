a13a620521f4c0c2290fc96262c704d6fe0fdc95
Michael Hudson Nkotagu <mnkotagu@gmail.com>
2024-10-02 09:05:40 +0300

    feat: Implement MediaData Collection

---
+++ apps/civicsignalblog/src/payload/collections/Main/MediaData.js
@@ -0,0 +1,43 @@
+import { slateEditor } from "@payloadcms/richtext-slate";
+
+import canRead from "#civicsignalblog/payload/access/applications/main";
+import image from "#civicsignalblog/payload/fields/image";
+import richText from "#civicsignalblog/payload/fields/richText";
+
+const MediaData = {
+  slug: "media-data",
+  access: {
+    read: canRead,
+  },
+  admin: {
+    defaultColumns: ["title", "updatedAt"],
+    enableRichTextLink: false,
+    group: "Publication",
+    useAsTitle: "title",
+  },
+  fields: [
+    {
+      name: "title",
+      type: "text",
+      required: true,
+      localized: true,
+    },
+    image({
+      overrides: {
+        name: "mediaDataImage",
+        required: true,
+        localized: true,
+      },
+    }),
+    richText({
+      name: "description",
+      editor: slateEditor({
+        admin: {
+          elements: ["link"],
+        },
+      }),
+    }),
+  ],
+};
+
+export default MediaData;
