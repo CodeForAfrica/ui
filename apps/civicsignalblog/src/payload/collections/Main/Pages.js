d968728e80556c92637a0a4936c9fb1efeaed72c
Michael Hudson Nkotagu <mnkotagu@gmail.com>
2024-10-08 08:38:17 +0300

    feat: Add ResourceList Block to civicsignal tools pages

--- apps/civicsignalblog/src/payload/collections/Main/Pages.js
+++ apps/civicsignalblog/src/payload/collections/Main/Pages.js
@@ -1,5 +1,6 @@
 import canRead from "#civicsignalblog/payload/access/applications/main";
 import PageHeader from "#civicsignalblog/payload/blocks/WebTools/PageHeader";
+import ResourceList from "#civicsignalblog/payload/blocks/WebTools/ResourceList";
 import { MAIN } from "#civicsignalblog/payload/lib/data/common/applications";
 import pages from "#civicsignalblog/payload/utils/createPagesCollection";
 
@@ -8,7 +9,7 @@ const Pages = pages({
   label: "Pages",
   group: "Publication",
   defaultColumns: ["fullTitle", "updatedAt"],
-  blocks: [PageHeader],
+  blocks: [PageHeader, ResourceList],
   access: {
     read: canRead,
   },
