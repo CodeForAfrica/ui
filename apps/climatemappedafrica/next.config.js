8262ac1750fe9967689943c1a055f5ef2e9096cf
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2024-10-09 16:43:26 +0300

    Working footer

--- apps/climatemappedafrica/next.config.js
+++ apps/climatemappedafrica/next.config.js
@@ -32,16 +32,19 @@ module.exports = {
     "@hurumap/next",
   ],
   webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        "@svgr/webpack",
-        {
-          loader: "svg-url-loader",
-          options: {},
-        },
-      ],
-    });
+    config.module.rules.push(
+      {
+        test: /\.svg$/i,
+        type: "asset",
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
+        use: ["@svgr/webpack"],
+      },
+    );
     // eslint-disable-next-line no-param-reassign
     config.resolve.fallback = {
       ...config.resolve.fallback,
