0b8453a0a4e7c3a490b009e15c3a2a227c61a200
Clemence Kyara <clemence@codeforafrica.org>
2024-10-14 09:19:31 +0300

    Lint and format

--- apps/climatemappedafrica/payload.config.ts
+++ apps/climatemappedafrica/payload.config.ts
@@ -68,12 +68,12 @@ export default buildConfig({
   globals: [Site] as GlobalConfig[],
   ...(locales?.length
     ? {
-      localization: {
-        locales,
-        defaultLocale,
-        fallback: true,
-      },
-    }
+        localization: {
+          locales,
+          defaultLocale,
+          fallback: true,
+        },
+      }
     : undefined),
   admin: {
     webpack: (config) => ({
