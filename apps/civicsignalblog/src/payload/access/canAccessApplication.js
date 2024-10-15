87182e3856ee4db04940e5aed2d49c34771a0e98
Michael Hudson Nkotagu <mnkotagu@gmail.com>
2024-10-15 11:43:34 +0300

    feat: Add API key authentication in users collection

--- apps/civicsignalblog/src/payload/access/canAccessApplication.js
+++ apps/civicsignalblog/src/payload/access/canAccessApplication.js
@@ -1,9 +1,11 @@
+import { ROLE_APP } from "./roles";
+
 export default function canAccessApplication(user, searchString) {
   // We are using this condition to only control what a user can see at a given time
   if (user) {
     const app = user.currentApp || user.defaultApp;
-    return app === searchString.toLowerCase();
+    // The assumption is that one account with ROLE_APP will be used to access all the app content
+    return app === searchString.toLowerCase() || user.roles.includes(ROLE_APP);
   }
-  // For APIs coming from external apps we are not going to use any restriction in reading
-  return true;
+  return false;
 }
