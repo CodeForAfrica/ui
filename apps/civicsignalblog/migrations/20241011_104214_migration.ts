5a8b3ffeacdab3730766bfef0f77cfd58dc413c1
Michael Hudson Nkotagu <mnkotagu@gmail.com>
2024-10-16 10:54:41 +0300

    fix: Add proper final messages after execution of migrations

--- apps/civicsignalblog/migrations/20241011_104214_migration.ts
+++ apps/civicsignalblog/migrations/20241011_104214_migration.ts
@@ -2,7 +2,7 @@ import { MigrateUpArgs, MigrateDownArgs } from "@payloadcms/db-mongodb";
 import mongoose from "mongoose";
 
 const mongoURL = process.env.MONGO_URL;
-const oldCollectionName = "pages";
+const oldCollectionName = "pages-pages";
 const newCollectionName = "research-pages";
 const oldCollectionVersionName = `_${oldCollectionName}_versions`;
 const newCollectionVersionName = `_${newCollectionName}_versions`;
@@ -11,6 +11,7 @@ export async function up({ payload }: MigrateUpArgs): Promise<void> {
   // Connect to the database
   await mongoose.connect(mongoURL);
   const db = mongoose.connection.db;
+  let failedInserts = 0;
 
   // Find all records in oldCollection
   let records = await db.collection(oldCollectionName).find().toArray();
@@ -26,6 +27,7 @@ export async function up({ payload }: MigrateUpArgs): Promise<void> {
       payload.logger.error(
         `Error migrating collection with slug: ${record.slug}, ${error.message}`,
       );
+      failedInserts++;
     }
   }
 
@@ -43,9 +45,14 @@ export async function up({ payload }: MigrateUpArgs): Promise<void> {
       payload.logger.error(
         `Error migrating version collection with slug: ${record.slug} from ${oldCollectionVersionName} to ${newCollectionVersionName}`,
       );
+      failedInserts++;
     }
   }
-  payload.logger.info(`✓ Successfully completed migration`);
+  if (failedInserts > 0){
+    payload.logger.warn(`⚠ Migrations completed with [${failedInserts}] errors`);
+  } else {
+    payload.logger.info(`✓ Successfully completed migration`);
+  }
 }
 
 export async function down({ payload }: MigrateDownArgs): Promise<void> {}
