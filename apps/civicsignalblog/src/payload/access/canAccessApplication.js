7452b85cdb03a85fd817429b2411e45042058e4a
Michael Hudson Nkotagu <mnkotagu@gmail.com>
2024-10-17 15:37:13 +0300

    feat: Add check on user allowed apps

--- apps/civicsignalblog/src/payload/access/canAccessApplication.js
+++ apps/civicsignalblog/src/payload/access/canAccessApplication.js
@@ -1,11 +1,10 @@
-import { ROLE_APP } from "./roles";
+export default async function canAccessApplication(req, searchString) {
+  const { user, headers } = req;
 
-export default function canAccessApplication(user, searchString) {
-  // We are using this condition to only control what a user can see at a given time
   if (user) {
-    const app = user.currentApp || user.defaultApp;
-    // The assumption is that one account with ROLE_APP will be used to access all the app content
-    return app === searchString.toLowerCase() || user.roles.includes(ROLE_APP);
+    const currentApp = headers["x-current-app"] || user.currentApp;
+    const app = currentApp || user.defaultApp;
+    return app === searchString.toLowerCase() && user.allowedApps.includes(app);
   }
   return false;
 }
