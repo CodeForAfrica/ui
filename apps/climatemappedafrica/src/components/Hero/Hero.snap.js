f43a69a576f052d3a9883da0e97efb57d53c1f76
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2024-11-13 17:14:04 +0300

    Regenerate snapshots

--- apps/climatemappedafrica/src/components/Hero/Hero.snap.js
+++ apps/climatemappedafrica/src/components/Hero/Hero.snap.js
@@ -50,43 +50,47 @@ exports[`<Hero /> renders unchanged 1`] = `
           </header>
           <div
             class="MuiBox-root css-0"
-            id="location-search"
           >
-            <p
-              class="MuiTypography-root MuiTypography-body1 css-1u0kiq2-MuiTypography-root"
-            >
-              Search for a location
-            </p>
             <div
-              class="MuiInputBase-root MuiInputBase-colorPrimary MuiInputBase-adornedEnd css-49e042-MuiInputBase-root"
+              class="MuiBox-root css-19idom"
+              id="location-search"
             >
-              <input
-                aria-label="search"
-                class="MuiInputBase-input MuiInputBase-inputAdornedEnd css-yz9k0d-MuiInputBase-input"
-                placeholder="Search for a location"
-                type="text"
-                value=""
-              />
-              <button
-                class="MuiButtonBase-root MuiIconButton-root MuiIconButton-colorPrimary MuiIconButton-sizeSmall css-oru44q-MuiButtonBase-root-MuiIconButton-root"
-                root="[object Object]"
-                tabindex="0"
-                type="button"
+              <p
+                class="MuiTypography-root MuiTypography-body1 css-1u0kiq2-MuiTypography-root"
               >
-                <svg
-                  aria-hidden="true"
-                  class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-1ny0eu4-MuiSvgIcon-root"
-                  focusable="false"
-                  viewBox="0 0 48 48"
+                Search for a location
+              </p>
+              <div
+                class="MuiInputBase-root MuiInputBase-colorPrimary MuiInputBase-adornedEnd css-49e042-MuiInputBase-root"
+              >
+                <input
+                  aria-label="search"
+                  class="MuiInputBase-input MuiInputBase-inputAdornedEnd css-yz9k0d-MuiInputBase-input"
+                  placeholder="Search for a location"
+                  type="text"
+                  value=""
                 />
-                <span
-                  class="MuiTouchRipple-root css-8je8zh-MuiTouchRipple-root"
-                />
-              </button>
+                <button
+                  class="MuiButtonBase-root MuiIconButton-root MuiIconButton-colorPrimary MuiIconButton-sizeSmall css-oru44q-MuiButtonBase-root-MuiIconButton-root"
+                  root="[object Object]"
+                  tabindex="0"
+                  type="button"
+                >
+                  <svg
+                    aria-hidden="true"
+                    class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-1ny0eu4-MuiSvgIcon-root"
+                    focusable="false"
+                    viewBox="0 0 48 48"
+                  />
+                  <span
+                    class="MuiTouchRipple-root css-8je8zh-MuiTouchRipple-root"
+                  />
+                </button>
+              </div>
+              <div
+                class="MuiBox-root css-79elbk"
+              />
             </div>
-            <div
-              class="MuiBox-root css-79elbk"
-            />
           </div>
         </div>
         <div
