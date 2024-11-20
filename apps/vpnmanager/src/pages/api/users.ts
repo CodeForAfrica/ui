e98c9c656b0c76e64ab317988368d097afba01bd
Kevin Koech <koechkevin92@gmail.com>
2024-11-20 10:14:50 +0300

    vpnmanager users script

--- apps/vpnmanager/src/pages/api/users.ts
+++ apps/vpnmanager/src/pages/api/users.ts
@@ -1,9 +1,18 @@
 import { NextApiResponse, NextApiRequest } from "next";
 import { processNewUsers } from "@/vpnmanager/lib/processUsers";
 import { RestMethodFunctions, RestMethods } from "@/vpnmanager/types";
+import { OutlineVPN } from "@/vpnmanager/lib/outline";
 
+async function users() {
+  const vpnManager = new OutlineVPN({
+    apiUrl: process.env.NEXT_APP_VPN_API_URL as string,
+  });
+  const out = await vpnManager.getUsers();
+  return out;
+}
 const methodToFunction: RestMethodFunctions = {
   POST: processNewUsers,
+  GET: users,
 };
 
 export async function handler(req: NextApiRequest, res: NextApiResponse) {
