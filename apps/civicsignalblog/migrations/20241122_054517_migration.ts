097555c5368d9b51f6e01dec8e36b78d51808854
Michael Hudson Nkotagu <mnkotagu@gmail.com>
2024-11-22 10:44:02 +0300

    feat: Implement migration for global site settings

---
+++ apps/civicsignalblog/migrations/20241122_054517_migration.ts
@@ -0,0 +1,50 @@
+import { MigrateUpArgs, MigrateDownArgs } from "@payloadcms/db-mongodb";
+import mongoose from "mongoose";
+
+const mongoURL = process.env.MONGO_URL;
+const oldGlobalConfigType = "settings-site";
+const newGlobalConfigType = "settings-research-site";
+
+export async function up({ payload }: MigrateUpArgs): Promise<void> {
+  await mongoose.connect(mongoURL);
+  const db = mongoose.connection.db;
+
+  const existingGlobalConfig = await db.collection("globals").findOne({
+    globalType: oldGlobalConfigType,
+  });
+
+  if (!existingGlobalConfig) {
+    payload.logger.error(
+      `No existing global settings [${oldGlobalConfigType}] config found, exiting...`,
+    );
+    return;
+  }
+
+  let newGlobalConfig = await db.collection("globals").findOne({
+    globalType: newGlobalConfigType,
+  });
+
+  if (newGlobalConfig) {
+    payload.logger.error(
+      `Migration failed: Unable to migrate [${oldGlobalConfigType}] to [${newGlobalConfigType}] because a global configuration with this name already exists.`,
+    );
+    return;
+  }
+
+  try {
+    newGlobalConfig = {
+      ...existingGlobalConfig,
+      globalType: newGlobalConfigType,
+    };
+
+    delete newGlobalConfig._id;
+    await db.collection("globals").insertOne(newGlobalConfig);
+    payload.logger.info(
+      `✓ Successfully migrated global config [${oldGlobalConfigType}] to [${newGlobalConfigType}]`,
+    );
+  } catch (error) {
+    payload.logger.error("Error: Migration failed.");
+    throw error;
+  }
+}
+export async function down({ payload }: MigrateDownArgs): Promise<void> {}
