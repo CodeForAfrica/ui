fef32c5032212c99d18b14c514eae1917733508f
Kevin Koech <koechkevin92@gmail.com>
2024-11-27 13:14:02 +0300

    FIxed root config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,6 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+const eslintConfig = require("eslint-config-commons-ui/next");
 
 export default [
-  ...compat.extends("eslint-config-commons-ui/next"),
   {
     settings: {
       next: {
@@ -20,4 +8,5 @@ export default [
       },
     },
   },
+  ...eslintConfig,
 ];
