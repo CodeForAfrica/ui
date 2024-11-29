473839fff2022efe117b5ec51d2c8a1122792015
Michael Hudson Nkotagu <mnkotagu@gmail.com>
2024-11-29 16:10:30 +0300

    feat: Add messages buttons sections for Login Form

--- apps/civicsignalblog/src/payload/globals/Forms/login/LoginTab.js
+++ apps/civicsignalblog/src/payload/globals/Forms/login/LoginTab.js
@@ -1,3 +1,5 @@
+import richText from "#civicsignalblog/payload/fields/richText";
+
 const LoginTab = {
   label: "Login Form",
   fields: [
@@ -46,19 +48,92 @@ const LoginTab = {
           },
           fields: [
             {
-              name: "name",
-              type: "text",
+              type: "row",
+              fields: [
+                {
+                  name: "name",
+                  label: "Name (lowercase, no special characters)",
+                  type: "text",
+                  required: true,
+                },
+                {
+                  name: "label",
+                  type: "text",
+                  required: true,
+                },
+              ],
             },
             {
-              name: "label",
-              type: "text",
-            },
-            {
-              name: "errorMessage",
-              type: "text",
+              type: "row",
+              fields: [
+                {
+                  name: "errorMessage",
+                  type: "text",
+                  required: true,
+                },
+                {
+                  name: "hint",
+                  type: "text",
+                },
+              ],
             },
           ],
         },
+      ],
+    },
+    {
+      type: "collapsible",
+      label: "Buttons",
+      fields: [
+        {
+          type: "row",
+          fields: [
+            {
+              name: "login",
+              type: "text",
+              defaultValue: "Login",
+              required: true,
+              localized: true,
+            },
+            {
+              name: "registration",
+              type: "text",
+              defaultValue: "No Account ? Register Now!",
+              required: true,
+              localized: true,
+            },
+            {
+              name: "password",
+              type: "text",
+              defaultValue: "Forgot your password ?",
+              required: true,
+              localized: true,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      type: "collapsible",
+      label: "Messages",
+      fields: [
+        {
+          type: "row",
+          fields: [
+            {
+              name: "loginFailed",
+              label: "Login Failed",
+              default: "Your email or password was wrong.",
+              type: "text",
+              required: true,
+            },
+          ],
+        },
+        richText({
+          name: "needsToActivate",
+          required: true,
+          localized: true,
+        }),
       ],
     },
   ],
