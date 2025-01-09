b492483996004a9695f50e98bc108d2923d96a4a
Kevin Koech <kevinkoech@MacBook-Pro.local>
2025-01-09 16:07:16 +0300

    Refactor ESLint configuration to use TypeScript settings and streamline config files

--- apps/roboshield/eslint.config.js
+++ apps/roboshield/eslint.config.js
@@ -1,32 +1,3 @@
-const eslintConfig = require("eslint-config-commons-ui/next");
+const eslintConfig = require("eslint-config-commons-ui/typescript");
 
-module.exports = [
-  {
-    settings: {
-      "import/resolver": {
-        webpack: {
-          config: "./eslint.webpack.config.js",
-        },
-        typescript: {
-          alwaysTryTypes: false,
-          project: "./tsconfig.json",
-        },
-      },
-    },
-  },
-  {
-    rules: {
-      "react/jsx-filename-extension": [1, { extensions: [".js", ".tsx"] }], // This rule allows JSX syntax in both .js and tsx files
-      // Disable requirement for importing file extensions for js and tsx files, without this we cant import custom components in Payload
-      "import/extensions": [
-        "error",
-        "ignorePackages",
-        {
-          js: "never",
-          tsx: "never",
-        },
-      ],
-    },
-  },
-  ...eslintConfig,
-];
+module.exports = eslintConfig;
