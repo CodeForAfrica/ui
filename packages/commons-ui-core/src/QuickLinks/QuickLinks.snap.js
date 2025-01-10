eb43ea95a8766e794c32c338877372cfb383517b
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2025-01-10 13:17:44 +0300

    Add QuickLinks tests
    Signed-off-by: Kipruto <43873157+kelvinkipruto@users.noreply.github.com>

---
+++ packages/commons-ui-core/src/QuickLinks/QuickLinks.snap.js
@@ -0,0 +1,35 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`<QuickLinks /> renders inchanged 1`] = `
+<div>
+  <div
+    class="MuiBox-root css-ufz638"
+  >
+    <p
+      class="MuiTypography-root MuiTypography-body1 css-1517230-MuiTypography-root"
+    >
+      Quick Links
+    </p>
+    <ul
+      class="MuiTypography-root MuiTypography-body1 css-1f5quei-MuiTypography-root"
+    >
+      <li>
+        <a
+          class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineNone css-1imuphz-MuiTypography-root-MuiLink-root"
+          href="/"
+        >
+          Home
+        </a>
+      </li>
+      <li>
+        <a
+          class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineNone css-1imuphz-MuiTypography-root-MuiLink-root"
+          href="/about"
+        >
+          About
+        </a>
+      </li>
+    </ul>
+  </div>
+</div>
+`;
