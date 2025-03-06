305bd07c8ea45f97acf0d45a1dc117e79bb5d357
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2025-03-06 16:16:05 +0300

    Update Dependencies

--- apps/pesayetu/jest.config.js
+++ apps/pesayetu/jest.config.js
@@ -11,7 +11,6 @@ module.exports = {
       "<rootDir>/../../packages/commons-ui-core/src/$1",
     "^@/commons-ui/next/(.*)$":
       "<rootDir>/../../packages/commons-ui-next/src/$1",
-    "^@/hurumap/core/(.*)$": "<rootDir>/../../packages/hurumap-core/src/$1",
   },
   transformIgnorePatterns: ["<rootDir>/node_modules/(?!camelcase-keys)"],
 };
