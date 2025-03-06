ddcfe23106b04dbbbd8db6e002666f1b40cd0d47
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2025-03-06 15:09:00 +0300

    revert caching
    Signed-off-by: Kipruto <43873157+kelvinkipruto@users.noreply.github.com>

--- apps/climatemappedafrica/src/utils/fetchJson.js
+++ apps/climatemappedafrica/src/utils/fetchJson.js
@@ -1,28 +1,5 @@
-const cache = new Map();
-const CACHE_TTL = 10 * 60 * 1000;
-
 async function fetchJson(resource, init) {
-  const cacheKey =
-    typeof resource === "string" ? resource : resource.toString();
-
-  const cached = cache.get(cacheKey);
-  if (cached) {
-    const { data, timestamp } = cached;
-    if (Date.now() - timestamp < CACHE_TTL) {
-      return data;
-    }
-    cache.delete(cacheKey);
-  }
-
-  const response = await fetch(resource, init);
-  const data = await response.json();
-
-  cache.set(cacheKey, {
-    data,
-    timestamp: Date.now(),
-  });
-
-  return data;
+  return fetch(resource, init).then((res) => res.json());
 }
 
 export default fetchJson;
