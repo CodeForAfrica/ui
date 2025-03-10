42aa7f27e70bf5c642e16b83ed8a7b9dc31b503f
Kevin Koech <koechkevin92@gmail.com>
2025-03-10 13:33:16 +0300

    Remove hostname from media `src`.

--- apps/charterafrica/src/payload/collections/Media.js
+++ apps/charterafrica/src/payload/collections/Media.js
@@ -24,6 +24,12 @@ const Media = {
       ({ doc }) => ({
         ...doc,
         alt: doc.alt ?? null,
+        /*
+        Since we are setting ENV PAYLOAD_PUBLIC_APP_URL=http://localhost:3000 at build time, generating static pages(404,500) ends up with image urls with `http://localhost:3000/media/cfalogobw.svg`.
+        In this proposal, we are just returning pathnames without hostname since our next js apps and payload are served from the same instance.
+        */
+        src: new URL(doc.url).pathname,
+        url: new URL(doc.url).pathname,
       }),
     ],
   },
--- apps/codeforafrica/src/payload/collections/Media.js
+++ apps/codeforafrica/src/payload/collections/Media.js
@@ -22,7 +22,17 @@ const Media = {
     },
   ],
   hooks: {
-    afterRead: [({ doc }) => ({ ...doc, src: doc.url })],
+    afterRead: [
+      ({ doc }) => ({
+        ...doc,
+        /*
+        Since we are setting PAYLOAD_PUBLIC_APP_URL=http://localhost:3000 at build time, generating static pages(404,500) ends up with image urls with `http://localhost:3000/media/cfalogobw.svg`.
+        In this proposal, we are just returning pathnames without hostname since our next js apps and payload are served from the same instance.
+        */
+        src: new URL(doc.url).pathname,
+        url: new URL(doc.url).pathname,
+      }),
+    ],
   },
 };
 
