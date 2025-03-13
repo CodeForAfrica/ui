9b59029ec3b97169ee6cd6751837b4ed48045a74
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2025-03-13 12:43:56 +0300

    Fetch cached profiles when cached
    Signed-off-by: Kipruto <43873157+kelvinkipruto@users.noreply.github.com>

--- apps/climatemappedafrica/src/pages/api/hurumap/geographies/[geoCode].js
+++ apps/climatemappedafrica/src/pages/api/hurumap/geographies/[geoCode].js
@@ -1,11 +1,11 @@
-import { fetchProfileGeography } from "@/climatemappedafrica/lib/hurumap";
+import { fetchCachedProfileGeography } from "@/climatemappedafrica/lib/hurumap";
 
 export default async function index(req, res) {
   const { profileId, baseUrl } = req.query;
   if (req.method === "GET") {
     try {
       const { geoCode } = req.query;
-      const result = await fetchProfileGeography(geoCode, {
+      const result = await fetchCachedProfileGeography(geoCode, {
         baseUrl,
         profileId,
       });
