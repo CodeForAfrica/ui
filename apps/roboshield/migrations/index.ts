7b02970292be137bda3a2e63694a6e7d7fcf1370
Michael Hudson Nkotagu <mnkotagu@gmail.com>
2025-05-05 17:04:58 +0300

    Rename payload migration to include descriptive name

--- apps/roboshield/migrations/index.ts
+++ apps/roboshield/migrations/index.ts
@@ -1,5 +1,5 @@
 import * as migration_20250422_064614_migration from "./20250422_064614_migration";
-import * as migration_20250425_082829_migration from "./20250425_082829_migration";
+import * as migration_20250425_082829_slate_to_lexical from "./20250425_082829_slate_to_lexical";
 
 export const migrations = [
   {
@@ -8,8 +8,8 @@ export const migrations = [
     name: "20250422_064614_migration",
   },
   {
-    up: migration_20250425_082829_migration.up,
-    down: migration_20250425_082829_migration.down,
-    name: "20250425_082829_migration",
+    up: migration_20250425_082829_slate_to_lexical.up,
+    down: migration_20250425_082829_slate_to_lexical.down,
+    name: "20250425_082829_slate_to_lexical",
   },
 ];
