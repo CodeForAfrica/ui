220ababa075d66eca2bbb98b727663cccc9ed23e
Clemence Kyara <clemence@codeforafrica.org>
2025-05-20 09:34:51 +0300

    Improve docker compose build
     + Add image name to trustlab service
     + Pass env file to `docker compose` in the `dc.sh` script

--- scripts/dc.sh
+++ scripts/dc.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 APP="$1"
+APP_ENV_FILE="./apps/$APP/.env.local"
 set -a
-source "./apps/$APP/.env.local"  # Load the environment variables from the .env.local file
+source "$APP_ENV_FILE"  # Load the environment variables from the .env.local file
 set +a
 
-BUILDKIT_PROGRESS=plain docker compose up "$APP" --build
+BUILDKIT_PROGRESS=plain docker compose --env-file "$APP_ENV_FILE" up "$APP" --build
