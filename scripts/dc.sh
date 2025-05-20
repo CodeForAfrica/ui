25b31f6b5f5602385126efdf7aec4136161c4765
Clemence Kyara <clemence@codeforafrica.org>
2025-05-20 11:30:07 +0300

    Use current SHA as image tag

--- scripts/dc.sh
+++ scripts/dc.sh
@@ -2,8 +2,9 @@
 
 APP="$1"
 APP_ENV_FILE="./apps/$APP/.env.local"
+REPO_SHA=$(git log --pretty=format:'%h' --max-count=1)
 set -a
 source "$APP_ENV_FILE"  # Load the environment variables from the .env.local file
 set +a
 
-BUILDKIT_PROGRESS=plain docker compose --env-file "$APP_ENV_FILE" up "$APP" --build
+IMAGE_TAG=${REPO_SHA} BUILDKIT_PROGRESS=plain docker compose --env-file "$APP_ENV_FILE" up "$APP" --build
