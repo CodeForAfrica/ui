a9bc17f0ed530d804dd643eb70c1171c8cedcc8d
Clemence Kyara <clemence@codeforafrica.org>
2025-05-20 12:47:36 +0300

    Prefer build pull_policy incase of matching tag
    Clean up the dc.sh script as well

--- scripts/dc.sh
+++ scripts/dc.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 APP="$1"
-APP_ENV_FILE="./apps/$APP/.env.local"
+APP_ENV_FILE="./apps/${APP}/.env.local"
 REPO_SHA=$(git log --pretty=format:'%h' --max-count=1)
 set -a
-source "$APP_ENV_FILE"  # Load the environment variables from the .env.local file
+source "${APP_ENV_FILE}"  # Load the environment variables from the .env.local file
 set +a
 
-IMAGE_TAG=${REPO_SHA} BUILDKIT_PROGRESS=plain docker compose --env-file "$APP_ENV_FILE" up "$APP" --build
+# Don't override IMAGE_TAG, BUILDKIT_PROGRESS, etc. if already set
+IMAGE_TAG=${IMAGE_TAG-${REPO_SHA}} \
+BUILDKIT_PROGRESS=${BUILDKIT_PROGRESS-plain} \
+docker compose --env-file "${APP_ENV_FILE}" \
+  up "${APP}" --build
