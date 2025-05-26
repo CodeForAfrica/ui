52db6e27d5f526da5ced646c1ed34169609bac9a
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2025-05-26 13:28:27 +0300

    Update snapshots
    Signed-off-by: Kipruto <43873157+kelvinkipruto@users.noreply.github.com>

--- packages/commons-ui-core/src/MobileNavBar/MobileNavBar.snap.js
+++ packages/commons-ui-core/src/MobileNavBar/MobileNavBar.snap.js
@@ -14,18 +14,3 @@ exports[`<MobileNavBar /> renders unchanged 1`] = `
   </div>
 </div>
 `;
-
-exports[`<MobileNavigation /> renders unchanged 1`] = `
-<div>
-  <div
-    class="MuiGrid-root MuiGrid-container css-9cyib4-MuiGrid-root"
-  >
-    <div
-      class="MuiGrid-root MuiGrid-item css-13i4rnv-MuiGrid-root"
-    />
-    <div
-      class="MuiGrid-root MuiGrid-item css-13i4rnv-MuiGrid-root"
-    />
-  </div>
-</div>
-`;
