19222aa83ab400c32c5fe72c6909f80dfca2048f
Kevin Koech <koechkevin92@gmail.com>
2025-05-26 09:58:16 +0300

    feat: Implement Hero component with slides and integrate into page structure

---
+++ apps/trustlab/src/components/Hero/Hero.js
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import { Box, Typography, Button } from "@mui/material";
+import { Section } from "@commons-ui/core";
+import { Slide } from "@mui/material";
+import { Link } from "@commons-ui/next";
+
+const Hero = ({ slides }) => {
+  const [activeStep, setActiveStep] = useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => (prevActiveStep + 1) % slides.length);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) =>
+      prevActiveStep === 0 ? slides.length - 1 : prevActiveStep - 1,
+    );
+  };
+
+  return (
+    <Box bgcolor="common.black" color="common.white">
+      <Box
+        sx={{
+          position: "relative",
+          width: "100%",
+          height: "500px",
+          overflow: "hidden",
+        }}
+      >
+        {slides.map((slide, index) => (
+          <Slide
+            key={index}
+            direction={"left"}
+            in={activeStep === index}
+            mountOnEnter
+            unmountOnExit
+            timeout={{ enter: 800, exit: 800 }}
+            appear={false}
+          >
+            <Box
+              sx={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                backgroundImage: slide.backgroundImage
+                  ? `url(${slide.backgroundImage})`
+                  : "none",
+                backgroundColor: slide.backgroundImage ? "transparent" : "#000",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                color: "#fff",
+              }}
+            >
+              <Section sx={{ mt: 10, px: { xs: 2.5, sm: 0 } }}>
+                <Typography variant="display1" gutterBottom>
+                  {slide.title}
+                </Typography>
+                <Typography variant="h3" gutterBottom>
+                  {slide.subtitle}
+                </Typography>
+                <Typography variant="subtitle1" sx={{ mt: 2 }}>
+                  {slide.description}
+                </Typography>
+                {slide.buttonText && (
+                  <Button
+                    component={slide.buttonLink ? Link : undefined}
+                    href={slide.buttonLink}
+                    variant="outlined"
+                    sx={{
+                      backgroundColor: "common.white",
+                      color: "#463E3E",
+                      border: "none",
+                      "&:hover": {
+                        transform: "scale(0.95)",
+                      },
+                    }}
+                  >
+                    {slide.buttonText}
+                  </Button>
+                )}
+              </Section>
+            </Box>
+          </Slide>
+        ))}
+        <Section
+          sx={{
+            position: "relative",
+            px: { xs: 2.5, sm: 6, md: 0 },
+          }}
+        >
+          <Box sx={{ position: "absolute", top: 312 }}>
+            {slides.map((_, index) => (
+              <Button
+                key={slides[index].title}
+                onClick={() => setActiveStep(index)}
+                sx={{
+                  width: 12,
+                  height: 12,
+                  minWidth: 0,
+                  borderRadius: "50%",
+                  border: "1px solid white",
+                  backgroundColor:
+                    activeStep === index ? "white" : "transparent",
+                  mx: 0.5,
+                  p: 0,
+                }}
+              />
+            ))}
+          </Box>
+        </Section>
+      </Box>
+    </Box>
+  );
+};
+
+export default Hero;
--- apps/trustlab/src/pages/[[...slugs]].page.js
+++ apps/trustlab/src/pages/[[...slugs]].page.js
@@ -3,10 +3,40 @@ import { SWRConfig } from "swr";
 
 import RefreshRouteOnSave from "@/trustlab/components/PayloadLivePreview";
 import { getPageStaticPaths, getPageStaticProps } from "@/trustlab/lib/data";
+import Hero from "@/trustlab/components/Hero";
 
-const componentsBySlugs = {};
+const componentsBySlugs = {
+  "our-hero": Hero,
+};
 
-function Page({ blocks, fallback }) {
+const slides = [
+  {
+    title: "Slide 1",
+    subtitle: "Subtitle 1",
+    description: "Description 1",
+    backgroundImage: "",
+    buttonText: "Explore our Resources",
+    buttonLink: "/",
+  },
+  {
+    title: "Slide 2",
+    subtitle: "Subtitle 2",
+    description: "Description 2",
+    backgroundImage: "",
+    buttonText: "Explore our Resources",
+    buttonLink: "/",
+  },
+  {
+    title: "Slide 3",
+    subtitle: "Subtitle 3",
+    description: "Description 3",
+    backgroundImage: "",
+    buttonText: "Explore our Resources",
+    buttonLink: "/",
+  },
+];
+
+function Page({ blocks = [{ slug: "our-hero", slides }], fallback }) {
   if (!blocks?.length) {
     return null;
   }
@@ -19,7 +49,7 @@ function Page({ blocks, fallback }) {
   }
   return (
     <PageComponent {...pageComponentProps}>
-      <RefreshRouteOnSave />
+      {/* <RefreshRouteOnSave /> */}
       {blocks.map((block) => {
         const Component = componentsBySlugs[block.slug];
         if (!Component) {
