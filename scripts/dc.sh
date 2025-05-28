59406d835e11af08e62674feea87cc6bb80e1a31
Clemence Kyara <clemence@codeforafrica.org>
2025-05-28 12:34:31 +0300

    Switch to using `--env-file` instead of `source`

--- scripts/dc.sh
+++ scripts/dc.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 APP="$1"
-APP_ENV_FILE="./apps/${APP}/.env.local"
 REPO_SHA=$(git log --pretty=format:'%h' --max-count=1)
-set -a
-source "${APP_ENV_FILE}"  # Load the environment variables from the .env.local file
-set +a
 
-# Don't override IMAGE_TAG, BUILDKIT_PROGRESS, etc. if already set
-IMAGE_TAG=${IMAGE_TAG-${REPO_SHA}} \
-BUILDKIT_PROGRESS=${BUILDKIT_PROGRESS-plain} \
-docker compose --env-file "${APP_ENV_FILE}" \
-  up "${APP}" --build
+# Don't override IMAGE_TAG and BUILDKIT_PROGRESS if already set.
+# `compose` now supports multiple `--env-file` args
+# see:
+#   https://github.com/docker/compose/releases/tag/v2.17.0-rc.1
+# verify via:
+#   `docker compose --env-file ./apps/trustlab/.env --env-file ./apps/trustlab/.env.local config --environment`
+IMAGE_TAG=${IMAGE_TAG:-${REPO_SHA}} \
+  BUILDKIT_PROGRESS=${BUILDKIT_PROGRESS:-plain} \
+  docker compose \
+  --env-file "./apps/${APP}/.env" \
+  --env-file "./apps/${APP}/.env.local" \
+  up "${APP}"
