ef5407269c8641ec5a7434188d5ca14f8fc879ba
Kevin Koech <koechkevin92@gmail.com>
2025-06-03 16:01:42 +0300

    Update admin group for Donors and Partners collections to "Project"

--- apps/trustlab/src/payload/collections/Donors.js
+++ apps/trustlab/src/payload/collections/Donors.js
@@ -19,7 +19,7 @@ const Donors = {
   admin: {
     defaultColumns: ["name", "updatedAt"],
     enableRichTextLink: false,
-    group: "Consortium",
+    group: "Project",
     useAsTitle: "name",
   },
   access: {
