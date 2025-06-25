dc7d45b3ea80fe07990c94dee23139e5e36411a3
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2025-06-25 11:21:19 +0300

    Add PostCard and PostList tests

---
+++ apps/trustlab/src/components/PostCard/PostCard.test.js
@@ -0,0 +1,29 @@
+import { createRender } from "@commons-ui/testing-library";
+import React from "react";
+
+import PostCard from "./PostCard";
+
+import theme from "@/trustlab/theme";
+
+const render = createRender({ theme });
+
+const defaultProps = {
+  deadline: "17 June 2025",
+  deadlineLabel: "Deadline On",
+  excerpt:
+    "The TrustLab supports Kenyan CSOs/CBOs to counter digital threats that affect them and the communities they serve",
+  title: "TrustLab Incubator Programme",
+  href: "/opportunities/trustlab-incubator-programme",
+  linkLabel: "Apply Now",
+  image: {
+    alt: "Resources 1",
+    src: "/resources-1.jpg",
+  },
+};
+
+describe("<PostCard />", () => {
+  it("renders unchanged", () => {
+    const { container } = render(<PostCard {...defaultProps} />);
+    expect(container).toMatchSnapshot();
+  });
+});
---
+++ apps/trustlab/src/components/PostList/PostList.snap.js
@@ -0,0 +1,70 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`<PostList /> renders unchanged 1`] = `
+<div>
+  <div
+    class="MuiBox-root css-148ace7"
+  >
+    <div
+      class="MuiContainer-root MuiContainer-maxWidthLg MuiContainer-fixed MuiContainer-disableGutters css-fqfx6w-MuiContainer-root"
+    >
+      <div
+        class="MuiGrid-root MuiGrid-container css-111q75m-MuiGrid-root"
+      >
+        <div
+          class="MuiPaper-root MuiPaper-elevation MuiPaper-rounded MuiPaper-elevation1 MuiCard-root css-g8xav8-MuiPaper-root-MuiCard-root"
+          style="--Paper-shadow: 0px 2px 1px -1px rgba(0,0,0,0.2),0px 1px 1px 0px rgba(0,0,0,0.14),0px 1px 3px 0px rgba(0,0,0,0.12);"
+        >
+          <div
+            class="MuiCardMedia-root css-1avv2mj-MuiCardMedia-root"
+            role="img"
+            style="background-image: url(/resources-1.jpg);"
+            title="TrustLab Incubator Programme"
+          />
+          <div
+            class="MuiCardHeader-root css-ofa8v-MuiCardHeader-root"
+          >
+            <div
+              class="MuiCardHeader-content css-1jkp9bu-MuiCardHeader-content"
+            >
+              <h2
+                class="MuiTypography-root MuiTypography-h2 css-cipl4i-MuiTypography-root"
+              >
+                TrustLab Incubator Programme
+              </h2>
+            </div>
+          </div>
+          <div
+            class="MuiCardContent-root css-yjf187-MuiCardContent-root"
+          >
+            <p
+              class="MuiTypography-root MuiTypography-p1 css-9tlvhj-MuiTypography-root"
+            >
+              Our fact-checkers & forensic investigators are on standby to verify claims or expose the puppet masters behind smear campaigns.
+            </p>
+          </div>
+          <div
+            class="MuiCardActions-root MuiCardActions-spacing css-1qg28ok-MuiCardActions-root"
+          >
+            <hr
+              class="MuiDivider-root MuiDivider-fullWidth css-146xkzv-MuiDivider-root"
+            />
+            <p
+              class="MuiTypography-root MuiTypography-p1 css-14y3bko-MuiTypography-root"
+            >
+              Deadline On 17 June 2025
+            </p>
+            <a
+              class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeSmall MuiButton-containedSizeSmall MuiButton-colorPrimary MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeSmall MuiButton-containedSizeSmall MuiButton-colorPrimary css-1y0cma0-MuiTypography-root-MuiLink-root-MuiButtonBase-root-MuiButton-root"
+              href="/opportunities/trustlab-incubator-programme"
+              tabindex="0"
+            >
+              Apply Now
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+`;
