abae2df7f8d78dc09943161b5b4b4be570bcef3c
Kevin Koech <koechkevin92@gmail.com>
2025-06-30 14:39:41 +0300

    feat: enhance Gallery component with new layout and add tests

--- apps/trustlab/src/components/Gallery/Gallery.js
+++ apps/trustlab/src/components/Gallery/Gallery.js
@@ -1,25 +1,56 @@
+import { Section } from "@commons-ui/core";
+import { Figure, RichTypography } from "@commons-ui/next";
+import { Grid } from "@mui/material";
 import React, { forwardRef } from "react";
-import { Grid, Typography } from "@mui/material";
 
 const Gallery = forwardRef(function Gallery({ title, images }, ref) {
   if (!images?.length) {
     return null;
   }
+
   return (
-    <div ref={ref}>
-      <Typography variant="h1">{title}</Typography>
+    <Section
+      sx={{
+        py: 8,
+        px: {
+          xs: 2.5,
+          sm: 0,
+        },
+      }}
+      ref={ref}
+    >
+      <RichTypography
+        variant="h3"
+        sx={{
+          mb: 5,
+          borderBottom: "1px solid",
+          pb: 1,
+        }}
+      >
+        {title}
+      </RichTypography>
       <Grid container spacing={2}>
-        {images.map((image, index) => (
-          <Grid item xs={12} sm={6} md={4} key={index}>
-            <img
-              src={image}
-              alt={`Gallery item ${index + 1}`}
-              style={{ width: "100%", borderRadius: "8px" }}
+        {images.map(({ image }) => (
+          <Grid item xs={12} sm={6} md={4} key={image.id}>
+            <Figure
+              ImageProps={{
+                alt: image.alt || "",
+                src: image.url,
+              }}
+              sx={{
+                m: 0,
+                height: 264,
+                position: "relative",
+                width: "auto",
+                "&:hover": {
+                  filter: "none",
+                },
+              }}
             />
           </Grid>
         ))}
       </Grid>
-    </div>
+    </Section>
   );
 });
 
