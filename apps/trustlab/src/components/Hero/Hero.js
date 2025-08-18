3c55d227516e7efc4d5a66730873396d3ebbdbbb
Kevin Koech <koechkevin92@gmail.com>
2025-08-18 11:02:07 +0300

    fix: Refactor Hero component for improved layout and transition effects

--- apps/trustlab/src/components/Hero/Hero.js
+++ apps/trustlab/src/components/Hero/Hero.js
@@ -47,8 +47,8 @@ const Hero = forwardRef(function Hero({ slides }, ref) {
           position: "relative",
           width: "100%",
           height: { xs: "828px", sm: "1174px", md: "741px" },
-          display: "flex",
           overflow: "hidden",
+          display: "flex",
         }}
       >
         {slides.map((slide, index) => (
@@ -57,117 +57,106 @@ const Hero = forwardRef(function Hero({ slides }, ref) {
             appear={false}
             direction={direction(activeStep, prevStepRef.current, index)}
             easing={{
-              // We need same enter/exit transition
-              enter: theme.transitions.easing.easeOut,
-              exit: theme.transitions.easing.easeOut,
+              enter: theme.transitions.easing.easeInOut,
+              exit: theme.transitions.easing.easeInOut,
             }}
             in={activeStep === index}
-            mountOnEnter
-            unmountOnExit
             timeout={800}
           >
             <Box
               sx={{
+                position: "absolute",
+                top: 0,
+                left: 0,
                 width: "100%",
                 height: "100%",
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                color: "#fff",
               }}
             >
               <Box
                 sx={{
-                  width: "100%",
-                  height: "100%",
-                  zIndex: 1,
+                  pt: 6,
+                  px: { xs: 2.5, sm: 0 },
+                  margin: "0 auto",
+                  maxWidth: theme.contentWidths.values,
                 }}
               >
-                <Box
-                  sx={{
-                    pt: 6,
-                    px: { xs: 2.5, sm: 0 },
-                    margin: "0 auto",
-                    maxWidth: theme.contentWidths.values,
-                  }}
-                >
-                  <Grid container spacing={2} alignItems="center">
-                    <Grid size={{ xs: 12, md: 6 }}>
-                      <LexicalRichText
-                        elements={slide.title}
-                        TypographyProps={{
-                          variant: "display2",
-                          color: slide.textColor,
-                          sx: {
-                            fontSize: { xs: "40px", sm: "64px" },
-                            lineHeight: { xs: "40px", sm: "64px" },
-                          },
-                        }}
-                      />
-
-                      <LexicalRichText
-                        elements={slide.description}
-                        TypographyProps={{
-                          variant: "h1",
-                          sx: {
-                            mt: 2,
-                            color: slide.textColor,
-                            fontSize: { xs: "24px", sm: "36px" },
-                            fontWeight: 500,
-                            lineHeight: { xs: "32px", sm: "40px" },
-                          },
-                        }}
-                      />
-                      {slide.buttons?.links?.length > 0 && (
-                        <Box
-                          sx={{
-                            display: "flex",
-                            flexDirection: "row",
-                            gap: 2,
-                            mt: 3,
-                          }}
-                        >
-                          {slide.buttons.links.map((link) => (
-                            <Button
-                              key={link.label}
-                              variant="contained"
-                              color="primary"
-                              href={link.href}
-                              component={link.href ? Link : undefined}
-                              sx={{
-                                backgroundColor: slide.buttons.backgroundColor,
-                                color: slide.buttons.textColor,
-                                border: "none",
-                                textTransform: "none",
-                                height: "50px",
-                              }}
-                            >
-                              {link.label}
-                            </Button>
-                          ))}
-                        </Box>
-                      )}
-                    </Grid>
-                    <Grid
-                      size={{ xs: 12, md: 6 }}
-                      sx={{
-                        display: "flex",
-                        justifyContent: { xs: "flex-start", md: "flex-end" },
+                <Grid container spacing={2} alignItems="center">
+                  <Grid size={{ xs: 12, md: 6 }}>
+                    <LexicalRichText
+                      elements={slide.title}
+                      TypographyProps={{
+                        variant: "display2",
+                        color: slide.textColor,
+                        sx: {
+                          fontSize: { xs: "40px", sm: "64px" },
+                          lineHeight: { xs: "40px", sm: "64px" },
+                        },
                       }}
-                    >
-                      <Figure
-                        ImageProps={{
-                          alt: slide.image.alt,
-                          src: slide.image.url,
-                          sx: { objectPosition: { xs: "left", md: "right" } },
-                        }}
+                    />
+
+                    <LexicalRichText
+                      elements={slide.description}
+                      TypographyProps={{
+                        variant: "h1",
+                        sx: {
+                          mt: 2,
+                          color: slide.textColor,
+                          fontSize: { xs: "24px", sm: "36px" },
+                          fontWeight: 500,
+                          lineHeight: { xs: "32px", sm: "40px" },
+                        },
+                      }}
+                    />
+                    {slide.buttons?.links?.length > 0 && (
+                      <Box
                         sx={{
-                          height: { xs: "502px", sm: "700px", md: "520px" },
-                          width: { xs: "100%", sm: "540px", md: "402px" },
+                          display: "flex",
+                          flexDirection: "row",
+                          gap: 2,
+                          mt: 3,
                         }}
-                      />
-                    </Grid>
+                      >
+                        {slide.buttons.links.map((link) => (
+                          <Button
+                            key={link.label}
+                            variant="contained"
+                            color="primary"
+                            href={link.href}
+                            component={link.href ? Link : undefined}
+                            sx={{
+                              backgroundColor: slide.buttons.backgroundColor,
+                              color: slide.buttons.textColor,
+                              border: "none",
+                              textTransform: "none",
+                              height: "50px",
+                            }}
+                          >
+                            {link.label}
+                          </Button>
+                        ))}
+                      </Box>
+                    )}
                   </Grid>
-                </Box>
+                  <Grid
+                    size={{ xs: 12, md: 6 }}
+                    sx={{
+                      display: "flex",
+                      justifyContent: { xs: "flex-start", md: "flex-end" },
+                    }}
+                  >
+                    <Figure
+                      ImageProps={{
+                        alt: slide.image.alt,
+                        src: slide.image.url,
+                        sx: { objectPosition: { xs: "left", md: "right" } },
+                      }}
+                      sx={{
+                        height: { xs: "502px", sm: "700px", md: "520px" },
+                        width: { xs: "100%", sm: "540px", md: "402px" },
+                      }}
+                    />
+                  </Grid>
+                </Grid>
               </Box>
             </Box>
           </Slide>
