e14c72da9038b65bc738018a1ae9799406e6bb82
Clemence Kyara <clemence@codeforafrica.org>
2025-09-15 16:01:24 +0300

    Remove sensitive ENVs from .env file (should be in .env.local)

--- apps/climatemappedafrica/src/pages/500.js
+++ apps/climatemappedafrica/src/pages/500.js
@@ -13,10 +13,26 @@ function ServerErrorPage({ blocks = [] }) {
 }
 
 export async function getStaticProps(context) {
-  return getPageStaticProps({
+  const data = getPageStaticProps({
     ...context,
     params: { slugs: ["500"] },
   });
+  // Unlikely to get 404 from within a 500 page, but just in case
+  // e.g. When the CMS is configured but no pages exist yet
+  if (data?.notFound) {
+    return {
+      props: {
+        blocks: [
+          {
+            slug: "error",
+            title: "Server Error",
+            statusCode: 500,
+          },
+        ],
+      },
+    };
+  }
+  return data;
 }
 
 export default ServerErrorPage;
