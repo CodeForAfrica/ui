a13f7d343909b0956a92edc9e22922ecf51b6e13
Kevin Koech <kevin@kevinkoech.com>
2025-11-20 11:00:53 +0300

    Remove unused research category logic and simplify report link generation in blockify and reports utility

--- apps/trustlab/src/utils/reports.js
+++ apps/trustlab/src/utils/reports.js
@@ -1,18 +1,12 @@
-import { formatPagePath } from "@commons-ui/payload";
-
 export async function getReports(api, options) {
   const { docs, totalPages, page } = await api.getCollection("reports", {
     ...options,
   });
 
   const reports = docs.map((doc) => {
-    const link = formatPagePath("research", doc);
     return {
       ...doc,
       date: new Date(doc.date).toISOString().split("T")[0],
-      link: {
-        href: link ? `${link}/${doc.slug}` : `/${doc.slug}`,
-      },
     };
   });
   return {
