3e96fde76c9ac0bd6288b158b7f3e1cfb84100fc
Clemence Kyara <clemence@codeforafrica.org>
2026-04-08 11:40:23 +0300

    Update to match new favicon design and sizes

--- apps/trustlab/src/pages/_document.page.js
+++ apps/trustlab/src/pages/_document.page.js
@@ -44,26 +44,19 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link
+            rel="icon"
+            type="image/png"
+            href="/favicon-96x96.png"
+            sizes="96x96"
+          />
+          <link
             rel="apple-touch-icon"
             sizes="180x180"
             href="/apple-touch-icon.png"
           />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
           <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1020e1" />
-          <meta name="msapplication-TileColor" content="#2b5797" />
-          <meta name="theme-color" content="#ffffff" />
+          <meta name="apple-mobile-web-app-title" content="TrustLab" />
+          <meta name="theme-color" content="#000" />
           <meta name="emotion-insertion-point" content="" />
           {this.props.emotionStyleTags}
           <script
